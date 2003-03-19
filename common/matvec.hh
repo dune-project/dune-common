b902c89033c74423811603d33e20dc4efa310645
Peter Bastian <peter@dune-project.org>
2003-03-19 13:07:07 +0000

    added lots of members to Vec and Mat
    [[Imported from SVN: r5]]

--- common/matvec.hh
+++ common/matvec.hh
@@ -3,6 +3,9 @@
 #ifndef __MATVEC_HH__
 #define __MATVEC_HH__
 
+#include <iostream>
+#include <math.h>
+
 namespace Dune {
   /** @defgroup Common Dune Common Module
 
@@ -32,10 +35,23 @@ namespace Dune {
       for (int i=0; i<n; i++) x[i] = 0, x[k] = t;
     }
 
+    //! Constructor making unit vector in direction k
+    Vec (int k)
+    {
+      for (int i=0; i<n; i++) x[i] = 0, x[k] = 1;
+    }
+
     //! Constructor making vector with identical coordinates
     Vec (T t)
     {
       for (int i=0; i<n; i++) x[i] = t;
+    }
+
+    //! assign component type to all components
+    Vec<n,T>& operator= (T t)
+    {
+      for (int i=0; i<n; i++) x[i]=t;
+      return *this;
     }
 
     //! operator () for read/write access to element of the vector
@@ -65,6 +81,39 @@ namespace Dune {
       Vec<n,T> z; for (int i=0; i<n; i++) z.x[i] =  k*x[i];return s;
     }
 
+    //! 1 norm
+    T norm1 ()
+    {
+      T s=0.0;
+      for (int i=0; i<n; i++) s += ABS(x[i]);
+      return s;
+    }
+
+    //! 2 norm
+    T norm2 ()
+    {
+      T s=0.0;
+      for (int i=0; i<n; i++) s += x[i]*x[i];
+      return sqrt(s);
+    }
+
+    //! \infty norm
+    T norminfty ()
+    {
+      T s=0.0;
+      for (int i=0; i<n; i++)
+        if (ABS(x[i])>s) s = ABS(x[i]);
+      return s;
+    }
+
+    void print (std::ostream& s, int indent)
+    {
+      for (int k=0; k<indent; k++) s << " ";
+      s << "Vec [ ";
+      for (int i=0; i<n; i++) s << v(i) << " ";
+      s << "]" << endl;
+    }
+
   private:
     //! built-in array to hold the data.
     T x[n];
@@ -72,18 +121,26 @@ namespace Dune {
 
   //! multiplication of scalar with vector
   template<int n, class T>
-  Vec<n,T> operator* (T k, Vec<n,T> b)
+  inline Vec<n,T> operator* (T k, Vec<n,T> b)
   {
     Vec<n,T> z; for (int i=0; i<n; i++) z(i) = k*b(i);return z;
   }
 
   //! unary minus
   template<int n, class T>
-  Vec<n,T> operator- (Vec<n,T> b)
+  inline Vec<n,T> operator- (Vec<n,T> b)
   {
     Vec<n,T> z; for (int i=0; i<n; i++) z(i) = -b(i);return z;
   }
 
+  template <int n, class T>
+  inline std::ostream& operator<< (std::ostream& s, Vec<n,T>& v)
+  {
+    s << "Vec [ ";
+    for (int i=0; i<n; i++) s << v(i) << " ";
+    s << "]" << endl;
+    return s;
+  }
 
   //************************************************************************
   /*!
@@ -110,10 +167,35 @@ namespace Dune {
       return z;
     }
 
+    void print (std::ostream& s, int indent)
+    {
+      for (int k=0; k<indent; k++) " ";
+      s << "Mat [n=" << n << ",m=" << m << "]" << endl;
+      for (int i=0; i<n; i++)
+      {
+        for (int k=0; k<indent+2; k++) s << " ";
+        s << "row i=" << i << " [ ";
+        for (int j=0; j<n; j++) s << A(i,j) << " ";
+        s << "]" << endl;
+      }
+    }
+
   private:
     //! built-in array to hold the data
     Vec<n,T> a[m];
   };
+
+  template <int n, int m, class T>
+  inline std::ostream& operator<< (std::ostream& s, Mat<n,m,T>& A)
+  {
+    for (int i=0; i<n; i++)
+    {
+      s << "Mat i=" << i << " [ ";
+      for (int j=0; j<n; j++) s << A(i,j) << " ";
+      s << "]" << endl;
+    }
+    return s;
+  }
 
   /** @} */
 
