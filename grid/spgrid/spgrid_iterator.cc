17c168b99519df84204a47ff5f3a3d2cd23382eb
Christian Engwer <christi@dune-project.org>
2003-04-28 14:17:44 +0000

    left/right aktualisiert
    [[Imported from SVN: r34]]

--- grid/spgrid/spgrid_iterator.cc
+++ grid/spgrid/spgrid_iterator.cc
@@ -96,54 +96,33 @@ namespace Dune {
   template<int DIM> inline
   typename spgrid<DIM>::iterator spgrid<DIM>::iterator::
   left(int dim) const {
-#define OLDLEFT
-#ifdef OLDLEFT
-    throw string("iterator::left(dim) needs to be updated - coord changed");
     array<DIM> coord = g.id_to_coord(l_, id_);
     coord[dim]--;
-    if (coord[dim] < - g.front_overlap(l_, dim)) {
-      coord[dim] = - g.front_overlap(l_, dim);
-      /* if we are only one Process in direction dim
-         and we have periodic boundry-conditions... */
-      if (!g.do_front_share(dim)
-          && !g.do_end_share(dim)
-          && (g.periodic_[dim]==true))
-        /* we are at the end again */
-        coord[dim] = g.size(l_,dim)+g.end_overlap(l_,dim)-1;
-    };
-    return iterator(l_, coord, g);
-#else
-    int subsize = 1;
-    for (int d=d-1; d>=0; d++) {
-      subsize = subsize * ( g.size(l_,d) +
-                            g.front_overlap(l_,d) +
-                            g.end_overlap(l_,d) );
+    if (coord[dim] < 0) {
+      if (g.periodic_[dim]==true)
+        coord[dim] = g.size(l_,dim) + g.front_overlap(l_,dim)
+                     + g.end_overlap(l_,dim) - 1;
+      else
+        coord[dim] = 0;
     }
-    int newid = id_ + move;
-    if (newid < g.max(l_))
-      newid = id_;
     return iterator(l_, coord, g);
-#endif
   }; /* end left */
 
   /** who is sitting right in direction dim? */
   template<int DIM> inline
   typename spgrid<DIM>::iterator spgrid<DIM>::iterator::
   right(int dim) const {
-    throw string("iterator::right(dim) nneds to be updated - coord changed");
+    int end = g.size(l_,dim)
+              + g.front_overlap(l_,dim) + g.end_overlap(l_,dim);
     array<DIM> coord = g.id_to_coord(l_, id_);
     coord[dim]++;
     /* check the border */
-    if (coord[dim] >= g.size(l_, dim) + g.end_overlap(l_, dim)) {
-      coord[dim] = g.size(l_, dim) + g.end_overlap(l_, dim)-1;
-      /* if we are only one Process in direction dim
-         and we have periodic boundry-conditions... */
-      if (!g.do_front_share(dim)
-          && !g.do_end_share(dim)
-          && (g.periodic_[dim]==true))
-        /* we are at the front again */
-        coord[dim] = - g.front_overlap(l_, dim);
-    };
+    if (coord[dim] >= end) {
+      if (g.periodic_[dim]==true)
+        coord[dim] = 0;
+      else
+        coord[dim] = end - 1;
+    }
     return iterator(l_, coord, g);
   }; /* end right */
 
