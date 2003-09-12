5c37587027c8ef79d477d945613a7f5ac140dfbf
Peter Bastian <peter@dune-project.org>
2003-09-12 08:35:08 +0000

    added documentation group
    [[Imported from SVN: r141]]

--- grid/yaspgrid/grids.hh
+++ grid/yaspgrid/grids.hh
@@ -19,6 +19,18 @@
 
 
 namespace SG {
+
+  /** @defgroup yaspgrid Yet Another Structured Parallel Grid
+
+     This is the basis of a parallel implementation of the dune grid interface
+     supporting codim 0 and dim.
+
+     You can also use the structured interface and write fast code.
+
+     @{
+   */
+
+
   // forward declarations
   template<int d, typename ct> class Grid;
   template<int d, typename ct> class SubGrid;
@@ -1947,6 +1959,8 @@ namespace SG {
     s << std::endl;
     return s;
   }
+
+  /** @} end documentation group */
 
 }
 
