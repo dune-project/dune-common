040462a95300a5240b240f1c933106ee7be5e051
Robert Klöfkorn <robertk@dune-project.org>
2003-10-14 17:29:46 +0000

    Added CGInverseOperator with Operator as tmeplate type. Added a applyLocal method for FEOperator for tests.
    [[Imported from SVN: r175]]

--- fem/feoperator.hh
+++ fem/feoperator.hh
@@ -3,7 +3,7 @@
 #ifndef __DUNE_FEOPERATOR_HH__
 #define __DUNE_FEOPERATOR_HH__
 
-#include <dune/common/operator.hh>
+#include "discreteoperator.hh"
 #include <dune/fem/basefunctions.hh>
 #include <dune/fem/dofiterator.hh>
 #include <dune/fem/discretefunction.hh>
@@ -11,14 +11,18 @@
 namespace Dune {
 
   template <class DiscFunctionType, class MatrixType, class FEOpImp>
-  class FiniteElementOperatorInterface : public Operator<DiscFunctionType::Traits<0>::RangeField,DiscFunctionType,DiscFunctionType> {
+  class FiniteElementOperatorInterface
+    : public Operator<DiscFunctionType::Traits<0>::RangeField,DiscFunctionType,DiscFunctionType>
+      //: public DiscreteOperator<DiscFunctionType , FiniteElementOperatorInterface
+      //<DiscFunctionType , MatrixType ,FEOpImp > >
+  {
   public:
     typedef typename DiscFunctionType::FunctionSpace FunctionSpaceType;
     typedef typename FunctionSpaceType::GridType GridType;
     typedef typename DiscFunctionType::LocalFunctionType LocalFunctionType;
-    typedef typename FunctionSpaceType::Range Range;
+    typedef typename FunctionSpaceType::Range RangeType;
     typedef typename FunctionSpaceType::JacobianRange JacobianRange;
-    typedef typename FunctionSpaceType::Domain Domain;
+    typedef typename FunctionSpaceType::Domain DomainType;
     typedef typename GridType::Traits<0>::Entity EntityType;
 
     void prepare( EntityType &entity ) const {
@@ -41,13 +45,19 @@ namespace Dune {
   };
 
   template <class DiscFunctionType, class MatrixType, class FEOpImp>
-  class FiniteElementOperator : public FiniteElementOperatorInterface<DiscFunctionType,MatrixType,FEOpImp> {
+  class FiniteElementOperator : public FiniteElementOperatorInterface<DiscFunctionType,MatrixType,FEOpImp>
+  {
+
+    typedef FiniteElementOperator <DiscFunctionType,MatrixType,FEOpImp> MyType;
+
   protected:
     mutable MatrixType *matrix_;
     const DiscFunctionType::FunctionSpaceType & functionSpace_;
     mutable bool matrix_assembled_;
 
-    void assemble ( ) const {
+    void assemble ( ) const
+    {
+      std::cout << "Assemble FiniteElementOperator \n";
       typedef typename DiscFunctionType::FunctionSpace FunctionSpaceType;
       typedef typename FunctionSpaceType::GridType GridType;
       typedef typename GridType::Traits<0>::LevelIterator LevelIterator;
@@ -55,35 +65,86 @@ namespace Dune {
 
       GridType &grid = const_cast<GridType &> (functionSpace_.getGrid());
 
-      typedef typename DiscFunctionType::LocalFunctionType LocalFunctionType;
-      typedef typename FunctionSpaceType::Range Range;
-      typedef typename FunctionSpaceType::JacobianRange JacobianRange;
-      typedef typename FunctionSpaceType::Domain Domain;
+      //typedef typename DiscFunctionType::LocalFunctionType LocalFunctionType;
+      //typedef typename FunctionSpaceType::JacobianRange JacobianRange;
+      {
+        LevelIterator it = grid.lbegin<0>( grid.maxlevel() );
+        LevelIterator endit = grid.lend<0> ( grid.maxlevel() );
+        for( it ; it != endit; ++it )
+        {
+          prepare( *it );
 
-      LevelIterator it = grid.lbegin<0>( grid.maxlevel() );
-      LevelIterator endit = grid.lend<0> ( grid.maxlevel() );
-      for( ; it != endit; ++it ) {
-        prepare( *it );
+          const BaseFunctionSetType & baseSet = functionSpace_.getBaseFunctionSet( *it );
+          int numOfBaseFct = baseSet.getNumberOfBaseFunctions();
 
-        const BaseFunctionSetType & baseSet = functionSpace_.getBaseFunctionSet( *it );
-        int numOfBaseFct = baseSet.getNumberOfBaseFunctions();
+          // setup matrix
+          for(int i=0; i<numOfBaseFct; i++)
+          {
+            int row = functionSpace_.mapToGlobal( *it , i );
+            for (int j=0; j<numOfBaseFct; j++ )
+            {
+              int col = functionSpace_.mapToGlobal( *it , j );
+              double val = getLocalMatrixEntry( *it, i, j );
 
-        // setup matrix
-        for(int i=0; i<numOfBaseFct; i++) {
-          int row = functionSpace_.mapToGlobal( *it , i );
-          for (int j=0; j<numOfBaseFct; j++ ) {
-            int col = functionSpace_.mapToGlobal( *it , j );
-
-            double val = getLocalMatrixEntry( *it, i, j );
-
-            matrix_->add( row , col , val);
+              matrix_->add( row , col , val);
+            }
           }
         }
       }
+
+#if 0
+      {
+
+        // eliminate the Dirichlet rows
+        typedef typename GridType::Traits<0>::Entity EntityType;
+        typedef typename EntityType::Traits::NeighborIterator NeighIt;
+        typedef typename NeighIt::Traits::BoundaryEntity BoundaryEntityType;
+
+        LevelIterator it = grid.lbegin<0>( grid.maxlevel() );
+        LevelIterator endit = grid.lend<0> ( grid.maxlevel() );
+        for( it ; it != endit; ++it )
+        {
+          NeighIt nit = it->nbegin();
+          NeighIt endnit = it->nend();
+          for(nit; nit != endnit ; ++nit)
+          {
+
+            if(nit.boundary())
+            {
+              BoundaryEntityType & bEl = nit.boundaryEntity();
+
+              if( bEl.type() == Dirichlet )
+              {
+                int neigh = nit.number_in_self();
+
+                if((*it).geometry().type() == triangle)
+                {
+                  int numDof = 3;
+                  for(int i=1; i<numDof; i++)
+                  {
+                    // funktioniert nur fuer Dreiecke
+                    // hier muss noch gearbeitet werden. Wie kommt man von den
+                    // Intersections zu den localen Dof Nummern?
+                    int col = functionSpace_.mapToGlobal(*it,(neigh+i)%numDof);
+                    // unitRow unitCol for boundary
+                    matrix_->kroneckerKill(col,col);
+                  }
+                }
+              }
+            }
+
+          }
+        }
+      }
+#endif
       matrix_assembled_ = true;
     }
 
-    void multiplyOnTheFly( const DiscFunctionType &arg, DiscFunctionType &dest ) const {
+    template <class EntityType>
+    void multiOnTheFly( EntityType &en,
+                        const DiscFunctionType &arg, DiscFunctionType &dest ) const
+    {
+      //std::cout << "Mulitipla on the fla local\n";
       typedef typename DiscFunctionType::FunctionSpace FunctionSpaceType;
       typedef typename FunctionSpaceType::GridType GridType;
       typedef typename GridType::Traits<0>::LevelIterator LevelIterator;
@@ -92,49 +153,90 @@ namespace Dune {
       GridType &grid = const_cast<GridType &> (functionSpace_.getGrid());
 
       typedef typename DiscFunctionType::LocalFunctionType LocalFunctionType;
-      typedef typename FunctionSpaceType::Range Range;
+      typedef typename FunctionSpaceType::Range RangeType;
       typedef typename FunctionSpaceType::JacobianRange JacobianRange;
-      typedef typename FunctionSpaceType::Domain Domain;
+      typedef typename FunctionSpaceType::Domain DomainType;
 
-      typedef typename DiscFunctionType::GlobalDofIteratorType DofIteratorType;
+      typedef typename DiscFunctionType::DofIteratorType DofIteratorType;
       int level = arg.getFunctionSpace().getGrid().maxlevel();
 
       DofIteratorType dest_it = dest.dbegin( level );
       DofIteratorType arg_it = const_cast<DiscFunctionType&>(arg).dbegin( level );
 
-      dest.clear( );
+      dest.clear();
+
+      //prepare( *it );
+
+      const BaseFunctionSetType & baseSet = functionSpace_.getBaseFunctionSet( en );
+      int numOfBaseFct = baseSet.getNumberOfBaseFunctions();
+
+      for(int i=0; i<numOfBaseFct; i++)
+      {
+        int row = functionSpace_.mapToGlobal( en , i );
+        for (int j=0; j<numOfBaseFct; j++ )
+        {
+          int col = functionSpace_.mapToGlobal( en , j );
+
+          double val = getLocalMatrixEntry( en , i, j );
+
+          dest_it[col] += arg_it[ row ] * val;
+        }
+      }
+    }
+
+    void multiplyOnTheFly( const DiscFunctionType &arg, DiscFunctionType &dest ) const
+    {
+      //std::cout << "Mulitipla on the fla\n";
+      typedef typename DiscFunctionType::FunctionSpace FunctionSpaceType;
+      typedef typename FunctionSpaceType::GridType GridType;
+      typedef typename GridType::Traits<0>::LevelIterator LevelIterator;
+      typedef typename FunctionSpaceType::BaseFunctionSetType BaseFunctionSetType;
+
+      GridType &grid = const_cast<GridType &> (functionSpace_.getGrid());
+
+      typedef typename DiscFunctionType::LocalFunctionType LocalFunctionType;
+      typedef typename FunctionSpaceType::Range RangeType;
+      typedef typename FunctionSpaceType::JacobianRange JacobianRange;
+      typedef typename FunctionSpaceType::Domain DomainType;
+
+      typedef typename DiscFunctionType::DofIteratorType DofIteratorType;
+      int level = arg.getFunctionSpace().getGrid().maxlevel();
+
+      DofIteratorType dest_it = dest.dbegin( level );
+      DofIteratorType arg_it = const_cast<DiscFunctionType&>(arg).dbegin( level );
+
+      dest.clear();
 
       LevelIterator it = grid.lbegin<0>( grid.maxlevel() );
       LevelIterator endit = grid.lend<0> ( grid.maxlevel() );
-      for( ; it != endit; ++it ) {
+      for( ; it != endit; ++it )
+      {
         prepare( *it );
 
         const BaseFunctionSetType & baseSet = functionSpace_.getBaseFunctionSet( *it );
         int numOfBaseFct = baseSet.getNumberOfBaseFunctions();
 
-        for(int i=0; i<numOfBaseFct; i++) {
+        for(int i=0; i<numOfBaseFct; i++)
+        {
           int row = functionSpace_.mapToGlobal( *it , i );
-          for (int j=0; j<numOfBaseFct; j++ ) {
+          for (int j=0; j<numOfBaseFct; j++ )
+          {
             int col = functionSpace_.mapToGlobal( *it , j );
 
             double val = getLocalMatrixEntry( *it, i, j );
 
-            //arg_it.reset();
-            //arg_it.operator++( col );
-            //dest_it.reset();
-            //dest_it.operator++( row );
-            //(*dest_it) += (*arg_it) * val;
             dest_it[col] += arg_it[ row ] * val;
           }
         }
       }
     }
+
   public:
 
     enum OpMode { ON_THE_FLY, ASSEMBLED };
 
     FiniteElementOperator( const DiscFunctionType::FunctionSpaceType &fuspace,
-                           OpMode opMode = ASSEMBLED ) :
+                           OpMode opMode = ON_THE_FLY ) :
       functionSpace_( fuspace ),
       matrix_ (NULL),
       matrix_assembled_( false ),
@@ -150,11 +252,21 @@ namespace Dune {
           matrix_ = newEmptyMatrix( );
           assemble();
         }
+        std::cout << "Matrix apply \n";
         matrix_->apply( arg, dest );
       } else {
         multiplyOnTheFly( arg, dest );
       }
     }
+
+  public:
+    template <class EntityType>
+    void applyLocal ( EntityType &en , const DiscFunctionType &Arg , DiscFunctionType &Dest ) //const
+    {
+      std::cout << "FEOperator::applyLocal \n";
+      //multiOnTheFly(en , Arg, Dest );
+    }
+
   private:
     OpMode opMode_;
   };
--- fem/inverseoperators.hh
+++ fem/inverseoperators.hh
@@ -14,12 +14,16 @@ namespace Dune {
 
     DiscreteFunctionType::Traits<0>::RangeField epsilon_;
     int maxIter_;
+
+    double _redEps;
+
   public:
 
     CGInverseOperator( const Mapping<DiscreteFunctionType::Traits<0>::RangeField,
-                           DiscreteFunctionType,DiscreteFunctionType>& op ) : op_(op),
-                                                                              epsilon_( 1e-16 ),
-                                                                              maxIter_( 1000 ) {}
+                           DiscreteFunctionType,DiscreteFunctionType>& op ,
+                       double redEps , double absLimit , int maxIter , int verbose ) : op_(op),
+                                                                                       _redEps ( redEps ), epsilon_ ( absLimit*absLimit ) ,
+                                                                                       maxIter_ (maxIter ) , _verbose ( verbose ) {}
 
     void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) const {
 
@@ -43,11 +47,14 @@ namespace Dune {
 
       spn = r.scalarProductDofs( r );
 
-      while((spn > epsilon_) && (count++ < maxIter_)) {
+      while((spn > epsilon_) && (count++ < maxIter_))
+      {
 
+        std::cout << "CG Step\n";
         // fall ab der zweiten iteration *************
 
-        if(count > 1) {
+        if(count > 1)
+        {
           const Field e = spn / spa;
           p *= e;
           p -= r;
@@ -68,13 +75,135 @@ namespace Dune {
         // residuum neu berechnen *********************
 
         spn = r.scalarProductDofs( r );
-        std::cerr << count << " cg-Iterationen  " << count << " Residuum:" << spn << "        \r";
+        if(_verbose > 0)
+          std::cerr << count << " cg-Iterationen  " << count << " Residuum:" << spn << "        \r";
       }
-      std::cerr << "\n";
+      if(_verbose > 0)
+        std::cerr << "\n";
     }
 
   private:
     const Mapping<DiscreteFunctionType::Traits<0>::RangeField,DiscreteFunctionType,DiscreteFunctionType> &op_;
+    int _verbose ;
+  };
+
+
+  template <class DiscreteFunctionType, class OperatorType>
+  class CGInverseOp : public Operator<DiscreteFunctionType::Traits<0>::RangeField,
+                          DiscreteFunctionType,DiscreteFunctionType> {
+
+    DiscreteFunctionType::Traits<0>::RangeField epsilon_;
+    int maxIter_;
+
+    double _redEps;
+
+  public:
+
+    //CGInverseOp( const OperatorType & op , double  redEps , double absLimit , int maxIter , int verbose ) : op_(op),
+    CGInverseOp( OperatorType & op , double redEps , double absLimit , int maxIter , int verbose ) : op_(op),
+                                                                                                     _redEps ( redEps ), epsilon_ ( absLimit ) ,
+                                                                                                     maxIter_ (maxIter ) , _verbose ( verbose ) {}
+
+    void apply( const DiscreteFunctionType& arg, DiscreteFunctionType& dest ) //const
+    {
+
+      typedef typename DiscreteFunctionType::FunctionSpace FunctionSpaceType;
+      typedef FunctionSpaceType::RangeField Field;
+
+      int count = 0;
+      Field spa=0, spn, q, quad;
+
+      DiscreteFunctionType r( arg );
+      DiscreteFunctionType p( arg );
+      DiscreteFunctionType h( arg );
+
+      op_( dest, h );
+
+      r.assign( h );
+      r -= arg;
+
+      p.assign( arg );
+      p -= h;
+
+      spn = r.scalarProductDofs( r );
+
+      while((spn > epsilon_) && (count++ < maxIter_))
+      {
+
+        // fall ab der zweiten iteration *************
+
+        if(count > 1)
+        {
+          const Field e = spn / spa;
+          p *= e;
+          p -= r;
+        }
+
+        // grund - iterations - schritt **************
+
+        op_( p, h );
+
+        quad = p.scalarProductDofs( h );
+        q    = spn / quad;
+
+        dest.add( p, q );
+        r.add( h, q );
+
+        spa = spn;
+
+        // residuum neu berechnen *********************
+
+        spn = r.scalarProductDofs( r );
+        if(_verbose > 0)
+          std::cerr << count << " cg-Iterationen  " << count << " Residuum:" << spn << "        \r";
+      }
+      if(_verbose > 0)
+        std::cerr << "\n";
+    }
+
+  private:
+    //const OperatorType &op_;
+    OperatorType &op_;
+    int _verbose ;
+  };
+
+
+
+  //********************************************************
+  //
+  //  with CGnew
+  //
+  //********************************************************
+  //template <class DiscreteFunctionType>
+  template <class DiscreteFunctionType, class OperatorType>
+  class CGInvOp : public Operator<DiscreteFunctionType::Traits<0>::RangeField,
+                      DiscreteFunctionType,DiscreteFunctionType> {
+
+    DiscreteFunctionType::Traits<0>::RangeField epsilon_;
+    int maxIter_;
+
+    double _redEps;
+
+  public:
+
+    //CGInvOp( const Mapping<DiscreteFunctionType::Traits<0>::RangeField,
+    //	     DiscreteFunctionType,DiscreteFunctionType>& op ,
+    CGInvOp( const OperatorType & op ,
+             double redEps , double absLimit , int maxIter , int verbose ) : op_(op),
+                                                                             _redEps ( redEps ), epsilon_ ( absLimit ) ,
+                                                                             maxIter_ (maxIter ) , _verbose ( verbose ) {}
+
+    void apply( DiscreteFunctionType& arg, DiscreteFunctionType& dest )
+    {
+      int status = CGnew(op_,arg,dest,_redEps, epsilon_,maxIter_,_verbose);
+    }
+  private:
+    //const Mapping<DiscreteFunctionType::Traits<0>::RangeField,
+    //        DiscreteFunctionType,DiscreteFunctionType> &op_;
+    const OperatorType & op_;
+
+    int _verbose ;
+
   };
 
 }
