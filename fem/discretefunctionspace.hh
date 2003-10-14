c891a78a2a2dbc12b6e856eb765f72d5e56e52ae
Robert Klöfkorn <robertk@dune-project.org>
2003-10-14 17:01:29 +0000

    Added int polynomOrder () and int localPolynomOrder (Entity &en) as interface methods to the DiscreteFunctionSpace class.
    [[Imported from SVN: r169]]

--- fem/discretefunctionspace.hh
+++ fem/discretefunctionspace.hh
@@ -57,6 +57,19 @@ namespace Dune {
       return asImp().getBaseFunctionSet( en );
     }
 
+    //! Get maximal polynom order of functionspace.
+    int polynomOrder () const
+    {
+      return asImp().polynomOrder();
+    }
+
+    //! Get polynom order for special element.
+    template <class EntityType>
+    int localPolynomOrder ( EntityType &en ) const
+    {
+      return asImp().localPolynomOrder( en );
+    }
+
     //! return the corresponding Grid
     const GridType & getGrid () const { return grid_; }
 
