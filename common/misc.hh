5db3e6c5fec6ced922b9faab9d627614084b4149
Robert Klöfkorn <robertk@dune-project.org>
2003-10-21 13:57:38 +0000

    Added function genFilename which generates out of a filename 'grid' and a timestep (=0) a filename grid000000000.
    [[Imported from SVN: r186]]

--- common/misc.hh
+++ common/misc.hh
@@ -60,6 +60,41 @@ namespace Dune {
     return static_cast<T*>(::malloc(size*sizeof(T)));
   }
 
+  //********************************************************************
+  //
+  // generate filenames with timestep number in it
+  //
+  //********************************************************************
+
+  template <typename T>
+  inline const char *genFilename(T *path, T *fn, int ntime)
+  {
+    static char name[256];
+    char         *cp;
+
+    if (path == NULL || path[0] == '\0')
+    {
+      sprintf(name, "./%s", fn);
+    }
+    else
+    {
+      const char *cp = path;
+      while (*cp)
+        cp++;
+      cp--;
+      if (*cp == '/')
+        sprintf(name, "%s%s", path, fn);
+      else
+        sprintf(name, "%s/%s", path, fn);
+    }
+    cp = name;
+    while (*cp)
+      cp++;
+    sprintf(cp, "%010d", ntime);
+
+    return( (T *) name);
+  }
+
   /** @} */
 
 }
