61504bbe973d5283b07f16c233930fcd40299481
Thimo Neubauer <thimo@dune-project.org>
2003-10-30 17:05:55 +0000

    removed most of the functionality because the applications should do the tests
    [[Imported from SVN: r214]]

--- autogen.sh
+++ autogen.sh
@@ -7,9 +7,15 @@ set -e
 # prepare everything
 echo "--> aclocal..."
 aclocal
-echo "--> autoheader..."
-autoheader
+
+# applications should provide a config.h for now
+#echo "--> autoheader..."
+#autoheader
+
 echo "--> automake..."
 automake --add-missing
+
 echo "--> autoconf..."
 autoconf
+
+echo "files successfully created, please run ./configure now"
