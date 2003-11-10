919d93c58c3de3aac94f7c99e7b935e009cba838
Thimo Neubauer <thimo@dune-project.org>
2003-11-10 21:06:39 +0000

    added libtool-stuff
    [[Imported from SVN: r252]]

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,11 @@
 # barf on errors
 set -e
 
+echo "--> libtoolize..."
+# this script won't rewrite the files if they already exist. This is a
+# PITA when you want to upgrade libtool, thus I'm setting --force
+libtoolize --force
+
 # prepare everything
 echo "--> aclocal..."
 aclocal
