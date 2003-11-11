028b6253b259f481d1776af4c14f5402c558408b
Thimo Neubauer <thimo@dune-project.org>
2003-11-11 10:07:15 +0000

    copied compiler-switch from duneapps, now autogen.sh has the same interface in both directories (automagically calls ./configure)
    [[Imported from SVN: r257]]

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,73 @@
 # barf on errors
 set -e
 
+DEFAULTCONFOPT=
+
+usage () {
+    echo "Usage: ./autogen.sh [options]"
+    echo "  -i, --intel        use intel compiler"
+    echo "  -g, --gnu          use gnu compiler (default)"
+    echo "  -m, --mpi          use mpicc"
+    echo "  --opts=FILE        use compiler-options from FILE"
+    echo "  -d, --debug        switch debug-opts on"
+    echo "  -n, --nodebug      switch debug-opts off"
+    echo "  -o, --optim        switch optimization on"
+    echo "  -h, --help         you already found this :)"
+}
+
+# no compiler set yet
+COMPSET=0
+for OPT in $* ; do
+
+    set +e
+    # stolen from configure...
+    # when no option is set, this returns an error code
+    arg=`expr "x$OPT" : 'x[^=]*=\(.*\)'`
+    set -e
+
+    case "$OPT" in
+	-i|--intel)   . ./icc.opts ; COMPSET=1 ;;
+	-g|--gnu)     . ./gcc.opts ; COMPSET=1 ;;
+	-m|--mpi)     . ./mpi.opts ; COMPSET=1 ;;
+	--opts=*)
+	    if [ -r $arg ] ; then
+	      echo "reading options from $arg..."
+	      . ./$arg ;
+	      COMPSET=1;
+	    else
+	      echo "Cannot open compiler options file $arg!" ;
+	      exit 1;
+	    fi ;;
+	-d|--debug)   DEBUG=1 ;;
+	-n|--nodebug) DEBUG=0 ;;
+	-o|--optim)   OPTIM=1 ;;
+	-h|--help) usage ; exit 0 ;;
+	# pass unknown opts to ./configure
+	*) CONFOPT="$CONFOPT $OPT" ;;
+    esac
+done
+
+# use the nonfree compiler as default :-(
+if [ "$COMPSET" != "1" ] ; then
+    echo "No compiler set, using Intel compiler as default"
+    . ./icc.opts
+fi
+
+# create flags
+COMPFLAGS="$FLAGS"
+
+# maybe add debug flag
+if [ "$DEBUG" = "1" ] ; then	
+    COMPFLAGS="$COMPFLAGS $DEBUGFLAGS"
+fi
+
+# maybe add optimization flag
+if [ "$OPTIM" = "1" ] ; then	
+    COMPFLAGS="$COMPFLAGS $OPTIMFLAGS"
+fi
+
+## run autotools
+
 echo "--> libtoolize..."
 # this script won't rewrite the files if they already exist. This is a
 # PITA when you want to upgrade libtool, thus I'm setting --force
@@ -23,4 +90,13 @@ automake --add-missing
 echo "--> autoconf..."
 autoconf
 
-echo "files successfully created, please run ./configure now"
+#### start configure with special environment
+
+export CC=$COMP
+export CXX=$CXXCOMP
+export CPP="$COMP -E"
+
+export CFLAGS="$COMPFLAGS"
+export CXXFLAGS="$COMPFLAGS"
+
+./configure $DEFAULTCONFOPT $CONFOPT
