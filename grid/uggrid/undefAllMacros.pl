9077a3072a32515487edeb78b764886366c1f53a
Oliver Sander <sander@dune-project.org>
2004-01-22 14:01:48 +0000

    This little Perl program takes a list of UG headers as argument. It then creates a list of all UG headers that are directly or indirectly included by them.  All these files are parsed for preprocessor macro definitions, and a file ug_undefs.hh is written, that contains an #undef for each macro found.  ug_undef.hh needs to be included directly after all UG includes, to be a seal against 'macro leaking'.
    Hello you AlbertGrid guys.  Might this be of interest to you too?
    
    [[Imported from SVN: r315]]

---
+++ grid/uggrid/undefAllMacros.pl
@@ -0,0 +1,69 @@
+# TODO: Detect location of UG from the DUNE build system
+$UGROOT = "../../../UG/ug";
+
+# Counts the total number of #undefs emitted
+$counter = 0;
+
+#$header = "../../../UG/ug/gm/gm.h";
+
+# open output file
+$outfilename = "ug_undefs.hh";
+unless (open OUTFILE, ">$outfilename") {
+  die "Couldn't open $outfilename for writing!\n";
+}
+
+# Loop over all the files given in the command line and
+# recursively parse them for macro definitions.
+foreach $file (@ARGV) {
+
+  # Makedepend recursively searches for more included headers.
+  # Warnings appear because the system headers will not be found.
+  # Those warnings get piped to /dev/null.
+  foreach $_ (`makedepend -f- -w 20 -Y$UGROOT/include $file 2>/dev/null`) {
+    
+    # The output of makedepend has the form
+    # file.o: header.h
+    # We look for the ".o:" Everything that follows must be a header file  
+    if (/\.o\:\s(\S+\.h)/) {
+      
+      emitUndefs($1);
+      
+    }
+    
+  }
+
+}
+
+
+print "Undef'ed $counter #defines!\n";
+
+close(OUTFILE);
+
+
+##############################################################
+# This subroutine reads the file given as the first argument
+# and issues an #undef for each macro found.
+##############################################################
+sub emitUndefs() {
+
+  print "Parsing $_[0]\n";
+
+  unless (open INFILE, "$_[0]") {
+    die "Couldn't open $_[0] for reading!\n";
+}
+
+  while(<INFILE>) {
+    
+    # looks for lines that define a C macro
+    if (/\#define\s+(\w+)/) {
+      
+      print OUTFILE "#undef $1\n";
+      $counter = $counter + 1;
+      
+    }
+    
+  }
+
+  close (INFILE);
+
+}
