e1a63afd7eb4aa12398bd2d0c9afbdb57c3f04ed
Robert Klöfkorn <robertk@dune-project.org>
2004-03-02 09:18:53 +0000

    Added the methods operator +, operator *, getLocalOp. These methods perform the combination of discrete operators in such a way, that the local operators of two discrete operators are combined to a new local operator and with that a new discrete oeprator is created. A reference to the new create operator is kept in the object where it was created.
    [[Imported from SVN: r352]]

--- fem/discreteoperatorimp.hh
+++ fem/discreteoperatorimp.hh
@@ -1,57 +1,135 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef __DUNE_DISCRETEOPERATOR_HH__
-#define __DUNE_DISCRETEOPERATOR_HH__
+#ifndef __DUNE_DISCRETEOPERATORIMP_HH__
+#define __DUNE_DISCRETEOPERATORIMP_HH__
 
 #include <dune/fem/common/discreteoperator.hh>
-#include <dune/fem/common/combinedoperator.hh>
+#include <dune/fem/common/localoperator.hh>
 
 namespace Dune {
 
 
-  // Note: Range has to have Vector structure as well.
+  /*!  One Implementation of a discrete Operator
+
+     A discrete operator of this type consists of a local operator and a grid
+     walkthrough. On each Entity then the local operator is called.
+     Before strating the walkthrough the prepareGlobal method of the local
+     operator is called once. Then during the walkthrough on each entity the
+     methods prepareLocal, applyLocal, and finalizeLocal are called respectively.
+     After the walkthorugh the method finalizeGlobal of the local operator is
+     called once.
+
+     There are two ways to change a discrete operator, namely by adding
+     another discrete operator or by multiplying with a scalar.
+     The result of a sum has to be a mapping ( :-( ).
+
+   */
   template <class DiscreteFunctionType, class LocalOperatorImp >
   class DiscreteOperator
-  //: public DiscreteOperatorDefault
-  //    < DiscreteFunctionType , LocalOperatorImp,
-  //      DiscreteOperator <DiscreteFunctionType,LocalOperatorImp > >
-    : public Operator <typename DiscreteFunctionType::RangeFieldType,
-          DiscreteFunctionType , DiscreteFunctionType>
+    : public DiscreteOperatorDefault <DiscreteFunctionType, DiscreteFunctionType >
   {
-    typedef typename DiscreteFunctionType::FunctionSpaceType::RangeField RangeFieldType;
+    typedef typename DiscreteFunctionType::FunctionSpaceType::
+    RangeField RangeFieldType;
+    typedef DiscreteOperator<DiscreteFunctionType,LocalOperatorImp> MyType;
+
+    typedef ScaledLocalOperator < LocalOperatorImp > ScalOperatorType;
+    typedef DiscreteOperator<DiscreteFunctionType,ScalOperatorType> ScalDiscrType;
 
   public:
-    //! get LocalOperator
+    //! create DiscreteOperator with a LocalOperator
     DiscreteOperator (LocalOperatorImp &op, bool leaf=false )
-      : localOp_ ( op ) , leaf_ (leaf), level_ (-1) , prepared_ (false) {};
-
-    //! remember time step size
-    void prepare ( int level , const Domain &Arg, Range &Dest,
-                   Range *tmp , RangeFieldType & a, RangeFieldType & b)
+      : localOp_ ( op ) , leaf_ (leaf), prepared_ (false)
     {
-      level_ = level;
-      localOp_.prepareGlobal(level,Arg,Dest,tmp,a,b);
+      std::cout << "Make new Operator "<< this << "\n";
+    }
+
+    /*! add another discrete operator by combining the two local operators
+       and creating a new discrete operator whereby a pointer to the new
+       object is kept in the object where the operator was called.
+       If this object is deleted then all objects created during operator +
+       called are deleted too.
+     */
+    template <class LocalOperatorType>
+    DiscreteOperator<DiscreteFunctionType,
+        CombinedLocalOperator<LocalOperatorImp,LocalOperatorType> > &
+    operator + (const DiscreteOperator<DiscreteFunctionType,LocalOperatorType> &op)
+    {
+      typedef DiscreteOperator<DiscreteFunctionType,LocalOperatorType> CopyType;
+      typedef CombinedLocalOperator <LocalOperatorImp,LocalOperatorType> COType;
+
+      COType *locOp =
+        new COType ( addLocalOperators ( localOp_ , const_cast<CopyType &> (op).getLocalOp ()));
+      typedef DiscreteOperator < DiscreteFunctionType , COType > OPType;
+
+      OPType *discrOp = new OPType ( *locOp );
+
+      // memorize this new generated object because is represents this
+      // operator and is deleted if this operator is deleted
+      saveObjPointer( discrOp );
+
+      return *discrOp;
+    }
+
+    //! This method work just the same way like the operator +
+    ScalDiscrType & operator * (const RangeFieldType& scalar)
+    {
+      ScalOperatorType * sop = new ScalOperatorType ( localOp_ , scalar);
+      ScalDiscrType *discrOp = new ScalDiscrType ( *sop );
+
+      // memorize this new generated object because is represents this
+      // operator and is deleted if this operator is deleted
+      saveObjPointer ( discrOp );
+
+      return (*discrOp);
+    }
+
+    //! no public method, but has to be public, because all discrete operator
+    //! must be able to call this method an the template parameters are
+    //! allways diffrent
+    LocalOperatorImp & getLocalOp ()
+    {
+      return localOp_;
+    }
+
+    //********************************************************************
+    // Interface methods
+    //********************************************************************
+    //! apply operator which means make a grid walktrough on spezified level
+    //! and call the local operator on each entity
+    void apply ( const DomainType &Arg, RangeType &Dest ) const
+    {
+      std::cout << "DiscrOP::apply \n";
+      apply(level_,Arg,Dest);
+    }
+
+    //! apply the operator, see apply
+    void operator()( const DomainType &Arg, RangeType &Dest ) const
+    {
+      std::cout << "DiscrOP::apply \n";
+      apply(level_,Arg,Dest);
+    }
+
+    //********************************************************************
+    //  end of interface methods
+    //********************************************************************
+
+  private:
+    //! remember time step size
+    void prepare ( const DomainType &Arg, RangeType &Dest ) const
+    {
+      localOp_.prepareGlobal(level_,Arg,Dest);
       prepared_ = true;
     }
 
-    //! remember time step size
-    virtual void prepareG ( int level , const Domain &Arg, Range &Dest,
-                            Range *tmp , RangeFieldType & a, RangeFieldType & b) const
-    {
-      level_ = level;
-      localOp_.prepareGlobal(level,Arg,Dest,tmp,a,b);
-      prepared_ = true;
-    }
 
     //! go over all Entitys and call the LocalOperator.applyLocal Method
     //! Note that the LocalOperator can be an combined Operator
     //! Domain and Range are defined through class Operator
-    void apply ( const Domain &Arg, Range &Dest ) const
+    void apply ( int level, const DomainType &Arg, RangeType &Dest ) const
     {
       if(!prepared_)
       {
-        std::cerr << "DiscreteOperator::apply: I was not prepared! \n";
-        abort();
+        prepare( Arg, Dest);
       }
 
       // useful typedefs
@@ -81,34 +159,20 @@ namespace Dune {
         LevelIterator endit = grid.template lend<0>  ( level_ );
         applyOnGrid( it, endit , Arg, Dest );
       }
+
+      finalize( Arg, Dest );
     }
 
     //! finalize the operation
-    void finalize ( int level , const Domain &Arg, Range &Dest,
-                    Range *tmp , RangeFieldType & a, RangeFieldType & b)
+    void finalize ( const DomainType &Arg, RangeType &Dest ) const
     {
       prepared_ = false;
-      localOp_.finalizeGlobal(level,Arg,Dest,tmp,a,b);
+      localOp_.finalizeGlobal(Arg,Dest);
     }
 
-    //! finalize the operation
-    virtual void finalizeG ( int level , const Domain &Arg, Range &Dest,
-                             Range *tmp , RangeFieldType & a, RangeFieldType & b) const
-    {
-      prepared_ = false;
-      localOp_.finalizeGlobal(level,Arg,Dest,tmp,a,b);
-    }
-
-    //! apply the operator
-    void operator()( const Domain &Arg, Range &Dest ) const
-    {
-      apply(Arg,Dest);
-    }
-
-  private:
     template <class GridIteratorType>
     void applyOnGrid ( GridIteratorType &it, GridIteratorType &endit,
-                       const Domain &Arg, Range &Dest ) const
+                       const DomainType &Arg, RangeType &Dest ) const
     {
       // erase destination function
       Dest.clearLevel ( level_ );
@@ -122,16 +186,24 @@ namespace Dune {
       }
     }
 
+    //! true if operator was prepared for apply
     mutable bool prepared_;
 
     //! if true use LeafIterator else LevelIterator
     mutable bool leaf_;
 
-    //! Level on which we operate
-    mutable int level_;
-
     //! Operator which is called on each entity
     LocalOperatorImp & localOp_;
+
+    //*******************************************************
+    // derived from mappiung, don't need this here
+    //*******************************************************
+    virtual MappingType operator * (const Field &) const
+    {
+      std::cout << "MappingType DiscreteOperator::operator * ( const Field &) const: do not use this method\n";
+      abort();
+      return (*this);
+    }
   };
 
 }
