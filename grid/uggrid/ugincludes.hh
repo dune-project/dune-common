d0e7a94cce443432bab7044688a8ff7a7a952781
Oliver Sander <sander@dune-project.org>
2004-03-02 11:11:39 +0000

    all UG-includes separated into a designated header file
    [[Imported from SVN: r355]]

---
+++ grid/uggrid/ugincludes.hh
@@ -0,0 +1,21 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// All UG includes have to be made from this file, and from
+// this file only!  This is because undefAllMacros.pl takes
+// all headers from this file and undefs the macros defined
+// therein.
+
+#ifndef DUNE_UGINCLUDES_HH
+#define DUNE_UGINCLUDES_HH
+
+#include "../../../UG/ug/gm/gm.h"
+#include "../../../UG/ug/dom/std/std_domain.h"
+#include "../../../UG/ug/initug.h"
+#include "../../../UG/ug/ui/commands.h"
+#include "../../../UG/ug/np/udm/formats.h"
+
+
+// for debugging
+#include "../../../UG/ug/dev/ugdevices.h"
+
+#endif
