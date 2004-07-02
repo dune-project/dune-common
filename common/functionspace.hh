20ff2a09a87a6f339fe0d85f95b746cf61d41b5e
Oliver Sander <sander@dune-project.org>
2004-07-02 14:51:27 +0000

    doku
    [[Imported from SVN: r532]]

--- common/functionspace.hh
+++ common/functionspace.hh
@@ -15,21 +15,30 @@ namespace Dune {
      @{
    */
 
+  /** \todo Please doc me! */
   template< typename DomainFieldType, typename RangeFieldType, int n, int m >
   class FunctionSpace : public DynamicType {
   public:
 
+    /** \todo Please doc me! */
     typedef DomainFieldType DomainField;
+    /** \todo Please doc me! */
     typedef RangeFieldType RangeField;
 
+    /** \todo Please doc me! */
     typedef Mat < n, m, RangeField> JacobianRange;
+    /** \todo Please doc me! */
     typedef Vec < m,  Mat< n, n, RangeField> > HessianRange ;
 
+    /** \todo Please doc me! */
     typedef Vec<n, DomainField> Domain;
+    /** \todo Please doc me! */
     typedef Vec<m, RangeField> Range;
 
+    /** \todo Please doc me! */
     enum { DimDomain = n, DimRange = m};
 
+    /** \todo Please doc me! */
     FunctionSpace ( int ident ) : DynamicType (ident){} ;
 
   };
