c9c65477f41c5045057e6ca6d64be60e7c960e98
Oliver Sander <sander@dune-project.org>
2004-07-02 14:52:26 +0000

    vec steht nicht mehr in dieser Datei
    [[Imported from SVN: r533]]

--- common/matvec.hh
+++ common/matvec.hh
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #include "misc.hh"
+#include "fixedvector.hh"
 
 namespace Dune {
   /** @defgroup Common Dune Common Module
@@ -17,188 +18,6 @@ namespace Dune {
      @{
    */
 
-
-  //************************************************************************
-  /*!
-     Generic vector class for short vectors in d dimensions. Used e.g.
-     for global or local coordinates.
-   */
-  template<int dim, class T = double>
-  class Vec {
-  public:
-
-    //! remember the storage type
-    typedef T MemberType;
-
-    enum { n = (dim > 0) ? dim : 1 };
-
-    //! know length
-    enum { dimension = dim };
-
-    //! Constructor making uninitialized vector
-    Vec() {}
-
-    //! Constructor making vector from built-in array
-    Vec (T* y) {for(int i=0; i<n; i++) x[i]=y[i];}
-
-    //! Constructor making vector with one coordinate set, others zeroed
-    Vec (int k, T t)
-    {
-      for (int i=0; i<n; i++) x[i] = 0;
-      x[k] = t;
-    }
-
-    //! Constructor making vector with identical coordinates
-    Vec (T t)
-    {
-      for (int i=0; i<n; i++) x[i] = t;
-    }
-
-    //! assign component type to all components
-    Vec<n,T>& operator= (const Vec<n,T>& b)
-    {
-      for (int i=0; i<n; i++) x[i] = b.x[i];
-      return *this;
-    }
-
-    //! assign component type to all components
-    Vec<n,T>& operator= (T t)
-    {
-      for (int i=0; i<n; i++) x[i]=t;
-      return *this;
-    }
-    Vec<n,T>& operator= (T* t)
-    {
-      for (int i=0; i<n; i++) x[i]=t[i];
-      return *this;
-    }
-
-    //! operator () for read/write access to element of the vector
-    T& operator() (int i) {return x[i];}
-
-    //! operator () for read access to element of the vector
-    const T& operator() (int i) const {return x[i];}
-
-    //! Index operator[] for compatibility with C
-    T& operator[] (int i) {return x[i];}
-
-    //! const index operator[] for compatibility with C
-    const T& operator[] (int i) const {return x[i];}
-
-    //! read only operation needed
-    T read (int i) const { return x[i];}
-
-    //! operator+ adds two vectors
-    Vec<n,T>& operator+= (const Vec<n,T>& b)
-    {
-      for (int i=0; i<n; i++) x[i] += b.x[i];
-      return *this;
-    }
-    Vec<n,T> operator+ (const Vec<n,T>& b) const
-    {
-      Vec<n,T> z = *this;
-      return (z+=b);
-    }
-
-    //! operator- binary minus
-    Vec<n,T>& operator-= (const Vec<n,T>& b)
-    {
-      for (int i=0; i<n; i++) x[i] -= b.x[i];
-      return *this;
-    }
-    Vec<n,T> operator- (const Vec<n,T>& b) const
-    {
-      Vec<n,T> z = *this;
-      return (z-=b);
-    }
-
-    //! scalar product of two vectors with operator*
-    T operator* (const Vec<n,T>& b) const
-    {
-      T s=0; for (int i=0; i<n; i++) s += x[i]*b.x[i];return s;
-    }
-
-    //! multiplication of vector with scalar
-    T operator* (T k) const
-    {
-      Vec<n,T> z; for (int i=0; i<n; i++) z.x[i] =  k*x[i];return z;
-    }
-
-    //! multiplication assignment with scalar
-    Vec<n,T>& operator*= (T k)
-    {
-      for (int i=0; i<n; i++) x[i] *= k;
-      return *this;
-    }
-
-    //! 1 norm
-    T norm1 () const
-    {
-      T s=0.0;
-      for (int i=0; i<n; i++) s += ABS(x[i]);
-      return s;
-    }
-
-    //! 2 norm
-    T norm2 () const
-    {
-      T s=0.0;
-      for (int i=0; i<n; i++) s += x[i]*x[i];
-      return sqrt(s);
-    }
-
-    //! Infinity norm
-    T norminfty () const
-    {
-      T s=0.0;
-      for (int i=0; i<n; i++)
-        if (ABS(x[i])>s) s = ABS(x[i]);
-      return s;
-    }
-
-    //! Euclidean distance of two vectors
-    T distance (const Vec<n,T>& b) const
-    {
-      T s=0.0;
-      for (int i=0; i<n; i++) s += (x[i]-b.x[i])*(x[i]-b.x[i]);
-      return sqrt(s);
-    }
-
-    //! Print content of vector to a stream
-    void print (std::ostream& s, int indent) const
-    {
-      for (int k=0; k<indent; k++) s << " ";
-      s << "Vec [ ";
-      for (int i=0; i<n; i++) s << x[i] << " ";
-      s << "]";
-    }
-
-  private:
-    //! built-in array to hold the data.
-    T x[n];
-  };
-
-  //! multiplication of scalar with vector
-  template<int n, class T>
-  inline Vec<n,T> operator* (T k, Vec<n,T> b)
-  {
-    Vec<n,T> z; for (int i=0; i<n; i++) z(i) = k*b(i);return z;
-  }
-
-  //! unary minus
-  template<int n, class T>
-  inline Vec<n,T> operator- (Vec<n,T> b)
-  {
-    Vec<n,T> z; for (int i=0; i<n; i++) z(i) = -b(i);return z;
-  }
-
-  //! \todo Please doc me!
-  template <int n, class T>
-  inline std::ostream& operator<< (std::ostream& s, const Vec<n,T>& v)
-  {
-    v.print(s,0);
-    return s;
-  }
 
   //************************************************************************
   /*! Generic class for static rectangular matrices
@@ -212,7 +31,7 @@ namespace Dune {
     enum { dimRow = dim };
     enum { dimCol = n };
 
-    //! Constructor making uninizialized matrix
+    //! Constructor making uninitialized matrix
     Mat() {}
 
     //! Constructor setting all entries to t
@@ -239,7 +58,7 @@ namespace Dune {
     {
       Vec<n,T> z(0.0);
       for (int j=0; j<m; j++)
-        for (int i=0; i<n; i++) z(i) += a[j](i) * x.read(j);
+        for (int i=0; i<n; i++) z(i) += a[j](i) * x[j];
       return z;
     }
 
