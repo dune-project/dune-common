4633ded5759bfc684ebea4e8db714e579d355f0d
Robert Klöfkorn <robertk@dune-project.org>
2004-07-07 08:10:42 +0000

    Changed pure virtuality of operator = back. This is only for a wilhe until I checked to hole code. I dont have the time rigth now.
    [[Imported from SVN: r547]]

--- common/vectorspace.hh
+++ common/vectorspace.hh
@@ -24,7 +24,12 @@ namespace Dune {
     //virtual Vector<Field> operator - (const Vector<Field> &) const = 0;
     //virtual Vector<Field> operator * (const Field &) const = 0;
     //virtual Vector<Field> operator / (const Field &) const = 0;
-    virtual Vector<Field>& operator  = (const Vector<Field> &) = 0;
+
+    //! note: Oli changed all method to, I changed it back, because
+    //! otherwise the code will not compile and I fixed this hack later.
+    virtual Vector<Field>& operator  = (const Vector<Field> &) { return *this;};
+
+    //! Methods are pure virtual, that the user is forced to implement them
     virtual Vector<Field>& operator += (const Vector<Field> &) = 0;
     virtual Vector<Field>& operator -= (const Vector<Field> &) = 0;
     virtual Vector<Field>& operator *= (const Field &) = 0;
