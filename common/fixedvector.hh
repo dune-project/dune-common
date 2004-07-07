16b0c995f95716e0c1f73a3349c241fee93a51db
Robert Klöfkorn <robertk@dune-project.org>
2004-07-07 09:42:16 +0000

    Replaced all n with dim. Furthermore gcc 3.4.(x) conform.
    [[Imported from SVN: r554]]

--- common/fixedvector.hh
+++ common/fixedvector.hh
@@ -32,71 +32,71 @@ namespace Dune {
     Vec() {}
 
     //! Constructor making vector from built-in array
-    Vec (T* y) {for(int i=0; i<n; i++) a[i]=y[i];}
+    Vec (T* y) {for(int i=0; i<dim; i++) this->a[i]=y[i];}
 
     //! Constructor making vector with one coordinate set, others zeroed
     Vec (int k, T t)
     {
-      for (int i=0; i<n; i++) a[i] = 0;
-      a[k] = t;
+      for (int i=0; i<dim; i++) this->a[i] = 0;
+      this->a[k] = t;
     }
 
     //! Constructor making vector with identical coordinates
     Vec (T t)
     {
-      for (int i=0; i<n; i++) a[i] = t;
+      for (int i=0; i<dim; i++) this->a[i] = t;
     }
 
     //! operator () for read/write access to element of the vector
-    T& operator() (int i) {return a[i];}
+    T& operator() (int i) {return this->a[i];}
 
     //! operator () for read access to element of the vector
-    const T& operator() (int i) const {return a[i];}
+    const T& operator() (int i) const {return this->a[i];}
 
     //! operator+ adds two vectors
-    Vec<n,T>& operator+= (const Vec<n,T>& b)
+    Vec<dim,T>& operator+= (const Vec<dim,T>& b)
     {
-      for (int i=0; i<n; i++) a[i] += b.a[i];
+      for (int i=0; i<dim; i++) this->a[i] += b.a[i];
       return *this;
     }
 
     //! Vector addition
-    Vec<n,T> operator+ (const Vec<n,T>& b) const
+    Vec<dim,T> operator+ (const Vec<dim,T>& b) const
     {
-      Vec<n,T> z = *this;
+      Vec<dim,T> z = *this;
       return (z+=b);
     }
 
     //! operator- binary minus
-    Vec<n,T>& operator-= (const Vec<n,T>& b)
+    Vec<dim,T>& operator-= (const Vec<dim,T>& b)
     {
-      for (int i=0; i<n; i++) a[i] -= b.a[i];
+      for (int i=0; i<dim; i++) this->a[i] -= b.a[i];
       return *this;
     }
 
     //! Vector subtraction
-    Vec<n,T> operator- (const Vec<n,T>& b) const
+    Vec<dim,T> operator- (const Vec<dim,T>& b) const
     {
-      Vec<n,T> z = *this;
+      Vec<dim,T> z = *this;
       return (z-=b);
     }
 
     //! scalar product of two vectors with operator*
-    T operator* (const Vec<n,T>& b) const
+    T operator* (const Vec<dim,T>& b) const
     {
-      T s=0; for (int i=0; i<n; i++) s += a[i]*b.a[i];return s;
+      T s=0; for (int i=0; i<dim; i++) s += this->a[i]*b.a[i];return s;
     }
 
     //! multiplication of vector with scalar
-    Vec<n,T> operator* (T k) const
+    Vec<dim,T> operator* (T k) const
     {
-      Vec<n,T> z; for (int i=0; i<n; i++) z.a[i] =  k*a[i];return z;
+      Vec<dim,T> z; for (int i=0; i<dim; i++) z.a[i] =  k*this->a[i];return z;
     }
 
     //! multiplication assignment with scalar
-    Vec<n,T>& operator*= (T k)
+    Vec<dim,T>& operator*= (T k)
     {
-      for (int i=0; i<n; i++) a[i] *= k;
+      for (int i=0; i<dim; i++) this->a[i] *= k;
       return *this;
     }
 
@@ -104,7 +104,7 @@ namespace Dune {
     T norm1 () const
     {
       T s=0.0;
-      for (int i=0; i<n; i++) s += ABS(a[i]);
+      for (int i=0; i<dim; i++) s += ABS(this->a[i]);
       return s;
     }
 
@@ -112,7 +112,7 @@ namespace Dune {
     T norm2 () const
     {
       T s=0.0;
-      for (int i=0; i<n; i++) s += a[i]*a[i];
+      for (int i=0; i<dim; i++) s += this->a[i]*this->a[i];
       return sqrt(s);
     }
 
@@ -120,34 +120,32 @@ namespace Dune {
     T norminfty () const
     {
       T s=0.0;
-      for (int i=0; i<n; i++)
-        if (ABS(a[i])>s) s = ABS(a[i]);
+      for (int i=0; i<dim; i++)
+        if (ABS(this->a[i])>s) s = ABS(this->a[i]);
       return s;
     }
 
     //! Euclidean distance of two vectors
-    T distance (const Vec<n,T>& b) const
+    T distance (const Vec<dim,T>& b) const
     {
       T s=0.0;
-      for (int i=0; i<n; i++) s += (a[i]-b.a[i])*(a[i]-b.a[i]);
+      for (int i=0; i<dim; i++) s += (this->a[i]-b.a[i])*(this->a[i]-b.a[i]);
       return sqrt(s);
     }
-
-
   };
 
   //! multiplication of scalar with vector
-  template<int n, class T>
-  inline Vec<n,T> operator* (T k, Vec<n,T> b)
+  template<int dim, class T>
+  inline Vec<dim,T> operator* (T k, Vec<dim,T> b)
   {
-    Vec<n,T> z; for (int i=0; i<n; i++) z(i) = k*b(i);return z;
+    Vec<dim,T> z; for (int i=0; i<dim; i++) z(i) = k*b(i);return z;
   }
 
   //! unary minus
-  template<int n, class T>
-  inline Vec<n,T> operator- (Vec<n,T> b)
+  template<int dim, class T>
+  inline Vec<dim,T> operator- (Vec<dim,T> b)
   {
-    Vec<n,T> z; for (int i=0; i<n; i++) z(i) = -b(i);return z;
+    Vec<dim,T> z; for (int i=0; i<dim; i++) z(i) = -b(i);return z;
   }
 
   /** @} */
