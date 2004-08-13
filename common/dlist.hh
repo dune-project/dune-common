fde9f1d1d918f27ec69e3c14bc5f1cfdd2618232
Oliver Sander <sander@dune-project.org>
2004-08-13 08:26:53 +0000

    Doxygen
    [[Imported from SVN: r674]]

--- common/dlist.hh
+++ common/dlist.hh
@@ -1,41 +1,92 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef __DLIST_HH__
-#define __DLIST_HH__
+#ifndef __DUNE_DLIST_HH__
+#define __DUNE_DLIST_HH__
 
 namespace Dune {
 
-  template <class T> class DoubleLinkedList {
+  /** \brief A doubly-linked list
+   */
+  template <class T>
+  class DoubleLinkedList {
   private:
     struct Element;       // Vorwaertsdeklaration fuer das Listenelement
   public:
-    class Iterator {      // Iteratorklasse zum
-    private:              // Durchlaufen der Elemente des Containers
-      Element* p;       // Iterator ist ein Zeiger auf ein Listenelement
+
+    /** \brief Iterator class for the doubly-linked list
+     */
+    class Iterator {
+    private:
+      //! Iterator is a pointer to a list element
+      Element* p;
     public:
+
+      //! ???
       Iterator();
+
+      //! ???
       bool operator!= (Iterator x);
+
+      //! ???
       bool operator== (Iterator x);
-      Iterator operator++ ();       // prefix Stroustrup p. 292
-      Iterator operator++ (int);    // postfix
-      Iterator operator-- ();       // prefix
-      Iterator operator-- (int);    // postfix
+
+      //! Prefix increment
+      Iterator operator++ ();
+
+      //! Postfix increment
+      Iterator operator++ (int);
+
+      //! Prefix decrement
+      Iterator operator-- ();
+
+      //! Postfix decrement
+      Iterator operator-- (int);
+
+      //! ???
       T& operator* () const;
+
+      //! ???
       T* operator-> () const;     // Stroustrup p. 289
+
+      //! ???
       friend class DoubleLinkedList<T>;
     } ;
+
+    //! ???
     Iterator begin () const;
+
+    //! ???
     Iterator end () const;
+
+    //! ???
     Iterator rbegin () const;
+
+    //! ???
     Iterator rend () const;
 
+
+    //! ???
     DoubleLinkedList();
+
+    //! ???
     DoubleLinkedList (const DoubleLinkedList<T>&);
+
+    //! ???
     ~DoubleLinkedList();
+
+    //! ???
     DoubleLinkedList<T>& operator= (const DoubleLinkedList<T>&);
+
+    //! ???
     int size () const;
+
+    //! ???
     Iterator insert_after (Iterator i, T& t);
+
+    //! ???
     Iterator insert_before (Iterator i, T& t);
+
+    //! ???
     void erase (Iterator i);
 
   private:
