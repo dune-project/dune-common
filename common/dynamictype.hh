1d046253f7a4a32dcf9bb98d85f98a4438be9504
Oliver Sander <sander@dune-project.org>
2004-08-23 08:18:08 +0000

    Doxygen
    [[Imported from SVN: r698]]

--- common/dynamictype.hh
+++ common/dynamictype.hh
@@ -7,17 +7,29 @@ namespace Dune {
 
   typedef int IdentifierType;
 
+  //! ???
   class DynamicType {
   public:
+
+    //! ???
     static const IdentifierType undefined = 0;
 
     //! set identifier of this class
     DynamicType (int ident) : identifier_ (ident) {};
+
+    //! Make undefined identifier
     DynamicType () : identifier_ (undefined) {};
 
+    //! ???
     virtual bool operator == ( const DynamicType &) {return true;};
+
+    //! ???
     virtual bool operator != ( const DynamicType &) {return true;};
+
+    //! ???
     virtual bool operator <= ( const DynamicType &) {return true;};
+
+    //! ???
     virtual bool operator >= ( const DynamicType &) { return true; };
 
     //! return identifier
@@ -29,13 +41,13 @@ namespace Dune {
 
 
   protected:
-    // method to set identifier later, which we need for quadratures
+    //! Method to set identifier later, which we need for quadratures
     void setIdentifier (IdentifierType ident)
     {
       identifier_ = ident;
     }
 
-    // the identifier, up to now an unique int
+    //! The identifier, up to now an unique int
     IdentifierType identifier_;
   };
 
