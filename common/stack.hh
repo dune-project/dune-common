32c8a03d6d14e57437174264187ae5cbbf8e5e75
Oliver Sander <sander@dune-project.org>
2004-08-31 10:04:51 +0000

    Crude error handling if pop_front() or front() is called for an empty stack
    [[Imported from SVN: r717]]

--- common/stack.hh
+++ common/stack.hh
@@ -60,6 +60,10 @@ namespace Dune {
   template<class T>
   inline T Stack<T>::pop_front ()
   {
+    if (isempty()) {
+      std::cerr << "Stack::pop_front(): The stack is empty!\n";
+      assert(false);
+    }
     typename DoubleLinkedList<T>::Iterator i=this->rbegin();
     T t = *i;
     erase(i);
@@ -69,6 +73,10 @@ namespace Dune {
   template<class T>
   inline T Stack<T>::front () const
   {
+    if (isempty()) {
+      std::cerr << "Stack::front(): The stack is empty!\n";
+      assert(false);
+    }
     typename DoubleLinkedList<T>::Iterator i=this->rbegin();
     T t = *i;
     return t;
