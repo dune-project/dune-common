f79a32f9f369e1ef8717d51eb2408410b7427d4d
Oliver Sander <sander@dune-project.org>
2004-08-31 10:20:23 +0000

    The adaptoperator formerly in examples/transport
    [[Imported from SVN: r722]]

---
+++ fem/transfer/adaptoperator.hh
@@ -0,0 +1,288 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef __DUNE_ADAPTOPERATORIMP_HH__
+#define __DUNE_ADAPTOPERATORIMP_HH__
+
+#include <dune/common/array.hh>
+
+namespace Dune {
+
+  /** @defgroup AdaptOperatorImp AdaptOperatorImp
+      @ingroup Adaptation
+
+     @{
+   */
+
+  /*! this could be seen as a hack, but is not
+     With this mehtod we define the class CombineRestProl which is only
+     for combining of local grid operations without using virtual mehtods.
+     The Interface of the two defined methods of the class (restrictLocal and
+     prolongLocal) is given by the implementation (see below ) and
+     has to be the same for all local operators you want to combine
+   */
+#define PARAM_CLASSNAME CombinedRestProl
+#define PARAM_FUNC_1 restrictLocal
+#define PARAM_FUNC_2 prolongLocal
+#include <dune/fem/common/combine.hh>
+
+  /*!
+     This Class is the result of a combination of different
+     AdaptationOperators. It ist the same principle as with Mapping and
+     DiscreteOperatorImp.
+   */
+  class AdaptMapping
+  {
+  public:
+    AdaptMapping () : am_ (0) {}
+
+    //! all adaptation operators have this method which adapts the
+    //! corresponding grid and organizes the restriction prolongation process
+    //! of the underlying function spaces
+    virtual void adapt () const
+    {
+      if(am_) am_->adapt();
+      else
+      {
+        std::cerr << "WARNING: adapt! \n";
+      }
+    };
+
+    AdaptMapping & operator = (const AdaptMapping & am)
+    {
+      am_ = const_cast<AdaptMapping *> (&am);
+      return (*this);
+    }
+  private:
+    AdaptMapping *am_;
+  };
+
+  template <class GridType, class RestProlOperatorImp, class DofManagerType >
+  class AdaptOperator
+    :
+      public AdaptMapping , public ObjPointerStorage
+  {
+    typedef AdaptOperator<GridType,RestProlOperatorImp,DofManagerType> MyType;
+  public:
+    //! create DiscreteOperator with a LocalOperator
+    AdaptOperator (GridType & grid, DofManagerType & dm, RestProlOperatorImp & rpOp) :
+      grid_(grid) , dm_ ( dm ), rpOp_ (rpOp)  {}
+
+    virtual ~AdaptOperator () {}
+
+    /*!
+       Add to AdaptOperators means that the RestProlOperators will be combined.
+       See DiscreteOperatorImp.
+     */
+    template <class RestProlOperatorType>
+    AdaptOperator<GridType,
+        CombinedRestProl <RestProlOperatorImp,RestProlOperatorType>, DofManagerType > &
+    operator + (const AdaptOperator<GridType,RestProlOperatorType,DofManagerType> &op)
+    {
+      std::cout << "Operator + of AdaptOperator\n";
+      typedef AdaptOperator<GridType,RestProlOperatorType,DofManagerType> CopyType;
+      typedef CombinedRestProl <RestProlOperatorImp,RestProlOperatorType> COType;
+
+      COType *newRPOp = new COType ( rpOp_  , const_cast<CopyType &> (op).getRestProlOp() );
+
+      typedef AdaptOperator < GridType, COType, DofManagerType > OPType;
+
+      OPType *adaptOp = new OPType ( grid_ , dm_ , *newRPOp );
+
+      // memorize this new generated object because is represents this
+      // operator and is deleted if this operator is deleted
+      saveObjPointer( adaptOp );
+
+      //std::cout << "Created " << adaptOp << " \n";
+      return *adaptOp;
+    }
+
+    //! no public method, but has to be public, because all AdaptOperators
+    //! must be able to call this method and the template parameters are
+    //! allways different
+    RestProlOperatorImp & getRestProlOp ()
+    {
+      return rpOp_;
+    }
+
+    //! adapt defines the grid walkthrough before and after grid adaptation.
+    //! Note that the LocalOperator can be an combined Operator
+    //! Domain and Range are defined through class Operator
+    void adapt () const
+    {
+      bool restr = grid_.preAdapt();
+
+      if(restr)
+      {
+        dm_.resizeTmp();
+        typedef typename GridType::template Traits<0>::LevelIterator LevelIterator;
+
+        // make run through grid
+        for(int l=0; l<grid_.maxlevel(); l++)
+        {
+          LevelIterator it    = grid_.template lbegin<0> ( l );
+          LevelIterator endit = grid_.template lend<0>   ( l );
+          for( ; it != endit; ++it )
+          {
+            hierarchicRestrict( *it );
+          }
+        }
+      }
+
+      bool ref = grid_.adapt();
+
+      if(ref)
+      {
+        dm_.resize();
+
+        typedef typename GridType::template Traits<0>::LevelIterator LevelIterator;
+
+        // make run through grid
+        LevelIterator it    = grid_.template lbegin<0> ( 0 );
+        LevelIterator endit = grid_.template lend<0>   ( 0 );
+
+        for( ; it != endit; ++it )
+        {
+          hierarchicProlong( *it );
+        }
+      }
+
+      // if grid was coarsend of refined, do dof compress
+      if(restr || ref)
+        dm_.dofCompress();
+
+      // do cleanup
+      grid_.postAdapt();
+    }
+
+  private:
+    // make hierarchic walk trough
+    template <class EntityType>
+    void hierarchicRestrict ( EntityType &en) const
+    {
+      if(en.hasChildren())
+      {
+        typedef typename EntityType::Traits::HierarchicIterator HierarchicIterator;
+        HierarchicIterator it    = en.hbegin( en.level() + 1 );
+
+        // if the children have children then we have to go deeper
+        if(it->hasChildren()) return;
+
+        HierarchicIterator endit = en.hend  ( en.level() + 1 );
+
+        // true for first child, otherwise false
+        bool initialize = true;
+
+        for( ; it != endit; ++it)
+        {
+          if((*it).state() == COARSEN)
+          {
+            // create index for fatty, here set indices equal
+            if(initialize)
+              dm_.createFatherIndex( en );
+
+            rpOp_.restrictLocal( en , *it, initialize);
+            initialize = false;
+          }
+        }
+      }
+    }
+
+    template <class EntityType>
+    void hierarchicProlong ( EntityType &en) const
+    {
+      typedef typename EntityType::Traits::HierarchicIterator HierarchicIterator;
+      HierarchicIterator it    = en.hbegin( grid_.maxlevel() );
+      HierarchicIterator endit = en.hend  ( grid_.maxlevel() );
+
+      bool initialize = true;
+
+      EntityType father = en.newEntity();
+
+      for( ; it != endit; ++it)
+      {
+        if((*it).state() == REFINED)
+        {
+          (*it).father( father );
+          rpOp_.prolongLocal( father, *it , initialize );
+          initialize = false;
+        }
+      }
+    }
+
+
+    //! corresponding grid
+    mutable GridType & grid_;
+
+    //! DofManager corresponding to grid
+    mutable DofManagerType & dm_;
+
+    //! Restriction and Prolongation Operator
+    mutable RestProlOperatorImp & rpOp_;
+
+  };
+
+
+  //***********************************************************************
+
+  template <class DiscreteFunctionType>
+  class RestProlOperatorFV
+  {
+    typedef typename DiscreteFunctionType::LocalFunctionType LocalFunctionType;
+  public:
+    RestProlOperatorFV ( DiscreteFunctionType & df ) : df_ (df) ,
+                                                       vati_ ( df_.newLocalFunction() ) , sohn_ ( df_.newLocalFunction() )
+    {}
+
+    template <class EntityType>
+    void restrictLocal ( EntityType &father, EntityType &son, bool initialize ) const
+    {
+      if(father.hasChildren())
+      {
+        if(son.state() == COARSEN)
+        {
+          df_.localFunction( father, vati_ );
+          df_.localFunction( son   , sohn_ );
+
+          //std::cout << father.global_index() << " Father Element\n";
+          //std::cout << son.global_index() << " Sohn Element\n";
+
+          if(initialize)
+          {
+            for(int i=0; i<vati_.numberOfDofs(); i++)
+              vati_[i] = 0.5 * sohn_[i];
+          }
+          else
+          {
+            for(int i=0; i<vati_.numberOfDofs(); i++)
+              vati_[i] += 0.5 * sohn_[i];
+          }
+
+        }
+      }
+    }
+
+    template <class EntityType>
+    void prolongLocal ( EntityType &father, EntityType &son, bool initialize ) const
+    {
+      if(son.state() == REFINED)
+      {
+        df_.localFunction( father, vati_ );
+        df_.localFunction( son   , sohn_ );
+        for(int i=0; i<vati_.numberOfDofs(); i++)
+          sohn_[i] = vati_[i];
+      }
+    }
+
+  private:
+    mutable DiscreteFunctionType & df_;
+
+    mutable LocalFunctionType vati_;
+    mutable LocalFunctionType sohn_;
+  };
+
+
+  /** @} end documentation group */
+
+}
+
+#endif
