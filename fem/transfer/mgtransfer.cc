30260b5b94fa10153f4b7f4fe5bfd01dcae5743a
Oliver Sander <sander@dune-project.org>
2004-09-01 14:51:56 +0000

    This class implements the restriction and prolongation operators for standard multigrid algorithms.  So far it only works on linear Lagrangian elements.  Eventually it will get generalized, though.
    [[Imported from SVN: r736]]

---
+++ fem/transfer/mgtransfer.cc
@@ -0,0 +1,160 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <dune/fem/dofmanager.hh>
+#include <dune/fem/lagrangebase.hh>
+#include <dune/common/functionspace.hh>
+//#include <dune/fem/discfuncarray.hh>
+#include <dune/fem/fastquad.hh>
+
+
+template<class DiscFuncType>
+void Dune::MGTransfer<DiscFuncType>::setup(const FunctionSpaceType& fS, int cL, int fL)
+{
+  coarseLevel = cL;
+  fineLevel   = fL;
+
+  assert(fL == cL+1);
+  //assert(level<grid_->maxlevel());
+
+  //typedef typename DiscFuncType::FunctionSpaceType FunctionSpaceType;
+  typedef typename FunctionSpaceType::GridType GridType;
+
+  int rows = fS.size(coarseLevel);
+  int cols = fS.size(fineLevel);
+
+  GridType& grid = fS.getGrid();
+
+  //
+  matrix_.resize(rows, cols, GridType::dimension*10);
+  matrix_.clear();
+
+  typedef typename GridType::template Traits<0>::LevelIterator ElementIterator;
+  typedef typename FunctionSpaceType::BaseFunctionSetType BaseFunctionSetType;
+
+  ElementIterator cIt    = grid.template lbegin<0>(coarseLevel);
+  ElementIterator cEndIt = grid.template lend<0>(coarseLevel);
+
+  for (; cIt != cEndIt; ++cIt) {
+    //std::cout << "Coarse: " << cIt->index() << "\n";
+
+    const BaseFunctionSetType& coarseBaseSet = fS.getBaseFunctionSet( *cIt );
+    const int numCoarseBaseFct = coarseBaseSet.getNumberOfBaseFunctions();
+
+
+    typedef typename GridType::template Traits<0>::Entity EntityType;
+    typedef typename EntityType::Traits::HierarchicIterator HierarchicIterator;
+
+    HierarchicIterator fIt    = cIt->hbegin(fineLevel);
+    HierarchicIterator fEndIt = cIt->hend(fineLevel);
+
+    for (; fIt != fEndIt; ++fIt) {
+
+      //std::cout << "Coarse: " << cIt->index() << "   fine: " << fIt->index() << "\n";
+
+      const BaseFunctionSetType& fineBaseSet = fS.getBaseFunctionSet( *fIt );
+      const int numFineBaseFct = fineBaseSet.getNumberOfBaseFunctions();
+
+      for (int i=0; i<numCoarseBaseFct; i++) {
+
+        int globalCoarse = fS.mapToGlobal(*cIt, i);
+
+        for (int j=0; j<numFineBaseFct; j++) {
+
+          int globalFine = fS.mapToGlobal(*fIt, j);
+
+
+          //std::cout << "globalCoarse: " << globalCoarse
+          //        << "   globalFine: " << globalFine << "\n";
+
+          // Evaluate coarse grid base function at the location of the fine grid dof
+
+          // first determine local fine grid dof position
+          Vec<GridType::dimension, double> local = cIt->geometry().local(fIt->geometry()[j]);
+
+          //std::cout << "finePos" << fIt->geometry()[j] << "\n";
+          //std::cout << "locally" << local << "\n";
+
+          // Evaluate coarse grid base function
+
+          Vec<GridType::dimension, double> value;
+          Vec<0, int> diffVariable;
+          coarseBaseSet.evaluate(i, diffVariable, local, value);
+
+          //std::cout << "value: " << value << "\n";
+
+          matrix_.set(globalCoarse, globalFine, value[0]);
+        }
+
+
+      }
+
+    }
+
+  }
+
+  //matrix_.print(std::cout);
+}
+
+
+template<class DiscFuncType>
+void Dune::MGTransfer<DiscFuncType>::restrict (const DiscFuncType & f, DiscFuncType& t) const
+{
+  assert(t.getFunctionSpace().size(coarseLevel) == matrix_.rows());
+  assert(f.getFunctionSpace().size(fineLevel)   == matrix_.cols());
+
+  typedef typename DiscFuncType::DofIteratorType DofIteratorType;
+  typedef typename SparseRowMatrix<double>::ColumnIterator ColumnIterator;
+
+
+  DofIteratorType tIt = t.dbegin( coarseLevel );
+  DofIteratorType fIt = f.dbegin( fineLevel );
+
+  for(int row=0; row<matrix_.rows(); row++) {
+
+    *tIt = 0.0;
+    /** \todo Remove casts */
+    ColumnIterator cIt    = const_cast<SparseRowMatrix<double>&>(matrix_).template rbegin(row);
+    ColumnIterator cEndIt = const_cast<SparseRowMatrix<double>&>(matrix_).template rend(row);
+
+    for(; cIt!=cEndIt; ++cIt) {
+      *tIt += *cIt * fIt[cIt.col()];
+    }
+
+    ++tIt;
+  }
+}
+
+template<class DiscFuncType>
+void Dune::MGTransfer<DiscFuncType>::prolong(const DiscFuncType& f, DiscFuncType& t) const
+{
+  assert(f.getFunctionSpace().size(coarseLevel) == matrix_.rows());
+  assert(t.getFunctionSpace().size(fineLevel)   == matrix_.cols());
+
+  typedef typename DiscFuncType::DofIteratorType DofIteratorType;
+  typedef typename SparseRowMatrix<double>::ColumnIterator ColumnIterator;
+
+  t.clearLevel(fineLevel);
+
+  DofIteratorType tIt = t.dbegin( fineLevel );
+  DofIteratorType fIt = f.dbegin( coarseLevel );
+
+
+  for (int row=0; row<matrix_.rows(); row++) {
+
+    /** \todo Remove casts */
+    ColumnIterator cIt    = const_cast<SparseRowMatrix<double>&>(matrix_).template rbegin(row);
+    ColumnIterator cEndIt = const_cast<SparseRowMatrix<double>&>(matrix_).template rend(row);
+
+    for(; cIt!=cEndIt; ++cIt)
+      tIt[cIt.col()] += fIt[row] * (*cIt);
+
+  }
+}
+
+
+template<class DiscFuncType>
+Dune::SparseRowMatrix<double> Dune::MGTransfer<DiscFuncType>::
+galerkinRestrict(const Dune::SparseRowMatrix<double>& fineMat) const
+{
+  return matrix_.applyFromLeftAndRightTo(fineMat);
+}
---
+++ fem/transfer/mgtransfer.hh
@@ -0,0 +1,56 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef __DUNE_MG_TRANSFER_HH__
+#define __DUNE_MG_TRANSFER_HH__
+
+#include <dune/fem/feop/spmatrix.hh>
+
+namespace Dune {
+
+  /** \brief Standard multigrid restriction and prolongation operator
+   *
+   * Currently only works for first-order Lagrangian elements!
+   */
+  template<class DiscFuncType>
+  class MGTransfer {
+
+    typedef typename DiscFuncType::FunctionSpaceType FunctionSpaceType;
+
+  public:
+
+    /** \brief Sets up the operator between levels cL and fL
+     *
+     * \param fS The function space hierarchy between levels of which we're mapping
+     * \param cL The coarse level
+     * \param fL The fine level
+     */
+    void setup(const FunctionSpaceType& fS, int cL, int fL);
+
+    /** \brief Restrict level fL of f and store the result in level cL of t
+     */
+    void restrict (const DiscFuncType & f, DiscFuncType &t) const;
+
+    /** \brief Prolong level cL of f and store the result in level fL of t
+     */
+    void prolong(const DiscFuncType& f, DiscFuncType &t) const;
+
+    /** \brief Galerkin assemble a coarse stiffness matrix
+     */
+    SparseRowMatrix<double> galerkinRestrict(const SparseRowMatrix<double>& fineMat) const;
+
+
+  protected:
+    int coarseLevel;
+
+    int fineLevel;
+
+    /** \todo Should we extract the value type from DiscFuncType? */
+    SparseRowMatrix<double> matrix_;
+
+  };
+
+}
+
+#include "mgtransfer.cc"
+
+#endif
