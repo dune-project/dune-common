0340242b434f1827c660571c451fa2f4e171a3ab
Robert Klöfkorn <robertk@dune-project.org>
2004-09-02 14:50:29 +0000

    Removed PointsOnLins struct which is only used in fastquad/gausquadimp.*
    [[Imported from SVN: r753]]

--- fem/common/quadrature.hh
+++ fem/common/quadrature.hh
@@ -24,49 +24,6 @@ namespace Dune {
   // to the user.
   //
   //************************************************************************
-  // calculates the number of points on on segment, i.e. a line
-  template <int order>
-  struct PointsOnLine
-  {
-    // from Peters GaussQuadrature, see Constructor
-    enum { points = ( order > 17 ) ? 10 : 1 };
-  };
-
-  // specialization for the given order
-  template <> struct PointsOnLine <3>  { enum { points = 2 }; };
-  template <> struct PointsOnLine <4>  { enum { points = 3 }; };
-  template <> struct PointsOnLine <5>  { enum { points = 3 }; };
-  template <> struct PointsOnLine <6>  { enum { points = 4 }; };
-  template <> struct PointsOnLine <7>  { enum { points = 4 }; };
-  template <> struct PointsOnLine <8>  { enum { points = 5 }; };
-  template <> struct PointsOnLine <9>  { enum { points = 5 }; };
-  template <> struct PointsOnLine <10> { enum { points = 6 }; };
-  template <> struct PointsOnLine <11> { enum { points = 6 }; };
-  template <> struct PointsOnLine <12> { enum { points = 7 }; };
-  template <> struct PointsOnLine <13> { enum { points = 7 }; };
-  template <> struct PointsOnLine <14> { enum { points = 8 }; };
-  template <> struct PointsOnLine <15> { enum { points = 8 }; };
-  template <> struct PointsOnLine <16> { enum { points = 9 }; };
-  template <> struct PointsOnLine <17> { enum { points = 9 }; };
-  // other specialization possible
-
-  // calculates m^p
-  template <int m, int p>
-  struct power_M_P
-  {
-    // power stores m^p
-    enum { power = (m * power_M_P<m,p-1>::power ) };
-  };
-
-
-  // end of recursion via specialization
-  template <int m>
-  struct power_M_P< m , 0>
-  {
-    // m^0 = 1
-    enum { power = 1 };
-  };
-
 
   /** @defgroup Quadrature Quadrature
      @ingroup DiscreteFunction
