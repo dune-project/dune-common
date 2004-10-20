df3568183ba1e2667e80d3e65759da2a57de5d7f
Robert Klöfkorn <robertk@dune-project.org>
2004-10-20 11:42:21 +0000

    Changed elapsed method. The Command times returns real time which we won't use for time calculation. Now the method returns user+sys time which is the time the process really needed. This time should be equal for diffrent  run on the same machine, not depending on how busy the machine is.
    [[Imported from SVN: r936]]

--- common/timer.hh
+++ common/timer.hh
@@ -3,56 +3,55 @@
 #ifndef __DUNE_TIMER_HH__
 #define __DUNE_TIMER_HH__
 
-#include <math.h>
-#include <complex>
-#include <iostream>
-#include <iomanip>
-#include <string>
+//#include<math.h>
+//#include<complex>
+//#include<iostream>
+//#include<iomanip>
+//#include<string>
 #include <sys/times.h>            // for timing measurements
-
+#include <asm/param.h>
 
 /*! \file
-
     A simple timing class.
  */
 
 namespace Dune {
 
   /** @addtogroup ISTL
-          @{
+     @{
    */
 
-
   //! a simple stop watch
+  //! using the C command 'times' defined in sys/times.h
   class Timer
   {
   public:
     //! a new timer, start immediately
     Timer ()
     {
-      struct tms buf;
-      cstart = times(&buf);
+      reset();
     }
 
     //! reset timer
     void reset ()
     {
       struct tms buf;
-      cstart = times(&buf);
+      times(&buf);
+      cstart = buf.tms_utime + buf.tms_stime;
     }
 
-    //! get elapsed time in seconds
+    //! get elapsed user+sys time in seconds
     double elapsed ()
     {
       struct tms buf;
-      cend = times(&buf);
-      return ((double)(cend-cstart))/100.0;
+      times(&buf);
+      clock_t cend = buf.tms_utime + buf.tms_stime;
+      return difftime(cend,cstart)/HZ;
     }
 
   private:
-    clock_t cstart,cend;
-  };
-
+    clock_t cstart;
+  }; // end class Timer
 
   /** @} end documentation */
 
