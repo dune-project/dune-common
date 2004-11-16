6e61ca0d766f3fd1ab7f76a3fab1053327dc05ac
Christian Engwer <christi@dune-project.org>
2004-11-16 15:55:24 +0000

    Use the better ugquadrature
    [[Imported from SVN: r1099]]

--- quadrature/fixedorder/quadtetratri.hh
+++ quadrature/fixedorder/quadtetratri.hh
@@ -35,7 +35,7 @@ namespace Dune {
   int QuadraturePoints<Domain,RangeField,triangle,polOrd>::
   numberOfQuadPoints()
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     return quad->nip;
   }
 
@@ -44,7 +44,7 @@ namespace Dune {
   int QuadraturePoints<Domain,RangeField,triangle,polOrd>::
   order()
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     return quad->order;
   }
   //! the weight is the volume of the reference element
@@ -52,9 +52,10 @@ namespace Dune {
   RangeField QuadraturePoints<Domain,RangeField,triangle,polOrd>::
   getWeight(int i)
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
+    RangeField ref(referenceVol_triangle);
     RangeField
-    w(referenceVol_triangle * static_cast<RangeField> (quad->weight[i]));
+    w(ref * static_cast<RangeField> (quad->weight[i]));
     return w;
   }
 
@@ -63,73 +64,13 @@ namespace Dune {
   Domain QuadraturePoints<Domain,RangeField,triangle,polOrd>::
   getPoint(int i)
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     Domain tmp;
     for(int j=0; j<dim; j++)
       tmp[j] = quad->local[i][j];
     return tmp;
   }
 
-  //**********************************************************************
-  //! specialization triangles , polOrd 3
-  //**********************************************************************
-#if 0
-  HAVE_ALBERT
-  template <class Domain, class RangeField, int polOrd>
-  struct QuadraturePoints<Domain,RangeField,triangle,polOrd>
-  {
-    enum { dim = 2 };
-    enum { numberOfCorners = dim+1 };
-    typedef UG_Quadratures::QUADRATURE QUADRATURE;
-    static int numberOfQuadPoints ();
-    static int order ();
-    static Domain getPoint (int i);
-    static RangeField getWeight (int i);
-  };
-
-  //! the weight is the volume of the reference element
-  template <class Domain, class RangeField, int polOrd>
-  int QuadraturePoints<Domain,RangeField,triangle,polOrd>::
-  numberOfQuadPoints()
-  {
-    const ALBERT QUAD * quad = ALBERT get_quadrature(2,3);
-    return quad->n_points;
-  }
-
-  //! the weight is the volume of the reference element
-  template <class Domain, class RangeField, int polOrd>
-  int QuadraturePoints<Domain,RangeField,triangle,polOrd>::
-  order()
-  {
-    const ALBERT QUAD * quad = ALBERT get_quadrature(2,3);
-    return quad->degree;
-  }
-  //! the weight is the volume of the reference element
-  template <class Domain, class RangeField,int polOrd>
-  RangeField QuadraturePoints<Domain,RangeField,triangle,polOrd>::
-  getWeight(int i)
-  {
-    const ALBERT QUAD * quad = ALBERT get_quadrature(2,polOrd);
-    //assert((i>=0) && (i<quad->n_points));
-    return referenceVol_triangle * quad->w[i];
-  }
-
-  //! the weight is the volume of the reference element
-  template <class Domain, class RangeField,int polOrd>
-  Domain QuadraturePoints<Domain,RangeField,triangle,polOrd>::
-  getPoint(int i)
-  {
-    const ALBERT QUAD * quad = ALBERT get_quadrature(2,3);
-    assert((i>=0) && (i<quad->n_points));
-
-    // summ of coordinates of Dune reference element
-    Domain tmp = 0.0;
-    tmp(0) += quad->lambda[i][0];
-    tmp(1) += quad->lambda[i][1];
-
-    return tmp;
-  }
-#endif
   //*************************************************************
   //! specialization tetrahedrons
   template <class Domain, class RangeField, int polOrd>
@@ -149,7 +90,7 @@ namespace Dune {
   int QuadraturePoints<Domain,RangeField,tetrahedron,polOrd>::
   numberOfQuadPoints()
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     return quad->nip;
   }
 
@@ -158,7 +99,7 @@ namespace Dune {
   int QuadraturePoints<Domain,RangeField,tetrahedron,polOrd>::
   order()
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     return quad->order;
   }
 
@@ -167,9 +108,10 @@ namespace Dune {
   RangeField QuadraturePoints<Domain,RangeField,tetrahedron,polOrd>::
   getWeight(int i)
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
+    RangeField ref(referenceVol_tetrahedron);
     RangeField
-    w(referenceVol_tetrahedron * static_cast<RangeField> (quad->weight[i]));
+    w(ref * static_cast<RangeField> (quad->weight[i]));
     return w;
   }
 
@@ -178,7 +120,7 @@ namespace Dune {
   Domain QuadraturePoints<Domain,RangeField,tetrahedron,polOrd>::
   getPoint(int i)
   {
-    QUADRATURE * quad = UG_Quadratures::GetQuadrature(dim,numberOfCorners,polOrd);
+    QUADRATURE * quad = UG_Quadratures::GetQuadratureRule(dim,numberOfCorners,polOrd);
     Domain tmp;
     for(int j=0; j<dim; j++)
       tmp[j] = quad->local[i][j];
