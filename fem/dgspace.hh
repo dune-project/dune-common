18f9d79029c16dab7f89818093cc4902014532e8
Robert Klöfkorn <robertk@dune-project.org>
2004-12-09 15:18:43 +0000

    DGMapper moved to seperate file.
    [[Imported from SVN: r1262]]

--- fem/dgspace.hh
+++ fem/dgspace.hh
@@ -7,70 +7,11 @@
 
 #include <dune/fem/common/discretefunctionspace.hh>
 #include <dune/fem/dgspace/monomialbase.hh>
+#include <dune/fem/dgspace/dgmapper.hh>
 #include <dune/fem/dofmanager.hh>
 
 namespace Dune {
 
-  //***************************************************************************
-  //
-  //!  DG Mapper for mapping of local dof numbers to global dof numbers,
-  //!  i.e. the entry in the vector of unknowns
-  //
-  //***************************************************************************
-  template <class IndexSetType, int polOrd>
-  class DGMapper
-    : public DofMapperDefault < DGMapper <IndexSetType,polOrd> >
-  {
-    // index set of grid, i.e. the element indices
-    IndexSetType &indexSet_;
-
-    // number of dofs on element
-    int numberOfDofs_;
-    int level_;
-  public:
-    //! Constructor
-    DGMapper ( IndexSetType &iset , int numDof , int level) :
-      indexSet_ (iset), numberOfDofs_ (numDof), level_(level)  {};
-
-    //! return size of function space
-    //! see dofmanager.hh for definition of IndexSet, which
-    //! is a wrapper for en.index
-    int size () const
-    {
-      // return number of dofs * number of elements
-      return (numberOfDofs_ * indexSet_.size( level_ , 0 ));
-    }
-
-    //! return boundary type for given boundary id
-    //! uses the parameter class BoundaryIdentifierType
-    BoundaryType boundaryType ( int id ) const
-    {
-      // to be expanded for user needs
-      return (id < 0) ? Dirichlet : Neumann;
-    }
-
-    //! map Entity an local Dof number to global Dof number
-    //! see dofmanager.hh for definition of IndexSet, which
-    //! is a wrapper for en.index
-    template <class EntityType>
-    int mapToGlobal (EntityType &en, int localNum ) const
-    {
-      // indexSet_.index<0>(en,0) return 0 entity of codim the codim 0
-      // entitys of en which is the element index
-      return ((indexSet_.template index<0>(en,0) * numberOfDofs_) + localNum);
-    };
-
-    //** size methods needed by the dofmanager ***
-    //! calc new start point for element dof , vertex dof ...
-    //not needed here at the moment
-    virtual void calcInsertPoints () {};
-
-    //! only called once, if grid was adapted
-    virtual int newSize() const
-    {
-      return this->size();
-    }
-  };
 
   //**********************************************************************
   //
