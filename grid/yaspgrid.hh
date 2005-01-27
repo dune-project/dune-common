d928a6b807991b6e9c69960e09640f409c8352de
Christian Engwer <christi@dune-project.org>
2005-01-27 12:41:55 +0000

    replaced some operator() by operator[]
    [[Imported from SVN: r1403]]

--- grid/yaspgrid.hh
+++ grid/yaspgrid.hh
@@ -337,8 +337,8 @@ namespace Dune {
     {
       for (int i=0; i<dim; ++i)
       {
-        Jinv(i) = 0.0;                        // set column to zero
-        Jinv(i,i) = 1.0/extension[i];         // set diagonal element
+        Jinv[i] = 0.0;                        // set column to zero
+        Jinv[i][i] = 1.0/extension[i];         // set diagonal element
       }
       return Jinv;
     }
