41805070d4430504fa6b87a4f220e71aac60832c
Oliver Sander <sander@dune-project.org>
2005-02-03 18:54:21 +0000

    deprecated operator() in K11Matrix removed
    [[Imported from SVN: r1439]]

--- istl/fmatrix.hh
+++ istl/fmatrix.hh
@@ -1137,20 +1137,6 @@ namespace Dune {
     //! export size
     enum {rows = 1, cols = 1};
 
-    //===== random access interface to rows of the matrix
-
-    //! random access to data
-    K& operator() () DUNE_DEPRECATED
-    {
-      return a;
-    }
-
-    //! same for read only access
-    const K& operator() () const DUNE_DEPRECATED
-    {
-      return a;
-    }
-
     //===== assignment from scalar
 
     K11Matrix& operator= (const K& k)
