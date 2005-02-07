574cf21ed355723730de554db45a99bdead4458c
Oliver Sander <sander@dune-project.org>
2005-02-07 10:48:07 +0000

    added a define to block a warning from the amiramesh header
    [[Imported from SVN: r1443]]

--- io/file/amiramesh/amirameshwriter.cc
+++ io/file/amiramesh/amirameshwriter.cc
@@ -1,6 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+#define NEWSTYLE_IOSTREAM
 #include <amiramesh/AmiraMesh.h>
+#undef NEWSTYLE_IOSTREAM
 
 #include <algorithm>
 
