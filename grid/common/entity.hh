e9a1b61110b10b366031e812182cf9a60e500437
Christian Engwer <christi@dune-project.org>
2005-02-21 16:56:20 +0000

    now it works also with icc
    [[Imported from SVN: r1530]]

--- grid/common/entity.hh
+++ grid/common/entity.hh
@@ -44,6 +44,10 @@ namespace Dune
 
     //! copy constructor from EntityImp
     Entity(const EntityImp<codim,dim,GridImp> & e) : realEntity(e) {};
+
+    typedef typename RemoveConst<GridImp>::Type mutableGridImp;
+
+    friend EntityImp<codim,dim,GridImp>& mutableGridImp::getRealEntity(typename GridImp::Traits::template codim<codim>::Entity& e );
   };
 
   template<int dim, class GridImp, template<int,int,class> class EntityImp>
@@ -54,8 +58,7 @@ namespace Dune
 
     typedef typename RemoveConst<GridImp>::Type mutableGridImp;
 
-    template <int cd>
-    friend EntityImp<cd,dim,GridImp>& mutableGridImp::getRealEntity(typename GridImp::Traits::template codim<cd>::Entity& e );
+    friend EntityImp<0,dim,GridImp>& mutableGridImp::getRealEntity(typename GridImp::Traits::template codim<0>::Entity& e );
 
   protected:
     EntityImp<0,dim,GridImp> realEntity;
@@ -191,8 +194,7 @@ namespace Dune
 
     typedef typename RemoveConst<GridImp>::Type mutableGridImp;
 
-    template <int cd>
-    friend EntityImp<cd,dim,GridImp>& mutableGridImp::getRealEntity(typename GridImp::Traits::template codim<cd>::Entity& e );
+    friend EntityImp<dim,dim,GridImp>& mutableGridImp::getRealEntity(typename GridImp::Traits::template codim<dim>::Entity& e );
 
   protected:
     EntityImp<dim,dim,GridImp> realEntity;
