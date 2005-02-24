d2da395c7ad7ac05e07f1a50a8cc6cdfdf496f7e
Christian Engwer <christi@dune-project.org>
2005-02-24 15:40:54 +0000

    include new headers
    [[Imported from SVN: r1553]]

--- common/test/parsetest.cc
+++ common/test/parsetest.cc
@@ -4,24 +4,38 @@
 
 /* test if the common headers can be parsed without errors */
 #include "config.h"
+#include <dune/common/alignment.hh>
 #include <dune/common/array.hh>
+#include <dune/common/arraylist.hh>
+#include <dune/common/bitfield.hh>
+#include <dune/common/capabilities.hh>
 #include <dune/common/debugstream.hh>
 #include <dune/common/dlist.hh>
 #include <dune/common/dynamictype.hh>
+#include <dune/common/enumset.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/fixedarray.hh>
+#include <dune/common/fmatrix.hh>
 #include <dune/common/function.hh>
 #include <dune/common/functionspace.hh>
 #include <dune/common/fvector.hh>
+#include <dune/common/genericiterator.hh>
+#include <dune/common/helpertemplates.hh>
+#include <dune/common/iteratorfacades.hh>
+#include <dune/common/logictraits.hh>
 #include <dune/common/mapping.hh>
 #include <dune/common/matrix.hh>
-#include <dune/common/matvec.hh>
 #include <dune/common/misc.hh>
 #include <dune/common/operator.hh>
+#include <dune/common/poolallocator.hh>
 #include <dune/common/simplevector.hh>
+#include <dune/common/sllist.hh>
+#include <dune/common/smartpointer.hh>
 #include <dune/common/stack.hh>
 #include <dune/common/stdstreams.hh>
 #include <dune/common/timer.hh>
+#include <dune/common/typeinfo.hh>
+#include <dune/common/typetraits.hh>
 #include <dune/common/vectorspace.hh>
 
 int main () {};
