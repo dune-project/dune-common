f17fe27da2f0a8a90855dc11a956ab10b3f6697f
Robert Klöfkorn <robertk@dune-project.org>
2005-03-03 17:10:18 +0000

    little change in recursive. Calls recursively the calcLevelIndex methods for each codim started with codim = dim.
    [[Imported from SVN: r1617]]

--- grid/common/defaultindexsets.hh
+++ grid/common/defaultindexsets.hh
@@ -289,6 +289,27 @@ namespace Dune {
    * a number in an array.
    */
   //*********************************************************************
+
+  template <class DefaultLevelIndexSetType, int codim>
+  struct CalcLevelForCodim
+  {
+    static void recursive(DefaultLevelIndexSetType & d)
+    {
+      d.template calcLevelIndexForCodim<codim> ();
+      CalcLevelForCodim<DefaultLevelIndexSetType,codim-1>::recursive(d);
+    }
+  };
+
+  template <class DefaultLevelIndexSetType>
+  struct CalcLevelForCodim<DefaultLevelIndexSetType,0>
+  {
+    static void recursive(DefaultLevelIndexSetType & d)
+    {
+      d.template calcLevelIndexForCodim<0> ();
+    }
+  };
+
+
   template <class GridType>
   class DefaultLevelIndexSet
   {
@@ -297,6 +318,7 @@ namespace Dune {
     typedef typename GridType :: HierarchicIndexSetType HierarchicIndexSetType;
     typedef typename GridType :: Traits :: template codim<0> :: Entity EntityCodim0Type;
 
+    typedef DefaultLevelIndexSet<GridType> MyType;
   public:
     DefaultLevelIndexSet(const GridType & grid ) :
       grid_(grid) , hIndexSet_ ( grid.hierarchicIndexSet() )
@@ -336,6 +358,7 @@ namespace Dune {
       return levelIndex_[cd][level][hIndex];
     };
 
+  public:
     template <int cd>
     void calcLevelIndexForCodim ()
     {
@@ -361,19 +384,6 @@ namespace Dune {
       }
     }
 
-    void recursiveCall (int d)
-    {
-      switch (d)
-      {
-      //case 3:  calcLevelIndexForCodim<3> (); recursiveCall(2);
-      case 2 :  calcLevelIndexForCodim<2> (); recursiveCall(1);
-      case 1 :  calcLevelIndexForCodim<1> (); recursiveCall(0);
-      case 0 :  calcLevelIndexForCodim<0> ();
-      default : return;
-      }
-    }
-
-  public:
     void calcNewIndex ()
     {
       int numLevel = grid_.maxlevel() + 1;
@@ -381,9 +391,10 @@ namespace Dune {
       if( (numLevel) * (numCodim) > size_.size() )
         makeNewSize(size_, 2*((numLevel) * numCodim) );
 
-      recursiveCall(dim) ;
+      CalcLevelForCodim<MyType,dim>::recursive(*this);
     }
 
+  private:
     void makeNewSize(Array<int> &a, int newNumberOfEntries)
     {
       if(newNumberOfEntries > a.size())
