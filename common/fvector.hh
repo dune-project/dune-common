5901622f5e9705463d7d6a701bf69101a3e4d842
Adrian Burri <burriad@dune-project.org>
2005-03-09 09:04:12 +0000

    Added doxygen comment for assignment operator
    [[Imported from SVN: r1640]]

--- common/fvector.hh
+++ common/fvector.hh
@@ -354,6 +354,7 @@ namespace Dune {
     }
 
     //===== assignment from scalar
+    //! Assignment operator for scalar
     FieldVector& operator= (const K& k)
     {
       fvmeta_assignscalar<n-1>::assignscalar(*this,k);
