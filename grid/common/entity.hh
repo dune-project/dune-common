423e5d43ef68b48429b78eeb63d40f388e02aafe
Oliver Sander <sander@dune-project.org>
2005-03-11 15:37:45 +0000

    better documention of the Entity classes
    [[Imported from SVN: r1651]]

--- grid/common/entity.hh
+++ grid/common/entity.hh
@@ -19,6 +19,8 @@ namespace Dune
   protected:
     EntityImp<codim,dim,GridImp> realEntity;
   public:
+
+    //! The corresponding geometry type
     typedef typename GridImp::template codim<codim>::Geometry Geometry;
     //! know your own codimension
     enum { codimension=codim };
@@ -31,21 +33,21 @@ namespace Dune
     //! define type used for coordinates in grid module
     typedef ct ctype;
 
-    //! level of this entity
+    //! The level of this entity
     int level () const { return realEntity.level(); }
-    //! index is unique and consecutive per level and codim used for access to degrees of freedom
+    //! Unique and consecutive index per level and codim
     int index () const { return realEntity.index(); }
-    //! return the global unique index in grid
+    //! The global unique index
     int globalIndex () const { return realEntity.globalIndex(); }
-    //! return partition type attribute
+    //! Return partition type attribute
     PartitionType partitionType () const { return realEntity.partitionType(); }
-    //! id of the boundary which is associated with
-    //! the entity, returns 0 for inner entities, arbitrary int otherwise
+    /** \brief Id of the boundary which is associated with
+        the entity, returns 0 for inner entities, arbitrary int otherwise */
     int boundaryId () const { return realEntity.boundaryId(); }
-    //! geometry of this entity
+    //! Geometry of this entity
     const Geometry& geometry () const { return realEntity.geometry(); }
 
-    //! copy constructor from EntityImp
+    //! Copy constructor from EntityImp
     Entity(const EntityImp<codim,dim,GridImp> & e) : realEntity(e) {};
 
     typedef typename RemoveConst<GridImp>::Type mutableGridImp;
@@ -90,73 +92,92 @@ namespace Dune
   protected:
     EntityImp<0,dim,GridImp> realEntity;
   public:
+
+    /** \brief The geometry type of this entity */
     typedef typename GridImp::template codim<0>::Geometry Geometry;
+
+    /** \brief EntityPointer types of the different codimensions */
     template <int cd>
     struct codim
     {
       typedef typename GridImp::template codim<cd>::EntityPointer EntityPointer;
     };
+
+    /** \brief The codim==0 EntityPointer type */
     typedef typename GridImp::template codim<0>::EntityPointer EntityPointer;
+
+    /** \brief The IntersectionIterator type*/
     typedef typename GridImp::template codim<0>::IntersectionIterator IntersectionIterator;
+
+    /** \brief The HierarchicIterator type*/
     typedef typename GridImp::template codim<0>::HierarchicIterator HierarchicIterator;
-    //! know your own codimension
+
+    //! Know your own codimension
     enum { codimension=0 };
-    //! know your own dimension
+    //! Know your own dimension
     enum { dimension=dim };
-    //! know dimension of the entity
+    /** \brief Know dimension of the entity
+     *
+     * \bug Shouldn't this be dim instead of 0?
+     */
     enum { mydimension=0 };
-    //! know your own dimension of world
+    //! Know the world dimension
     enum { dimensionworld=dimworld };
-    //! define type used for coordinates in grid module
+    //! Type used for coordinates in grid module
     typedef ct ctype;
 
-    //! level of this entity
+    //! Level of this entity
     int level () const { return realEntity.level(); }
-    //! index is unique and consecutive per level and codim used for access to degrees of freedom
+    //! Unique and consecutive index per level and codim
     int index () const { return realEntity.index(); }
-    //! return the global unique index in grid
+    //! Global unique index per grid
     int globalIndex () const { return realEntity.globalIndex(); }
-    //! return partition type attribute
+    //! Partition type attribute
     PartitionType partitionType () const { return realEntity.partitionType(); }
     //! geometry of this entity
     const Geometry& geometry () const { return realEntity.geometry(); }
-    /*! Intra-element access to entities of codimension cc > codim. Return number of entities
-       with codimension cc.
+
+    /** \brief Number of subentities with codimension <tt>cc</tt>.
      */
     template<int cc> int count () const { return realEntity.count<cc>(); }
-    /*! Provide access to mesh entity i of given codimension. Entities
-       are numbered 0 ... count<cc>()-1
+
+    /** \brief Access to subentity <tt>i</tt> of codimension <tt>cc</tt>.
      */
     template<int cc> typename codim<cc>::EntityPointer entity (int i) const
     {
       return realEntity.entity<cc>(i);
     }
-    /*! Intra-level access to intersections with neighboring elements.
+    /** \brief Intra-level access to intersections with neighboring elements.
+
        A neighbor is an entity of codimension 0
-       which has an entity of codimension 1 in commen with this entity. Access to neighbors
-       is provided using iterators. This allows meshes to be nonmatching. Returns iterator
-       referencing the first neighbor.
+       which has an entity of codimension 1 in common with this entity. Access to neighbors
+       is provided using the IntersectionIterator. This allows meshes to be nonmatching.
+       This method returns an iterator referencing the first neighbor.
      */
     IntersectionIterator ibegin () const
     {
       return realEntity.ibegin();
     }
-    //! Reference to one past the last intersection
+
+    //! Reference to an IntersectionIterator  past the last intersection
     IntersectionIterator iend () const
     {
       return realEntity.iend();
     }
-    //! Inter-level access to father element on coarser grid. Assumes that meshes are nested.
+    //! Inter-level access to father entity on the next-coarser grid. Assumes that meshes are nested.
     EntityPointer father () const
     {
       return realEntity.father();
     }
-    //! return true if the entity is leaf
+
+    //! Returns true if the entity is a leaf in the grid hierarchy
     bool isLeaf ()
     {
       return realEntity.isLeaf();
     }
-    /*! Location of this element relative to the reference element of the father.
+
+    /** \brief Location of this element relative to the reference element of the father.
+
        This is sufficient to interpolate all dofs in conforming case.
        Nonconforming may require access to neighbors of father and
        computations with local coordinates.
@@ -169,33 +190,38 @@ namespace Dune
     {
       return realEntity.geometryInFather();
     }
-    /*! Inter-level access to son elements on higher levels<=maxlevel.
+
+    /** \brief Inter-level access to son elements on higher levels<=maxlevel.
+
        This is provided for sparsely stored nested unstructured meshes.
        Returns iterator to first son.
+       \todo Does this point to the first son or the entity itself?
      */
     HierarchicIterator hbegin (int maxlevel) const
     {
       return realEntity.hbegin(maxlevel);
     }
+
     //! Returns iterator to one past the last son
     HierarchicIterator hend (int maxlevel) const
     {
       return realEntity.hend(maxlevel);
     }
-    /*!
-     * Default implementation for access to subIndex via interface method entity
-     * default is to return the index of the sub entity, is very slow, but works
+
+    /** \brief The index of the i-th subentity of codimension <tt>cc</tt>
+     *
+     * This does the same as <code>entity<cc>(i).index()</code>, but it is
+     * usually a lot faster.
      */
     template <int cc> int subIndex ( int i ) const
     {
       return realEntity.subIndex<cc>(i);
     }
 
-    /*!
-     * Default implementation for access to boundaryId via interface method
-     * entity<codim>.boundaryId(), default is very slow, but works, can be
-     * overloaded be the actual grid implementation, works the same way as
-     * subIndex
+    /** \brief The boundaryId of the i-th subentity of codimension <tt>cc</tt>
+     *
+     * This does the same as <code>entity<cc>(i).boundaryId()</code>, but it is
+     * usually a lot faster.
      */
     template <int cc> int subBoundaryId  ( int i ) const
     {
@@ -205,13 +231,15 @@ namespace Dune
     //***************************************************************
     //  Interface for Adaptation
     //***************************************************************
-    //! return whether entity could be coarsened (COARSEN) or was refined
-    //! (REFINED) or nothing happend (NONE)
-    //! @return The default implementation returns NONE for grid with no
-    //! adaptation
+    /* \brief Return whether entity could be coarsened (COARSEN) or was refined
+       (REFINED) or nothing happened (NONE)
+
+       @return The default implementation returns NONE for grid with no
+       adaptation
+     */
     AdaptationState state () const { return realEntity.state(); }
 
-    //! copy constructor from EntityImp
+    //! Copy constructor from EntityImp
     Entity(const EntityImp<0,dim,GridImp> & e) : realEntity(e) {};
   };
 
@@ -241,38 +269,42 @@ namespace Dune
   protected:
     EntityImp<dim,dim,GridImp> realEntity;
   public:
+
+    /** \brief Geometry type of this entity */
     typedef typename GridImp::template codim<dim>::Geometry Geometry;
+
+    /** \brief Codim 0 EntityPointer type*/
     typedef typename GridImp::template codim<0>::EntityPointer EntityPointer;
-    //! know your own codimension
+    //! Know your own codimension
     enum { codimension=dim };
-    //! know your own dimension
+    //! Know your own dimension
     enum { dimension=dim };
-    //! know dimension of the entity
+    /** \brief Know dimension of the entity
+     * \bug Shouldn't this be 0 instead of dim?*/
     enum { mydimension=dim };
-    //! know your own dimension of world
+    //! Know your own dimension of world
     enum { dimensionworld=dimworld };
-    //! define type used for coordinates in grid module
+    //! Type used for coordinates in grid module
     typedef ct ctype;
 
-    //! level of this entity
+    //! Level of this entity
     int level () const { return realEntity.level(); }
-    //! index is unique and consecutive per level and codim used for access to degrees of freedom
+    //! Unique and consecutive index per level and codim
     int index () const { return realEntity.index(); }
-    //! return the global unique index in grid
+    //! Global unique index per grid
     int globalIndex () const { return realEntity.globalIndex(); }
-    //! return partition type attribute
+    //! Partition type attribute
     PartitionType partitionType () const { return realEntity.partitionType(); }
-    //! geometry of this entity
+    //! Geometry of this entity
     const Geometry& geometry () const { return realEntity.geometry(); }
-    /*! Location of this vertex within a mesh entity of codimension 0 on the coarse grid.
-       This can speed up on-the-fly interpolation for linear conforming elements
-       Possibly this is sufficient for all applications we want on-the-fly.
-     */
+
+    /** \todo Please doc me! */
     EntityPointer ownersFather () const { return realEntity.ownersFather(); }
-    //! my position in local coordinates of the owners father
+
+    //! This entity's position in local coordinates of the owners father
     FieldVector<ct, dim>& positionInOwnersFather () const { return realEntity.positionInOwnersFather(); }
 
-    //! copy constructor from EntityImp
+    //! Copy constructor from EntityImp
     Entity(const EntityImp<dim,dim,GridImp> & e) : realEntity(e) {};
   };
 
