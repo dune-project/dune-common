4568b1fdcbbf8928942d5c80cda73fc360add89a
Robert Klöfkorn <robertk@dune-project.org>
2005-03-18 14:33:51 +0000

    bigfix in elDeterminant <2,3> .
    [[Imported from SVN: r1694]]

--- grid/albertagrid/albertagrid.cc
+++ grid/albertagrid/albertagrid.cc
@@ -689,10 +689,11 @@ namespace Dune
     // calculate scaled outer normal
     for(int i=0; i<dim; i++)
     {
-      tmpZ_[i] = 0.5 * (  tmpU_[(i+1)%dim] * tmpV_[(i+2)%dim]
-                          - tmpU_[(i+2)%dim] * tmpV_[(i+1)%dim] );
+      tmpZ_[i] = (  tmpU_[(i+1)%dim] * tmpV_[(i+2)%dim]
+                    - tmpU_[(i+2)%dim] * tmpV_[(i+1)%dim] );
     }
 
+    // tmpZ is the same as 2.0 * the outer normal
     return std::abs( tmpZ_.two_norm() );
   }
 
