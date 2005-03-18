aeb3520a8895f9087cb4155e6f011155405ba9e0
Robert Klöfkorn <robertk@dune-project.org>
2005-03-18 16:03:38 +0000

    Added the adaptation functions such as additionalSizeEstimate and newSize ...
    Also added specialisation for dimRange = 1.
    
    [[Imported from SVN: r1699]]

--- fem/dgspace/dgmapper.hh
+++ fem/dgspace/dgmapper.hh
@@ -26,6 +26,106 @@ namespace Dune {
   public:
     //! Constructor
     DGMapper ( IndexSetType &iset , int numDof , int level) :
+      indexSet_ (iset), numberOfDofs_ (numDof*dimRange), level_(level)  {};
+
+    //! return size of function space
+    //! see dofmanager.hh for definition of IndexSet, which
+    //! is a wrapper for en.index
+    int size () const
+    {
+      // return number of dofs * number of elements
+      return (numberOfDofs_ * indexSet_.size( level_ , 0 ));
+    }
+
+    //! return boundary type for given boundary id
+    //! uses the parameter class BoundaryIdentifierType
+    BoundaryType boundaryType ( int id ) const
+    {
+      // to be expanded for user needs
+      return (id < 0) ? Dirichlet : Neumann;
+    }
+
+    //! map Entity an local Dof number to global Dof number
+    //! see dofmanager.hh for definition of IndexSet, which
+    //! is a wrapper for en.index
+    template <class EntityType>
+    int mapToGlobal (EntityType &en, int localNum ) const
+    {
+      // indexSet_.index<0>(en,0) return 0 entity of codim the codim 0
+      // entitys of en which is the element index
+      return ((indexSet_.template index<0>(en,0) * numberOfDofs_) + localNum);
+    };
+
+    //** size methods needed by the dofmanager ***
+    //! calc new start point for element dof , vertex dof ...
+    //not needed here at the moment
+    void calcInsertPoints () {};
+
+    //! default implementation if not overlaoded
+    int numberOfDofs () const
+    {
+      return numberOfDofs_;
+    }
+
+    //! only called once, if grid was adapted
+    int newSize() const
+    {
+      return this->size();
+    }
+
+    //! for dof manager, to check whether it has to copy dof or not
+    bool indexNew (int num)
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.template indexNew(newn,0);
+    }
+
+    //! return old index, for dof manager only
+    int oldIndex (int num) const
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.oldIndex(newn,0);
+    }
+
+    //! return new index, for dof manager only
+    int newIndex (int num) const
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.newIndex(newn,0);
+    }
+
+    //! return size of grid entities per level and codim
+    //! for dof mapper
+    int oldSize () const
+    {
+      // this index set works only for codim = 0 at the moment
+      return numberOfDofs_ * indexSet_.oldSize(level_,0);
+    }
+
+    // is called once and calcs the insertion points too
+    int additionalSizeEstimate () const
+    {
+      return numberOfDofs_ * indexSet_.additionalSizeEstimate();
+    }
+
+
+  };
+
+
+  // specialisation for dimRange = 1
+  template <class IndexSetType, int polOrd>
+  class DGMapper<IndexSetType,polOrd,1>
+    : public DofMapperDefault < DGMapper <IndexSetType,polOrd,1> >
+  {
+    // index set of grid, i.e. the element indices
+    IndexSetType &indexSet_;
+
+    // number of dofs on element
+    int numberOfDofs_;
+    int level_;
+  public:
+    //! Constructor
+    DGMapper ( IndexSetType &iset , int numDof , int level) :
       indexSet_ (iset), numberOfDofs_ (numDof), level_(level)  {};
 
     //! return size of function space
@@ -59,18 +159,53 @@ namespace Dune {
     //** size methods needed by the dofmanager ***
     //! calc new start point for element dof , vertex dof ...
     //not needed here at the moment
-    virtual void calcInsertPoints () {};
+    void calcInsertPoints () {};
 
     //! default implementation if not overlaoded
-    virtual int numberOfDofs () const
+    int numberOfDofs () const
     {
       return numberOfDofs_;
     }
 
     //! only called once, if grid was adapted
-    virtual int newSize() const
+    int newSize() const
     {
       return this->size();
+    }
+
+    //! for dof manager, to check whether it has to copy dof or not
+    bool indexNew (int num)
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.template indexNew(newn,0);
+    }
+
+    //! return old index, for dof manager only
+    int oldIndex (int num) const
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.oldIndex(newn,0);
+    }
+
+    //! return new index, for dof manager only
+    int newIndex (int num) const
+    {
+      int newn = (num / numberOfDofs_) + num % numberOfDofs_;
+      return indexSet_.newIndex(newn,0);
+    }
+
+    //! return size of grid entities per level and codim
+    //! for dof mapper
+    int oldSize () const
+    {
+      // this index set works only for codim = 0 at the moment
+      return numberOfDofs_ * indexSet_.oldSize(level_,0);
+    }
+
+    // is called once and calcs the insertion points too
+    int additionalSizeEstimate () const
+    {
+      return numberOfDofs_ * indexSet_.additionalSizeEstimate();
     }
   };
 
