d7272c4a7a84e83808aade6393d04d054fb071b6
Robert Klöfkorn <robertk@dune-project.org>
2005-03-23 10:37:46 +0000

    facelifting and Destructor implemented correctly and copy constructor and assignment operator private now.
    [[Imported from SVN: r1725]]

--- grid/common/indexstack.hh
+++ grid/common/indexstack.hh
@@ -3,154 +3,198 @@
 #ifndef DUNE_INDEXSTACK_HH
 #define DUNE_INDEXSTACK_HH
 
-//******************************************************
-//
-//  IndexStack providing indices via getIndex and freeIndex
-//  indices that are freed, are put on a stack and get
-//
-//******************************************************
+#include <assert.h>
+#include <stack>
 
-#include <dune/common/dlist.hh>
 #include <dune/common/stack.hh>
 
 namespace Dune {
 
-  //! \todo Please doc me!
+  //!  IndexStack providing indices via getIndex and freeIndex
+  //!  indices that are freed, are put on a stack and get
   template <class T, int length>
   class IndexStack
   {
     typedef typename Dune::FiniteStack<T,length> StackType;
-    typedef typename Dune::DoubleLinkedList< StackType * > StackListType;
-
+    typedef typename std::stack < StackType * > StackListType;
 
     StackListType fullStackList_;
     StackListType emptyStackList_;
 
-    typedef typename StackListType::Iterator DListIteratorType;
+    //typedef typename StackListType::Iterator DListIteratorType;
     StackType * stack_;
 
     // current maxIndex
     int maxIndex_;
   public:
     //! Constructor, create new IndexStack
-    IndexStack() : maxIndex_ (0)
-    {
-      stack_ = new StackType ();
-    }
+    inline IndexStack();
+
+    //! Destructor, deleting all stacks
+    inline ~IndexStack ();
 
     //! set index as maxIndex if index is bigger than maxIndex
-    void checkAndSetMax(T index)
-    {
-      if(index > maxIndex_) maxIndex_ = index;
-    }
+    inline void checkAndSetMax(T index) { if(index > maxIndex_) maxIndex_ = index;}
 
     //! set index as maxIndex
-    void setMaxIndex(T index)
-    {
-      maxIndex_ = index;
-    }
+    inline void setMaxIndex(T index) { maxIndex_ = index; }
 
     //! set index as maxIndex
-    int getMaxIndex() const
-    {
-      return maxIndex_;
-    }
+    inline int getMaxIndex() const { return maxIndex_;  }
 
     //! restore index from stack or create new index
-    T getIndex ()
-    {
-      if(stack_->empty())
-      {
-        if( fullStackList_.size() <= 0)
-        {
-          return maxIndex_++;
-        }
-        else
-        {
-          emptyStackList_.insert_after( emptyStackList_.rbegin(), stack_);
-          DListIteratorType it = fullStackList_.begin();
-          stack_ = (*it);
-          fullStackList_.erase( it );
-        }
-      }
-      return stack_->pop();
-    }
+    inline T getIndex ();
 
     //! store index on stack
-    void freeIndex(T index)
-    {
-      if(stack_->full())
-      {
-        fullStackList_.insert_before( fullStackList_.begin() , stack_ );
-        if(emptyStackList_.size() <= 0)
-        {
-          stack_ = new StackType ();
-        }
-        else
-        {
-          DListIteratorType it = emptyStackList_.begin();
-          stack_ = (*it);
-          emptyStackList_.erase( it );
-        }
-      }
-      stack_->push(index);
-    }
+    inline void freeIndex(T index);
 
     //! test stack funtcionality
-    void test ()
-    {
-      T vec[2*length];
-
-      for(int i=0; i<2*length; i++)
-        vec[i] = getIndex();
-
-      for(int i=0; i<2*length; i++)
-        freeIndex(vec[i]);
-
-      for(int i=0; i<2*length; i++)
-        vec[i] = getIndex();
-
-      for(int i=0; i<2*length; i++)
-        printf(" index [%d] = %d \n",i,vec[i]);
-    }
+    inline void test ();
 
     // backup set to out stream
-    void backupIndexSet ( std::ostream & os )
-    {
-      // holes are not stored at the moment
-      os.write( ((const char *) &maxIndex_ ), sizeof(int) ) ;
-      return ;
-    }
+    inline void backupIndexSet ( std::ostream & os );
 
     // restore from in stream
-    void restoreIndexSet ( std::istream & is )
-    {
-      is.read ( ((char *) &maxIndex_), sizeof(int) );
-      clearStack ();
-
-      return ;
-    }
-
+    inline void restoreIndexSet ( std::istream & is );
   private:
-    // clear all stored indices
-    void clearStack ()
+    // no copy constructor allowed
+    IndexStack( const IndexStack<T,length> & s) : maxIndex_ (0) , stack_(0) {}
+
+    // no assignment operator allowed
+    IndexStack<T,length> & operator = ( const IndexStack<T,length> & s)
     {
-      if(stack_)
-      {
-        delete stack_;
-        stack_ = new StackType();
-        assert(stack_);
-      }
-
-      DListIteratorType endit = fullStackList_.end();
-      for(DListIteratorType it = fullStackList_.begin(); it != endit; ++it)
-      {
-        fullStackList_.erase( it );
-      }
-
-      return;
+      std::cerr << "IndexStack::operator = () not allowed! in: " __FILE__ << " line:" << __LINE__ << "\n";
+      abort();
+      return *this;
     }
+
+    // clear fullStacks
+    void clearStack ();
+
   }; // end class IndexStack
+
+  //****************************************************************
+  // Inline implementation
+  // ***************************************************************
+  template <class T, int length>
+  inline IndexStack<T,length>::IndexStack()
+    : stack_ ( new StackType () ) , maxIndex_ (0) {}
+
+  template <class T, int length>
+  inline IndexStack<T,length>::~IndexStack ()
+  {
+    if(stack_)
+    {
+      delete stack_;
+      stack_ = new StackType();
+      assert(stack_);
+    }
+
+    while( !fullStackList_.empty() )
+    {
+      StackType * st = fullStackList_.top();
+      if(st) delete st;
+      fullStackList_.pop();
+    }
+    while( !emptyStackList_.empty() )
+    {
+      StackType * st = emptyStackList_.top();
+      if(st) delete st;
+      emptyStackList_.pop();
+    }
+  }
+
+  template <class T, int length>
+  inline T IndexStack<T,length>::getIndex ()
+  {
+    if((*stack_).empty())
+    {
+      if( fullStackList_.size() <= 0)
+      {
+        return maxIndex_++;
+      }
+      else
+      {
+        emptyStackList_.push( stack_ );
+        stack_ = fullStackList_.top();
+        fullStackList_.pop();
+      }
+    }
+    return (*stack_).pop();
+  }
+
+  template <class T, int length>
+  inline void IndexStack<T,length>::freeIndex ( T index )
+  {
+    if((*stack_).full())
+    {
+      fullStackList_.push(  stack_ );
+      if(emptyStackList_.size() <= 0)
+      {
+        stack_ = new StackType ();
+      }
+      else
+      {
+        stack_ = emptyStackList_.top();
+        emptyStackList_.pop();
+      }
+    }
+    (*stack_).push(index);
+  }
+
+  template <class T, int length>
+  inline void IndexStack<T,length>::test ()
+  {
+    T vec[2*length];
+
+    for(int i=0; i<2*length; i++)
+      vec[i] = getIndex();
+
+    for(int i=0; i<2*length; i++)
+      freeIndex(vec[i]);
+
+    for(int i=0; i<2*length; i++)
+      vec[i] = getIndex();
+
+    for(int i=0; i<2*length; i++)
+      printf(" index [%d] = %d \n",i,vec[i]);
+  }
+
+  template <class T, int length>
+  inline void IndexStack<T,length>::backupIndexSet ( std::ostream & os )
+  {
+    // holes are not stored at the moment
+    os.write( ((const char *) &maxIndex_ ), sizeof(int) ) ;
+    return ;
+  }
+
+  template <class T, int length>
+  inline void IndexStack<T,length>::restoreIndexSet ( std::istream & is )
+  {
+    is.read ( ((char *) &maxIndex_), sizeof(int) );
+    clearStack ();
+
+    return ;
+  }
+
+  template <class T, int length>
+  inline void IndexStack<T,length>::clearStack ()
+  {
+    if(stack_)
+    {
+      delete stack_;
+      stack_ = new StackType();
+      assert(stack_);
+    }
+
+    while( !fullStackList_.empty() )
+    {
+      StackType * st = fullStackList_.top();
+      if(st) delete st;
+      fullStackList_.pop();
+    }
+    return;
+  }
 
 } // end namespace Dune
 
