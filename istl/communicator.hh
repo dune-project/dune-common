5b28780031137088ff7fc5a85161441d40307283
Markus Blatt <mblatt@dune-project.org>
2005-04-11 15:11:02 +0000

    typename before EnableIf was missing.
    [[Imported from SVN: r1865]]

--- istl/communicator.hh
+++ istl/communicator.hh
@@ -308,7 +308,7 @@ namespace Dune
     BufferedCommunicator();
 
     template<class Data>
-    EnableIf<SameType<SizeOne,typename CommPolicy<Data>::IndexedTypeFlag>::value, void>
+    typename EnableIf<SameType<SizeOne,typename CommPolicy<Data>::IndexedTypeFlag>::value, void>::Type
     build(const Interface<TG,TA,N>& interface);
 
     template<class Data>
@@ -674,7 +674,7 @@ namespace Dune
 
   template<typename TG, typename TA, int N>
   template<class Data>
-  EnableIf<SameType<SizeOne, typename CommPolicy<Data>::IndexedTypeFlag>::value, void>
+  typename EnableIf<SameType<SizeOne, typename CommPolicy<Data>::IndexedTypeFlag>::value, void>::Type
   BufferedCommunicator<TG,TA,N>::build(const Interface<TG,TA,N>& interface)
   {
     typedef typename Interface<TG,TA,N>::InformationMap::const_iterator const_iterator;
@@ -699,7 +699,6 @@ namespace Dune
     buffers_[0] = new char[sendStart];
     buffers_[1] = new char[recvStart];
     interface_ = &interface;
-    return EnableIf<SameType<SizeOne, typename CommPolicy<Data>::IndexedTypeFlag>::value, void>();
 
   }
 
