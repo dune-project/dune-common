20dc9111888e8efcc04ce15f00837855b7236adf
Oliver Sander <sander@dune-project.org>
2005-04-25 12:38:50 +0000

    constification
    [[Imported from SVN: r1936]]

--- fem/norms/l2norm.hh
+++ fem/norms/l2norm.hh
@@ -31,7 +31,7 @@ namespace Dune {
       typedef typename DiscreteFunctionType::LocalFunctionType LocalFuncType;
 
 
-      GridType & grid = functionSpace_.getGrid();
+      const GridType & grid = functionSpace_.getGrid();
 
       typename FunctionSpaceType::Range phi (0.0);
 
