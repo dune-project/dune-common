ce93b359f178415b5b65518a1663db954fa67128
Oliver Sander <sander@dune-project.org>
2005-04-26 15:19:00 +0000

    the OneDGridEntityPointer class
    [[Imported from SVN: r1951]]

---
+++ grid/onedgrid/onedgridentitypointer.hh
@@ -0,0 +1,47 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_ONEDGRID_ENTITY_POINTER_HH
+#define DUNE_ONEDGRID_ENTITY_POINTER_HH
+
+
+namespace Dune {
+
+  /*! Acts as a pointer to an  entities of a given codimension.
+   */
+  template<int codim, class GridImp>
+  class OneDGridEntityPointer
+    : public EntityPointerDefault <codim, GridImp, Dune::OneDGridEntityPointer<codim,GridImp> >
+  {
+    enum { dim = GridImp::dimension };
+  public:
+    typedef typename GridImp::template codim<codim>::Entity Entity;
+
+    //! equality
+    bool equals(const OneDGridEntityPointer<codim,GridImp>& other) const {
+      return other.virtualEntity_.target() == virtualEntity_.target();
+    }
+
+    //! dereferencing
+    Entity& dereference() const {return virtualEntity_;}
+
+    //! ask for level of entity
+    int level () const {return virtualEntity_->level();}
+
+    OneDGridEntityPointer() {}
+  protected:
+
+    /** \brief Constructor from a given iterator */
+    OneDGridEntityPointer(OneDEntityImp<dim-codim>* it) {
+      virtualEntity_.setToTarget(it);
+    };
+
+  protected:
+
+    mutable OneDEntityWrapper<codim,GridImp::dimension,GridImp> virtualEntity_;
+
+  };
+
+
+} // end namespace Dune
+
+#endif
