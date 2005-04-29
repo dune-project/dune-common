e54fff73023b541e4ce54edcf2299cbb3d171b51
Markus Blatt <mblatt@dune-project.org>
2005-04-29 14:58:48 +0000

    Hint is optional to call of allocate.
    [[Imported from SVN: r1990]]

--- common/poolallocator.hh
+++ common/poolallocator.hh
@@ -229,7 +229,7 @@ namespace Dune
      * @param hint Ignored hint.
      * @return A pointer tp the allocated elements.
      */
-    inline pointer allocate(size_t n, const_pointer hint);
+    inline pointer allocate(size_t n, const_pointer hint=0);
 
     /**
      * @brief Free objects.
@@ -372,7 +372,7 @@ namespace Dune
   template<class T, std::size_t s>
   inline T* PoolAllocator<T,s>::allocate(std::size_t n, const T* hint)
   {
-    assert(n<=(Pool<T,s>::elements));
+    assert(n==1); //<=(Pool<T,s>::elements));
     return static_cast<T*>(memoryPool_.allocate());
   }
 
