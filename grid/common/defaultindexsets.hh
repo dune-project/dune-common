f988b7cf2360d781ccd2e0fae907e4711feb792b
Robert Klöfkorn <robertk@dune-project.org>
2005-05-07 12:28:46 +0000

    genFilename uses string now.
    [[Imported from SVN: r2018]]

--- grid/common/defaultindexsets.hh
+++ grid/common/defaultindexsets.hh
@@ -78,13 +78,14 @@ namespace Dune {
     }
 
     //! write index set to xdr file
-    bool write_xdr(const char * filename , int timestep)
+    bool write_xdr(const std::basic_string<char> filename , int timestep)
     {
       FILE  *file;
       XDR xdrs;
       const char *path = NULL;
 
-      const char * fn  = genFilename(path,filename, timestep);
+      std::basic_string<char> fnstr  = genFilename(path,filename, timestep);
+      const char * fn = fnstr.c_str();
       file = fopen(fn, "wb");
       if (!file)
       {
@@ -103,13 +104,14 @@ namespace Dune {
     }
 
     //! read index set to xdr file
-    bool read_xdr(const char * filename , int timestep)
+    bool read_xdr(const std::basic_string<char> filename , int timestep)
     {
       FILE   *file;
       XDR xdrs;
       const char *path = NULL;
 
-      const char * fn  = genFilename(path,filename, timestep);
+      std::basic_string<char> fnstr = genFilename(path,filename, timestep);
+      const char * fn  = fnstr.c_str();
       std::cout << "Reading <" << fn << "> \n";
       file = fopen(fn, "rb");
       if(!file)
