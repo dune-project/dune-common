17d1e0a9d2433fc71d846c0a6fff77ec999062fc
Robert Klöfkorn <robertk@dune-project.org>
2005-05-10 05:50:54 +0000

    implemented mark method for entity pointers.
    [[Imported from SVN: r2035]]

--- grid/alu3dgrid.hh
+++ grid/alu3dgrid.hh
@@ -1175,11 +1175,8 @@ namespace Dune
     //! no interface method, but has to be public
     void updateStatus ();
 
-    bool mark( int refCount , typename Traits::template codim<0>::EntityPointer & ep )
-    {
-      return this->mark(*ep);
-    }
-    bool mark( int refCount , const typename Traits::template codim<0>::Entity & en ) const;
+    bool mark( int refCount , typename Traits::template codim<0>::EntityPointer & ep );
+    bool mark( int refCount , const typename Traits::template codim<0>::Entity & en );
 
     template <int cd>
     ALU3dGridEntity<cd,dim,const ALU3dGrid<dim,dimworld> >&
