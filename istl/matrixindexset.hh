083b206dd89a7b49b2ce44ab786b192b82f2f25c
Oliver Sander <sander@dune-project.org>
2005-05-17 12:55:12 +0000

    use a few unsigned ints to calm the compiler
    [[Imported from SVN: r2067]]

--- istl/matrixindexset.hh
+++ istl/matrixindexset.hh
@@ -100,10 +100,9 @@ namespace Dune {
       matrix.endrowsizes();
 
       for (int i=0; i<rows_; i++) {
-        for (int j=0; j<indices[i].size(); j++) {
-          //std::cout << "setting index " << i <<", " << rowSizes[i][j] << std::endl;
+        for (unsigned int j=0; j<indices[i].size(); j++)
           matrix.addindex(i, indices[i][j]);
-        }
+
       }
 
       matrix.endindices();
@@ -133,7 +132,7 @@ namespace Dune {
 
       // Insert
       nb.push_back(0);
-      for (int j=nb.size()-1; j>i; j--)
+      for (unsigned int j=nb.size()-1; j>i; j--)
         nb[j] = nb[j-1];
 
       nb[i] = idx;
