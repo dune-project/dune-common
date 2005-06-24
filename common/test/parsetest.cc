f4b7ce50dcad9d916b96fd0e9cac5e215625f987
Thimo Neubauer <thimo@dune-project.org>
2005-06-24 13:49:56 +0000

    updated and put in a nice commandline
    [[Imported from SVN: r2247]]

--- common/test/parsetest.cc
+++ common/test/parsetest.cc
@@ -4,11 +4,22 @@
 
 /* test if the common headers can be parsed without errors */
 #include "config.h"
+
+/*
+
+   to create all the includes the following commandline is pretty handy:
+
+   find dune/common/ -maxdepth 1 -name \*.hh -exec echo '#include <{}>' \; | sort
+
+ */
+
 #include <dune/common/alignment.hh>
 #include <dune/common/array.hh>
 #include <dune/common/arraylist.hh>
+#include <dune/common/bigunsignedint.hh>
 #include <dune/common/bitfield.hh>
 #include <dune/common/capabilities.hh>
+#include <dune/common/configparser.hh>
 #include <dune/common/debugstream.hh>
 #include <dune/common/dlist.hh>
 #include <dune/common/dynamictype.hh>
@@ -19,21 +30,27 @@
 #include <dune/common/function.hh>
 #include <dune/common/functionspace.hh>
 #include <dune/common/fvector.hh>
+#include <dune/common/gcd.hh>
 #include <dune/common/genericiterator.hh>
 #include <dune/common/helpertemplates.hh>
+#include <dune/common/interfaces.hh>
 #include <dune/common/iteratorfacades.hh>
+#include <dune/common/lcm.hh>
 #include <dune/common/logictraits.hh>
 #include <dune/common/mapping.hh>
 #include <dune/common/matrix.hh>
 #include <dune/common/misc.hh>
 #include <dune/common/operator.hh>
 #include <dune/common/poolallocator.hh>
+#include <dune/common/precision.hh>
+#include <dune/common/propertymap.hh>
 #include <dune/common/simplevector.hh>
 #include <dune/common/sllist.hh>
 #include <dune/common/smartpointer.hh>
 #include <dune/common/stack.hh>
 #include <dune/common/stdstreams.hh>
 #include <dune/common/timer.hh>
+#include <dune/common/tuples.hh>
 #include <dune/common/typeinfo.hh>
 #include <dune/common/typetraits.hh>
 #include <dune/common/vectorspace.hh>
