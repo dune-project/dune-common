86916b0b2be7a0db05cb4b9b3512dffa9adb8fc6
Carsten Gräser <graeser@dune-project.org>
2005-06-24 11:29:44 +0000

    added documentation
    [[Imported from SVN: r2238]]

--- common/configparser.hh
+++ common/configparser.hh
@@ -10,49 +10,174 @@
 
 namespace Dune {
 
-  /** \brief Parser for hierarchical parameter files
+  /** \brief Parser for hierarchical config files
    * \ingroup Common
    *
-   * This class parses files into a hierachical structure.
+   * This class parses config files into a hierachical structure.
+   * Config files should look like this
+   *
+     \verbatim
+   # this file configure fruit colors in fruitsalad
+
+
+   ##these are no fruit but could also apper in fruit salad
+     honeydewmelon = yellow
+     watermelon = green
+
+     fruit.tropicalfruit.orange = orange
+
+     [fruit]
+     strawberry = red
+     pomegranate = red
+
+     [fruit.pipfruit]
+     apple = green/red/yellow
+     pear = green
+
+     [fruit.stonefruit]
+     cherry = red
+     plup = purple
+
+     \endverbatim
+   *
+   *
+   * If a '[prefix]' statement appears all following entries use this prefix
+   * until the next '[prefix]' statement. Fruitsalads for example contain:
+     \verbatim
+     honeydewmelon = yellow
+     fruit.tropicalfruit.orange = orange
+     fruit.pipfruit.apple = green/red/yellow
+     fruit.stonefruit.cherry = red
+     \endverbatim
+   *
+   * All keys with a commen 'prefix.' belong to the same substructre called 'prefix'.
    *
    */
-
   class ConfigParser
   {
   public:
 
 
-    /** \brief Parser for hierarchical parameter files
-     *
-     * This class parses files into a hierachical structure.
-     *
+    /** \brief Create new ConfigParser
      */
     ConfigParser();
 
+
+    /** \brief parse file
+     *
+     * Parses file with given name and build hierarchical config structure.
+     *
+     * \param file filename
+     */
     void parseFile(std::string file);
+
+
+    /** \brief parse command line
+     *
+     * Parses command line options and build hierarchical config structure.
+     *
+     * \param argc arg count
+     * \param argv arg values
+     */
     void parseCmd(int argc, char* argv []);
 
-    bool hasKey(const std::string& key);
-    bool hasSub(const std::string& key);
 
+    /** \brief test for key
+     *
+     * Tests wether given key exists.
+     *
+     * \param key key name
+     * \return true if key exists in structure, otherwise false
+     */
+    bool hasKey(const std::string& key);
+
+
+    /** \brief test for substructure
+     *
+     * Tests wether given substructure exists.
+     *
+     * \param sub substructure name
+     * \return true if substructure exists in structure, otherwise false
+     */
+    bool hasSub(const std::string& sub);
+
+
+    /** \brief get value reference for key
+     *
+     * Returns refference to value for given key name.
+     * This creates the key, if not existent.
+     *
+     * \param key key name
+     * \return reference to coresponding value
+     */
     std::string& operator[] (const std::string& key);
 
+
+    /** \brief print structure to std::cout
+     */
     void report() const;
+
+
+    /** \brief print distinct substructure to std::cout
+     *
+     * Prints all entries with given prefix.
+     *
+     * \param prefix for key and substructure names
+     */
     void report(const std::string prefix) const;
 
-    ConfigParser& sub(const std::string& key);
 
+    /** \brief get substructure by name
+     *
+     * \param sub substructure name
+     * \return reference to substructure
+     */
+    ConfigParser& sub(const std::string& sub);
+
+
+    /** \brief get value as string
+     *
+     * Returns pure string value for given key.
+     *
+     * \param key key name
+     * \param defaultValue default if key does not exist
+     * \return value as string
+     */
     std::string get(const std::string& key, std::string defaultValue);
+
+
     std::string get(const std::string& key, char* defaultValue);
+
+
+    /** \brief get value as int
+     *
+     * Returns value for given key interpreted as int.
+     *
+     * \param key key name
+     * \param defaultValue default if key does not exist
+     * \return value as int
+     */
     int get(const std::string& key, int defaultValue);
+
+
+    /** \brief get value as double
+     *
+     * Returns value for given key interpreted as double.
+     *
+     * \param key key name
+     * \param defaultValue default if key does not exist
+     * \return value as double
+     */
     double get(const std::string& key, double defaultValue);
 
-    /** \brief kurz
+
+    /** \brief get value as bool
      *
-     * langes
-     * \param key Der Suchschlüssel
-     * \param defaultValue
-     * \return
+     * Returns value for given key interpreted as bool.
+     *
+     * \param key key name
+     * \param defaultValue default if key does not exist
+     * \return value as bool, false if values = '0', true if value = '1'
      */
     bool get(const std::string& key, bool defaultValue);
 
