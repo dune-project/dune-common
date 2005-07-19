2d244d396324afbc6a997ecb99f50416280afdd1
Oliver Sander <sander@dune-project.org>
2005-07-19 09:06:57 +0000

    added missing cases to avoid some compiler warnings
    [[Imported from SVN: r2407]]

--- grid/uggrid/uggrid.cc
+++ grid/uggrid/uggrid.cc
@@ -619,7 +619,7 @@ void Dune::UGGrid<dim,dimworld>::getChildrenOfSubface(typename Traits::template 
   // //////////////////////////////////////////////////////////////////////
 
   switch (e->geometry().type()) {
-  case cube : {
+  case cube :
 
     if (dim==3) {
       // Dune numbers the faces of a hexahedron differently than UG.
@@ -628,8 +628,8 @@ void Dune::UGGrid<dim,dimworld>::getChildrenOfSubface(typename Traits::template 
       elementSide = renumbering[elementSide];
     }
     break;
-  }
-  case simplex : {
+
+  case simplex :
 
     if (dim==3) {      // Tetrahedron
 
@@ -648,12 +648,16 @@ void Dune::UGGrid<dim,dimworld>::getChildrenOfSubface(typename Traits::template 
     }
 
     break;
-  }
-#if 0
+
+  case vertex :
+  case prism :
+  case pyramid :
+    // Do nothing
+    break;
+
   default :
     DUNE_THROW(NotImplemented, "Unknown element type '"
                << e->geometry().type() << "'found!");
-#endif
   }
 
   // ///////////////
@@ -754,11 +758,15 @@ void Dune::UGGrid<dim,dimworld>::getChildrenOfSubface(typename Traits::template 
       }
       break;
 
-#if 0
+    case vertex :
+    case prism :
+    case pyramid :
+      // Do nothing
+      break;
+
     default :
       DUNE_THROW(NotImplemented, "Unknown element type '"
                  << e->geometry().type() << "'found!");
-#endif
     }
 
     childElements[i].setToTarget(Element<0>::get(*f), Element<2>::get(*f));
