ab5c7edfaf869e8a71cba018f60ae04cb853a320
Robert Klöfkorn <robertk@dune-project.org>
2005-08-22 13:01:26 +0000

    signOut get const referenc on disc func.
    [[Imported from SVN: r2581]]

--- fem/lagrangebase.hh
+++ fem/lagrangebase.hh
@@ -148,7 +148,7 @@ namespace Dune {
 
     //! sign out to dofmanager, dofmanager frees the memory
     template <class DiscFuncType>
-    bool signOut (DiscFuncType & df) const;
+    bool signOut (const DiscFuncType & df) const;
 
   protected:
     // create functions space with basefunction set for given level
