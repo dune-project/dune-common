0b53f8d16ad569ad4e4203467d90cd6aa5b87915
Adrian Burri <burriad@dune-project.org>
2005-08-23 13:19:14 +0000

    Removed include of reftopology.hh
    [[Imported from SVN: r2606]]

--- grid/alu3dgrid/faceutility.cc
+++ grid/alu3dgrid/faceutility.cc
@@ -1,7 +1,6 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 #include "faceutility.hh"
-#include "../common/reftopology.hh"
 
 namespace Dune {
   //- class ALU3dGridGeometricFaceInfo
