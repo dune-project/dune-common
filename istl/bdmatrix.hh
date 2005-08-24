2d52e34d21f99c3a9433f21a80c0aafc7db5e1fe
Oliver Sander <sander@dune-project.org>
2005-08-24 14:08:14 +0000

    added const operator[]
    [[Imported from SVN: r2622]]

--- istl/bdmatrix.hh
+++ istl/bdmatrix.hh
@@ -47,6 +47,9 @@ namespace Dune {
     /** \brief Get the i-th diagonal element */
     B& operator[](int i) {return data_[i];}
 
+    /** \brief Get the i-th diagonal element */
+    const B& operator[](int i) const {return data_[i];}
+
     /** \brief y += Ax */
     template <class X, class Y>
     void umv(const X& x, Y& y) const {
