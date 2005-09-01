62fce448235449863bd88c6d38631d786e02f8ac
Adrian Burri <burriad@dune-project.org>
2005-09-01 15:40:34 +0000

    New subspaces for the combined space
    [[Imported from SVN: r2715]]

---
+++ fem/space/subspace.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_SUBSPACE_HH
+#define DUNE_SUBSPACE_HH
+
+namespace Dune {} // end namespace Dune
+
+#include "subspace.cc"
+
+#endif
