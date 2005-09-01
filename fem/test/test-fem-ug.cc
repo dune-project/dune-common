3bf470a1d892f05a68f6f8f7f41f95d682b60b83
Oliver Sander <sander@dune-project.org>
2005-09-01 08:03:32 +0000

    initial rudimentary testsuite for testing the fem stuff with the different grids
    [[Imported from SVN: r2700]]

---
+++ fem/test/test-fem-ug.cc
@@ -0,0 +1,53 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// $Id$
+
+#include <config.h>
+
+#include <iostream>
+
+/*
+
+   Instantiate UG-Grid and feed it to the generic gridcheck()
+
+   Currently UGGrid can only be initialized via the AmiraMeshReader
+
+ */
+
+#include <dune/grid/uggrid.hh>
+#include <dune/io/file/amirameshreader.hh>
+
+#include "femcheck.cc"
+
+int main () {
+  try {
+
+    // extra-environment to check destruction
+    {
+      std::cout << std::endl << "UGGrid<2,2> with grid file: ug-testgrid-2.am"
+                << std::endl << std::endl;
+      Dune::UGGrid<2,2> grid;
+      Dune::AmiraMeshReader<Dune::UGGrid<2,2> >::read(grid, "ug-testgrid-2.am");
+
+      femCheck(grid);
+    };
+
+    {
+      std::cout << std::endl << "UGGrid<3,3> with grid file: ug-testgrid-3.am"
+                << std::endl << std::endl;
+      Dune::UGGrid<3,3> grid;
+      Dune::AmiraMeshReader<Dune::UGGrid<3,3> >::read(grid, "ug-testgrid-3.am");
+
+      femCheck(grid);
+    };
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+
+  return 0;
+};
