170b678f26636ebf6a544ad80a2c990e86c60734
Adrian Burri <burriad@dune-project.org>
2005-09-05 14:10:50 +0000

    Added tagging interface
    [[Imported from SVN: r2736]]

--- common/interfaces.hh
+++ common/interfaces.hh
@@ -8,11 +8,16 @@ namespace Dune {
   //! An interface class for cloneable objects
   struct Cloneable {
 
-    /** \brief ???
-       \todo Please doc me!!!
+    /** \brief Clones the object
+       clone needs to be redefined by an implementation class, with the
+       return type covariantly adapted. Remember to
+       delete the resulting pointer.
      */
     virtual Cloneable* clone() const = 0;
   };
+
+  //! Tagging interface to indicate that Grid provides typedef ObjectStreamType
+  struct HasObjectStream {};
 
 } // end namespace Dune
 
