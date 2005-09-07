f98a6e593c5fbb4e8c10cbb5166155d23b9a645d
Peter Bastian <peter@dune-project.org>
2005-09-07 13:46:04 +0000

    started file for the standard mappers. Not functional yet.
    [[Imported from SVN: r2784]]

---
+++ grid/common/mapper.hh
@@ -0,0 +1,39 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+// $Id$
+
+#ifndef __DUNE_MAPPER2_HH__
+#define __DUNE_MAPPER2_HH__
+
+#include <iostream>
+#include "dune/common/helpertemplates.hh"
+/**
+ * @file
+ * @brief  Mapper classes are used to attach data to a grid
+ * @author Peter Bastian
+ */
+namespace Dune
+{
+  /**
+   * @defgroup GridMapper Mapper
+   * @ingroup Grid
+   * @brief Mapper classes are used to attach data to a grid
+   *
+   */
+  /**
+   * @addtogroup GridMapper
+   *
+   * @{
+   */
+
+  /** @file
+   * @author Peter Bastian
+   * @brief Provides classes with basic mappers which are used to attch data to a grid
+   *
+   */
+
+
+
+  /** @} */
+}
+#endif
