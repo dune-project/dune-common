3f3153f4c8958622106bc8131f05453d1a5db782
Oliver Sander <sander@dune-project.org>
2005-09-08 10:19:04 +0000

    some documentation
    [[Imported from SVN: r2799]]

--- grid/common/boundarysegment.hh
+++ grid/common/boundarysegment.hh
@@ -9,12 +9,26 @@
 
 namespace Dune {
 
+  /** \brief Base class for classes implementing geometries of boundary segments
+
+     Some grid implementations, as for example UGGrid, allow to use boundary segments of
+     arbitrary geometry.  That means that you can have grid boundaries approach smooth
+     shapes when refining the grid.
+
+     Such curved boundary segments are specified by giving classes that implement them.
+     Each boundary segment is implemented by an object of a class derived from
+     BoundarySegment.  The set of these objects is handed over to the grid upon grid
+     construction.
+   */
   template <int dimworld>
   class BoundarySegment {
   public:
 
+    /** \brief Dummy virtual destructor */
     virtual ~BoundarySegment() {}
 
+    /** \brief A function mapping local coordinates on a boundary segment to world coordinates
+     */
     virtual FieldVector<double,dimworld> operator()(const FieldVector<double, dimworld-1>& local) const = 0;
 
   };
