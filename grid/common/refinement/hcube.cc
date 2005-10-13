4ef749f7e5c1ba6e4cbe644d871f3792d9ca414b
Markus Blatt <mblatt@dune-project.org>
2005-10-13 12:38:50 +0000

    malevel() -> maxLevel()
    [[Imported from SVN: r3167]]

--- grid/common/refinement/hcube.cc
+++ grid/common/refinement/hcube.cc
@@ -211,8 +211,8 @@ namespace Dune {
       template<int dimension>
       void RefinementGrid<dimension>::refineTo(int level /*! The refinement level to enforce */)
       {
-        if(SGrid<dimension, dimension>::maxlevel() < level)
-          globalRefine(level - SGrid<dimension, dimension>::maxlevel());
+        if(SGrid<dimension, dimension>::maxLevel() < level)
+          globalRefine(level - SGrid<dimension, dimension>::maxLevel());
       }
 
 
