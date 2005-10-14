ffd542e4e6c87ce23ba156b93027ecc3af4386b5
Markus Blatt <mblatt@dune-project.org>
2005-10-14 13:01:59 +0000

    Do not postprocess smoothers if we only have 1 level.
    [[Imported from SVN: r3189]]

--- istl/paamg/amg.hh
+++ istl/paamg/amg.hh
@@ -138,7 +138,6 @@ namespace Dune
       DIterator lhs = lhs_->finest();
 
       if(rhs!=rhs_->coarsest())
-        // No smoothers!
         smoother->pre(*lhs,*rhs);
 
       if(smoother != coarsest)
@@ -243,7 +242,8 @@ namespace Dune
       Iterator smoother = smoothers_.finest();
       DIterator lhs = lhs_->finest();
 
-      smoother->post(*lhs);
+      if(lhs!= lhs->coarsest)
+        smoother->post(*lhs);
 
       if(smoother != coarsest)
         for(++smoother; smoother != coarsest; ++smoother, ++lhs)
