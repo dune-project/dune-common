22aee845cc51f5f10789f012905f75780efd43b5
Robert Klöfkorn <robertk@dune-project.org>
2005-10-16 22:07:50 +0000

    minor changes in the buildSurfaceMapping methods. Also minor changes in the referenceElementCoordinatesUnrefined method. But mostly face liftings.
    [[Imported from SVN: r3205]]

--- grid/alu3dgrid/faceutility.cc
+++ grid/alu3dgrid/faceutility.cc
@@ -7,41 +7,29 @@ namespace Dune {
 
   // just for simplicity
   //typedef BilinearSurfaceMapping BilinearSurfaceMappingType;
-  typedef ALU3DSPACE BilinearSurfaceMapping BilinearSurfaceMappingType;
+  //typedef ALU3DSPACE BilinearSurfaceMapping BilinearSurfaceMappingType;
+  typedef BilinearSurfaceMapping BilinearSurfaceMappingType;
 
   //- class ALU3dGridGeometricFaceInfo
   template <>
   ALU3DSPACE LinearSurfaceMapping*
   ALU3dGridGeometricFaceInfo<tetra>::
   buildSurfaceMapping(const CoordinateType& coords) const {
-    alu3d_ctype tmp[3][3];
-    convert2CArray(coords[0], tmp[0]);
-    convert2CArray(coords[1], tmp[1]);
-    convert2CArray(coords[2], tmp[2]);
-    return new ALU3DSPACE LinearSurfaceMapping(tmp[0],
-                                               tmp[1],
-                                               tmp[2]);
+
+    return new ALU3DSPACE LinearSurfaceMapping(
+             fieldVector2alu3d_ctype ( coords[0] ),
+             fieldVector2alu3d_ctype ( coords[1] ),
+             fieldVector2alu3d_ctype ( coords[2] ) );
   }
 
   template <>
   BilinearSurfaceMappingType*
   ALU3dGridGeometricFaceInfo<hexa>::
   buildSurfaceMapping(const CoordinateType& coords) const {
-    /*
-       return new BilinearSurfaceMappingType(coords[0],
-                                      coords[1],
-                                      coords[2],
-                                      coords[3]);
-     */
-    alu3d_ctype tmp[4][3];
-    convert2CArray(coords[0], tmp[0]);
-    convert2CArray(coords[1], tmp[1]);
-    convert2CArray(coords[2], tmp[2]);
-    convert2CArray(coords[3], tmp[3]);
-    return new BilinearSurfaceMappingType(tmp[0],
-                                          tmp[1],
-                                          tmp[2],
-                                          tmp[3]);
+    return new BilinearSurfaceMappingType(coords[0],
+                                          coords[1],
+                                          coords[2],
+                                          coords[3]);
   }
 
   // new methods
@@ -62,45 +50,29 @@ namespace Dune {
   ALU3dGridGeometricFaceInfo<hexa>::
   buildSurfaceMapping(const GEOFaceType & face) const
   {
-    /*
-       CoordinateType coords;
-       for (int i = 0; i < numVerticesPerFace; ++i) {
-       const double (&p)[3] = face.myvertex(FaceTopo::dune2aluVertex(i))->Point();
-       convert2FieldVector(p, coords[i] );
-       } // end for
+    // this is dangerous
+    CoordinateType coords;
+    for (int i = 0; i < numVerticesPerFace; ++i) {
+      const double (&p)[3] = face.myvertex(FaceTopo::dune2aluVertex(i))->Point();
+      convert2FieldVector(p, coords[i] );
+    } // end for
 
-       // this is the new implementation using FieldVector
-       // see mappings.hh
-       return new BilinearSurfaceMappingType(coords[0],
-                                      coords[1],
-                                      coords[2],
-                                      coords[3]);
-     */
-
-    // this is the original ALUGrid LinearSurfaceMapping,
-    // see mapp_cube_3d.* in ALUGrid code
-    // typedef of BilinearSurfaceMappingType see above
-    return new BilinearSurfaceMappingType(face.myvertex(0)->Point(),
-                                          face.myvertex(1)->Point(),
-                                          face.myvertex(2)->Point(),
-                                          face.myvertex(3)->Point());
+    // this is the new implementation using FieldVector
+    // see mappings.hh
+    return new BilinearSurfaceMappingType(coords[0],
+                                          coords[1],
+                                          coords[2],
+                                          coords[3]);
   }
 
   template <>
   ALU3dGridGeometricFaceInfo<tetra>::NormalType
   ALU3dGridGeometricFaceInfo<tetra>::
   calculateNormal(const SurfaceMappingType& mapping,
-                  const FieldVector<alu3d_ctype, 2>& local) const {
-    enum { dimworld = 3 };
-
-    //alu3d_ctype normal[dimworld];
-    //mapping.normal(normal);
-
+                  const FieldVector<alu3d_ctype, 2>& local) const
+  {
     NormalType result;
-    //convert2FieldVector(normal, result);
-
     mapping.normal( fieldVector2alu3d_ctype ( result ) );
-
     return result;
   }
 
@@ -110,15 +82,8 @@ namespace Dune {
   calculateNormal(const SurfaceMappingType& mapping,
                   const FieldVector<alu3d_ctype, 2>& local) const
   {
-    // the type save way
-    //const double localTmp[2] = { local[0] , local [1] };
-    //alu3d_ctype normal[dimworld];
-    //mapping.normal(localTmp,normal);
-    //convert2FieldVector(normal, result);
-
     NormalType result;
-    mapping.normal( fieldVector2alu3d_ctype ( local ) ,
-                    fieldVector2alu3d_ctype ( result ) );
+    mapping.normal(local,result);
     return result;
   }
 
@@ -145,23 +110,33 @@ namespace Dune {
 
     // temporary variables
     LocalCoordinateType parentLocal;
-    alu3d_ctype parLocalArray[numComponents];
-    alu3d_ctype resultArray[numComponents];
+
+    //alu3d_ctype parLocalArray[numComponents];
+    //alu3d_ctype resultArray[numComponents];
 
     // do the mappings
-    for (int i = 0; i < numCorners; ++i) {
+    for (int i = 0; i < numCorners; ++i)
+    {
       const FieldVector<alu3d_ctype, 2>& childLocal =
         refFace_.position(i, 2);
 
+      // transform from coordinates of R^2 to barycentric coordinates
+      // for barycentric coordinates see the child2parent method in
+      // mappings.cc
       LocalCoordinateType childLocalBary;
       childLocalBary[0] = 1.0 - childLocal[0] - childLocal[1];
       childLocalBary[1] = childLocal[0];
       childLocalBary[2] = childLocal[1];
 
       faceMapper.child2parent(childLocalBary, parentLocal);
-      convert2CArray(parentLocal, parLocalArray);
-      referenceElementMapping->map2world(parLocalArray, resultArray);
-      convert2FieldVector(resultArray, result[i]);
+      referenceElementMapping->map2world(
+        fieldVector2alu3d_ctype ( parentLocal ) ,
+        fieldVector2alu3d_ctype ( result[i]   ) );
+
+      //convert2CArray(parentLocal, parLocalArray);
+
+      //referenceElementMapping->map2world(parLocalArray, resultArray);
+      //convert2FieldVector(resultArray, result[i]);
     }
 
   }
@@ -179,10 +154,11 @@ namespace Dune {
     CoordinateType cornerCoords;
     referenceElementCoordinatesRefined(side, cornerCoords);
 
-    BilinearSurfaceMapping referenceElementMapping(cornerCoords[0],
-                                                   cornerCoords[1],
-                                                   cornerCoords[2],
-                                                   cornerCoords[3]);
+    BilinearSurfaceMapping
+    referenceElementMapping(cornerCoords[0],
+                            cornerCoords[1],
+                            cornerCoords[2],
+                            cornerCoords[3]);
 
     NonConformingMappingType faceMapper(connector_.face().parentRule(),
                                         connector_.face().nChild());
@@ -195,9 +171,8 @@ namespace Dune {
       const LocalCoordinateType& childLocal = refFace_.position(i, 2);
 
       faceMapper.child2parent(childLocal, parentLocal);
-      referenceElementMapping.map2world(parentLocal, result[i]);
+      referenceElementMapping.map2world( parentLocal , result[i] );
     }
-
   }
 
 } // end namespace Dune
