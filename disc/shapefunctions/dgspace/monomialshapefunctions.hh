4ef65fa70c13743d81c2e62afb291c706acaa68f
Christian Engwer <christi@dune-project.org>
2005-10-19 12:26:34 +0000

    * now MonomialShapeFunction is completely independent from dimension   and order * updated documentation
    [[Imported from SVN: r3259]]

--- disc/shapefunctions/dgspace/monomialshapefunctions.hh
+++ disc/shapefunctions/dgspace/monomialshapefunctions.hh
@@ -4,45 +4,44 @@
 #define DUNE_MONOMIALSHAPEFUNCTIONS_HH
 
 #include <iostream>
-#include "common/fvector.hh"
-#include "common/simplevector.hh"
-#include "common/exceptions.hh"
-#include "common/misc.hh"
-#include "dune/grid/common/grid.hh"
-#include "dune/grid/common/referenceelements.hh"
+#include <dune/common/fvector.hh>
+#include <dune/common/array.hh>
+#include <dune/common/exceptions.hh>
+#include <dune/common/stdstreams.hh>
+#include <dune/common/misc.hh>
+#include <dune/grid/common/grid.hh>
+#include <dune/grid/common/referenceelements.hh>
 
-#include "dune/disc/shapefunctions/shapefunctions.hh"
+#include <dune/disc/shapefunctions/shapefunctions.hh>
+
+/**
+   @file
+   @brief  define monomial type shape functions
+   @author Christian Engwer
+ */
 
 namespace Dune
 {
 
   /**
-     \brief calculates the size of a MonomialShapeFunctionSet
+     @addtogroup DISC
+     @{
    */
-  template <int dim, int order>
-  struct MonomialShapeFunctionSetSize
-  {
-    enum
-    {
-      maxSize =
-        MonomialShapeFunctionSetSize<dim-1,order>::maxSize
-        * (order+dim) / dim
-    };
-  };
 
-  template <int order>
-  struct MonomialShapeFunctionSetSize<1,order>
-  {
-    enum { maxSize = order + 1 };
+  enum {
+    /**
+       Default value for maximum order for the
+       MonomialShapeFunctionSetContainer
+     */
+    MonomialShapeFunctionDefaultMaxOrder = 5
   };
-
-  enum { MonomialShapeFunctionDefaultMaxOrder = 5 };
 
   template<typename C,typename T, int dim>
   class MonomialShapeFunction;
   template<typename C,typename T, int dim>
   class MonomialShapeFunctionSet;
-  template<typename C,typename T, int dim, int maxOrder=MonomialShapeFunctionDefaultMaxOrder>
+  template<typename C,typename T, int dim,
+      int maxOrder=MonomialShapeFunctionDefaultMaxOrder>
   class MonomialShapeFunctionSetContainer;
 
   /**
@@ -58,7 +57,7 @@ namespace Dune
     typedef C CoordType;
     typedef T ResultType;
 
-    /*
+    /**
        Create the n-th MonomialShapeFunction of the ShapeFunctionSet of
        the given order.
 
@@ -68,7 +67,7 @@ namespace Dune
     MonomialShapeFunction(int n, const FieldVector<int, dim> & exp) :
       n_(n), exp_(exp) {}
 
-    //! \copydoc ShapeFunction::evaluateFunction
+    /** \copydoc ShapeFunction::evaluateFunction */
     virtual ResultType
     evaluateFunction (int, const FieldVector<CoordType,dim>& x) const
     {
@@ -79,11 +78,10 @@ namespace Dune
       {
         phi *= power(x[c],exp_[c]);
       }
-      //        phi = power(x[0],exp_[0])*(power(x[1],exp_[1]));
       return phi;
     }
 
-    //! \copydoc ShapeFunction::evaluateDerivative
+    /** \copydoc ShapeFunction::evaluateDerivative */
     virtual ResultType
     evaluateDerivative(int, int dir, const FieldVector<CoordType,dim>& x) const
     {
@@ -94,41 +92,41 @@ namespace Dune
         dphi *= power(x[c],exp_[c]);
       }
       dphi *= exp_[dir] / x[dir];
-      //         if(dir==0)
-      //           // dphi/dx= (alpha-beta) * x^(alpha-beta-1) * y^beta
-      //           dphi = exp_[0] * power(x[0],exp_[0]-1) * power(x[1],exp_[1]);
-      //         else
-      //           // dphi/dy=  x^(alpha-beta) * beta * y^(beta-1)
-      //           dphi = power(x[0],exp_[0]) * exp_[1] * power(x[1],exp_[1]-1);
       return dphi;
     }
 
-    //! \copydoc ShapeFunction::localindex
+    /** \copydoc ShapeFunction::localindex */
     virtual int localindex (int comp) const { return n_; };
 
-    //! \copydoc ShapeFunction::codim()
+    /** \copydoc ShapeFunction::codim */
     virtual int codim () const { return 0; };
 
-    //! \copydoc ShapeFunction::entity()
+    /** \copydoc ShapeFunction::entity */
     virtual int entity () const { return 0; };
 
-    //! \copydoc ShapeFunction::entityindex()
+    /** \copydoc ShapeFunction::entityindex */
     virtual int entityindex () const { return 0; };
 
-    //! pretty print this shape function
+    /** pretty print this shape function */
     void print (std::ostream& s) const
     {
-      static const char names[] = { 'x', 'y', 'z', 'u', 'v', 'w', 'q', 'r', 's' };
+      static const char names[] =
+      { 'x', 'y', 'z', 'u', 'v', 'w', 'q', 'r', 's' };
       assert( dim < 9 ); /* avoid buffer overrun un names */;
-      int exp_sum = 0;
+      bool all_zero = true;
       for (int c=0; c<dim; c++)
       {
         if (exp_[c] > 0) {
-          s << names[c] << "^" << exp_[c] << " ";
+          if (exp_[c] == 1) {
+            s << names[c] << " ";
+          }
+          else {
+            s << names[c] << "^" << exp_[c] << " ";
+          }
+          all_zero = false;
         }
-        exp_sum *= exp_[c];
       }
-      if (exp_sum == 0) {
+      if (all_zero) {
         s << 1;
       }
     };
@@ -145,33 +143,109 @@ namespace Dune
     }
   };
 
-  template<typename C,typename T>
-  class MonomialShapeFunctionSet<C, T, 2> : public ShapeFunctionSet<C, T, 2, 1>
+  /**
+     \class MonomialShapeFunctionSetCreator
+     \internal
+     \brief Template Metaprogramm creating the entries for
+            MonomialShapeFunctionSet
+   */
+  template <typename C,typename T, int dim, int c>
+  struct MonomialShapeFunctionSetCreator
+  {
+    typedef MonomialShapeFunction<C,T,dim> ShapeFunction;
+    typedef FieldVector<int, dim> expV;
+    typedef Array< ShapeFunction* > sfV;
+    static void create (int bound, int& cnt, expV& exp, sfV& SFnts)
+    {
+      for (int a=0; a<=bound; a++)
+      {
+        exp[c-1] = bound-a;
+        MonomialShapeFunctionSetCreator<C,T,dim,c+1>::create(a,cnt,exp,SFnts);
+      }
+    }
+  };
+
+  /**
+     \copydoc MonomialShapeFunctionSetCreator
+     \internal
+     Specializeses the beginning of the recursion
+   */
+  template <typename C,typename T, int dim>
+  struct MonomialShapeFunctionSetCreator<C,T,dim,0>
+  {
+    typedef MonomialShapeFunction<C,T,dim> ShapeFunction;
+    typedef FieldVector<int, dim> expV;
+    typedef Array< ShapeFunction* > sfV;
+    static int create (int order, sfV& SFnts)
+    {
+      SFnts = 0;
+      expV exp(0);
+      int cnt = 0;
+      for (int a=0; a<=order; a++)
+      {
+        MonomialShapeFunctionSetCreator<C,T,dim,1>::create(a,cnt,exp,SFnts);
+      }
+      return cnt;
+    }
+  };
+
+  /**
+     \copydoc MonomialShapeFunctionSetCreator
+     \internal
+     Specializeses the end of the recursion
+   */
+  template <typename C,typename T, int dim>
+  struct MonomialShapeFunctionSetCreator<C,T,dim,dim>
+  {
+    typedef MonomialShapeFunction<C,T,dim> ShapeFunction;
+    typedef FieldVector<int, dim> expV;
+    typedef Array< ShapeFunction* > sfV;
+    static void create (int bound, int& cnt, expV& exp, sfV& SFnts)
+    {
+      exp[dim-1] = bound;
+      SFnts[cnt] = new ShapeFunction(cnt, exp);
+      for (int i=0; i<dim; i++)
+        dvverb << exp[i] << " ";
+      dvverb << std::endl;
+      cnt++;
+    }
+  };
+
+  /**
+     \class MonomialShapeFunctionSet
+     \brief a ShapeFunctionSet of monomial shapefunctions
+
+     The shape functions map from C^d to T.
+
+     template parameters are:
+     <ul>
+     <li>C: coordinate type
+     <li>T: result type
+     <li>d: dimension of the function space
+     </ul>
+
+     The order of the shape functions is a parameter to the constructor
+   */
+  template<typename C,typename T, int d>
+  class MonomialShapeFunctionSet : public ShapeFunctionSet<C, T, d, 1>
   {
   public:
-    enum { dim=2 /*!< Dimension of the ShapeFunctionSet */ };
+    enum { dim=d /*!< Dimension of the ShapeFunctionSet */ };
     typedef C CoordType;
     typedef T ResultType;
-    typedef MonomialShapeFunction<C,T,2> ShapeFunction;
+    typedef MonomialShapeFunction<C,T,dim> ShapeFunction;
 
+    /**
+       Construct a monomial shape function set of given order
+     */
     MonomialShapeFunctionSet(int order) :
-      order_(order), n_((order+1)*(order+2)/2), shapeFunctions(n_)
+      order_(order), n_(sz(order)), shapeFunctions(n_)
     {
-      shapeFunctions = 0;
-      int i = 0;
-      FieldVector<int, dim> exp;
-      for (int alpha=0; alpha<=order_; alpha++)
-      {
-        for (int beta=0; beta<=alpha; beta++)
-        {
-          //  x^(alpha-beta) * y^beta
-          exp[0] = alpha-beta;
-          exp[1] = beta;
-          shapeFunctions[i] =
-            new ShapeFunction(i, exp);
-          i++;
-        }
-      }
+      dvverb << "Constructing monomial shape function set of order "
+             << order << std::endl;
+      int i = MonomialShapeFunctionSetCreator<C,T,dim,0>::
+              create(order,shapeFunctions);
+      assert (n_==i);
     }
 
     virtual ~MonomialShapeFunctionSet()
@@ -182,9 +256,10 @@ namespace Dune
       }
     }
 
-    //! \copydoc ShapeFunctionSet::size()
+    //! \copydoc ShapeFunctionSet::size
     virtual int size() const
     {
+      assert(shapeFunctions.size() == n_);
       return n_;
     }
 
@@ -195,19 +270,21 @@ namespace Dune
       return 0;
     };
 
-    //! \copydoc ShapeFunctionSet::operator[](int i)
+    //! \copydoc ShapeFunctionSet::operator[]
     virtual const ShapeFunction& operator[](int i) const
     {
+      assert (i < n_);
+      assert (n_ == shapeFunctions.size());
       return *shapeFunctions[i];
     }
 
-    //! \copydoc ShapeFunctionSet::order()
+    //! \copydoc ShapeFunctionSet::order
     virtual int order() const
     {
       return order_;
     }
 
-    //! \copydoc ShapeFunctionSet::type()
+    //! \copydoc ShapeFunctionSet::type
     virtual GeometryType type () const
     {
       //! throw an exception... we dont' have a special geometry
@@ -220,10 +297,61 @@ namespace Dune
     const int order_;
     /** total number of shapefunctions in this set */
     const int n_;
+    /** \brief calculate total number of shapefunctions in a set of given order */
+    static int sz(int order)
+    {
+      int s;
+      s = 1;
+      for (int i=1; i<=dim; i++) {
+        s = s * (order + i) / i;
+      }
+      return s;
+    }
     /** vector which holds the shape functions */
-    SimpleVector< ShapeFunction* > shapeFunctions;
+    Array< ShapeFunction* > shapeFunctions;
   };
 
+  /**
+     \class MonomialShapeFunctionSetSize
+     \internal
+     \brief calculates the size of a MonomialShapeFunctionSet
+   */
+  template <int dim, int order>
+  struct MonomialShapeFunctionSetSize
+  {
+    enum
+    {
+      maxSize =
+        MonomialShapeFunctionSetSize<dim-1,order>::maxSize
+        * (order+dim) / dim
+    };
+  };
+
+  /**
+     \copydoc MonomialShapeFunctionSetSize
+     \internal
+     end of recursion
+   */
+  template <int order>
+  struct MonomialShapeFunctionSetSize<1,order>
+  {
+    enum { maxSize = order + 1 };
+  };
+
+  /**
+     \class MonomialShapeFunctionSetContainer
+     \brief a ShapeFunctionSetContainer of monomial shapefunctions
+
+     The shape functions map from C^d to T.
+
+     template parameters are:
+     <ul>
+     <li>C: coordinate type
+     <li>T: result type
+     <li>d: dimension of the function space
+     <li>maxOrder: container will contain shapefunctions up to order maxOrder
+     </ul>
+   */
   template<typename C,typename T, int d, int maxOrder>
   class MonomialShapeFunctionSetContainer :
     public ShapeFunctionSetContainer<C,T,d,1,MonomialShapeFunctionSetSize<d,maxOrder>::maxSize >
@@ -235,6 +363,9 @@ namespace Dune
     typedef T ResultType;
     typedef MonomialShapeFunctionSet<C,T,dim> ShapeFunctionSet;
 
+    /**
+       \brief Create a ShapeFunctionSetContainer of monomial shapefunctions
+     */
     MonomialShapeFunctionSetContainer() :
       shapeFunctionSets(maxsize)
     {
@@ -252,7 +383,8 @@ namespace Dune
         if (shapeFunctionSets[i]) delete shapeFunctionSets[i];
       }
     }
-    //! \copydoc ShapeFunctionSetContainer::operator()
+
+    /** \copydoc ShapeFunctionSetContainer::operator()(Geometry type, int order) */
     virtual const ShapeFunctionSet& operator() (GeometryType type, int order) const
     {
       assert(order <= maxOrder);
@@ -260,9 +392,12 @@ namespace Dune
     }
   private:
     /** vector which holds the shape functions */
-    SimpleVector< ShapeFunctionSet* > shapeFunctionSets;
+    Array< ShapeFunctionSet* > shapeFunctionSets;
   };
 
+  /** @} */
+
 } //end of namespace
+
 
 #endif // DUNE_MONOMIALSHAPEFUNCTIONS_HH
