f9a6bbb413e0f732007a3f05962944cd74848922
Robert Klöfkorn <robertk@dune-project.org>
2005-10-21 16:13:32 +0000

    Samll bug fix in getCoordVec which is used in geometryInFahter.
    [[Imported from SVN: r3278]]

--- grid/albertagrid/albertagrid.cc
+++ grid/albertagrid/albertagrid.cc
@@ -242,6 +242,13 @@ namespace Dune
   inline FieldVector<albertCtype, cdim>& AlbertaGridGeometry<mydim,cdim,GridImp>::
   getCoordVec (int i)
   {
+    assert( i >= 0 );
+    assert( i < mydim+1 );
+
+    // if global, or jacobianInverse is called then
+    // matrix has to be calculated again , because coord might have changed
+    builtElMat_ = false;
+
     return coord_[i];
   }
 
