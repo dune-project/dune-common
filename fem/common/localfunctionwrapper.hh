043a90cca01acb1dd10d98c530d545473725d3c6
Adrian Burri <burriad@dune-project.org>
2005-11-09 16:26:01 +0000

    Extended interface
    [[Imported from SVN: r3367]]

--- fem/common/localfunctionwrapper.hh
+++ fem/common/localfunctionwrapper.hh
@@ -223,6 +223,12 @@ namespace Dune {
       localFunc().jacobian( en, x, ret);
     }
 
+    //! assign a vector of dofs
+    void assign(int dofNum, const RangeType& dofs)
+    {
+      lf_->assign(dofNum, dofs);
+    }
+
     //! update local function for given Entity
     //! deprecated method
     template <class EntityType >
