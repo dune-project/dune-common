1f1db3ac161d474aedf74e25ecb2bc8c59f8dbd4
Robert Klöfkorn <robertk@dune-project.org>
2005-11-09 17:56:27 +0000

    implemented IntersectionItWrapper class which gets reference obj IntersectionImp where the object comes from object stack, that no memory has to be allocated and freed.
    [[Imported from SVN: r3370]]

---
+++ grid/common/intersectioniteratorwrapper.hh
@@ -0,0 +1,147 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_INTERSECTIONITERATORWRAPPER_HH
+#define DUNE_INTERSECTIONITERATORWRAPPER_HH
+
+namespace Dune {
+
+  //! \brief Class that wraps IntersectionIteratorImp of a grid adn gets it's
+  //! internal object from a object stack hold by the grid
+  template <class GridImp>
+  class IntersectionIteratorWrapper
+  {
+    enum { dim = GridImp :: dimension };
+    enum { dimworld = GridImp :: dimensionworld };
+
+    typedef IntersectionIteratorWrapper<GridImp> ThisType;
+
+    typedef typename GridImp :: IntersectionIteratorProviderType
+    IntersectionIteratorProviderType;
+
+    typedef typename GridImp :: IntersectionIteratorImp IntersectionIteratorImp;
+  public:
+    //! dimension
+    enum { dimension      = dim };
+    //! dimensionworld
+    enum { dimensionworld = dimworld };
+
+    //! define type used for coordinates in grid module
+    typedef typename GridImp :: ctype ctype;
+
+    //! Entity type
+    typedef typename GridImp::template Codim<0>::Entity Entity;
+    //! type of EntityPointer
+    typedef typename GridImp::template Codim<0>::EntityPointer EntityPointer;
+
+    //! type of intersectionGlobal
+    typedef typename GridImp::template Codim<1>::Geometry Geometry;
+    //! type of intersection*Local
+    typedef typename GridImp::template Codim<1>::LocalGeometry LocalGeometry;
+
+    //! type of normal vector
+    typedef FieldVector<ctype , dimworld> NormalType;
+
+    //! constructor called from the ibegin and iend method
+    template <class EntityImp>
+    IntersectionIteratorWrapper(const GridImp & grid , const EntityImp & en, int wLevel , bool end )
+      : storage_(grid.intersetionIteratorProvider())
+        , it_(*(storage_.getObject(grid,wLevel)))
+    {
+      if(end)
+        it().done();
+      else
+        it().first(en,wLevel);
+    }
+
+    //! The copy constructor
+    IntersectionIteratorWrapper(const ThisType & org)
+      : storage_(org.storage_) , it_(*(storage_.getObjectCopy(org.it_)))
+    {
+      it().assign(org.it_);
+    }
+
+    //! The Destructor
+    ~IntersectionIteratorWrapper()
+    {
+      storage_.freeObject( &it_ );
+    }
+
+    //! The copy constructor
+    bool equals (const ThisType & i) const { return it().equals(i.it()); }
+
+    //! increment iterator
+    void increment () { it().increment(); }
+
+    //! access neighbor
+    EntityPointer outside() const { return it().outside(); }
+
+    //! access entity where iteration started
+    EntityPointer inside() const { return it().inside(); }
+
+    //! return true if intersection is with boundary. \todo connection with
+    //! boundary information, processor/outer boundary
+    bool boundary () const { return it().boundary(); }
+
+    //! return true if across the intersection a neighbor on this level exists
+    bool neighbor () const { return it().neighbor(); }
+
+    //! return information about the Boundary
+    int boundaryId () const { return it().boundaryId(); }
+
+    //! intersection of codimension 1 of this neighbor with element where
+    //! iteration started.
+    //! Here returned element is in LOCAL coordinates of the element
+    //! where iteration started.
+    const LocalGeometry & intersectionSelfLocal () const { return it().intersectionSelfLocal(); }
+
+    //! intersection of codimension 1 of this neighbor with element where
+    //!  iteration started.
+    //! Here returned element is in GLOBAL coordinates of the element where
+    //! iteration started.
+    const Geometry & intersectionGlobal () const { return it().intersectionGlobal(); }
+
+    //! local number of codim 1 entity in self where intersection is contained
+    //!  in
+    int numberInSelf () const { return it().numberInSelf(); }
+
+    //! intersection of codimension 1 of this neighbor with element where
+    //! iteration started.
+    //! Here returned element is in LOCAL coordinates of neighbor
+    const LocalGeometry & intersectionNeighborLocal () const { return it().intersectionNeighborLocal(); }
+
+    //! local number of codim 1 entity in neighbor where intersection is
+    //! contained
+    int numberInNeighbor () const { return it().numberInNeighbor(); }
+
+    //! return unit outer normal, this should be dependent on local
+    //! coordinates for higher order boundary
+    NormalType & unitOuterNormal (const FieldVector<alu3d_ctype, dim-1>& local) const
+    {
+      return it().unitOuterNormal(local);
+    }
+
+    //! return outer normal, this should be dependent on local
+    //! coordinates for higher order boundary
+    NormalType & outerNormal (const FieldVector<alu3d_ctype, dim-1>& local) const
+    {
+      return it().outerNormal(local);
+    }
+
+    //! return outer normal, this should be dependent on local
+    //! coordinates for higher order boundary
+    NormalType & integrationOuterNormal (const FieldVector<alu3d_ctype, dim-1>& local) const
+    {
+      return it().integrationOuterNormal(local);
+    }
+
+  private:
+    //! returns reference to underlying intersection iterator implementation
+    IntersectionIteratorImp & it() { return it_; }
+    const IntersectionIteratorImp & it() const { return it_; }
+
+    IntersectionIteratorProviderType & storage_;
+    IntersectionIteratorImp & it_;
+  }; // end class IntersectionIteratorWrapper
+
+} // end namespace Dune
+#endif
