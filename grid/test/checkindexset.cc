78a8eea31d709e6eddb56ae37a300b8d3731c371
Robert Klöfkorn <robertk@dune-project.org>
2005-11-18 14:37:32 +0000

    enabled check of niehgbor entities and checks for the sizes of the index sets.
    [[Imported from SVN: r3492]]

--- grid/test/checkindexset.cc
+++ grid/test/checkindexset.cc
@@ -30,17 +30,228 @@ namespace Dune {
     return comp;
   }
 
+  // check som functionality of grid
+  template <int codim, class GridType, class EntityType,
+      class IndexSetType, class OutputStreamImp,
+      class MapType1 , class MapType2 , class MapType3 >
+  void checkSubEntity ( const GridType & grid,
+                        const EntityType &en , const IndexSetType & lset,
+                        OutputStreamImp & sout , MapType1 & subEntities , MapType2 & vertices ,
+                        MapType3 & vertexCoordsMap )
+  {
+    GeometryType type = en.geometry().type();
+    enum { dim = EntityType::dimension };
+    typedef typename EntityType :: ctype coordType;
+
+    const ReferenceElement< coordType, dim > & refElem =
+      ReferenceElements< coordType, dim >::general(type);
+
+
+    // check all subEntities of codimension  codim
+    if(en.template count<codim>() != refElem.size(0,0,codim))
+    {
+      std::cerr << "entity index = " << lset.index(en)
+                << ", type = " << GeometryName(type)
+                << std::endl
+                << "codim = " << codim
+                << std::endl
+                << "count<codim>() = " << en.template count<codim>()
+                << std::endl
+                << "refElem.size(codim) = " << refElem.size(0,0,codim)
+                << std::endl;
+      DUNE_THROW(GridError,
+                 "wrong number of subEntities of codim " << codim);
+    }
+    for(int subEntity = 0; subEntity < refElem.size(0,0,codim); subEntity++)
+    {
+      {
+        int numSubEntities = refElem.size(subEntity,codim,dim);
+        // every entity have at least one vertex
+        assert( numSubEntities > 0 );
+
+        // create vectors of number of vertices on sub entity
+        std::vector<int> local (numSubEntities,-1);
+        std::vector<int> global(numSubEntities,-1);
+
+        for(int j=0 ; j<numSubEntities; j++ )
+          local[j] = refElem.subEntity(subEntity , codim , j , dim );
+
+
+        sout << numSubEntities << " Vertices on subEntity<codim=" << codim << ">\n";
+        sout << "check suben [";
+        for(int j=0 ; j<numSubEntities-1; j++ )
+          sout << local[j] <<  ", ";
+        sout << local[numSubEntities-1] << "]\n";
+
+        for(int j=0 ; j<numSubEntities; j++ )
+        {
+          global[j] = lset.template subIndex<dim> ( en, local[j]);
+        }
+
+        int globalSubEntity = lset.template subIndex<codim>(en,subEntity);
+        assert( globalSubEntity >= 0 );
+        sout << "local subentity " << subEntity << " consider subentity with global index " << globalSubEntity << " on en = " << lset.index(en) << "\n";
+
+        sout << "Found global numbers of entity [ ";
+        for(int j=0 ; j<numSubEntities; j++ )
+        {
+          sout << global[j] << " ";
+        }
+        sout << "]\n";
+
+        for(int j=0; j<numSubEntities; j++)
+        {
+          {
+            // get entity pointer of sub entity codim=dim (Vertex)
+            typedef typename GridType :: template Codim<dim> :: EntityPointer VertexPointerType;
+            VertexPointerType vxp = en.template entity<dim> (local[j]);
+
+            FieldVector<coordType,dim> vx ( vxp->geometry()[0]);
+            if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
+            {
+              FieldVector<coordType,dim> vxcheck ( vertexCoordsMap[global[j]] );
+              if( ! compareVec( vxcheck, vx ) )
+              {
+                std::cerr << "map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
+                assert( compareVec( vxcheck, vx ) );
+              }
+            }
+          }
+
+          typedef typename GridType :: template Codim<codim> :: EntityPointer SubEnPointerType;
+          SubEnPointerType subenp = en.template entity<codim> (subEntity);
+
+          FieldVector<coordType,dim> vx ( subenp->geometry()[j]);
+          if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
+          {
+            FieldVector<coordType,dim> vxcheck ( vertexCoordsMap[global[j]] );
+            if( ! compareVec( vxcheck, vx ) )
+            {
+              std::cerr << "map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
+              assert( compareVec( vxcheck, vx ) );
+            }
+          }
+          sout << "vx[" << global[j] << "] = "  << vx << "\n";
+        }
+
+        // sort vector of global vertex number for storage in map
+        // the smallest entry is the first entry
+        std::sort( global.begin(), global.end() );
+
+        // check whether vertex key is already stored in map
+        if(vertices.find(global) == vertices.end())
+        {
+          vertices[global] = globalSubEntity;
+        }
+        else
+        {
+          int otherSubEntity = vertices[global];
+          // ??? WTF ???
+          assert( globalSubEntity == otherSubEntity );
+        }
+
+        // check whether subEntity is already stored in map
+        if(subEntities.find(globalSubEntity) == subEntities.end() )
+        {
+          subEntities[globalSubEntity] = global;
+        }
+        else
+        {
+          std::vector<int> globalcheck = subEntities[globalSubEntity];
+          if(! (global == globalcheck ))
+          {
+            std::cerr << "For subEntity " << globalSubEntity << "\n";
+            std::cerr << "Got ";
+            for(int j=0 ; j<numSubEntities; j++ )
+            {
+              std::cerr << global[j] << " ";
+            }
+            std::cerr << "\n";
+            std::cerr << "Found ";
+            for(int j=0 ; j<numSubEntities; j++ )
+            {
+              std::cerr << globalcheck [j] << " ";
+            }
+            std::cerr << "\n";
+            DUNE_THROW(Dune::GridError, "global != globalcheck");
+          }
+        }
+      }
+    }   // end check sub entities
+  }
+
   // check some functionality of grid
   template <int codim, class GridType,
       class IndexSetType, class OutputStreamImp >
   void checkIndexSetForCodim ( const GridType &grid , const IndexSetType & lset,
-                               OutputStreamImp & sout )
+                               OutputStreamImp & sout , bool levelIndex )
   {
     enum { dim = GridType :: dimension };
 
     sout <<"\n\nStart consistency check of index set \n\n";
     typedef typename IndexSetType :: template Codim<0>::template Partition<All_Partition> :: Iterator Iterator;
     typedef typename GridType :: ctype coordType;
+    typedef typename GridType :: template Codim<0>:: Entity EntityCodim0Type;
+    typedef typename EntityCodim0Type :: IntersectionIterator IntersectionIterator;
+
+
+    //*****************************************************************
+    // check size of index set
+    int gridsize = 0;
+    {
+      typedef typename IndexSetType :: template Codim<codim>::
+      template Partition<All_Partition> :: Iterator IteratorType;
+
+      int count = 0;
+      IteratorType endit  = lset.template end<codim,All_Partition>   ();
+      for(IteratorType it = lset.template begin<codim,All_Partition> ();
+          it != endit ; ++it )
+      {
+        count ++ ;
+      }
+
+      int lsetsize = lset.size(codim);
+      if( count != lsetsize)
+      {
+        derr << "WARNING: walk = "<< count << " entities | set = "
+             << lsetsize << " for codim " << codim << std::endl;
+      }
+      gridsize = count;
+      // lsetsize should be at least the size of iterated entities
+      assert( count <= gridsize );
+    }
+
+    {
+      typedef typename GridType :: Traits :: LocalIdSet :: IdType IdType;
+      std::map < IdType , bool > entityfound;
+      int mycount = 0;
+      Iterator endit  = lset.template end  <0,All_Partition> ();
+      for(Iterator it = lset.template begin<0,All_Partition> ();
+          it != endit ; ++it )
+      {
+        int suben = it->template count<codim> ();
+        for(int i=0 ; i<suben; i++)
+        {
+          IdType id = grid.localIdSet().id ( *(it->template entity<codim>(i) ) );
+          if( entityfound.find(id) == entityfound.end())
+          {
+            mycount ++ ;
+            entityfound[id] = true;
+          }
+        }
+      }
+
+      if ( gridsize != (int)entityfound.size() )
+      {
+        derr << "WARNING: gridsize = "<< gridsize << " entities | map of entites = "
+             << entityfound.size() << " for codim " << codim << std::endl;
+      }
+      // gridsize should be at least the size of found entities
+      assert( gridsize <= (int) entityfound.size() );
+    }
+
+
+    //******************************************************************
 
     typedef std::map < int , std::pair<int,int> > subEntitymapType;
     std::map < int , std::vector<int> > subEntities;
@@ -107,11 +318,6 @@ namespace Dune {
       for(Iterator it = lset.template begin<0,All_Partition>();
           it != endit; ++it)
       {
-        GeometryType type = it->geometry().type();
-
-        const ReferenceElement< coordType, dim > & refElem =
-          ReferenceElements< coordType, dim >::general(type);
-
         sout << "****************************************\n";
         sout << "Element = " << lset.index(*it) << " on level " << it->level () << "\n";
         sout << "Vertices      = [";
@@ -155,134 +361,31 @@ namespace Dune {
           }
         }
 
-        // check all subEntities of codimension  codim
-        if(it->template count<codim>() != refElem.size(0,0,codim))
+        ////////////////////////////////////////////////////////////
+        // check sub entities
+        ////////////////////////////////////////////////////////////
+        checkSubEntity<codim> (grid, *it, lset, sout,
+                               subEntities, vertices, vertexCoordsMap);
+
+        // check neighbors
+        if(codim == 1)
         {
-          std::cerr << "entity index = " << lset.index(*it)
-                    << ", type = " << GeometryName(type)
-                    << std::endl
-                    << "codim = " << codim
-                    << std::endl
-                    << "count<codim>() = " << it->template count<codim>()
-                    << std::endl
-                    << "refElem.size(codim) = " << refElem.size(0,0,codim)
-                    << std::endl;
-          DUNE_THROW(GridError,
-                     "wrong number of subEntities of codim " << codim);
-        }
-        for(int subEntity = 0; subEntity < refElem.size(0,0,codim); subEntity++)
-        {
+          IntersectionIterator endnit  = it->iend();
+          for(IntersectionIterator nit = it->ibegin(); nit != endnit; ++nit)
           {
-            int numSubEntities = refElem.size(subEntity,codim,dim);
-            // every entity have at least one vertex
-            assert( numSubEntities > 0 );
-
-            // create vectors of number of vertices on sub entity
-            std::vector<int> local (numSubEntities,-1);
-            std::vector<int> global(numSubEntities,-1);
-
-            for(int j=0 ; j<numSubEntities; j++ )
-              local[j] = refElem.subEntity(subEntity , codim , j , dim );
-
-
-            sout << numSubEntities << " Vertices on subEntity<codim=" << codim << ">\n";
-            sout << "check suben [";
-            for(int j=0 ; j<numSubEntities-1; j++ )
-              sout << local[j] <<  ", ";
-            sout << local[numSubEntities-1] << "]\n";
-
-            for(int j=0 ; j<numSubEntities; j++ )
+            if(nit.neighbor())
             {
-              global[j] = lset.template subIndex<dim> (*it, local[j]);
-            }
+              typedef typename GridType :: template Codim<0> :: EntityPointer EnPointer;
+              EnPointer ep = nit.outside();
+              const EntityCodim0Type & en = *ep;
 
-            int globalSubEntity = lset.template subIndex<codim>(*it,subEntity);
-            assert( globalSubEntity >= 0 );
-            sout << "local subentity " << subEntity << " consider subentity with global index " << globalSubEntity << " on en = " << lset.index(*it) << "\n";
-
-            sout << "Found global numbers of entity [ ";
-            for(int j=0 ; j<numSubEntities; j++ )
-            {
-              sout << global[j] << " ";
-            }
-            sout << "]\n";
-
-            for(int j=0; j<numSubEntities; j++)
-            {
+              // dont check neighbour, if we check level index sets
+              if(levelIndex)
               {
-                // get entity pointer of sub entity codim=dim (Vertex)
-                typedef typename GridType :: template Codim<dim> :: EntityPointer VertexPointerType;
-                VertexPointerType vxp = it->template entity<dim> (local[j]);
-
-                FieldVector<coordType,dim> vx ( vxp->geometry()[0]);
-                if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
-                {
-                  FieldVector<coordType,dim> vxcheck ( vertexCoordsMap[global[j]] );
-                  if( ! compareVec( vxcheck, vx ) )
-                  {
-                    std::cerr << "map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
-                    assert( compareVec( vxcheck, vx ) );
-                  }
-                }
+                if(it->level() != en.level()) continue;
               }
-
-              typedef typename GridType :: template Codim<codim> :: EntityPointer SubEnPointerType;
-              SubEnPointerType subenp = it->template entity<codim> (subEntity);
-
-              FieldVector<coordType,dim> vx ( subenp->geometry()[j]);
-              if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
-              {
-                FieldVector<coordType,dim> vxcheck ( vertexCoordsMap[global[j]] );
-                if( ! compareVec( vxcheck, vx ) )
-                {
-                  std::cerr << "map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
-                  assert( compareVec( vxcheck, vx ) );
-                }
-              }
-              sout << "vx[" << global[j] << "] = "  << vx << "\n";
-            }
-
-            // sort vector of global vertex number for storage in map
-            // the smallest entry is the first entry
-            std::sort( global.begin(), global.end() );
-
-            // check whether vertex key is already stored in map
-            if(vertices.find(global) == vertices.end())
-            {
-              vertices[global] = globalSubEntity;
-            }
-            else
-            {
-              int otherSubEntity = vertices[global];
-              // ??? WTF ???
-              assert( globalSubEntity == otherSubEntity );
-            }
-
-            // check whether subEntity is already stored in map
-            if(subEntities.find(globalSubEntity) == subEntities.end() )
-            {
-              subEntities[globalSubEntity] = global;
-            }
-            else
-            {
-              std::vector<int> globalcheck = subEntities[globalSubEntity];
-              if(! (global == globalcheck ))
-              {
-                std::cerr << "For subEntity " << globalSubEntity << "\n";
-                std::cerr << "Got ";
-                for(int j=0 ; j<numSubEntities; j++ )
-                {
-                  std::cerr << global[j] << " ";
-                }
-                std::cerr << "\n";
-                std::cerr << "Found ";
-                for(int j=0 ; j<numSubEntities; j++ )
-                {
-                  std::cerr << globalcheck [j] << " ";
-                }
-                std::cerr << "\n";
-                DUNE_THROW(Dune::GridError, "global != globalcheck");
-              }
+              checkSubEntity<codim> (grid, en, lset, sout,
+                                     subEntities, vertices, vertexCoordsMap);
             }
           }
         }
@@ -296,12 +399,12 @@ namespace Dune {
   struct CheckIndexSet
   {
     static void checkIndexSet( const GridType &grid ,
-                               const IndexSetType & iset, OutputStreamImp & sout )
+                               const IndexSetType & iset, OutputStreamImp & sout, bool levelIndex )
     {
-      checkIndexSetForCodim<codim> (grid,iset,sout);
+      checkIndexSetForCodim<codim> (grid,iset,sout,levelIndex);
       CheckIndexSet<GridType,IndexSetType,OutputStreamImp,
           codim-1, Dune::Capabilities::hasEntity<GridType, codim-1>::v > ::
-      checkIndexSet( grid, iset, sout );
+      checkIndexSet( grid, iset, sout,levelIndex );
     }
   };
 
@@ -310,12 +413,12 @@ namespace Dune {
   struct CheckIndexSet<GridType,IndexSetType,OutputStreamImp,codim,false>
   {
     static void checkIndexSet( const GridType &grid ,
-                               const IndexSetType & iset, OutputStreamImp & sout )
+                               const IndexSetType & iset, OutputStreamImp & sout , bool levelIndex )
     {
-      derr << "ERROR: entities for codim " << codim << " are not working for some reason!" << std::endl;
+      derr << "WARNING: entities for codim " << codim << " are not working for some reason!" << std::endl;
       CheckIndexSet<GridType,IndexSetType,OutputStreamImp,
           codim-1, Dune::Capabilities::hasEntity<GridType, codim-1>::v > ::
-      checkIndexSet( grid, iset, sout );
+      checkIndexSet( grid, iset, sout , levelIndex );
     }
   };
 
@@ -324,19 +427,19 @@ namespace Dune {
   struct CheckIndexSet<GridType,IndexSetType,OutputStreamImp,0,true>
   {
     static void checkIndexSet( const GridType &grid ,
-                               const IndexSetType & iset, OutputStreamImp & sout )
+                               const IndexSetType & iset, OutputStreamImp & sout , bool levelIndex )
     {
-      checkIndexSetForCodim<0> (grid,iset,sout);
+      checkIndexSetForCodim<0> (grid,iset,sout,levelIndex);
     }
   };
 
   template <class GridType, class IndexSetType, class OutputStreamImp>
   void checkIndexSet( const GridType &grid , const IndexSetType & iset,
-                      OutputStreamImp & sout )
+                      OutputStreamImp & sout ,  bool levelIndex = false )
   {
     CheckIndexSet<GridType,IndexSetType,OutputStreamImp,
         GridType::dimension, true> ::
-    checkIndexSet (grid,iset,sout);
+    checkIndexSet (grid,iset,sout,levelIndex);
   }
 
 } // end namespace Dune
