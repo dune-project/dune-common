4e20a018eac08c70c18e206827e02709c9d95755
Robert Klöfkorn <robertk@dune-project.org>
2005-11-24 10:30:05 +0000

    bug fix when setting up the base functions for dimrange > 1
    [[Imported from SVN: r3516]]

--- fem/lagrangebase/lagrangebasefunctions.hh
+++ fem/lagrangebase/lagrangebasefunctions.hh
@@ -770,9 +770,9 @@ namespace Dune {
       {
         for(int k=0; k<dimrange; k++)
         {
-          //baseFuncList_[ i*dimrange + k ] = new LagrangeBaseFunctionType ( i ) ;
           size_t idx = i*dimrange + k;
-          this->setBaseFunctionPointer ( idx, new LagrangeBaseFunctionType(i) );
+          // for each dimrange we have a base function
+          this->setBaseFunctionPointer ( idx, new LagrangeBaseFunctionType(idx) );
         }
       }
       this->setNumOfDiffFct ( numOfDifferentFuncs );
