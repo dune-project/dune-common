57b96ce99063e1d2040c3ad1b5376b834f8ace7b
Christian Engwer <christi@dune-project.org>
2005-12-12 17:29:29 +0000

    all source files should include config.h
    [[Imported from SVN: r3609]]

--- doc/istl/comm/indexset.cc
+++ doc/istl/comm/indexset.cc
@@ -1,6 +1,9 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 // $Id$
+
+#include "config.h"
+
 #include <dune/istl/indexset.hh>
 #include <dune/istl/plocalindex.hh>
 #include <iostream>
