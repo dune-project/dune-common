fb4be5df0a681fdd5958628983f0e12a3ff06dbc
Oliver Sander <sander@dune-project.org>
2005-12-19 09:22:32 +0000

    minor doc improvement
    [[Imported from SVN: r3678]]

--- common/helpertemplates.hh
+++ common/helpertemplates.hh
@@ -5,11 +5,11 @@
 #ifndef DUNE_COMMON_HELPERTEMPLATES
 #define DUNE_COMMON_HELPERTEMPLATES
 
-/*
-   Compile-time constraint checks from the Stroustrup FAQ
+/** \file
+    \brief Compile-time constraint checks from the Stroustrup FAQ
  */
 
-/*
+/**
    Derived_from<A,B>();
 
    or
@@ -23,23 +23,25 @@ template<class T, class B> struct Derived_from {
   Derived_from() { void (*p)(T*) = constraints; }
 };
 
+/** \todo Please doc me! */
 template<class T1, class T2> struct Can_copy {
   static void constraints(T1 a, T2 b) { T2 c = a; b = a; }
   Can_copy() { void (*p)(T1,T2) = constraints; }
 };
 
+/** \todo Please doc me! */
 template<class T1, class T2 = T1> struct Can_compare {
   static void constraints(T1 a, T2 b) { a==b; a!=b; a<b; }
   Can_compare() { void (*p)(T1,T2) = constraints; }
 };
 
+/** \todo Please doc me! */
 template<class T1, class T2, class T3 = T1> struct Can_multiply {
   static void constraints(T1 a, T2 b, T3 c) { c = a*b; }
   Can_multiply() { void (*p)(T1,T2,T3) = constraints; }
 };
 
-/*
-   helper template so that compilation fails if condition is not true
+/** \brief Helper template so that compilation fails if condition is not true
  */
 template <bool condition>
 struct IsTrue
@@ -47,6 +49,8 @@ struct IsTrue
   static void no() {};
 };
 
+/** \brief Helper template so that compilation fails if condition is not true
+ */
 template <>
 struct IsTrue<true>
 {
