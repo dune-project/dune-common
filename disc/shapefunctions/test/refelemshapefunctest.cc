4c26023afa1a466e7e1e4f5896dc119faa9becd3
Oliver Sander <sander@dune-project.org>
2005-12-19 14:02:49 +0000

    compiler warnings removed
    [[Imported from SVN: r3691]]

--- disc/shapefunctions/test/refelemshapefunctest.cc
+++ disc/shapefunctions/test/refelemshapefunctest.cc
@@ -7,7 +7,7 @@
     \brief Test for a segmentation fault caused by the interplay
     between reference elements and shape functions
 
-    On some compilers and some optimization levels :-) the code in this test
+    On some compilers, e.g., g++-4.0 and some optimization levels :-) the code in this test
     used to produce a segfault.  This was apparently due to initialization
     order subtilities.
  */
@@ -16,8 +16,8 @@ using namespace Dune;
 
 int main (int argc, char *argv[])
 {
-  int a=ReferenceElements<double,2>::general(cube).subEntity(0,1,0,2);
+  ReferenceElements<double,2>::general(cube).subEntity(0,1,0,2);
 
-  const LagrangeShapeFunctionSetContainer<double, double, 1>& lagrangeShapeFunctionSet =
-    Dune::LagrangeShapeFunctions<double, double, 1>::general;
+  Dune::LagrangeShapeFunctions<double, double, 1>::general(cube,1);
+
 }
