6d714d5115a53806bf26105625cd845cd6c3b415
Oliver Sander <sander@dune-project.org>
2006-01-12 09:42:12 +0000

    Added a test which checks whether geometryInFather() returns identical results on identical grids with and without boundary parametrization. Currently it doesn't.  This is bug #74.
    [[Imported from SVN: r3779]]

--- grid/test/test-ug.cc
+++ grid/test/test-ug.cc
@@ -19,6 +19,99 @@
 
 #include "gridcheck.cc"
 
+class ArcOfCircle : public Dune::BoundarySegment<2>
+{
+public:
+
+  ArcOfCircle(const Dune::FieldVector<double,2>& center, double radius,
+              double fromAngle, double toAngle)
+    : center_(center), radius_(radius), fromAngle_(fromAngle), toAngle_(toAngle)
+  {}
+
+  Dune::FieldVector<double,2> operator()(const Dune::FieldVector<double,1>& local) const {
+
+    double angle = fromAngle_ + local[0]*(toAngle_ - fromAngle_);
+
+    Dune::FieldVector<double,2> result = center_;
+    result[0] += radius_ * std::cos(angle);
+    result[1] += radius_ * std::sin(angle);
+
+    return result;
+  }
+
+  Dune::FieldVector<double,2> center_;
+
+  double radius_;
+
+  double fromAngle_;
+
+  double toAngle_;
+};
+
+
+void makeHalfCircleQuad(Dune::UGGrid<2,2>& grid, bool parametrization)
+{
+  using namespace Dune;
+
+  grid.createbegin();
+
+  // /////////////////////////////
+  //   Create boundary segments
+  // /////////////////////////////
+  if (parametrization) {
+
+    FieldVector<double,2> center(0);
+    center[1] = 15;
+
+    std::vector<int> vertices(2);
+
+    vertices[0] = 1;  vertices[1] = 2;
+    grid.insertBoundarySegment(vertices, new ArcOfCircle(center, 15, M_PI, M_PI*4/3));
+
+    vertices[0] = 2;  vertices[1] = 3;
+    grid.insertBoundarySegment(vertices, new ArcOfCircle(center, 15, M_PI*4/3, M_PI*5/3));
+
+    vertices[0] = 3;  vertices[1] = 0;
+    grid.insertBoundarySegment(vertices, new ArcOfCircle(center, 15, M_PI*5/3, M_PI*2));
+
+  }
+
+  // ///////////////////////
+  //   Insert vertices
+  // ///////////////////////
+  FieldVector<double,2> pos;
+
+  pos[0] = 15;  pos[1] = 15;
+  grid.insertVertex(pos);
+
+  pos[0] = -15; pos[1] = 15;
+  grid.insertVertex(pos);
+
+  pos[0] = -7.5; pos[1] = 2.00962;
+  grid.insertVertex(pos);
+
+  pos[0] = 7.5; pos[1] = 2.00962;
+  grid.insertVertex(pos);
+  // /////////////////
+  // Insert elements
+  // /////////////////
+
+  std::vector<unsigned int> cornerIDs(4);
+  cornerIDs[0] = 0;
+  cornerIDs[1] = 1;
+  cornerIDs[2] = 3;
+  cornerIDs[3] = 2;
+
+  grid.insertElement(cube, cornerIDs);
+
+  // //////////////////////////////////////
+  //   Finish initialization
+  // //////////////////////////////////////
+  grid.createend();
+
+}
+
+
 template <class GridType >
 void markOne ( GridType & grid , int num , int ref )
 {
@@ -41,6 +134,10 @@ void markOne ( GridType & grid , int num , int ref )
 int main () {
   try {
 
+    // ////////////////////////////////////////////////////////////////////////
+    //  Do the standard grid test for a 2d UGGrid
+    // ////////////////////////////////////////////////////////////////////////
+
     // extra-environment to check destruction
     {
       std::cout << std::endl << "UGGrid<2,2> with grid file: ug-testgrid-2.am"
@@ -59,6 +156,9 @@ int main () {
       gridcheck(grid);
     }
 
+    // ////////////////////////////////////////////////////////////////////////
+    //  Do the standard grid test for a 3d UGGrid
+    // ////////////////////////////////////////////////////////////////////////
     {
       std::cout << std::endl << "UGGrid<3,3> with grid file: ug-testgrid-3.am"
                 << std::endl << std::endl;
@@ -74,6 +174,39 @@ int main () {
 
       grid.globalRefine(1);
       gridcheck(grid);
+    }
+
+    // ////////////////////////////////////////////////////////////////////////
+    //   Check whether geometryInFather returns equal results with and
+    //   without parametrized boundaries
+    // ////////////////////////////////////////////////////////////////////////
+
+    Dune::UGGrid<2,2> gridWithParametrization, gridWithoutParametrization;
+
+    makeHalfCircleQuad(gridWithParametrization, true);
+    makeHalfCircleQuad(gridWithoutParametrization, false);
+
+    gridWithParametrization.globalRefine(1);
+    gridWithoutParametrization.globalRefine(1);
+
+    typedef Dune::UGGrid<2,2>::Codim<0>::LevelIterator ElementIterator;
+    ElementIterator eIt    = gridWithParametrization.lbegin<0>(1);
+    ElementIterator eWoIt  = gridWithoutParametrization.lbegin<0>(1);
+    ElementIterator eEndIt = gridWithParametrization.lend<0>(1);
+
+    for (; eIt!=eEndIt; ++eIt, ++eWoIt) {
+
+      // The grids where constructed identically and they are traversed identically
+      // Thus their respective output from geometryInFather should be the same
+      for (int i=0; i<eIt->geometry().corners(); i++) {
+
+        Dune::FieldVector<double,2> diff = eIt->geometryInFather()[i] - eWoIt->geometryInFather()[i];
+
+        if ( diff.two_norm() > 1e-5 )
+          DUNE_THROW(Dune::GridError, "output of geometryInFather() depends on boundary parametrization!");
+
+      }
+
     }
 
   } catch (Dune::Exception &e) {
