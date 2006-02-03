b6880bae066d5b74fc5b9952a947a330a4e8d8e5
Robert Klöfkorn <robertk@dune-project.org>
2006-02-03 17:44:19 +0000

    accidently undefed M_PI.
    [[Imported from SVN: r3997]]

--- io/visual/grape/grape_undefs.hh
+++ io/visual/grape/grape_undefs.hh
@@ -2358,18 +2358,6 @@
   #undef G_UPPER_BOUND
 #endif // G_UPPER_BOUND
 
-#ifdef M_PI
-  #undef M_PI
-#endif // M_PI
-
-#ifdef M_PI_2
-  #undef M_PI_2
-#endif // M_PI_2
-
-#ifdef M_E
-  #undef M_E
-#endif // M_E
-
 #ifdef G_VOID_P_TO_ANY_P
   #undef G_VOID_P_TO_ANY_P
 #endif // G_VOID_P_TO_ANY_P
@@ -2873,10 +2861,6 @@
 #ifdef G_NOEDGE
   #undef G_NOEDGE
 #endif // G_NOEDGE
-
-#ifdef restrict
-  #undef restrict
-#endif // restrict
 
 #ifdef G_MAX_BND_FUNCTIONS
   #undef G_MAX_BND_FUNCTIONS
