b79c24b72308cac7c8c6da2bb6bba975034c0d51
Markus Blatt <mblatt@dune-project.org>
2006-02-09 15:42:04 +0000

    Added missing function implementations.
    [[Imported from SVN: r4044]]

--- istl/paamg/pinfo.hh
+++ istl/paamg/pinfo.hh
@@ -119,9 +119,9 @@ namespace Dune
 
       typedef int GlobalLookupIndexSet;
 
-      void buildGlobalLookup(std::size_t);
+      void buildGlobalLookup(std::size_t){};
 
-      void freeGlobalLookup();
+      void freeGlobalLookup(){};
 
       const GlobalLookupIndexSet& globalLookup() const
       {
