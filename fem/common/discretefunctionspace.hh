07faac3c7cc49669154852cc13ce1198cb015fbb
Robert Klöfkorn <robertk@dune-project.org>
2006-02-13 15:39:41 +0000

    type conversion improved.
    [[Imported from SVN: r4056]]

--- fem/common/discretefunctionspace.hh
+++ fem/common/discretefunctionspace.hh
@@ -3,6 +3,9 @@
 #ifndef DUNE_DISCRETEFUNCTIONSPACE_HH
 #define DUNE_DISCRETEFUNCTIONSPACE_HH
 
+//- system includes
+#include <assert.h>
+
 //- Dune includes
 #include <dune/common/functionspace.hh>
 
@@ -20,59 +23,125 @@ namespace Dune {
 
   class GeometryIdentifier {
   public:
-    enum { numTypes = 9 };
-    enum IdentifierType { Vertex = 0, Line, Triangle, Quadrilateral,
-                          Tetrahedron, Hexahedron, Pyramid, Prism, Unknown };
+
+    enum { numTypes = 10 };
+
+    // in GeometryType
+    // simplex = 0
+    // cube    = 1
+    // pyramid = 2
+    // prism   = 3
+    //
+    // formula: id = (2 * type) + dimension
+    enum IdentifierType {
+      Vertex        = 0,
+      Line          = 1,
+      Triangle      = 2 * (GeometryType::simplex) + 2, // = 2
+      Tetrahedron   = 2 * (GeometryType::simplex) + 3, // = 3
+      Quadrilateral = 2 * (GeometryType::cube)    + 2, // = 4
+      Hexahedron    = 2 * (GeometryType::cube)    + 3, // = 5
+      Pyramid       = 2 * (GeometryType::pyramid) + 3, // = 7
+      Prism         = 2 * (GeometryType::prism)   + 3, // = 9
+      Unknown       = -1
+    };
+
+    struct CheckNumbers
+    {
+      static void check()
+      {
+        assert(Vertex        == 0);
+        assert(Line          == 1);
+        assert(Triangle      == 2);
+        assert(Tetrahedron   == 3);
+        assert(Quadrilateral == 4);
+        assert(Hexahedron    == 5);
+        assert(Pyramid       == 7);
+        assert(Prism         == 9);
+      }
+    };
+
+    // convert GeometryType::BasicType to IdentifierType by given formula above
+    template <int cd,int dim>
+    struct FromGeometry
+    {
+      inline static IdentifierType convert (const GeometryType :: BasicType type)
+      {
+        return static_cast<IdentifierType> ((2 * type) + dim);
+      }
+    };
+
+    // dim = 0 is Vertex
+    template <int cd>
+    struct FromGeometry<cd,0>
+    {
+      inline static IdentifierType convert (const GeometryType :: BasicType type)
+      {
+        return Vertex;
+      }
+    };
+
+    // dim = 1 is Line
+    template <int cd>
+    struct FromGeometry<cd,1>
+    {
+      inline static IdentifierType convert (const GeometryType :: BasicType type)
+      {
+        return Line;
+      }
+    };
+
   public:
+
     GeometryIdentifier(IdentifierType idType) :
       identifier_(idType) {}
 
-    GeometryIdentifier(int dimension, NewGeometryType geoType) :
+    GeometryIdentifier(int dimension, const GeometryType & geoType) :
       identifier_(GeometryIdentifier::fromGeo(dimension, geoType)) {}
 
-    operator NewGeometryType() const {
+    inline operator GeometryType() const {
       return GeometryIdentifier::toGeo(identifier_);
     }
-    operator IdentifierType() const { return identifier_; }
+    inline operator IdentifierType() const { return identifier_; }
 
-    static NewGeometryType toGeo(IdentifierType id) {
+    static inline GeometryType toGeo(IdentifierType id) {
       switch(id) {
       case Vertex :
-        return NewGeometryType(NewGeometryType::simplex,0);
+        return GeometryType(GeometryType::simplex,0);
       case Line :
       {
         //lines are simplices, more than cubes ;)
-        return NewGeometryType(NewGeometryType::simplex,1);
+        return GeometryType(GeometryType::cube,1);
       }
       case Triangle :
-        return NewGeometryType(NewGeometryType::simplex,2);
+        return GeometryType(GeometryType::simplex,2);
       case Tetrahedron :
-        return NewGeometryType(NewGeometryType::simplex,3);
+        return GeometryType(GeometryType::simplex,3);
       case Quadrilateral :
-        return NewGeometryType(NewGeometryType::cube,2);
+        return GeometryType(GeometryType::cube,2);
       case Hexahedron :
-        return NewGeometryType(NewGeometryType::cube,3);
+        return GeometryType(GeometryType::cube,3);
       case Pyramid :
-        return NewGeometryType(NewGeometryType::pyramid,3);
+        return GeometryType(GeometryType::pyramid,3);
       case Prism :
-        return NewGeometryType(NewGeometryType::prism,3);
+        return GeometryType(GeometryType::prism,3);
       default :
       {
         assert(false);
-        abort();
+        DUNE_THROW(NotImplemented,"GeometryType not available");
         // vertex is the new unknown ;)
-        return NewGeometryType(NewGeometryType::simplex,0);
+        return GeometryType(GeometryType::simplex,0);
       }
       }
     }
 
     //! return conversion from geometry to identifier
-    static IdentifierType fromGeo(NewGeometryType geo) {
+    static inline IdentifierType fromGeo(const GeometryType & geo) {
       return fromGeo(geo.dim(),geo);
     }
 
     //! return conversion from geometry to identifier
-    static IdentifierType fromGeo(int dimension, NewGeometryType geo) {
+    static inline IdentifierType fromGeo(int dimension, const GeometryType & geo)
+    {
 
       switch(dimension) {
       case 0 : return Vertex;
@@ -105,6 +174,18 @@ namespace Dune {
         abort();
         return Unknown;
       }
+    }
+
+    template <class GeometryImp>
+    inline static IdentifierType fromGeometry (const GeometryImp & geo)
+    {
+#ifndef NDEBUG
+      CheckNumbers::check();
+      IdentifierType id = FromGeometry<0,GeometryImp::mydimension>::convert(geo.type().basicType());
+      assert(id >= 0);
+      assert(id == fromGeo(geo.type()));
+#endif
+      return FromGeometry<0,GeometryImp::mydimension>::convert(geo.type().basicType());
     }
 
   private:
