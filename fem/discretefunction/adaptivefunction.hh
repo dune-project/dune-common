c210cacee076692ad9d9be2cc09a6e7ba2734744
Robert Klöfkorn <robertk@dune-project.org>
2006-02-15 22:41:21 +0000

    removed double typedef
    [[Imported from SVN: r4093]]

--- fem/discretefunction/adaptivefunction.hh
+++ fem/discretefunction/adaptivefunction.hh
@@ -205,7 +205,6 @@ namespace Dune {
     //! Function space type
     typedef typename SpaceTraits::FunctionSpaceType FunctionSpaceType;
     //! The base function set of DiscreteFunctionSpaceType
-    typedef typename SpaceTraits::BaseFunctionSetType BaseFunctionSetType;
 
     //! Intrinsic data type for range field
     typedef typename Traits::RangeFieldType RangeFieldType;
