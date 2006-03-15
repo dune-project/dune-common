a9de67c2adb1762e16c0ddfb983961fd230d6091
Peter Bastian <peter@dune-project.org>
2006-03-15 13:12:56 +0000

    update documentation
    [[Imported from SVN: r4312]]

--- grid/common/indexidset.hh
+++ grid/common/indexidset.hh
@@ -15,29 +15,58 @@
         @brief Provides base classes for index and id sets
  */
 
-/** @addtogroup IndexIdSets Index and Id Sets
-
-        @section Intro Introduction
-
-        still missing
-
- */
-
 namespace Dune
 {
-  /** @brief Index Set %Interface.
+  /** @brief Index Set %Interface base class.
 
      This class template is used as a base class for all index set implementations.
      It uses the Barton-Nackman trick to ensure conformity to the interface.
 
      Template parameters are:
 
-     \par GridImp
-     A %Dune grid type.
-     \par IndexSetImp
-     An implementation of the index set interface.
-     \par IndexSetTypes
-     A class providing the types returned by the methods of IndexSet.
+     - <tt>GridImp</tt> Type that is a model of Dune::Grid.
+     - <tt>IndexSetImp</tt> Type that is a model of Dune::IndexSet.
+     - <tt>IndexSetTypes</tt> Traits class containing return types depending on implementation.
+
+     <H3>Overview</H3>
+
+     An index set provides a map \f[ m : E \to \mathbf{N}\f] where
+     \f$E\f$ is a subset of the entities of a grid and \f$\mathbf{N}\f$ is the set of
+     natural numbers (including 0).
+
+     We define the subsets
+     \f[ E_g^c = \{e\in E \ | \ \textrm{$e$ has codimension $c$ and geometry type $g$} \}.\f]
+
+     The index map \f$m\f$ has the following properties:
+
+     - It is unique within the subsets \f$E_g^c\f$, i.e. for any \f$e,e^\prime\in E_g^c\f$
+     we have \f$e\neq e^\prime \rightarrow m(e)\neq m(e^\prime)\f$.
+     - It is consecutive and zero-starting within the subsets \f$E_g^c\f$, i.e. we have
+     \f$0\leq m(e) < |E_g^c|\f$ for any \f$e\in E_g^c\f$.
+
+     Index sets are used to assign user defined data (e.g. degrees of freedom
+     of a discretization) to entities of the grid. For efficiency reasons the prefered
+     data structure for user data is the array. In order to access the data from the
+     entity, its index (with respect to an index set - there may be several) is evaluated
+     and used as an index to an array (or some other container providing random access).
+
+     Usually an index set is not used directly but a Mapper is used to
+     compute the array index from the information supplied by an index set.
+
+     It is important to note that the index assigned to an entity may change during
+     grid modification (i.e. refinement or dynamic load balancing). The user is reponsible
+     for reorganizing the information stored in the external arrays appropriately. In
+     order to do this the IdSet concept is supplied.
+
+     <H3>Level index</H3>
+
+     Index set where \f$E\f$ corresponds to all entities of a given grid level.
+     All grid implementations provide level indices.
+
+     <H3>Leaf Index</H3>
+
+     Index set where \f$E\f$ corresponds to all entities of the leaf grid.
+     All grid implementations provide a leaf index.
 
      @ingroup IndexIdSets
    */
@@ -60,7 +89,14 @@ namespace Dune
       };
     };
 
-    /** @brief Map entity to index.
+    //===========================================================
+    /** @name Index access from entity
+     */
+    //@{
+    //===========================================================
+
+    /** @brief Map entity to index. The result of calling this method with an entity that is not
+            in the index set is undefined.
 
             \param e Reference to codim cc entity, where cc is the template parameter of the function.
             \return An index in the range 0 ... Max number of entities in set - 1.
@@ -76,7 +112,10 @@ namespace Dune
       return asImp().template index<cc>(e);
     }
 
-    /** @brief Map entity to index.
+    /** @brief Map entity to index. Easier to use than the above because codimension template
+            parameter need not be supplied explicitely.
+            The result of calling this method with an entity that is not
+            in the index set is undefined.
 
             \param e Reference to codim cc entity. Since
            entity knows its codimension, automatic extraction is possible.
@@ -89,7 +128,9 @@ namespace Dune
       return asImp().template index<cc>(e);
     }
 
-    /** @brief Map subentity of codim 0 entity to index.
+    /** @brief Map subentity of codim cc of codim 0 entity to index.
+            The result of calling this method with an entity that is not
+            in the index set is undefined.
 
        \param e Reference to codim 0 entity.
        \param i Number of codim cc subentity of e, where cc is the template parameter of the function.
@@ -105,22 +146,42 @@ namespace Dune
     {
       return asImp().template subIndex<cc>(e,i);
     }
+    //@}
 
-    /** @brief Return total number of entities of given type in this index set.
 
-       \param type A valid geometry type.
+    //===========================================================
+    /** @name Access to entity set
+     */
+    //@{
+    //===========================================================
 
-       \return number of entities.
+    /** @brief Return vector with all geometry types of entities in domain of index map.
+            Return a vector with all geometry types of a given codimension
+            contained in the Entity set \f$E\f$.
+
+       \param[in] codim A valid codimension.
+       \return Const reference to a vector of geometry types.
+     */
+    const std::vector<GeometryType>& geomTypes (int codim) const
+    {
+      return asImp().geomTypes(codim);
+    }
+
+    /** @brief Return total number of entities of given geometry type in entity set \f$E\f$.
+
+       \param[in] type A valid geometry type.
+       \return         number of entities.
      */
     int size (GeometryType type) const
     {
       return asImp().size(type);
     }
 
-    /** @brief Return total number of entities of given codim as a sum
-       for all geometry types in this index set.
-       \param codim A valid codimension
-       To be moved to the DefaultImplementation class.
+    /** @brief Return total number of entities of given codim in the entity set \f$E\f$. This
+            is simply a sum over all geometry types.
+
+       \param[in] codim A valid codimension
+            \return    number of entities.
      */
     int size (int codim) const
     {
@@ -128,20 +189,10 @@ namespace Dune
       const std::vector<GeometryType>& geomTs = this->geomTypes(codim);
       for (unsigned int i=0; i<geomTs.size(); i++)
         s += this->size(geomTs[i]);
-
       return s;
     }
 
-    /** @brief Return vector with all geometry types of entities in domain of index map.
-
-       \return reference to vector of geometry types.
-     */
-    const std::vector<GeometryType>& geomTypes (int codim) const
-    {
-      return asImp().geomTypes(codim);
-    }
-
-    /** @brief Does the index set contain this Entity?
+    /** @brief Return true if the given entity is contained in \f$E\f$.
      */
     template<class EntityType>
     bool contains (const EntityType& e) const
@@ -157,7 +208,8 @@ namespace Dune
       return false;
     }
 
-    /** @brief Iterator to first entity of given codimension and partition type.
+    /** @brief Iterator to first entity of given codimension and partition type in \f$E\f$.
+            The iterator type is available via the public Codim struct.
      */
     template<int cd, PartitionIteratorType pitype>
     typename Codim<cd>::template Partition<pitype>::Iterator begin () const
@@ -165,15 +217,17 @@ namespace Dune
       return asImp().begin<cd,pitype>();
     }
 
-    /** @brief Iterator to one past the last entity of given codim for partition type
+    /** @brief Iterator to one past the last entity of given codim and partition type in \f$E\f$.
+            The iterator type is available via the public Codim struct.
      */
     template<int cd, PartitionIteratorType pitype>
     typename Codim<cd>::template Partition<pitype>::Iterator end () const
     {
       return asImp().end<cd,pitype>();
     }
+    //@}
 
-    //! Default constructor
+    // Must be explicitely defined although this class should get a default constructor.
     IndexSet() {}
 
   private:
@@ -241,12 +295,61 @@ namespace Dune
 
      Template parameters are:
 
-     \par GridImp
-     A %Dune grid type.
-     \par IdSetImp
-     An implementation of the id set interface.
-     \par IdTypeImp
-     The type used for the ids (which is returned by functions returning an id).
+     - <tt>GridImp</tt> Type that is a model of Dune::Grid.
+     - <tt>IdSetImp</tt> Type that is a model of Dune::IdSet.
+     - <tt>IdTypeImp</tt> Traits class containing return types depending on implementation.
+
+     <H3>Overview</H3>
+
+     An id set provides a map \f[ m : E \to \mathbf{N}\f] where
+     \f$E\f$ is a subset of the entities of a grid and \f$\mathbf{N}\f$ is the set of
+     natural numbers (including 0).
+
+     The index map \f$m\f$ has the following properties:
+
+     - It is injective, i.e. for any \f$e,e^\prime\in E\f$
+     we have \f$e\neq e^\prime \Rightarrow m(e)\neq m(e^\prime)\f$.
+     - It is persistent with respect to grid modification, i.e. if there exits an entity \f$e\f$ with
+     id \f$i\f$ before grid modification and an entity \f$e^\prime\f$ with id \f$i\f$ after mesh
+     modification it is guaranteed that \f$e=e^\prime\f$.
+
+     The set of ids \f$\{i\in\textbf{N}\ |\ \exists e\in E : m(e)=i\}\f$ used by the
+     id set is not necessarily consecutive. In practice the numbers can be quite large, especially
+     in a parallel implementation. Therefore the type used to represent the id can be chosen
+     by the application.
+
+     <H3>Ids and leaf entities</H3>
+
+     An element is a copy of its father element if it is the only son. This
+     concept can be transfered to all higher codimensions because in a nested grid
+     structure the entities of any codimension form a set of trees. However, the roots
+     of these trees are not necessarily on level 0.
+     Thus, we define that an entity is a copy of another entity if it is the only descendant
+     of this entity in the refinement tree. This is illustrated in the following figure where,
+     for example, vertex w is a copy of vertex v.
+
+     \image html  idlocalref.png "Sharing of ids."
+     \image latex idlocalref.eps "Sharing of ids." width=\textwidth
+
+     The copy relation can be trivially extended to be an equivalence relation.
+     With respect to ids we define that <EM> all copies of an entity share the same id.</EM>
+     In the example of the figure the vertices v and w would have the same id.
+
+     This definition is useful to transfer data related to the leaf grid during grid modification.
+
+     <H3>Global id set</H3>
+
+     A global id set provides ids that are unique over all processes over wich the
+     grid is distributed.
+     All grid implementations provide a global id set.
+
+     <H3>Local id set</H3>
+
+     A local id set provides ids that are unique within one process but two entities
+     in different processes may have the same id. Obviously, a global id set is also
+     a local id set. A grid implementation may provide an extra local id set for efficiency reasons.
+     In sequential grids local and global id set are identical.
+     All grid implementations provide a local id set.
 
      @ingroup IndexIdSets
    */
@@ -254,10 +357,10 @@ namespace Dune
   class IdSet
   {
   public:
-    //! define the type used for persistent indices
+    //! Type used to represent an id.
     typedef IdTypeImp IdType;
 
-    //! get id of an entity
+    //! Get id of an entity. This method is simpler to use than the one below.
     template<class EntityType>
     IdType id (const EntityType& e) const
     {
@@ -265,7 +368,7 @@ namespace Dune
       return asImp().template id<cc>(e);
     }
 
-    //! get id of an entity of codim cc
+    //! Get id of an entity of codim cc. Unhandy because template parameter must be supplied explicitely.
     /*
        We use the RemoveConst to extract the Type from the mutable class,
        because the const class is not instatiated yet.
@@ -277,7 +380,7 @@ namespace Dune
       return asImp().template id<cc>(e);
     }
 
-    //! get id of subentity i of codim cc
+    //! Get id of subentity i of codim cc of a codim 0 entity.
     /*
        We use the RemoveConst to extract the Type from the mutable class,
        because the const class is not instatiated yet.
@@ -289,7 +392,7 @@ namespace Dune
       return asImp().template subId<cc>(e,i);
     }
 
-    //! Default constructor
+    // Default constructor (is not provided automatically because copy constructor is private)
     IdSet() {}
 
   private:
--- grid/common/mapper.hh
+++ grid/common/mapper.hh
@@ -34,7 +34,7 @@
    set to the index set.
 
    Access from a grid entity \f$e\in E^\prime\f$ to its associated data element \f$d_e\f$ then is
-   two step process: \f[ a(m(e)) = d_e. \f]
+   a two step process: \f[ a(m(e)) = d_e. \f]
 
    @section Mapper2 Different Kinds of Mappers
    <!--====================================-->
@@ -45,16 +45,16 @@
 
    @subsection para1 Index based Mappers
 
-   An index-based mapper is allocated for a grid and can be used as as the grid is not changed
+   An index-based mapper is allocated for a grid and can be used as long as the grid is not changed
    (i.e. refined, coarsened or load balanced). The implementation of static mappers
-   is based on index sets and is typically of \f$O(1)\f$ complexity with a very
-   small constant. Index-based mappers are only available for special kinds (but usually sufficient)
+   is based on a Dune::IndexSet and is typically of \f$O(1)\f$ complexity with a very
+   small constant. Index-based mappers are only available for restricted (but usually sufficient)
    entity sets.
 
    @subsection para2 Id based Mappers
 
-   An id-based mapper can also be used after the grid has been changed. For that it
-   has to be implemented on the basis of id sets. This may be relatively slow
+   An id-based mapper can also be used while a grid changes. For that it
+   has to be implemented on the basis of a Dune::IdSet. This may be relatively slow
    because the data type used for ids is usually not an int and the non-consecutive
    ids require more complicated search data structures (typically a map). Access is therefore
    at least \f$O(\log |E^\prime|)\f$. On the other hand, id-based mappers can treat arbitrary
@@ -102,10 +102,9 @@ namespace Dune
 
      Template parameters are:
 
-     \par G
-     A Dune grid type.
-     \par MapperImp
-     An implementation of the mapper interface.
+     - <tt>G</tt> Type that is a model of Dune::Grid.
+     - <tt>MapperImp</tt> Type that is a model of Dune::Mapper.
+
    */
   template <typename G, typename MapperImp>
   class Mapper {
@@ -122,7 +121,7 @@ namespace Dune
       return asImp().map(e);
     }
 
-    /** @brief Map subentity of codim 0 entity to array index.
+    /** @brief Map subentity i of codim cc of a codim 0 entity to array index.
 
        \param e Reference to codim 0 entity.
        \param i Number of codim cc subentity of e, where cc is the template parameter of the function.
@@ -148,10 +147,11 @@ namespace Dune
     }
 
 
-    /** @brief Returns true if the entity is contained in the index set
+    /** @brief Returns true if the entity is contained in the index set and at the same
+            time the array index is returned.
 
-       \param e Reference to entity
-       \param result integer reference where corresponding index is  stored if true
+       \param[in] e Reference to entity
+       \param[out] result Filled with array index if entity is contained
        \return true if entity is in entity set of the mapper
      */
     template<class EntityType>
@@ -160,11 +160,12 @@ namespace Dune
       return asImp().contains(e,result );
     }
 
-    /** @brief Returns true if the entity is contained in the index set
+    /** @brief Returns true if the subentity is contained in the index set and at the same time
+            the array index is returned.
 
-       \param e Reference to codim 0 entity
-       \param i subentity number
-       \param result integer reference where corresponding index is  stored if true
+       \param[in] e Reference to codim 0 entity
+       \param[in] i subentity number
+       \param[out] result Filled with array index if entity is contained
        \return true if entity is in entity set of the mapper
      */
     template<int cc>     // this is now the subentity's codim
@@ -173,7 +174,7 @@ namespace Dune
       return asImp().template contains<cc>(e,i,result);
     }
 
-    /** @brief Recalculates map after mesh adaptation
+    /** @brief Reinitialize mapper after grid has been modified.
      */
     void update ()
     {
