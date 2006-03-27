c1c19d1f67a11b08bd7cdd1de89e718f364a0c3f
Markus Blatt <mblatt@dune-project.org>
2006-03-27 21:23:15 +0000

    Some more asserts.
    [[Imported from SVN: r4362]]

--- istl/indexset.hh
+++ istl/indexset.hh
@@ -886,10 +886,13 @@ namespace Dune
     : indexSet_(indexset), size_(size),
       indices_(size_, static_cast<const IndexPair*>(0))
   {
+    assert(size>=indexset.size());
     const_iterator end_ = indexSet_.end();
     size_t i=0;
-    for(const_iterator pair = indexSet_.begin(); pair!=end_; ++pair, ++i)
+    for(const_iterator pair = indexSet_.begin(); pair!=end_; ++pair, ++i) {
+      assert(pair->local()<size_);
       indices_[pair->local()] = &(*pair);
+    }
   }
 
   template<class I>
