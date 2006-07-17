b49e31ae7aa670bee44c77314106f37e1719b24a
Markus Blatt <mblatt@dune-project.org>
2006-07-17 13:53:08 +0000

    Fixed Typo!
    [[Imported from SVN: r4603]]

--- common/singleton.hh
+++ common/singleton.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_SINGLETON_HH
-#define DUEN_DINGLETON_HH
+#define DUNE_SINGLETON_HH
 /**
  * @file
  * @brief Usefull wrapper for creating singletons.
