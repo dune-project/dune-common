3f492a9ba9a6b6ac73b9c57a921d971040ada607
Markus Blatt <mblatt@dune-project.org>
2006-07-21 13:57:40 +0000

    More elabrate documentation.
    [[Imported from SVN: r4625]]

--- common/mpihelper.hh
+++ common/mpihelper.hh
@@ -9,7 +9,50 @@
 
 namespace Dune
 {
-
+  /**
+   * @file
+   * @brief This file contains helpers for dealing with MPI.
+   *
+   * Basically there are two helpers available:
+   * <dl>
+   *   <dt>FakeMPIHelper</dt>
+   *   <dd>A class adhering to the interface of MPIHelper
+   *       that does not need MPI at all. This can be used
+   *       to create a sequential program even if MPI is
+   *       used to compile it.
+   *   </dd>
+   *   <dt>MPIHelper</dt>
+   *   <dd>A real MPI helper. When the singleton
+   *       gets instantiated MPI_Init will be
+   *       called and before the program exits
+   *       MPI_Finalize will be called.
+   *   </dd>
+   * </dl>
+   *
+   * Example of who to use these classes:
+   *
+   *   A program that is parallel if compiled with MPI
+   *   and sequential otherwise:
+   *   \code
+   *   int main(int argc, char** argv){
+   *      typedef Dune::MPIHelper MPIHelper;
+   *      MPIHelper::instance(&argc, &argv);
+   *      typename MPIHelper::MPICommunicator world =
+   *        MPIHelper::getCommunicator();
+   *      ...
+   *   \endcode
+   *
+   * If one wants to have sequential program even if the code is
+   * compiled with mpi then one simply has to exchange the typedef
+   * with \code typedef Dune::MPIHelper FakeMPIHelper; \endcode.
+   */
+  /**
+   * @brief A fake mpi helper.
+   *
+   * This helper can be used if no MPI is available
+   * or one wants to run sequentially even if MPI is
+   * available and used.
+   */
   class FakeMPIHelper
   {
   public:
@@ -66,11 +109,31 @@ namespace Dune
      */
     typedef int MPICommunicator;
 
+    /**
+     * @brief Get the default communicator.
+     *
+     * @return -1 As we are fake.
+     */
     static MPICommunicator getCommunicator()
     {
       return -1;
     }
 
+    /**
+     * @brief Get the singleton instance of the helper.
+     *
+     * This method has to be called with the same arguments
+     * that the main method of the program was called:
+     * \code
+     * int main(int argc, char** argv){
+     *   MPIHelper::instance(&argc, &argv);
+     *   // program code comes here
+     *   ...
+     * }
+     * \endcode
+     * @param argc The number of arguments provided to main.
+     * @param argv The arguments provided to main.
+     */
     static FakeMPIHelper& instance(int argc, char** argv)
     {
       if(instance_.get() == 0)
@@ -90,7 +153,11 @@ namespace Dune
   FakeMPIHelper::InstancePointer FakeMPIHelper::instance_ = FakeMPIHelper::InstancePointer();
 
 #ifdef HAVE_MPI
-
+  /**
+   * @brief A real mpi helper.
+   *
+   * This helper should be used for parallel programs.
+   */
   class MPIHelper
   {
   public:
@@ -146,10 +213,30 @@ namespace Dune
      */
     typedef MPI_Comm MPICommunicator;
 
+    /**
+     * @brief Get the default communicator.
+     *
+     * @return MPI_COMM_WORLD
+     */
     static MPICommunicator getCommunicator(){
       return MPI_COMM_WORLD;
     }
 
+    /**
+     * @brief Get the singleton instance of the helper.
+     *
+     * This method has to be called with the same arguments
+     * that the main method of the program was called:
+     * \code
+     * int main(int argc, char** argv){
+     *   MPIHelper::instance(&argc, &argv);
+     *   // program code comes here
+     *   ...
+     * }
+     * \endcode
+     * @param argc The number of arguments provided to main.
+     * @param argv The arguments provided to main.
+     */
     static MPIHelper& instance(int& argc, char**& argv)
     {
       if(instance_.get() == 0)
@@ -177,6 +264,7 @@ namespace Dune
 #else
   // We do not have MPI therefore FakeMPIHelper
   // is the MPIHelper
+  /** @brief If no MPI is available FakeMPIHelper becomes the MPIHelper */
 #define MPIHelper FakeMPIHelper
 
 #endif
