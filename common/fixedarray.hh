07a285e2a19e6b1ccaf7c017b7feb4128e6601ff
Oliver Sander <sander@dune-project.org>
2007-04-10 08:50:45 +0000

    Added a class 'array' which provides the same functionality as FixedArray, but conforms to the naming of the tr1 array class.  I would have liked to put this into a separate header, but unfortunately, array.hh exists already.  In a future step, the stl implementation of 'array' will be used when available.
    [[Imported from SVN: r4895]]

--- common/fixedarray.hh
+++ common/fixedarray.hh
@@ -25,6 +25,85 @@ namespace Dune
    *
    */
   template<class T, int N>
+  class array {
+  public:
+
+    //! Remember the storage type
+    typedef T value_type;
+
+    /** \brief Reference to an object */
+    typedef value_type&                             reference;
+
+    /** \brief Const reference to an object */
+    typedef const value_type&                       const_reference;
+
+    /** \brief Iterator type */
+    typedef value_type*                             iterator;
+
+    /** \brief Const iterator type */
+    typedef const value_type*                       const_iterator;
+
+    /** \brief Type used for array indices */
+    typedef std::size_t size_type;
+
+    /** \brief Difference type */
+    typedef std::ptrdiff_t difference_type;
+
+    /** \brief Reverse iterator type */
+    typedef std::reverse_iterator<iterator>         reverse_iterator;
+
+    /** \brief Const reverse iterator type */
+    typedef std::reverse_iterator<const_iterator>   const_reverse_iterator;
+
+    //! Create uninitialized array
+    array () {}
+
+    //! Initialize all components with same size
+    array (const T& t)
+    {
+      for (int i=0; i<N; i++) a[i]=t;
+    }
+
+    /** \brief Return array size */
+    size_type size() const {return N;}
+
+    //! Assign value to all entries
+    array<T,N>& operator= (const T& t)
+    {
+      for (int i=0; i<N; i++) a[i]=t;
+      return (*this);
+    }
+
+    //! Component access
+    reference operator[] (size_type i)
+    {
+      return a[i];
+    }
+
+    //! Const component access
+    const_reference operator[] (size_type i) const
+    {
+      return a[i];
+    }
+
+  protected:
+    T a[(N > 0) ? N : 1];
+  };
+
+  //! Output operator for array
+  template <class T, int N>
+  inline std::ostream& operator<< (std::ostream& s, array<T,N> e)
+  {
+    s << "[";
+    for (int i=0; i<N-1; i++) s << e[i] << ",";
+    s << e[N-1] << "]";
+    return s;
+  }
+
+  /** \brief Simple fixed size array class
+   *  \deprecated Replaced by array
+   */
+  template<class T, int N>
   class FixedArray {
   public:
 
