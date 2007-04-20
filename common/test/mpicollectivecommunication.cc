0db77c2b3851617b6dde716b3c7c5a133b9344fa
Robert Klöfkorn <robertk@dune-project.org>
2007-04-20 13:23:37 +0000

    disable test when MPI is missing.
    [[Imported from SVN: r4919]]

--- common/test/mpicollectivecommunication.cc
+++ common/test/mpicollectivecommunication.cc
@@ -3,7 +3,10 @@
 #include "config.h"
 
 #include <dune/common/mpihelper.hh>
+
+#if HAVE_MPI
 #include <dune/common/mpicollectivecommunication.hh>
+#endif
 
 #include <iostream>
 int main(int argc, char** argv)
@@ -11,6 +14,7 @@ int main(int argc, char** argv)
   typedef Dune::MPIHelper Helper;
   typedef Helper::MPICommunicator MPIComm;
 
+#if HAVE_MPI
   {
     Helper& mpi = Helper::instance(argc, argv);
 
@@ -44,5 +48,8 @@ int main(int argc, char** argv)
   }
 
   std::cout << "We are at the end!"<<std::endl;
+#else
+  std::cout << "WARNING: test Dune::CollectiveCommunication<MPI_Comm> disabled because MPI not available! " << std::endl;
+#endif
   return 0;
 }
