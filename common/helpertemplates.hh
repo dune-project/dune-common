0dcb3578c1ba80c3bdcd79c7e28f867160955d00
Christian Engwer <christi@dune-project.org>
2008-01-06 00:59:04 +0000

    allow multiple usage of --module=
    [[Imported from SVN: r5038]]

--- common/helpertemplates.hh
+++ common/helpertemplates.hh
@@ -41,33 +41,6 @@ template<class T1, class T2, class T3 = T1> struct Can_multiply {
   Can_multiply() { void (*p)(T1,T2,T3) = constraints; }
 };
 
-/**
-    \brief Helper template so that compilation fails if condition is not true.
-
-    If the condition is true a static function yes is available, othewise the
-    only function available is no().
-
-    Example for compile time check whether two types are the same:
-    \code
-    IsTrue<is_same<int,int>::value>::yes(); //
-    IsTrue<is_same<bool,int>::value>::yes(); // false, will trigger a compile time error
-    \endcode
-
-    A test that trigger a compile time error if condition is true:
-    \code
-    IsTrue<condition>::no()
-    \endcode
- */
-template <bool condition>
-struct IsTrue
-{
-  static void no() {};
-};
-
-template <>
-struct IsTrue<true>
-{
-  static void yes() {};
-};
+#include "static_assert.hh"
 
 #endif
