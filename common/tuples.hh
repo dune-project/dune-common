27741ca8dc1cead4af4b5b400d84ced02fc5979b
Peter Bastian <peter@dune-project.org>
2008-04-30 13:53:33 +0000

    const version of get was missing
    [[Imported from SVN: r5186]]

--- common/tuples.hh
+++ common/tuples.hh
@@ -548,6 +548,15 @@ namespace Dune {
     return Element<i>::get(t);
   }
 
+  template<int i, typename T1, typename T2, typename T3, typename T4,
+      typename T5, typename T6, typename T7, typename T8, typename T9>
+  typename TupleAccessTraits<typename tuple_element<i, tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> >::type>
+  ::ConstType
+  get(const tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9>& t)
+  {
+    return Element<i>::get(t);
+  }
+
 #endif
 
 #ifdef HAVE_TR1_TUPLE
