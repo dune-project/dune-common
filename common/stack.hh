e9450f8a728747fce72ed41c55cb2659e64af551
Oliver Sander <sander@dune-project.org>
2008-05-13 16:14:36 +0000

    class FiniteStack has been moved to the header finitestack.hh for consistency.
    [[Imported from SVN: r5193]]

--- common/stack.hh
+++ common/stack.hh
@@ -7,6 +7,11 @@
 #include <dune/common/exceptions.hh>
 #include <dune/common/deprecated.hh>
 
+// Backward compatibility
+#include <dune/common/finitestack.hh>
+
+#warning This header is deprecated.  Use std::stack instead of Dune::Stack.  The class Dune::FiniteStack has been moved to finitestack.hh.
+
 namespace Dune {
 
   /*! \addtogroup Common
@@ -15,9 +20,11 @@ namespace Dune {
 
   /*! \file
 
-     This file implements two stack-classes Stack and FiniteStack. They are
+     This file implements a stack-classes Stack. It is
      mainly used by the grid iterators where exact knowledge of the stack
-     implementation is needed to guarantee efficient execution
+     implementation is needed to guarantee efficient execution.
+
+     \deprecated std::stack is several times faster than this class.
    */
 
   //! Exception thrown by the stack
@@ -100,66 +107,6 @@ namespace Dune {
     T t = *i;
     return t;
   }
-
-
-  /** \brief A stack with static memory allocation
-
-     This class implements a very efficient stack where the maximum
-     depth is known in advance. Note that no error checking is
-     performed!
-
-     \param n Maximum number of stack entries
-   */
-  template<class T, int n>
-  class FiniteStack {
-  public:
-
-    //! Returns true if the stack is empty
-    bool empty () const
-    {
-      return f==0;
-    }
-
-    //! Returns true if the stack is full
-    bool full () const
-    {
-      return f>=n;
-    }
-
-    //! Puts a new object onto the stack
-    void push (const T& t)
-    {
-      s[f++] = t;
-    }
-
-    //! Removes and returns the uppermost object from the stack
-    T pop ()
-    {
-      return s[--f];
-    }
-
-    //! Returns the uppermost object on the stack
-    T top () const
-    {
-      return s[f-1];
-    }
-
-    //! Dynamic stacksize
-    int size () const
-    {
-      return f;
-    }
-
-    //! Makes empty stack
-    FiniteStack ()
-    {
-      f = 0;
-    }
-
-  private:
-    T s[n];
-    int f;
-  };
 
 }
 
