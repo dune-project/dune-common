7fd1208a36ac2550c674326c9f810624040e5552
Oliver Sander <sander@dune-project.org>
2008-10-13 07:50:00 +0000

    minor documentation fix
    [[Imported from SVN: r5311]]

--- common/iteratorfacades.hh
+++ common/iteratorfacades.hh
@@ -282,7 +282,7 @@ namespace Dune
      * Reference dereference() const;
      *
      * // Compare for equality with j
-     * equals(i);
+     * bool equals(j);
      *
      * // position the iterator at the next element.
      * void increment()
