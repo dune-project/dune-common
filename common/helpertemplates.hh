2d63694f3155f0031853fe4d4c0cf7046ddc3a4a
Oliver Sander <sander@dune-project.org>
2008-12-24 14:31:44 +0000

    Helper templates now in namespace Dune.  This fixes FlySpray entry 392.
    [[Imported from SVN: r5399]]

--- common/helpertemplates.hh
+++ common/helpertemplates.hh
@@ -9,36 +9,40 @@
     \brief Compile-time constraint checks from the Stroustrup FAQ
  */
 
-/**
-   Derived_from<A,B>();
+namespace Dune {
 
-   or
+  /**
+     Derived_from<A,B>();
 
-   template<class T> class Container : Derived_from<T,Mybase> {
-    ...
-   };
- */
-template<class T, class B> struct Derived_from {
-  static void constraints(T* p) { B* pb = p; }
-  Derived_from() { void (*p)(T*) = constraints; }
-};
+     or
 
-/** \todo Please doc me! */
-template<class T1, class T2> struct Can_copy {
-  static void constraints(T1 a, T2 b) { T2 c = a; b = a; }
-  Can_copy() { void (*p)(T1,T2) = constraints; }
-};
+     template<class T> class Container : Derived_from<T,Mybase> {
+      ...
+     };
+   */
+  template<class T, class B> struct Derived_from {
+    static void constraints(T* p) { B* pb = p; }
+    Derived_from() { void (*p)(T*) = constraints; }
+  };
 
-/** \todo Please doc me! */
-template<class T1, class T2 = T1> struct Can_compare {
-  static void constraints(T1 a, T2 b) { a==b; a!=b; a<b; }
-  Can_compare() { void (*p)(T1,T2) = constraints; }
-};
+  /** \todo Please doc me! */
+  template<class T1, class T2> struct Can_copy {
+    static void constraints(T1 a, T2 b) { T2 c = a; b = a; }
+    Can_copy() { void (*p)(T1,T2) = constraints; }
+  };
 
-/** \todo Please doc me! */
-template<class T1, class T2, class T3 = T1> struct Can_multiply {
-  static void constraints(T1 a, T2 b, T3 c) { c = a*b; }
-  Can_multiply() { void (*p)(T1,T2,T3) = constraints; }
-};
+  /** \todo Please doc me! */
+  template<class T1, class T2 = T1> struct Can_compare {
+    static void constraints(T1 a, T2 b) { a==b; a!=b; a<b; }
+    Can_compare() { void (*p)(T1,T2) = constraints; }
+  };
+
+  /** \todo Please doc me! */
+  template<class T1, class T2, class T3 = T1> struct Can_multiply {
+    static void constraints(T1 a, T2 b, T3 c) { c = a*b; }
+    Can_multiply() { void (*p)(T1,T2,T3) = constraints; }
+  };
+
+} // namespace Dune
 
 #endif
