f72ad6b3de5a64cf395de2a0cf7534eb79974383
Carsten Gräser <graeser@dune-project.org>
2009-10-08 14:51:59 +0000

    Interfaces is back again - containing only the Cloneable interface. We'll need this for virtual shapefunctions.
    [[Imported from SVN: r5628]]

---
+++ common/interfaces.hh
@@ -0,0 +1,30 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_INTERFACES_HH
+#define DUNE_INTERFACES_HH
+
+/** @file
+   @author Robert Kloefkorn
+   @brief Provides a Interfaces for detection of specific behavior
+ */
+
+namespace Dune {
+
+  //! An interface class for cloneable objects
+  struct Cloneable {
+
+    /** \brief Clones the object
+     * clone needs to be redefined by an implementation class, with the
+     * return type covariantly adapted. Remember to
+     * delete the resulting pointer.
+     */
+    virtual Cloneable* clone() const = 0;
+
+    /** \brief Destructor */
+    virtual ~Cloneable()
+    {}
+
+  };
+
+} // end namespace Dune
+#endif
