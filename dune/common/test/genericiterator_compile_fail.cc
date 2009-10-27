25ba1ebb91e5c53508a63533c1adaabc0863c1e7
Jorrit Fahlke <joe@dune-project.org>
2009-10-27 21:22:41 +0000

    Check that assigning a mutable iterator from a const iterator fails
    [[Imported from SVN: r5672]]

---
+++ dune/common/test/genericiterator_compile_fail.cc
@@ -0,0 +1,19 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <dune/common/test/iteratorfacadetest.hh>
+#include <dune/common/test/iteratortest.hh>
+
+int main(){
+  // Test the TestIterator;
+  typedef TestContainer<double, Dune::BidirectionalIteratorFacade> Container;
+  Container bidicontainer;
+
+  Container::const_iterator cit = bidicontainer.begin();
+  //This should fail since makeing a mutable iterator from a const iterator
+  //discard qualifiers
+  Container::iterator it = cit;
+}
