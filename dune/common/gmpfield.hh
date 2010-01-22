e25630f3d214896c515b914f68a51f2d2bca23d1
Martin Nolte <mnolte@dune-project.org>
2010-01-22 19:18:08 +0000

    if we move gmp.m4 to dune-common, we should also move gmpfield.hh
    [[Imported from SVN: r5853]]

---
+++ dune/common/gmpfield.hh
@@ -0,0 +1,95 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_GMPFIELD_HH
+#define DUNE_GMPFIELD_HH
+
+#include <iostream>
+
+#if HAVE_GMP
+
+#include <gmpxx.h>
+
+namespace Dune
+{
+
+  template< unsigned int precision >
+  class GMPField
+    : public mpf_class
+  {
+    typedef mpf_class Base;
+
+  public:
+    GMPField ()
+      : Base(0,precision)
+    {}
+
+    template< class T >
+    GMPField ( const T &v )
+      : Base( v,precision )
+    {}
+
+    /*
+       GMPField &operator=(const GMPField &other)
+       {
+       Base(*this) = Base(other);
+       return *this;
+       }
+     */
+  };
+
+
+
+  template< unsigned int precision >
+  inline GMPField< precision >
+  operator+ ( const GMPField< precision > &a, const GMPField< precision > &b )
+  {
+    typedef mpf_class F;
+    return ((const F &)a + (const F &)b);
+  }
+
+  template< unsigned int precision >
+  inline GMPField< precision >
+  operator- ( const GMPField< precision > &a, const GMPField< precision > &b )
+  {
+    typedef mpf_class F;
+    return ((const F &)a - (const F &)b);
+  }
+
+  template< unsigned int precision >
+  inline GMPField< precision >
+  operator- ( const GMPField< precision > &a )
+  {
+    typedef mpf_class F;
+    return -((const F &)a);
+  }
+
+  template< unsigned int precision >
+  inline GMPField< precision >
+  operator* ( const GMPField< precision > &a, const GMPField< precision > &b )
+  {
+    typedef mpf_class F;
+    return ((const F &)a * (const F &)b);
+  }
+
+  template< unsigned int precision >
+  inline GMPField< precision >
+  operator/ ( const GMPField< precision > &a, const GMPField< precision > &b )
+  {
+    typedef mpf_class F;
+    return ((const F &)a / (const F &)b);
+  }
+
+
+
+  template< unsigned int precision >
+  inline std::ostream &
+  operator<< ( std::ostream &out, const GMPField< precision > &value )
+  {
+    return out << value.get_d();
+  }
+
+}
+
+#endif // HAVE_GMP
+
+#endif // #ifndef DUNE_MULTIPRECISION_HH
