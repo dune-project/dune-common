d44c57a8964cd9793bf9fb81ea883e217cf13339
Christian Engwer <christi@dune-project.org>
2010-03-24 18:52:36 +0000

    * implementation and test for MPIGuard
    This class detect a thrown exception and communicate to all other
    processes.
    
    [[Imported from SVN: r5943]]

---
+++ dune/common/test/mpiguard.cc
@@ -0,0 +1,89 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+
+#include <dune/common/mpihelper.hh>
+#include <dune/common/mpiguard.hh>
+
+int main(int argc, char** argv)
+{
+  Dune::MPIHelper & mpihelper = Dune::MPIHelper::instance(argc, argv);
+
+  if (mpihelper.rank() == 0)
+    std::cout << "---- default constructor" << std::endl;
+  try
+  {
+    // at the end of this block the guard is destroyed and possible exceptions are communicated
+    {
+      Dune::MPIGuard guard;
+      if (mpihelper.rank() > 0)
+        DUNE_THROW(Dune::Exception, "Fakeproblem on process " << mpihelper.rank());
+      guard.finalize();
+    }
+  }
+  catch (Dune::Exception & e)
+  {
+    std::cout << "Error (rank " << mpihelper.rank() << "): "
+              << e.what() << std::endl;
+  }
+
+  mpihelper.getCollectiveCommunication().barrier();
+  if (mpihelper.rank() == 0)
+    std::cout << "---- guard(MPI_COMM_WORLD)" << std::endl;
+  try
+  {
+    // at the end of this block the guard is destroyed and possible exceptions are communicated
+    {
+      Dune::MPIGuard guard(MPI_COMM_WORLD);
+      if (mpihelper.rank() > 0)
+        DUNE_THROW(Dune::Exception, "Fakeproblem on process " << mpihelper.rank());
+      guard.finalize();
+    }
+  }
+  catch (Dune::Exception & e)
+  {
+    std::cout << "Error (rank " << mpihelper.rank() << "): "
+              << e.what() << std::endl;
+  }
+
+  mpihelper.getCollectiveCommunication().barrier();
+  if (mpihelper.rank() == 0)
+    std::cout << "---- guard(MPIHelper)" << std::endl;
+  try
+  {
+    // at the end of this block the guard is destroyed and possible exceptions are communicated
+    {
+      Dune::MPIGuard guard(mpihelper);
+      if (mpihelper.rank() > 0)
+        DUNE_THROW(Dune::Exception, "Fakeproblem on process " << mpihelper.rank());
+      guard.finalize();
+    }
+  }
+  catch (Dune::Exception & e)
+  {
+    std::cout << "Error (rank " << mpihelper.rank() << "): "
+              << e.what() << std::endl;
+  }
+
+
+  mpihelper.getCollectiveCommunication().barrier();
+  if (mpihelper.rank() == 0)
+    std::cout << "---- manual error" << std::endl;
+  try
+  {
+    // at the end of this block the guard is destroyed and possible exceptions are communicated
+    {
+      Dune::MPIGuard guard;
+      guard.finalize(mpihelper.rank() > 0);
+    }
+  }
+  catch (Dune::Exception & e)
+  {
+    std::cout << "Error (rank " << mpihelper.rank() << "): "
+              << e.what() << std::endl;
+  }
+
+  mpihelper.getCollectiveCommunication().barrier();
+  if (mpihelper.rank() == 0)
+    std::cout << "---- done" << std::endl;
+}
