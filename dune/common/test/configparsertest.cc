5f3d8d648afdc9149d76007361ab77d5786034d8
Jorrit Fahlke <joe@dune-project.org>
2010-05-25 11:39:29 +0000

    [ConfigParser] Make configparsertest print the information of thrown Dune::Exception objects
    [[Imported from SVN: r6010]]

--- dune/common/test/configparsertest.cc
+++ dune/common/test/configparsertest.cc
@@ -2,16 +2,23 @@
 // vi: set et ts=4 sw=2 sts=2:
 #include <config.h>
 
+#include <iostream>
 #include <string>
 
 #include <dune/common/configparser.hh>
+#include <dune/common/exceptions.hh>
 
 int main ()
 {
-  Dune::ConfigParser parameterSet;
+  try {
+    Dune::ConfigParser parameterSet;
 
-  double testDouble      = parameterSet.get<double>("testDouble");
-  int testInt            = parameterSet.get<int>("testInt");
-  std::string testString = parameterSet.get<std::string>("testString");
-
+    double testDouble      = parameterSet.get<double>("testDouble");
+    int testInt            = parameterSet.get<int>("testInt");
+    std::string testString = parameterSet.get<std::string>("testString");
+  }
+  catch(const Dune::Exception& e) {
+    std::cerr << "Exception thrown: " << e << std::endl;
+    throw;
+  }
 }
