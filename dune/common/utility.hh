904999255971c2952043e4fec36a93f2ba2c7f74
Jorrit Fahlke <joe@dune-project.org>
2010-07-30 21:35:48 +0000

    [Dune::Length] Make it work with native tuples (C++0x).
    [[Imported from SVN: r6069]]

--- dune/common/utility.hh
+++ dune/common/utility.hh
@@ -220,14 +220,10 @@ namespace Dune {
    * @brief Helper template to calculate length of a tuple.
    */
   template <class Tuple>
-  struct Length {};
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5,
-      typename T6, typename T7, typename T8, typename T9>
-  struct Length<tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> >
+  struct Length
   {
     enum {
-      value=tuple_size<tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> >::value
+      value=tuple_size<Tuple>::value
     };
   };
 
