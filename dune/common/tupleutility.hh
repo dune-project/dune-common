554becca914dd5bada2f40af400efbd8c559857a
Oliver Sander <sander@dune-project.org>
2010-08-17 15:29:56 +0000

    adapt single-inclusion guard to new filename
    [[Imported from SVN: r6097]]

--- dune/common/tupleutility.hh
+++ dune/common/tupleutility.hh
@@ -1,8 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
-#ifndef DUNE_UTILITY_HH
-#define DUNE_UTILITY_HH
+#ifndef DUNE_TUPLE_UTILITY_HH
+#define DUNE_TUPLE_UTILITY_HH
 
 #include <dune/common/static_assert.hh>
 
