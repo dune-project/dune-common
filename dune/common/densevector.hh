ea6a88f2af7a9649f3d698cee249941e8231eca4
Christian Engwer <christi@dune-project.org>
2010-11-13 13:17:03 +0000

    make operator= inline, use asImp() instead of (*this)
    [[Imported from SVN: r6244]]

--- dune/common/densevector.hh
+++ dune/common/densevector.hh
@@ -252,10 +252,10 @@ namespace Dune {
 
     //===== assignment from scalar
     //! Assignment operator for scalar
-    derived_type& operator= (const value_type& k)
+    inline derived_type& operator= (const value_type& k)
     {
       for (size_type i=0; i<size(); i++)
-        (*this)[i] = k;
+        asImp()[i] = k;
       return asImp();
     }
 
