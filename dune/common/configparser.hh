8f045ea161be5d00adde7b30623399cbec54b2a2
Jorrit Fahlke <joe@dune-project.org>
2011-02-23 19:02:31 +0000

    [ConfigParser] Don't warn about deprecated configparser.hh when compiling configparser.cc.
    Rationale: don't spam the user with warnings he can do nothing about.  He will
    still get warnings when he uses the ConfigParser in is his own programs, and
    we (as developers) will have to remove configparser.cc the same instant we
    remove configparser.hh anyway.
    
    [[Imported from SVN: r6356]]

--- dune/common/configparser.hh
+++ dune/common/configparser.hh
@@ -3,10 +3,12 @@
 #ifndef DUNE_CONFIGPARSER_HH
 #define DUNE_CONFIGPARSER_HH
 
+#ifndef SILENCE_CONFIGPARSER_HH_DEPRECATION
 #warning This file has been deprecated in dune-common 2.1, and will most likely\
   be removed in dune-common 2.2.  In its place, use parametertree.hh to keep\
   hierarchical string-based data, and parametertreeparser.hh to read such data\
   from files and streams.
+#endif // !SILENCE_CONFIGPARSER_HH_DEPRECATION
 
 #include <iostream>
 #include <istream>
