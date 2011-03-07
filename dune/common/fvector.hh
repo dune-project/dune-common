3974e594fa9c85db9f2d793d0837f96c94e25b07
Robert Klöfkorn <robertk@dune-project.org>
2011-03-07 15:17:27 +0000

    I doubt that this ever compiled. DONT COMMIT CODE THAT DOES NOT COMPILE. How difficult is this. I just don't get it.
    [[Imported from SVN: r6384]]

--- dune/common/fvector.hh
+++ dune/common/fvector.hh
@@ -92,7 +92,7 @@ namespace Dune {
     template<class K1, int SIZE1>
     explicit FieldVector (const FieldVector<K1,SIZE1> & x)
     {
-      static_assert(SIZE1 == SIZE, "FieldVector in costructor has wrong size");
+      dune_static_assert(SIZE1 == SIZE, "FieldVector in costructor has wrong size");
       for (size_type i = 0; i<SIZE; i++)
         _data[i] = x[i];
     }
