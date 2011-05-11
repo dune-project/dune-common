0882e092196846d08276c9fc57fda677bb1f8e04
Rebecca Neumann <rebecca@dune-project.org>
2011-05-11 13:19:08 +0000

    use right process ids, otherwise the new remote index sets are not consistent
    [[Imported from SVN: r6440]]

--- dune/common/parallel/indicessyncer.hh
+++ dune/common/parallel/indicessyncer.hh
@@ -1143,7 +1143,7 @@ namespace Dune
           }
 
         }else{
-          sourceAttributeList.push_back(std::make_pair(source,Attribute(attribute)));
+          sourceAttributeList.push_back(std::make_pair(process,Attribute(attribute)));
         }
       }
       assert(foundSelf);
