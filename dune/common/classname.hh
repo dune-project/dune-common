cdbe0ce3083eb9c34422b917e8dd6616a9e6c6db
Oliver Sander <sander@dune-project.org>
2011-11-24 06:09:20 +0000

    Merge patch 6402 from the trunk.  Original message:
    .------------------------------------------------------------------------
    r6402 | mnolte | 2011-03-22 15:39:56 +0100 (Di, 22. Mär 2011) | 2 Zeilen
    
    don't leak memory (the demangled asciiz string has to be freed!)
    
    
    This fixes FS 982.
    
    [[Imported from SVN: r6531]]

--- dune/common/classname.hh
+++ dune/common/classname.hh
@@ -8,38 +8,50 @@
  *        of a given object or type as a string
  */
 
+#include <cstdlib>
 #include <string>
 #include <typeinfo>
 
 #ifdef __GNUC__
 #include <cxxabi.h>
-#endif
+#endif // #ifdef __GNUC__
 
 namespace Dune {
 
   /** \brief Provide the demangled class name of a given object as a string */
   template <class T>
-  std::string className(T &t)
+  std::string className ( T &t )
   {
+    std::string className = typeid( t ).name();
 #ifdef __GNUC__
     int status;
-    return abi::__cxa_demangle(typeid(t).name(),0,0,&status);
-#else
-    return typeid(t).name();
-#endif
+    char *demangled = abi::__cxa_demangle( className.c_str(), 0, 0, &status );
+    if( demangled )
+    {
+      className = demangled;
+      std::free( demangled );
+    }
+#endif // #ifdef __GNUC__
+    return className;
   };
 
   /** \brief Provide the demangled class name of a type T as a string */
   template <class T>
-  std::string className()
+  std::string className ()
   {
+    std::string className = typeid( T ).name();
 #ifdef __GNUC__
     int status;
-    return abi::__cxa_demangle(typeid(T).name(),0,0,&status);
-#else
-    return typeid(T).name();
-#endif
+    char *demangled = abi::__cxa_demangle( className.c_str(), 0, 0, &status );
+    if( demangled )
+    {
+      className = demangled;
+      std::free( demangled );
+    }
+#endif // #ifdef __GNUC__
+    return className;
   };
 
-}
+} // namespace Dune
+
 #endif  // DUNE_CLASSNAME_HH
