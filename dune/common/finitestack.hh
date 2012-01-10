cf9be5002f545c3c3ac25049bbd2e25451558a81
Oliver Sander <sander@dune-project.org>
2012-01-10 10:34:36 +0000

    Deprecate the file finitestack.hh
    [[Imported from SVN: r6546]]

--- dune/common/finitestack.hh
+++ dune/common/finitestack.hh
@@ -3,6 +3,9 @@
 #ifndef DUNE_FINITE_STACK_HH
 #define DUNE_FINITE_STACK_HH
 
+#warning This file is deprecated and will be removed after the release of dune-common-2.2.\
+  Please use std::stack<Dune::ReservedVector> instead of FiniteStack.
+
 #include <stack>
 
 #include <dune/common/exceptions.hh>
