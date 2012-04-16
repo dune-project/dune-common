71250e7f690e176a6dfa1dcd35b66b6dd248c118
Markus Blatt <mblatt@dune-project.org>
2012-04-16 13:23:13 +0000

    Rudimentarily build the doxygen documentation.
    [[Imported from SVN: r6623]]

--- cmake/modules/DUNEDoxygen.cmake
+++ cmake/modules/DUNEDoxygen.cmake
@@ -1,16 +1,26 @@
 FIND_PACKAGE(Doxygen)
 
+#
+# Set DOT_TRUE for the Doxyfile generation.
+#
 if (NOT DOXYGEN_DOT_FOUND)
   set(DOT_TRUE '\#')
 endif (NOT DOXYGEN_DOT_FOUND)
 
-message('${DOT_TRUE}')
 #
-# prepare_doyfile()
+# prepare_doxyfile()
 # This functions adds the necessary routines for the generation of the Doxyfile[.in] files needed to doxygen.
-FUNCTION (prepare_doxyfile)
-    set(make_doxyfile_command ${CMAKE_COMMAND} -D DUNE_MOD_NAME=${DUNE_MOD_NAME} -D DUNE_MOD_VERSION=${DUNE_MOD_VERSION} -D DOXYSTYLE=${CMAKE_CURRENT_SOURCE_DIR}/Doxystyle  -D DOXYLOCAL=${CMAKE_CURRENT_SOURCE_DIR}/Doxylocal -D abs_top_srcdir=${CMAKE_SOURCE_DIR} -D srcdir=${CMAKE_CURRENT_SOURCE_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/scripts/CreateDoxyFile.cmake)
+MACRO (prepare_doxyfile)
+  set(make_doxyfile_command ${CMAKE_COMMAND} -D DOT_TRUE=${DOT_TRUE} -D DUNEWEB_TRUE=\# -D DUNE_MOD_NAME=${DUNE_MOD_NAME} -D DUNE_MOD_VERSION=${DUNE_MOD_VERSION} -D DOXYSTYLE=${CMAKE_CURRENT_SOURCE_DIR}/Doxystyle  -D DOXYLOCAL=${CMAKE_CURRENT_SOURCE_DIR}/Doxylocal -D abs_top_srcdir=${CMAKE_SOURCE_DIR} -D srcdir=${CMAKE_CURRENT_SOURCE_DIR} -D top_srcdir=${CMAKE_SOURCE_DIR} -P ${CMAKE_SOURCE_DIR}/cmake/scripts/CreateDoxyFile.cmake)
   add_custom_command (OUTPUT Doxyfile.in Doxyfile
     COMMAND ${make_doxyfile_command} COMMENT "Creating Doxyfile.in")
   add_custom_target(Doxyfile DEPENDS Doxyfile.in Doxyfile)
-ENDFUNCTION (prepare_doxyfile)
+ENDMACRO (prepare_doxyfile)
+
+MACRO (add_doxygen_target)
+  prepare_doxyfile()
+  add_custom_target(doxygen COMMAND
+    ${DOXYGEN_EXECUTABLE} Doxyfile
+    COMMENT "Running doxygen documentation" DEPENDS Doxyfile )
+  add_dependencies(doc doxygen)
+ENDMACRO (add_doxygen_target)
