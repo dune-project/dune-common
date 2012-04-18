69770655dfeb2cc1fdbcee00fc6bc2f83359c0b0
Christoph Grüninger <gruenich@dune-project.org>
2012-04-18 21:43:58 +0000

    Changed some DUNE_USUED uses - introduced by r6607 - to other, appropriate ways of surpressing a warning.
    [[Imported from SVN: r6631]]

--- dune/common/test/bitsetvectortest.cc
+++ dune/common/test/bitsetvectortest.cc
@@ -172,9 +172,11 @@ void doTest() {
   testConstContainer(cbbf);
 
   // iterator interface
-  ConstReferenceOp<BBF> cop DUNE_UNUSED;
+#ifndef NDEBUG
+  ConstReferenceOp<BBF> cop;
   assert(testIterator(bbf, cop) == 0);
   assert(testIterator(cbbf, cop) == 0);
+#endif
 }
 
 int main()
