9d055b40eabc57dfe0d96f93aae37229c1e16d6a
Markus Blatt <mblatt@dune-project.org>
2012-05-22 19:12:22 +0000

    Fixed path
    [[Imported from SVN: r6754]]

--- cmake/scripts/FindFiles.cmake
+++ cmake/scripts/FindFiles.cmake
@@ -1,6 +1,6 @@
 file(GLOB_RECURSE makefiles RELATIVE ${RELPATH} "CMakeLists.txt")
 foreach(_file ${makefiles})
-  string(REGEX MATCH ".*test" _testdir ${_file})
+  string(REGEX MATCH ".*/test" _testdir ${_file})
   if(_testdir)
     list(APPEND _makefiles ${_testdir})
   endif(_testdir)
