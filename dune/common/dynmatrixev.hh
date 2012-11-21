f2c143138bbe8feddff12dbf49a10a3bf8961052
Markus Blatt <mblatt@dune-project.org>
2012-11-21 16:19:49 +0000

    Capabilities to compute eigenvalues on non-symmetric matrices Code contributed by Arne Morten Kvarving.
    [[Imported from SVN: r7061]]

---
+++ dune/common/dynmatrixev.hh
@@ -0,0 +1,70 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DYNMATRIXEIGENVALUES_HH
+#define DUNE_DYNMATRIXEIGENVALUES_HH
+
+#include "dynmatrix.hh"
+
+namespace Dune {
+
+  namespace DynamicMatrixHelp {
+
+    // defined in fmatrixev_ext.cpp
+    extern void eigenValuesNonsymLapackCall(
+      const char* jobvl, const char* jobvr, const long
+      int* n, double* a, const long int* lda, double* wr, double* wi, double* vl,
+      const long int* ldvl, double* vr, const long int* ldvr, double* work,
+      const long int* lwork, const long int* info);
+
+    template <typename K, class C>
+    static void eigenValuesNonSym(const DynamicMatrix<K>& matrix,
+                                  DynamicVector<C>& eigenValues)
+    {
+      {
+        const long int N = matrix.rows();
+        const char jobvl = 'n';
+        const char jobvr = 'n';
+
+
+        // matrix to put into dgeev
+        double matrixVector[N * N];
+
+        // copy matrix
+        int row = 0;
+        for(int i=0; i<N; ++i)
+        {
+          for(int j=0; j<N; ++j, ++row)
+          {
+            matrixVector[ row ] = matrix[ i ][ j ];
+          }
+        }
+
+        // working memory
+        double eigenR[N];
+        double eigenI[N];
+        double work[3*N];
+
+        // return value information
+        long int info = 0;
+        long int lwork = 3*N;
+
+        // call LAPACK routine (see fmatrixev_ext.cc)
+        eigenValuesNonsymLapackCall(&jobvl, &jobvr, &N, &matrixVector[0], &N,
+                                    &eigenR[0], &eigenI[0], 0, &N, 0, &N, &work[0],
+                                    &lwork, &info);
+
+        if( info != 0 )
+        {
+          std::cerr << "For matrix " << matrix << " eigenvalue calculation failed! " << std::endl;
+          DUNE_THROW(InvalidStateException,"eigenValues: Eigenvalue calculation failed!");
+        }
+        for (int i=0; i<N; ++i)
+          eigenValues[i] = std::complex<double>(eigenR[i], eigenI[i]);
+      }
+    }
+
+  }
+
+}
+
+#endif
--- dune/common/fmatrixev.hh
+++ dune/common/fmatrixev.hh
@@ -30,6 +30,12 @@ namespace Dune {
       int* n, double* a, const long int* lda, double* w,
       double* work, const long int* lwork, long int* info);
 
+    extern void eigenValuesNonsymLapackCall(
+      const char* jobvl, const char* jobvr, const long
+      int* n, double* a, const long int* lda, double* wr, double* wi, double* vl,
+      const long int* ldvl, double* vr, const long int* ldvr, double* work,
+      const long int* lwork, const long int* info);
+
     /** \brief calculates the eigenvalues of a symetric field matrix
         \param[in]  matrix matrix eigenvalues are calculated for
         \param[out] eigenvalues FieldVector that contains eigenvalues in
@@ -120,6 +126,58 @@ namespace Dune {
           DUNE_THROW(InvalidStateException,"eigenValues: Eigenvalue calculation failed!");
         }
       }
+    }
+
+    template <int dim, typename K, class C>
+    static void eigenValuesNonSym(const FieldMatrix<K, dim, dim>& matrix,
+                                  FieldVector<C, dim>& eigenValues)
+    {
+      {
+        const long int N = dim ;
+        const char jobvl = 'n';
+        const char jobvr = 'n';
+
+        // length of matrix vector
+        const long int w = N * N ;
+
+        // matrix to put into dgeev
+        double matrixVector[dim * dim];
+
+        // copy matrix
+        int row = 0;
+        for(int i=0; i<dim; ++i)
+        {
+          for(int j=0; j<dim; ++j, ++row)
+          {
+            matrixVector[ row ] = matrix[ i ][ j ];
+          }
+        }
+
+        // working memory
+        double eigenR[dim];
+        double eigenI[dim];
+        double work[3*dim];
+
+        // return value information
+        long int info = 0;
+        long int lwork = 3*dim;
+
+        // call LAPACK routine (see fmatrixev_ext.cc)
+        eigenValuesNonsymLapackCall(&jobvl, &jobvr, &N, &matrixVector[0], &N,
+                                    &eigenR[0], &eigenI[0], 0, &N, 0, &N, &work[0],
+                                    &lwork, &info);
+
+        if( info != 0 )
+        {
+          std::cerr << "For matrix " << matrix << " eigenvalue calculation failed! " << std::endl;
+          DUNE_THROW(InvalidStateException,"eigenValues: Eigenvalue calculation failed!");
+        }
+        for (int i=0; i<N; ++i) {
+          eigenValues[i].real = eigenR[i];
+          eigenValues[i].imag = eigenI[i];
+        }
+      }
+
     }
 
   } // end namespace FMatrixHelp
