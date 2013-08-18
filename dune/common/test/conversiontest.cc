48b6d5d9889ce165072a8d0b1eb7384376baf6e9
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-08-18 23:00:05 +0200

    config.h was not included

--- dune/common/test/conversiontest.cc
+++ dune/common/test/conversiontest.cc
@@ -1,5 +1,9 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <iostream>
 #include <dune/common/typetraits.hh>
 
