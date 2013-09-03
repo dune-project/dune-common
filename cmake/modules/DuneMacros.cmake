dde56b314d50e7599b15b6989dcb81d8c9a83d7a
Christoph Grüninger <gruenich@dune-project.org>
2013-09-03 23:41:08 +0200

    [CMake] Get rid of warnings related to CMP0019: skip include/link variable re-expansion
    There is no obvious reason to quote one variable and not the other.
    If more CMP0019 warnings are found, they have to be fixed in the same
    way.
    This was only an issue with CMake 2.8.11 or newer.

--- cmake/modules/DuneMacros.cmake
+++ cmake/modules/DuneMacros.cmake
@@ -436,7 +436,7 @@ macro(dune_project)
   include_directories("${CMAKE_SOURCE_DIR}")
   link_directories("${CMAKE_SOURCE_DIR}/lib")
   include_directories("${CMAKE_CURRENT_BINARY_DIR}")
-  include_directories("/\${CMAKE_CURRENT_SOURCE_DIR}")
+  include_directories("${CMAKE_CURRENT_SOURCE_DIR}")
   add_definitions(-DHAVE_CONFIG_H)
 
   # Search for MPI and set the relevant variables.
