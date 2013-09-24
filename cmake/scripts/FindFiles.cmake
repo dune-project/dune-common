7664adbc26da9c3b0ed872b6e7f5fce82c7d5ee5
Christoph Grüninger <gruenich@dune-project.org>
2013-09-24 14:30:29 +0200

    [CMake] Look for folders "test", not "test*".
    Before this commit it matched e.g. "test1" and failed finding the directory "test".

--- cmake/scripts/FindFiles.cmake
+++ cmake/scripts/FindFiles.cmake
@@ -2,7 +2,7 @@
 cmake_policy(SET CMP0009 NEW)
 file(GLOB_RECURSE makefiles RELATIVE ${RELPATH} "CMakeLists.txt")
 foreach(_file ${makefiles})
-  string(REGEX MATCH ".*/test" _testdir ${_file})
+  string(REGEX MATCH ".*/test$" _testdir ${_file})
   if(_testdir)
     list(APPEND _makefiles ${_testdir})
   endif(_testdir)
