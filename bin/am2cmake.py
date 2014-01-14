d9c0917d6d9fff4b8019136322c335a63265f0da
Markus Blatt <markus@dr-blatt.de>
2014-01-14 11:34:16 +0100

    [am2cmake,release] Add directives for CMake package configuration with autotools during conversion

--- bin/am2cmake.py
+++ bin/am2cmake.py
@@ -800,6 +800,12 @@ def am_2_cmake_dir(directory):
         output.write('\n'.join(['# Create Doxyfile.in and Doxyfile, and doxygen documentation',
 'add_doxygen_target()']))
         output.close()
+    # Add directives to create CMake packe configuration files with autotools
+    output=open("Makefile.am'", "a")
+    output.write('\n'.join(['', '# Generate package configuration files for finding',
+                            '# installed modules with CMake',
+                            'include $(top_srcdir)/am/cmake-pkg-config\n']))
+    output.close()
 
 def main():
     usage = "usage: am2cmake "
