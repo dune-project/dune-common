15d783e1f5da0da949e6d2f4eb315f87be6fcef3
Markus Blatt <markus@dr-blatt.de>
2014-04-28 11:32:54 +0200

    [cmake,bugfix] Fixes adding ParMETIS flags
    Previously, parmetis flags were not added as we used PARMETIS_COMPILE_FLAGS
    which does not exist. This patch now sets the properties COMPILE_DEFINITIONS
    and INCLUDE_DIRECTORIES for the targets and uses the correct values.
    
    (cherry picked from commit 7f5eb715437436d324e911934c95b2e386260c1c)
    Signed-off-by: Markus Blatt <markus@dr-blatt.de>

--- cmake/modules/AddParMETISFlags.cmake
+++ cmake/modules/AddParMETISFlags.cmake
@@ -10,10 +10,14 @@ function(add_dune_parmetis_flags _targets)
   if(PARMETIS_FOUND)
     foreach(_target ${_targets})
       target_link_libraries(${_target} ${PARMETIS_LIBRARY} ${METIS_LIBRARY})
-      GET_TARGET_PROPERTY(_props ${_target} COMPILE_FLAGS)
+      GET_TARGET_PROPERTY(_props ${_target} INCLUDE_DIRECTORIES)
       string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
-      SET_TARGET_PROPERTIES(${_target} PROPERTIES COMPILE_FLAGS
-        "${_props} ${PARMETIS_COMPILE_FLAGS} -DENABLE_PARMETS=1")
+      SET_TARGET_PROPERTIES(${_target} PROPERTIES INCLUDE_DIRECTORIES
+        "${_props};${PARMETIS_INCLUDE_DIRS}")
+      GET_TARGET_PROPERTY(_props ${_target} COMPILE_DEFINITIONS)
+      string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
+      SET_TARGET_PROPERTIES(${_target} PROPERTIES COMPILE_DEFINITIONS
+        "${_props};ENABLE_PARMETIS")
     endforeach(_target ${_targets})
     add_dune_mpi_flags(${_targets})
   endif(PARMETIS_FOUND)
