9d441c81886ae3c930d2355f2693874fdbf83673
Markus Blatt <markus@dr-blatt.de>
2014-08-26 12:39:20 +0200

    [bugfix] include memory header to make make_shared available.
    According to the standard make_shared is defined in the memory header.
    While we did include the header when testing for make_shared support, we
    forgot to include it in dune/common/shared_ptr.hh. This patch now uses the
    same include statement for memory in dune/common/shared_ptr.h as in the
    m4 test.
    
    This problem does only exist in the release as Christoph already fixed in the
    trunk while stripping our own make_shared implementation.
    
    Kudos to Tatiana and Eike for finding this bug!

--- dune/common/shared_ptr.hh
+++ dune/common/shared_ptr.hh
@@ -8,6 +8,12 @@
 #if defined SHARED_PTR_HEADER
 # include SHARED_PTR_HEADER
 #endif
+#if defined(HAVE_MEMORY)
+# include <memory>
+#endif
+#if defined(HAVE_TR1_MEMORY)
+# include <tr1/memory>
+#endif
 #if defined HAVE_BOOST_SHARED_PTR_HPP
 #if defined HAVE_BOOST_MAKE_SHARED_HPP
 # include <boost/make_shared.hpp>
