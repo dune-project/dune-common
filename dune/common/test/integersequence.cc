e469a2fb87566ad64ac962f55faa3a204c2265d1
Christoph Gersbacher <christoph.gersbacher@mathematik.uni-freiburg.de>
2014-09-24 16:35:24 +0200

    [integer sequence] implement backwards compatible version of integer_sequence
    C++14 will introduce integer/index sequences in the header <utility>.
    This patch introduces a backwards compatible implementation similar in
    use. However, the implementation differs in the following technical
    details:
    
      - index_sequence is not a template alias for integer_sequence but
      inherits from it
    
      - make_{integer, index}_sequence are not a template aliases but free
      standing functions
    
    Both differences stem from the absence of template aliases introduced in
    GCC aa late as gcc-4.7. This implementation will work in gcc-4.4 and
    later.

---
+++ dune/common/test/integersequence.cc
@@ -0,0 +1,34 @@
+#include <config.h>
+
+#include <array>
+#include <iostream>
+#include <tuple>
+
+#include <dune/common/classname.hh>
+#include <dune/common/std/utility.hh>
+#include <dune/common/tuples.hh>
+
+template< class T, std::size_t... Ints >
+std::tuple< typename std::tuple_element< Ints, std::array< T, sizeof...( Ints ) > >::type... >
+array_to_tuple_impl ( const std::array< T, sizeof...( Ints ) > &array, Dune::Std::index_sequence< Ints... > )
+{
+  auto tuple = std::make_tuple( array[ Ints ]... );
+  return std::move( tuple );
+}
+
+template< class T, std::size_t N >
+auto array_to_tuple ( const std::array< T, N > &array )
+  -> decltype( array_to_tuple_impl( array, Dune::Std::make_index_sequence< N >() ) )
+{
+  return array_to_tuple_impl( array, Dune::Std::make_index_sequence< N >() );
+}
+
+int main ( int argc, char **argv )
+{
+  using Dune::operator<<;
+  std::array< int, 4 > array{{ 1, 2, 3, 4 }};
+
+  auto tuple = array_to_tuple_impl( array, Dune::Std::make_index_sequence< 4 >() );
+  std::cout << Dune::className( tuple ) << std::endl;
+  std::cout << tuple << std::endl;
+}
