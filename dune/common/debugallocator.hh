4441ec265c40e3cc38ba92df2206e096a3e44c4f
Christoph Grüninger <gruenich@dune-project.org>
2014-12-02 06:48:03 +0100

    Revert "[C++11] Follow the C++11 specification of operator new."
    This reverts commit d26e5ed363f29223d47791958afcfe157559d895.
    We have to wait until our minimal required compiler is GCC 4.7
    or newer.
    Thanks to Carsten for the heads-up.

--- dune/common/debugallocator.hh
+++ dune/common/debugallocator.hh
@@ -294,7 +294,7 @@ namespace Dune
 }
 
 #ifdef DEBUG_NEW_DELETE
-void * operator new(size_t size)
+void * operator new(size_t size) throw(std::bad_alloc)
 {
   // try to allocate size bytes
   void *p = Dune::DebugMemory::alloc_man.allocate<char>(size);
