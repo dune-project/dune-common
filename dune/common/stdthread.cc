92947aaf46d53706e823442597eec70789475b86
Jö Fahlke <jorrit@jorrit.de>
2014-12-18 05:05:51 +0100

    [threads] Introduce DUNE_ASSERT_CALL_ONCE and assertCallOnce().
    These check whether std::call_once() works and provide a helpful error message
    if it does not.  They should be used in any code that uses std::call_once().
    
    Call once may not work if one forgets to _link_ with -pthread (or similar
    options).  The nasty think about this is that linking still succeeds, so this
    can only be detected at run time.  We cannot (in general) run compiled
    programs during configure, since we may be cross-compiling, so whatever we get
    from configure is at best a guess.
    
    Even if configure detects the necessary flags correctly, there may still be
    errors in the build system such that the executable is linked without them.
    
    The reason to provide such a facility is that the bug is quite difficult to
    debug when it appears.  The error message is mangled due to a different bug,
    and is quite unhelpful anyway.  And then there is the fun with weak symbols...

---
+++ dune/common/stdthread.cc
@@ -0,0 +1,75 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <cstdlib>
+#include <iostream>
+#include <mutex>
+#include <ostream>
+
+#include <dune/common/stdthread.hh>
+
+namespace Dune
+{
+
+  namespace {
+
+    void printCallOnceError(const char *file, int line, const char *function,
+                            const char *msg)
+    {
+      if(file)
+        std::cerr << file << ":" << line << ": ";
+      std::cerr << "error: ";
+      if(function)
+        std::cerr << "(in " << function << "()) ";
+      std::cerr << "std::call_once() is broken.\n"
+                << "\n"
+                << msg << std::endl;
+    }
+
+  } // anonymous namespace
+
+
+  void doAssertCallOnce(const char *file, int line, const char *function)
+  {
+    std::once_flag once;
+    bool works = false;
+    try {
+      std::call_once(once, [&]{ works = true; });
+    }
+    catch(...) {
+      printCallOnceError(file, line, function,
+"std::call_once() throws an exception.  This suggests that the program was\n"
+"linked without a threading library.  Common ways to link to a threading\n"
+"libary is to specify one of the following during linking: -pthread, \n"
+"-lpthread, or -pthreads.  The build-system should have tried various of\n"
+"these options, but unfortunately that is only a guess and we cannot verify\n"
+"that we found a working configuration until runtime.\n"
+"\n"
+"Going to rethrow the exception now to give the system library a chance to\n"
+"print more information about it, just in case that helps with debugging.\n"
+                         );
+      throw;
+    }
+    if(!works)
+    {
+      printCallOnceError(file, line, function,
+"std::call_once() never calls the function.  This suggests that your\n"
+"libctdc++ or your gcc built without threading support (--disable-threads,\n"
+"see https://gcc.gnu.org/install/configure.html).  This is probably a bug in\n"
+"__gthread_once() in /usr/include/c++/4.7/x86_64-linux-gnu/bits/gthr-single.h\n"
+"(which should not silently return success without doing anything, but\n"
+"apparently does so in some versions).\n"
+"\n"
+"To fix the issue, either recompile gcc with a working threading\n"
+"implementation, or file a bug for gthr-single.h, or file a bug at\n"
+"https://dune-project.org/flyspray/ and request a workaround at the dune-side."
+                         );
+      std::abort();
+    }
+  }
+
+} // namespace Dune
---
+++ dune/common/stdthread.hh
@@ -0,0 +1,54 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_COMMON_STDTHREAD_HH
+#define DUNE_COMMON_STDTHREAD_HH
+
+#include <dune/common/unused.hh>
+
+namespace Dune
+{
+
+  // used internally by assertCallOnce for the actual check
+  void doAssertCallOnce(const char *file, int line, const char *function);
+
+  //! \brief Make sure call_once() works and provide a helpful error message
+  //!        otherwise.
+  /**
+   * For call_once() to work, certain versions of libstdc++ need to be
+   * _linked_ with -pthread or similar flags.  If that is not the case,
+   * call_once() will throw an exception.  This function checks that
+   * call_once() can indeed be used, i.e. that it does not throw an exception
+   * when it should not, and that the code does indeed get executed.  If
+   * call_once() cannot be used, assertCallOnce() aborts the program with a
+   * helpful error message.
+   *
+   * The check is only actually executed the first time assertCallOnce() is
+   * called.
+   *
+   * The arguments \c file and \c line specify the filename and line number
+   * that should appear in the error message.  They are ignored if \c file is
+   * 0.  The argument \c function specifies the name of the function to appear
+   * in the error message.  It is ignored if \c function is 0.
+   */
+
+  inline void assertCallOnce(const char *file = nullptr, int line = -1,
+                             const char *function = nullptr)
+  {
+    // make sure to call this only the first time this function is invoked
+    static const bool DUNE_UNUSED works
+      = (doAssertCallOnce(file, line, function), true);
+  }
+
+  //! \brief Make sure call_once() works and provide a helpful error message
+  //!        otherwise.
+  /**
+   * This calls assertCallOnce() and automatically provides information about
+   * the caller in the error message.
+   */
+#define DUNE_ASSERT_CALL_ONCE()                         \
+  ::Dune::assertCallOnce(__FILE__, __LINE__, __func__)
+
+} // namespace Dune
+
+#endif // DUNE_COMMON_STDTHREAD_HH
