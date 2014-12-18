14cbee7109a9aa569ea7bd37d70383c29cc4b425
Jö Fahlke <jorrit@jorrit.de>
2014-12-18 05:11:05 +0100

    [threads] Put a check for call_once() in the test suite.

---
+++ dune/common/test/calloncetest.cc
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <dune/common/stdthread.hh>
+
+int main() {
+  DUNE_ASSERT_CALL_ONCE();
+}
