9281f7176837d6b7787d9fa90456d20e382bf971
Christian Engwer <christi@dune-project.org>
2015-02-24 18:39:12 +0100

    [parametertree] remove catch throw block from get method to allow exceptions to propagate

--- dune/common/parametertree.hh
+++ dune/common/parametertree.hh
@@ -172,14 +172,7 @@ namespace Dune {
       if(not hasKey(key))
         DUNE_THROW(Dune::RangeError, "Key '" << key
           << "' not found in ParameterTree (prefix " + prefix_ + ")");
-      try {
-        return Parser<T>::parse((*this)[key]);
-      }
-      catch(const RangeError&) {
-        DUNE_THROW(RangeError, "Cannot parse value \"" << (*this)[key]
-          << "\" for key \"" << prefix_ << "." << key
-          << "\" as a " << className<T>());
-      }
+      return Parser<T>::parse((*this)[key]);
     }
 
     /** \brief get value keys
