fd11ee3e266128e06d5f6a6a6884a332b4b8ce7a
Ansgar Burchardt <burchardt@igpm.rwth-aachen.de>
2015-05-05 12:14:58 +0200

    Use variadic templates for tuple in- and output operators.

--- dune/common/tuples.hh
+++ dune/common/tuples.hh
@@ -123,153 +123,19 @@ namespace Dune {
   /**
    * \brief Print a tuple.
    */
-  template<typename T1>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1> & t)
+  template<typename... Ts>
+  inline std::ostream& operator<<(std::ostream& os, const tuple<Ts...>& t)
   {
-    typedef tuple<T1> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2> & t)
-  {
-    typedef tuple<T1,T2> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3> & t)
-  {
-    typedef tuple<T1,T2,T3> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4> & t)
-  {
-    typedef tuple<T1,T2,T3,T4> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5,T6> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5,T6,T7> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5,T6,T7,T8> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8, typename T9>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8, typename T9, typename T10>
-  inline std::ostream& operator<<( std::ostream& os, const tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::put(os, t);
+    return tuple_writer<sizeof...(Ts)>::put(os, t);
   }
 
   /**
    * \brief Read a tuple.
    */
-  template<typename T1>
-  inline std::istream& operator>>( std::istream& is, tuple<T1> & t)
+  template<typename... Ts>
+  inline std::istream& operator>>(std::istream& is, tuple<Ts...>& t)
   {
-    typedef tuple<T1> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2> & t)
-  {
-    typedef tuple<T1,T2> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3> & t)
-  {
-    typedef tuple<T1,T2,T3> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4> & t)
-  {
-    typedef tuple<T1,T2,T3,T4> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8, typename T9>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
-  }
-
-  template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7,
-      typename T8, typename T9, typename T10>
-  inline std::istream& operator>>( std::istream& is, tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> & t)
-  {
-    typedef tuple<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10> TupleT;
-    return tuple_writer<tuple_size<TupleT>::value>::get(is, t);
+    return tuple_writer<sizeof...(Ts)>::get(is, t);
   }
 
   /** }@ */
