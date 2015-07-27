09ec4971abfa99db7ee91b394bc0db1d0241ded3
dedner <a.s.dedner@warwick.ac.uk>
2015-07-27 18:32:02 +0100

    Improve the className function to provide more information

--- dune/common/classname.hh
+++ dune/common/classname.hh
@@ -18,28 +18,12 @@
 
 namespace Dune {
 
-  /** \brief Provide the demangled class name of a given object as a string */
-  template <class T>
-  std::string className ( T &t )
-  {
-    std::string className = typeid( t ).name();
-#if HAVE_CXA_DEMANGLE
-    int status;
-    char *demangled = abi::__cxa_demangle( className.c_str(), 0, 0, &status );
-    if( demangled )
-    {
-      className = demangled;
-      std::free( demangled );
-    }
-#endif // #if HAVE_CXA_DEMANGLE
-    return className;
-  }
-
   /** \brief Provide the demangled class name of a type T as a string */
   template <class T>
   std::string className ()
   {
-    std::string className = typeid( T ).name();
+    typedef typename std::remove_reference<T>::type TR;
+    std::string className = typeid( TR ).name();
 #if HAVE_CXA_DEMANGLE
     int status;
     char *demangled = abi::__cxa_demangle( className.c_str(), 0, 0, &status );
@@ -49,9 +33,23 @@ namespace Dune {
       std::free( demangled );
     }
 #endif // #if HAVE_CXA_DEMANGLE
+    if (std::is_const<TR>::value)
+        className += " const";
+    if (std::is_volatile<TR>::value)
+        className += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        className += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        className += "&&";
     return className;
   }
 
+  /** \brief Provide the demangled class name of a given object as a string */
+  template <class T>
+  std::string className ( T& t )
+  {
+    return className<T>();
+  }
 } // namespace Dune
 
 #endif  // DUNE_CLASSNAME_HH
