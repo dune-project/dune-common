1ce2e634f26403ff3966319cef13a7592d1e882d
Christian Engwer <christi@dune-project.org>
2015-10-31 13:44:54 +0100

    [parameterizedobject] add base class to factor out handling of default template parameters

--- dune/common/parameterizedobject.hh
+++ dune/common/parameterizedobject.hh
@@ -11,6 +11,73 @@ namespace Dune {
 /** default tag parameter for ParameterizedObjectFactory */
 struct ParameterizedObjectDefaultTag {};
 
+    namespace Impl {
+
+        template<typename Interface,
+                 typename Tag,
+                 typename KeyT,
+                 typename... Args
+                 >
+        class ParameterizedObjectFactoryBase {
+        public:
+            /** @brief The typ of the keys. */
+            typedef KeyT Key;
+            /** @brief The type of the shared pointer to the interface. */
+            typedef Dune::shared_ptr<Interface> Type;
+            /** @brief The type of the function that creates the object. */
+            typedef Type (*Creator)(const Args& ... );
+
+            /**
+             * @brief Creates an object identified by a key from a parameter object.
+             * @param key The key the object is registered with @see define.
+             * @param d The parameter object used for the construction.
+             * @return a shared_pointer to created object.
+             */
+            static
+            Type create(Key const& key, const Args & ... args) {
+                typename Registry::const_iterator i = registry().find(key);
+                if (i == registry().end()) {
+                    DUNE_THROW(Dune::InvalidStateException,
+                        "ParametrizedObjectFactory: key ``" <<
+                        key << "'' not registered");
+                }
+                else return i->second(args...);
+            }
+
+            /**
+             * @brief Registers a new type with a key.
+             *
+             * After registration objects of this type can be cnstructed with the
+             * specified key using the creat function.
+             * @tparam Impl The type. It must implement and subclass Interface.
+             */
+            template<class Impl>
+            static void define (Key const& key)
+            {
+                registry()[key] =
+                    ParameterizedObjectFactoryBase::create_func<Impl>;
+            }
+
+        private:
+
+            template<class Impl>
+            static
+            Type create_func(const Args & ... args) {
+                return Dune::make_shared<Impl>(args...);
+            }
+
+            typedef std::map<Key, Creator> Registry;
+
+            // singleton registry
+            static Registry & registry()
+            {
+                static Registry _registry;
+                return _registry;
+            }
+        };
+
+    } // end namespace Impl
+
 /**
  * @brief A factory class for parameterized objects.
  *
@@ -21,71 +88,17 @@ struct ParameterizedObjectDefaultTag {};
  * allows for easy registration of type with new keys.
  *
  * @tparam Interface The type that describes the interface of the objects.
- * @tparam Param The type of the parameter class used for constructing the objects.
- * @tparam Tag A class tag which allows to have different factories for the same interface
- * @tparam KeyT The type of the objects that are used as keys in the lookup.
+ * @tparam Param The type of the parameter class used for constructing the objects [DEFAULT: Dune::ParameterTree].
+ * @tparam Tag A class tag which allows to have different factories for the same interface [DEFAULT: ParameterizedObjectDefaultTag].
+ * @tparam KeyT The type of the objects that are used as keys in the lookup [DEFAULT: std::string].
  */
 template<class Interface,
          class Param=Dune::ParameterTree,
          class Tag=ParameterizedObjectDefaultTag,
          class KeyT=std::string>
-class ParameterizedObjectFactory {
-public:
-    /** @brief The typ of the keys. */
-    typedef KeyT Key;
-    /** @brief The type of the shared pointer to the interface. */
-    typedef Dune::shared_ptr<Interface> Type;
-    /** @brief The type of the function that creates the object. */
-    typedef Type (*Creator)(const Param &);
+class ParameterizedObjectFactory :
+        public Impl::ParameterizedObjectFactoryBase<Interface,Tag,KeyT,Param> {};
 
-    /**
-     * @brief Creates an object identified by a key from a parameter object.
-     * @param key The key the object is registered with @see define.
-     * @param d The parameter object used for the construction.
-     * @return a shared_pointer to created object.
-     */
-    static
-    Type create(Key const& key, const Param & d) {
-        typename Registry::const_iterator i = registry().find(key);
-        if (i == registry().end()) {
-            DUNE_THROW(Dune::InvalidStateException,
-                "ParametrizedObjectFactory: key ``" <<
-                key << "'' not registered");
-        }
-        else return i->second(d);
-    }
-
-    /**
-     * @brief Registers a new type with a key.
-     *
-     * After registration objects of this type can be cnstructed with the
-     * specified key using the creat function.
-     * @tparam Impl The type. It must implement and subclass Interface.
-     */
-    template<class Impl>
-    static void define (Key const& key)
-    {
-        registry()[key] =
-            ParameterizedObjectFactory<Interface,Param,Tag,KeyT>::create_func<Impl>;
-    }
-
-private:
-
-    template<class Impl>
-    static
-    Type create_func(const Param & d) {
-      return Dune::make_shared<Impl>(d);
-    }
-
-    typedef std::map<Key, Creator> Registry;
-
-    // singleton registry
-    static Registry & registry()
-    {
-        static Registry _registry;
-        return _registry;
-    };
-};
 
 } // end namespace Dune
 
