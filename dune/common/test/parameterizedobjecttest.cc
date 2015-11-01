e2c828084179023e3326fea56bb9dafd6aab0d10
Christian Engwer <christi@dune-project.org>
2015-11-01 01:19:03 +0100

    [test] extend the parameterizedobject test
    we want to allow for constructor signatures with no, one or multiple parameters. 
    The test now checks these different setups.
    
    Further we allow for a default parameter. Currently this is Dune::ParameterTree,
    but I'm not sure, whether this is a good idea or not.

--- dune/common/test/parameterizedobjecttest.cc
+++ dune/common/test/parameterizedobjecttest.cc
@@ -6,7 +6,7 @@
 
 #define DefineImplementation(IF,T,PARAM...)     \
     struct T : public IF {                      \
-        T(PARAM) {}                             \
+        T(PARAM) {}                          \
         virtual std::string info() {            \
             return #T;                          \
         }                                       \
@@ -24,23 +24,45 @@ struct InterfaceB
 
 DefineImplementation(InterfaceA, Ai, int);
 DefineImplementation(InterfaceA, Bi, int);
-DefineImplementation(InterfaceA, Aid, int, double);
-DefineImplementation(InterfaceA, Bid, int, double);
-
-using FactoryA = Dune::ParameterizedObjectFactory<InterfaceA, int>;
-// using FactoryB = Dune::ParameterizedObjectFactory<InterfaceB(int)>;
+DefineImplementation(InterfaceA, Ax);
+DefineImplementation(InterfaceA, Bx);
+DefineImplementation(InterfaceA, Ad, const Dune::ParameterTree&);
+DefineImplementation(InterfaceA, Bd, Dune::ParameterTree);
+DefineImplementation(InterfaceB, Ais, int, std::string);
+DefineImplementation(InterfaceB, Bis, int, std::string);
 
 #define CheckInstance(F,T,PARAM...)             \
-    assert(#T == F::create(#T,PARAM)->info())
+    assert(#T == F::create(#T,##PARAM)->info())
 
 
 int main()
 {
+    // int as parameter
+    using FactoryA = Dune::ParameterizedObjectFactory<InterfaceA(int)>;
     FactoryA::define<Ai>("Ai");
     FactoryA::define<Bi>("Bi");
-
     CheckInstance(FactoryA, Ai, 0);
     CheckInstance(FactoryA, Bi, 1);
-    // CheckInstance(FactoryB, Ai, 0, 2);
-    // CheckInstance(FactoryB, Bi, 1, "Hallo");
+
+    // default parameter
+    using FactoryAd = Dune::ParameterizedObjectFactory<InterfaceA>;
+    FactoryAd::define<Ad>("Ad");
+    FactoryAd::define<Bd>("Bd");
+    Dune::ParameterTree param;
+    CheckInstance(FactoryAd, Ad, param);
+    CheckInstance(FactoryAd, Bd, param);
+
+    // no parameter
+    using FactoryAx = Dune::ParameterizedObjectFactory<InterfaceA()>;
+    FactoryAx::define<Ax>("Ax");
+    FactoryAx::define<Bx>("Bx");
+    CheckInstance(FactoryAx, Ax);
+    CheckInstance(FactoryAx, Bx);
+
+    // multiple parameters
+    using FactoryB = Dune::ParameterizedObjectFactory<InterfaceB(int, std::string)>;
+    FactoryB::define<Ais>("Ais");
+    FactoryB::define<Bis>("Bis");
+    CheckInstance(FactoryB, Ais, 0, std::to_string(2));
+    CheckInstance(FactoryB, Bis, 1, "Hallo");
 }
