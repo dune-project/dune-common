01f8e9f4b544326ebfde645f173753c821c56338
Carsten Gräser <graeser@dune-project.org>
2015-11-13 23:37:39 +0100

    Adjust test to new implementation
    Explicitly select unique_ptr or shared_ptr

--- dune/common/test/parameterizedobjecttest.cc
+++ dune/common/test/parameterizedobjecttest.cc
@@ -3,6 +3,8 @@
 #include <cassert>
 #include <tuple>
 #include <dune/common/parameterizedobject.hh>
+#include <dune/common/parametertree.hh>
+#include <dune/common/shared_ptr.hh>
 
 #define DefineImplementation(IF,T,PARAM...)     \
     struct T : public IF {                      \
@@ -38,29 +40,30 @@ DefineImplementation(InterfaceB, Bis, int, std::string);
 int main()
 {
     // int as parameter
-    Dune::ParameterizedObjectFactory<InterfaceA, std::string, int> FactoryA;
+    Dune::ParameterizedObjectFactory<std::unique_ptr<InterfaceA>, std::string, int> FactoryA;
     FactoryA.define<Ai>("Ai");
     FactoryA.define<Bi>("Bi");
     CheckInstance(FactoryA, Ai, 0);
     CheckInstance(FactoryA, Bi, 1);
 
     // default constructor
-    Dune::ParameterizedObjectFactory<InterfaceA, std::string> FactoryAd;
+    Dune::ParameterizedObjectFactory<std::shared_ptr<InterfaceA>, std::string> FactoryAd;
     FactoryAd.define<Ax>("Ax");
     FactoryAd.define<Bx>("Bx");
     Dune::ParameterTree param;
     CheckInstance(FactoryAd, Ax);
     CheckInstance(FactoryAd, Bx);
+    CheckInstance(FactoryAd, Ai);
 
     // explicitly request the default constructor
-    Dune::ParameterizedObjectFactory<InterfaceA, std::string> FactoryAx;
+    Dune::ParameterizedObjectFactory<std::unique_ptr<InterfaceA>, std::string> FactoryAx;
     FactoryAx.define<Ax>("Ax");
     FactoryAx.define<Bx>("Bx");
     CheckInstance(FactoryAx, Ax);
     CheckInstance(FactoryAx, Bx);
 
     // multiple parameters
-    Dune::ParameterizedObjectFactory<InterfaceB, std::string, int, std::string> FactoryB;
+    Dune::ParameterizedObjectFactory<std::unique_ptr<InterfaceB>, std::string, int, std::string> FactoryB;
     FactoryB.define<Ais>("Ais");
     FactoryB.define<Bis>("Bis");
     CheckInstance(FactoryB, Ais, 0, std::to_string(2));
