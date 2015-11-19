7a4dbe085889f895b663499737d056422061aa7a
Carsten Gräser <graeser@dune-project.org>
2015-11-19 09:47:22 +0100

    [cleanup] Move PriorityTag to typeutilities.hh
    This removed the recently added prioritytag.hh
    header again. Having a separate header for each
    of these trivial helpers will only clutter the
    directory and make people lookup the correct
    header more often.

--- dune/common/typeutilities.hh
+++ dune/common/typeutilities.hh
@@ -3,6 +3,7 @@
 #ifndef DUNE_COMMON_TYPEUTILITIES_HH
 #define DUNE_COMMON_TYPEUTILITIES_HH
 
+#include <cstddef>
 #include <type_traits>
 #include <tuple>
 
@@ -29,6 +30,46 @@ namespace Dune {
   using disableCopyMove = typename std::enable_if<
     (not(std::is_same<This, typename std::tuple_element<0, std::tuple<typename std::decay<T>::type...> >::type >::value)
     and not(std::is_base_of<This, typename std::tuple_element<0, std::tuple<typename std::decay<T>::type...> >::type >::value)), int>::type;
+
+
+
+  /**
+   * \brief Helper class for tagging priorities.
+   *
+   * \ingroup TypeUtilities
+   *
+   * When using multiple overloads of a function
+   * where some are removed from the overload set
+   * via SFINAE, the remaining overloads may be ambiguous.
+   * A prototypic example would be a default overload
+   * that should be used if the others do not apply.
+   *
+   * By adding additional arguments of type PriorityTag<k>
+   * with increasing priority k to all overloads and calling
+   * the method with PriorityTag<m> where m is larger or equal
+   * to the maximal used priority, those can be made unambiguous.
+   *
+   * In this case the matching overload with highest priority
+   * will be used. This is achieved by the fact that PriorityTag<k>
+   * derives from all types PriorityTag<i> with i les than k.
+   *
+   * \tparam priority The priority of this tag.
+   */
+  template<std::size_t priority>
+  struct PriorityTag : public PriorityTag<priority-1>
+  {};
+
+  /**
+   * \brief Helper class for tagging priorities.
+   *
+   * \ingroup TypeUtilities
+   *
+   * PriorityTag<0> does not derive from any
+   * other PriorityTag.
+   */
+  template<>
+  struct PriorityTag<0>
+  {};
 
 
 
