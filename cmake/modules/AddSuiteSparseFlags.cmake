d547922ce6f629b953b2196a2d3b69d9fb91302b
Felix Gruber <felgru@gmx.de>
2015-11-24 09:33:44 +0100

    [cleanup] fix capitalization of some SuiteSparse_ variables
    There were still some variables with the old capitalization SUITESPARSE_
    that prevented my own code to find umfpack.h.

--- cmake/modules/AddSuiteSparseFlags.cmake
+++ cmake/modules/AddSuiteSparseFlags.cmake
@@ -11,13 +11,13 @@
 #
 
 function(add_dune_suitesparse_flags _targets)
-  if(SUITESPARSE_FOUND)
+  if(SuiteSparse_FOUND)
     foreach(_target ${_targets})
-      target_link_libraries(${_target} ${SUITESPARSE_DUNE_LIBRARIES})
+      target_link_libraries(${_target} ${SuiteSparse_DUNE_LIBRARIES})
       get_target_property(_props ${_target} COMPILE_FLAGS)
       string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
       set_target_properties(${_target} PROPERTIES COMPILE_FLAGS
-        "${_props} ${SUITESPARSE_DUNE_COMPILE_FLAGS} -DENABLE_SUITESPARSE=1")
+        "${_props} ${SuiteSparse_DUNE_COMPILE_FLAGS} -DENABLE_SUITESPARSE=1")
     endforeach(_target ${_targets})
-  endif(SUITESPARSE_FOUND)
+  endif(SuiteSparse_FOUND)
 endfunction(add_dune_suitesparse_flags)
