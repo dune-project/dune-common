c9f627eecccf0f3543225e65257e7db26b638258
Elias Pipping <elias.pipping@fu-berlin.de>
2016-01-28 21:21:28 +0100

    Add documentation

--- dune/common/boundschecking.hh
+++ dune/common/boundschecking.hh
@@ -11,6 +11,12 @@
       DUNE_THROW(Dune::RangeError, "Index out of bounds."); \
   } while (false)
 #else
+/**
+   If `DUNE_CHECK_BOUNDS` is defined: check if a condition holds;
+   otherwise, do nothing. Meant to be used for conditions that assure
+   writes and reads do not occur outside of memory limits or
+   pre-defined patterns and related conditions.
+*/
 #define DUNE_ASSERT_BOUNDS(cond)
 #endif
 #endif
