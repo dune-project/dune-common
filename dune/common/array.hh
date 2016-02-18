86e086fef09ac719655babc8f4a3cc3ae7ff16ef
Elias Pipping <elias.pipping@fu-berlin.de>
2016-02-18 18:22:11 +0100

    Move make_array to std/make_array.hh

--- dune/common/array.hh
+++ dune/common/array.hh
@@ -9,8 +9,9 @@
  */
 
 #include <array>
-#include <iostream>
-#include <type_traits>
+#include <ostream>
+
+#include "std/make_array.hh"
 
 namespace Dune
 {
@@ -38,24 +39,7 @@ namespace Dune
     return s;
   }
 
-  //! Create and initialize an array
-  /**
-   * \note This method is a somewhat limited dune-specific version of
-   *       make_array() as proposed for C++17 (see <a
-   *       href="http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4391.html">N4391</a>,
-   *       accepted <a
-   *       href="https://botondballo.wordpress.com/2015/06/05/trip-report-c-standards-meeting-in-lenexa-may-2015/">May
-   *       2015</a>).  The differeces are that this version should never be
-   *       used with expliclitly given template arguments, or with
-   *       std::reference_wrapper<...> arguments, and we do not give a
-   *       diagnostic when anyone happens to do that.
-   */
-  template < typename... Args >
-  std::array<typename std::common_type<Args...>::type, sizeof...(Args)> make_array(const Args&... args)
-  {
-    std::array<typename std::common_type<Args...>::type, sizeof...(Args)> result = { {args...} };
-    return result;
-  }
+  using Dune::Std::make_array;
 
   //! Create an array and fill it with copies of the provided value.
   /**
