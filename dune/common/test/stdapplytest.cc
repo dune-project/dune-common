0145976ff283a8da65754399b2b2e2b59031e480
Carsten Gräser <graeser@dune-project.org>
2016-06-10 22:17:43 +0200

    [std][test] Do a little bit more testing of Dune::Std::apply

--- dune/common/test/stdapplytest.cc
+++ dune/common/test/stdapplytest.cc
@@ -21,18 +21,24 @@ void nop(std::initializer_list<T>&&)
 
 int main()
 {
-  auto args = std::make_tuple(1, 2, 3, "abc");
+  auto args = std::make_tuple(true, 2, 3, "abc");
 
   Dune::TestSuite test;
 
-  std::stringstream stream;
-
-  Dune::Std::apply([&](auto&&... args) {
+  auto concat = [](auto&&... args) {
             bool first = true;
+            std::stringstream stream;
             nop({(stream << (first ? "":",") << args, first = false)...});
-        }, args);
+            return stream.str();
+          };
 
-  test.check(stream.str() == "1,2,3,abc") << "Dune::Std::apply failed";
+  test.check(Dune::Std::apply(concat, args) == "1,2,3,abc") << "Dune::Std::apply failed with concat lambda";
+
+  auto makeTuple = [](auto&&... args) {
+            return std::make_tuple(args...);
+          };
+
+  test.check(Dune::Std::apply(makeTuple, args) == args) << "Dune::Std::apply failed with makeTuple lambda";
 
   return test.exit();
 }
