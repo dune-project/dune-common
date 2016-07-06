4dbe8e237511f655058a9329c9d116dfcec88e14
Christoph Grüninger <gruenich@dune-project.org>
2016-07-06 21:46:35 +0200

    [CMake] Adapt dune_add_latex_document to new UseLATEX
    * Use FORCE_DVI instead of DEFAULT_SAFEPDF
    * FATHER doc and DEFAULT_PDF not needed
    * old arguments are filtered/translated and a
      deprecation warning is emitted

--- cmake/modules/DuneDoc.cmake
+++ cmake/modules/DuneDoc.cmake
@@ -64,7 +64,6 @@ if(NOT IMAGEMAGICK_CONVERT)
   set(LATEX_USABLE FALSE)
 endif()
 if(LATEX_USABLE)
-  set(LATEX_MANGLE_TARGET_NAMES "ON" CACHE INTERNAL "Mangle target names to allow multiple latex documents")
   include(UseLATEX)
 endif()
 
@@ -76,28 +75,48 @@ include(DuneSphinxCMakeDoc)
 include(DuneDoxygen)
 
 macro(dune_add_latex_document tex_file)
-  # We assume that we always generate a PDF file.
-  # If the corresponding pdf file already exists in the source tree
-  # we do not add a rule to build it.
-  string(REGEX REPLACE "(.+).tex" "\\1.pdf" file ${tex_file})
-  string(REGEX REPLACE "/" "_" "${CMAKE_CURRENT_SOURCE_DIR}/${file}" filevar ${file})
-  set(filevar "filevar-NOTFOUND")
-  find_file(filevar ${tex_file} ${CMAKE_CURRENT_SOURCE_DIR})
-  if(filevar)
-    if(LATEX_USABLE)
-      # add rule to create latex document
-      add_latex_document(${tex_file} ${ARGN} MANGLE_TARGET_NAMES)
-    else(LATEX_USABLE)
-      message(WARNING "Not adding rule to create ${file} as LaTeX is not usable!")
-    endif(LATEX_USABLE)
-  else(filevar)
-    # Check for the pdf file
-    set(pdffilevar "pdffilevar-NOTFOUND")
-    find_file(pdffilevar ${file} ${CMAKE_CURRENT_SOURCE_DIR})
-    if(NOT pdffilevar)
-      message(SEND_ERROR "No tex source ${tex_file} and no generated ${file} found!")
-    endif(NOT pdffilevar)
-  endif(filevar)
+  set(latex_arguments "${ARGN}")
+  # replace old DEFAULT_SAFEPDF by FORCE_DVI and emit warning
+  # this compatibility code can be removed after Dune 3.0
+  list(FIND latex_arguments DEFAULT_SAFEPDF position_safepdf)
+  if(NOT position_safepdf EQUAL -1)
+    list(REMOVE_AT latex_arguments ${position_safepdf})
+    list(APPEND latex_arguments FORCE_DVI)
+    message(WARNING "dune_add_latex_document's argument DEFAULT_SAFEPDF is deprecated, use FORCE_DVI instead")
+  endif()
+  # replace old DEFAULT_PDF and emit warning
+  # this compatibility code can be removed after Dune 3.0
+  list(FIND latex_arguments DEFAULT_PDF position_defaultpdf)
+  if(NOT position_defaultpdf EQUAL -1)
+    list(REMOVE_AT latex_arguments ${position_defaultpdf})
+    message(WARNING "dune_add_latex_document's argument DEFAULT_PDF is deprecated, just drop it")
+  endif()
+  # remove old argument FATHER_TARGET which is dropped and handled by an automatism now
+  # this compatibility code can be removed after Dune 3.0
+  list(FIND latex_arguments FATHER_TARGET position_fathertarget)
+  if(NOT position_fathertarget EQUAL -1)
+    list(REMOVE_AT latex_arguments ${position_fathertarget})
+    # father has an according argument trailing which is removed, too
+    list(REMOVE_AT latex_arguments ${position_fathertarget})
+    message(WARNING "dune_add_latex_document's argument FATHER_TARGET <target> is deprecated, this is handled automatically now")
+  endif()
+
+  if(LATEX_USABLE)
+    # add rule to create latex document
+    add_latex_document(${tex_file} ${latex_arguments}
+      EXCLUDE_FROM_ALL
+      EXCLUDE_FROM_DEFAULTS)
+    # add tependency for target doc, but first construct document's target name
+    string(REGEX REPLACE "(.+).tex" "\\1" tex_file_base_name ${tex_file})
+    list(FIND latex_arguments FORCE_DVI has_forcedvi)
+    if(has_forcedvi EQUAL -1)
+      add_dependencies(doc "${tex_file_base_name}")
+    else()
+      add_dependencies(doc "${tex_file_base_name}_safepdf")
+    endif()
+  else()
+    message(WARNING "Not adding rule to create ${file} as LaTeX is not usable!")
+  endif()
 endmacro(dune_add_latex_document tex_file)
 
 # this compatibility code can be removed after Dune 3.0
