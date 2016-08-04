805b6210899c2f7da13463826c2abb8282bdd53f
Robert K <robertk@posteo.org>
2016-08-04 15:33:55 +0200

    [bugfix] fix type decay_t --> decay. Otherwise this file does not compile with gcc 4.9.1.

--- dune/common/typetraits.hh
+++ dune/common/typetraits.hh
@@ -518,7 +518,7 @@ namespace Dune
    * The result is exported by deriving from std::true_type or std::false_type.
    */
   template<class T>
-  struct IsIntegralConstant : public Imp::IsIntegralConstant<std::decay_t<T>>
+  struct IsIntegralConstant : public Imp::IsIntegralConstant<std::decay<T>>
   {};
 
   /** @} */
