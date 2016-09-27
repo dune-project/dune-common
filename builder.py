c3505ae4dc3f5882790e27253547a853c76db3c4
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-09-27 10:55:34 +0200

    use Python logging module instead of plain print statements

--- builder.py
+++ builder.py
@@ -1,19 +1,21 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import importlib
+import logging
 import shlex
 import subprocess
 import sys
 import os
-import timeit
-
-try:
-    from mpi4py import MPI
-except:
-    pass
 
 from dune.common import comm
 import dune.common.module
+
+
+logger = logging.getLogger(__name__)
+
+
+def buffer_to_str(b):
+    return b if sys.version_info.major == 2 else b.decode('utf-8')
 
 
 class Builder:
@@ -22,22 +24,8 @@ class Builder:
         def __init__(self, error):
             Exception.__init__(self,error)
 
-    def __init__(self, force=False, verbose=None):
+    def __init__(self, force=False):
         self.force = force
-
-        if verbose is None:
-            try:
-                verbose = os.environ['DUNE_PY_BUILDER_VERBOSE'].lower()
-                if verbose in ['yes', 'true', '1']:
-                    self.verbose = True
-                elif verbose in ['no', 'false', '0']:
-                    self.verbose = False
-                else:
-                    raise RuntineError('Invalid value for environment variable DUNE_PY_BUILDER_VERBOSE: "' + verbose + '".')
-            except KeyError:
-                self.verbose = False
-        else:
-            self.verbose = verbose
 
         self.build_args = dune.common.module.get_default_build_args()
         self.dune_py_dir = dune.common.module.get_dune_py_dir()
@@ -46,21 +34,17 @@ class Builder:
         dune.__path__.insert(0,os.path.join(self.dune_py_dir, 'python', 'dune'))
 
         if comm.rank == 0:
-            if self.verbose:
-                print("Building dune-py module...")
-                start_time = timeit.default_timer()
+            logger.info('Started building dune-py module')
             foundModule = dune.common.module.make_dune_py_module(self.dune_py_dir)
             try:
                 output = dune.common.module.build_dune_py_module(self.dune_py_dir)
-                if self.verbose:
-                    print(output)
-                    print("Building dune-py module took", (timeit.default_timer() - start_time), "seconds")
+                logger.info('Successfully built dune-py module')
             except:
+                logger.exception('Failed to build dune-py module')
                 if not foundModule:
                     raise
                 else:
-                    if self.verbose:
-                        print("couldn't reconfigure - using existing configuration")
+                    logger.warn('Could not reconfigure dune-py - using existing configuration')
         comm.barrier()
 
 
@@ -70,28 +54,22 @@ class Builder:
                 with open(os.path.join(self.generated_dir, "generated_module.hh"), 'w') as out:
                     out.write(source)
 
-                if classType:
-                    print('generating: ', classType)
-                if self.verbose:
-                    print("Compiling " + moduleName + "...")
-                    start_time = timeit.default_timer()
+                logger.info("Started compiling " + moduleName)
 
                 cmake_args = ["cmake", "--build", self.dune_py_dir, "--target", "generated_module"]
                 if self.build_args is not None:
                     cmake_args += ['--'] + self.build_args
                 cmake = subprocess.Popen(cmake_args, cwd=self.generated_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                cmake.wait()
+                stdout, stderr = cmake.communicate()
+                logger.debug(buffer_to_str(stdout))
                 if cmake.returncode > 0:
-                    if sys.version_info.major == 2:
-                        raise self.CompileError(cmake.stdout.read() + cmake.stderr.read())
-                    else:
-                        raise self.CompileError(cmake.stdout.read().decode('utf-8') + cmake.stderr.read().decode("utf-8"))
+                    logger.error(buffer_to_str(stderr))
+                    raise self.CompileError(buffer_to_str(stderr))
 
                 os.rename(os.path.join(self.generated_dir, "generated_module.so"), os.path.join(self.generated_dir, moduleName + ".so"))
                 os.rename(os.path.join(self.generated_dir, "generated_module.hh"), os.path.join(self.generated_dir, moduleName + ".hh"))
 
-                if self.verbose:
-                    print("Compilation took", (timeit.default_timer() - start_time), "seconds")
+                logger.info('Successfully compiled ' + moduleName)
 
         comm.barrier()
 
