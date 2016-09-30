7ad08dbb6f1e88d379e2f49cfe61a250424f6211
dedner <a.s.dedner@warwick.ac.uk>
2016-09-30 17:33:51 +0100

    add a envorinment variable DUNE_BUILDER_FORCE to force rebuilding

--- __init__.py
+++ __init__.py
@@ -1,3 +1,12 @@
+import os
+
 from .builder import Builder
 
-builder = Builder()
+try:
+    env_force = os.environ['DUNE_FORCE_BUILD'].upper()
+    if env_force == "1" or env_force == "TRUE":
+        builder = Builder(True)
+    else:
+        builder = Builder(False)
+except KeyError:
+    builder = Builder(False)
