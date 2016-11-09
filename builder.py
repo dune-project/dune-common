9ab1744b64b5449ab1955f46ae13f48b18d2d5ff
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2016-11-09 23:37:06 +0000

    improve a bit on the notebook conversion from sphinx (don't fail on error during notebook execution). Also improve error handling in ConfigurationTest

--- builder.py
+++ builder.py
@@ -22,6 +22,10 @@ class Builder:
         '''raise this when there's a problem compiling an extension module'''
         def __init__(self, error):
             Exception.__init__(self,error)
+    class ConfigurationError(Exception):
+        '''raise this when there's a problem with the configuration of dune-py'''
+        def __init__(self, error):
+            Exception.__init__(self,error)
 
     def __init__(self, force=False):
         self.force = force
