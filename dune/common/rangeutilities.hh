67cd868c1655bcf888d80c161b7ddd2b4d95f709
Ansgar Burchardt <ansgar.burchardt@tu-dresden.de>
2016-11-18 12:05:23 +0000

    Merge branch 'cleanup/extra-semicolon' into 'master'
    [cleanup] fix warning: extra ‘;’ [-Wpedantic]
    
    This removes the extra ‘;’ warnings when compiling with -Wpedantic.
    
    See merge request !176

--- dune/common/rangeutilities.hh
+++ dune/common/rangeutilities.hh
@@ -35,11 +35,11 @@ namespace Dune
     for (const auto & e : v)
       m = max(e,m);
     return m;
-  };
+  }
 
   template <typename T,
             typename std::enable_if<!is_range<T>::value, int>::type = 0>
-  const T & max_value(const T & v) { return v; };
+  const T & max_value(const T & v) { return v; }
 
   /**
      \brief compute the minimum value over a range
@@ -55,11 +55,11 @@ namespace Dune
     for (const auto & e : v)
       m = min(e,m);
     return m;
-  };
+  }
 
   template <typename T,
             typename std::enable_if<!is_range<T>::value, int>::type = 0>
-  T & min_value(const T & v) { return v; };
+  T & min_value(const T & v) { return v; }
 
   /**
      \brief similar to std::bitset<N>::any() return true, if any entries is true
@@ -73,11 +73,11 @@ namespace Dune
     for (const auto & e : v)
       b = b or bool(e);
     return b;
-  };
+  }
 
   template <typename T,
             typename std::enable_if<!is_range<T>::value, int>::type = 0>
-  bool any_true(const T & v) { return v; };
+  bool any_true(const T & v) { return v; }
 
   template<std::size_t N>
   bool any_true(const std::bitset<N> & b)
@@ -97,11 +97,11 @@ namespace Dune
     for (const auto & e : v)
       b = b and bool(e);
     return b;
-  };
+  }
 
   template <typename T,
             typename std::enable_if<!is_range<T>::value, int>::type = 0>
-  bool all_true(const T & v) { return v; };
+  bool all_true(const T & v) { return v; }
 
   template<std::size_t N>
   bool all_true(const std::bitset<N> & b)
