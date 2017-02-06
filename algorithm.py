14eed552de71a9a931530f97e93fa38b3cd93fae
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-02-06 11:48:29 +0100

    add helper to load algorithms (i.e., functions) from C++

---
+++ algorithm.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import hashlib
+import numpy
+
+from . import builder
+
+def load(functionName, includes, *args):
+    source = ""
+    if isinstance(includes, str):
+        with open(includes, "r") as include:
+            source += include.read()
+        source += "\n"
+        includes = []
+
+    argTypes = []
+    for arg in args:
+        try:
+            t, i = arg._typeName + " &", arg._includes
+        except AttributeError:
+            if isinstance(arg, int):
+                t, i = "int", []
+            elif isinstance(arg, float):
+                t, i = "double", []
+            elif isinstance(arg, numpy.ndarray):
+                t, i = "pybind11::array", ["dune/corepy/pybind11/numpy.h"]
+            elif callable(arg):
+                t, i = "pybind11::function", ["dune/corepy/pybind11/pybind11.h"]
+            else:
+                raise Exception("Cannot deduce C++ type for the following argument: " + repr(arg))
+        argTypes.append(t)
+        includes += i
+
+    signature = functionName + "( " + ", ".join(argTypes) + " )"
+    moduleName = "algorithm_" + hashlib.md5(signature.encode('utf-8')).hexdigest()
+
+    source += "".join(["#include <" + i + ">\n" for i in includes])
+    source += "\n"
+
+    source += "PYBIND11_PLUGIN( " + moduleName + " )\n"
+    source += "{\n"
+    source += "  pybind11::module module( \"" + moduleName + "\" );\n"
+
+    source += "  module.def( \"execute\", [] ( " + ", ".join([argTypes[i] + " arg" + str(i) for i in range(len(argTypes))]) + " ) {\n"
+    source += "      return " + functionName + "( " + ", ".join(["arg" + str(i) for i in range(len(argTypes))]) + " );\n"
+    source += "    } );\n"
+
+    source += "  return module.ptr();\n"
+    source += "}\n"
+
+    return builder.load(moduleName, source, signature).execute
+
+
+def execute(functionName, includes, *args):
+    load(functionName, includes, *args)(*args)
