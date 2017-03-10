8cde17c4be09afc501d8c29d61c5986247437592
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-03-10 15:23:18 +0100

    [compatibility] add isInteger

--- compatibility.py
+++ compatibility.py
@@ -12,9 +12,15 @@ else:
 if sys.version_info.major == 2:
     def isString(s):
         return isinstance(s, (str, unicode))
+
+    def isInteger(i):
+        return isinstance(i, (int, long))
 else:
     def isString(s):
         return isinstance(s, str)
+
+    def isInteger(i):
+        return isinstance(i, int)
 
 
 if sys.version_info.major == 2:
