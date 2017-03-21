c804884abe35c7c26a30e27e911577e4e6450a36
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2017-03-21 17:21:50 +0100

    Have dune_python_find_package take an alternative interpreter
    This will be needed to check for the existence of pip in the
    created virtualenv.

--- cmake/modules/DunePythonFindPackage.cmake
+++ cmake/modules/DunePythonFindPackage.cmake
@@ -32,6 +32,12 @@
 #
 #       Whether the given version requirement has to be matched exactly.
 #
+#    .. cmake_param:: INTERPRETER
+#       :single:
+#
+#       The python interpreter, whose paths are searched for the package.
+#       Defaults to "${PYTHON_EXECUTABLE}", might differ when dealing with
+#       the configure-time virtualenv set up with :ref:`DUNE_PYTHON_VIRTUALENV_SETUP`.
 #
 #    Find a given python package on the system.
 #
@@ -39,7 +45,7 @@
 function(dune_python_find_package)
   # Parse Arguments
   set(OPTION REQUIRED EXACT)
-  set(SINGLE PACKAGE RESULT VERSION)
+  set(SINGLE PACKAGE RESULT VERSION INTERPRETER)
   set(MULTI)
   include(CMakeParseArguments)
   cmake_parse_arguments(PYPACKAGE "${OPTION}" "${SINGLE}" "${MULTI}" ${ARGN})
@@ -51,12 +57,15 @@ function(dune_python_find_package)
   if(NOT PYPACKAGE_RESULT)
     set(PYPACKAGE_RESULT DUNE_PYTHON_${PYPACKAGE_PACKAGE}_FOUND)
   endif()
+  if(NOT PYPACKAGE_INTERPRETER)
+    set(PYPACKAGE_INTERPRETER "${PYTHON_EXECUTABLE}")
+  endif()
   if(PYPACKAGE_EXACT AND NOT PYPACKAGE_VERSION)
     message(FATAL_ERROR "dune_python_find_package: EXACT given, but no VERSION specified.")
   endif()
 
   # Do the actual check
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import ${PYPACKAGE_PACKAGE}"
+  execute_process(COMMAND "${PYPACKAGE_INTERPRETER}" -c "import ${PYPACKAGE_PACKAGE}"
                   RESULT_VARIABLE PYPACKAGE_RETURN
                   ERROR_QUIET)
 
@@ -71,14 +80,14 @@ function(dune_python_find_package)
     # We cannot use find_package_handle_standard_args for that, as it is too
     # closely tied to using find_package(), which we cannot use for variable package
     # name...
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" "${scriptdir}/pyversion.py" "${PYPACKAGE_PACKAGE}"
+    execute_process(COMMAND "${PYPACKAGE_INTERPRETER}" "${scriptdir}/pyversion.py" "${PYPACKAGE_PACKAGE}"
                     RESULT_VARIABLE retcode
                     OUTPUT_VARIABLE VERSION_STRING
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     OUTPUT_QUIET
                     )
 
-    set(PYPACKAGE_RESULT TRUE)
+    set(${PYPACKAGE_RESULT} TRUE)
     if("${retcode}" STREQUAL "0")
       if(("${VERSION_STRING}" VERSION_LESS "${PYPACKAGE_VERSION}") OR
          (PYPACKAGE_EXACT AND NOT ("${VERSION_STRING}" VERSION_EQUAL "${PYPACKAGE_VERSION}")))
@@ -87,20 +96,20 @@ function(dune_python_find_package)
     else()
       set(VERSION_STRING "unknown version")
       if(PYPACKAGE_VERSION)
-        set(PYPACKAGE_RESULT FALSE)
+        set(${PYPACKAGE_RESULT} FALSE)
       endif()
     endif()
   else()
-    set(PYPACKAGE_RESULT FALSE)
+    set(${PYPACKAGE_RESULT} FALSE)
     if(PYPACKAGE_REQUIRED)
-      message(FATAL_ERROR "The python package ${PYCHECK_PACKAGE} could not be found! (for interpreter ${PYTHON_EXECUTABLE})")
+      message(FATAL_ERROR "The python package ${PYCHECK_PACKAGE} could not be found! (for interpreter ${PYPACKAGE_INTERPRETER})")
     endif()
   endif()
 
   # Set the result variable and print the result
   include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(${PYPACKAGE_PACKAGE}
-                                    "Failed to find the python package ${PYPACKAGE_PACKAGE} with interpreter ${PYTHON_EXECUTABLE}."
+  find_package_handle_standard_args(${PYPACKAGE_PACKAGE}_${PYPACKAGE_INTERPRETER}
+                                    "Failed to find the python package ${PYPACKAGE_PACKAGE} with interpreter ${PYPACKAGE_INTERPRETER}."
                                     ${PYPACKAGE_RESULT}
                                     )
   set(${PYPACKAGE_RESULT} ${${PYPACKAGE_RESULT}} PARENT_SCOPE)
