7fec16a77001e25225e74441efaaf8592b64500e
Jö Fahlke <jorrit@jorrit.de>
2017-03-31 20:16:49 +0200

    [tests] Test suite for (extended) arithmetic types.

---
+++ dune/common/test/arithmetictestsuitetest.cc
@@ -0,0 +1,31 @@
+#include <config.h>
+
+#include <tuple>
+
+#include <dune/common/hybridutilities.hh>
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/test/arithmetictestsuite.hh>
+
+int main(int argc, char **argv)
+{
+  Dune::MPIHelper::instance(argc, argv);
+
+  Dune::ArithmeticTestSuite test;
+
+  using ArithmeticTypes = std::tuple<
+    bool,
+    char, signed char, unsigned char,
+    short, unsigned short,
+    int, unsigned,
+    long, long unsigned,
+    long long, long long unsigned,
+    wchar_t, char16_t, char32_t,
+    float, double, long double>;
+
+  Dune::Hybrid::forEach(ArithmeticTypes(), [&](auto val) {
+      using T = decltype(val);
+      test.checkArithmetic<T, T>();
+    });
+
+  return test.exit();
+}
