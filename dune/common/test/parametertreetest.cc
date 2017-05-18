6bbb33fb3787fd7b51ca7cc78eb707904d5a5e4c
Elias Pipping <elias.pipping@fu-berlin.de>
2017-05-18 23:53:19 +0200

    Allow whitespace in unquoted strings

--- dune/common/test/parametertreetest.cc
+++ dune/common/test/parametertreetest.cc
@@ -289,7 +289,7 @@ int main()
     s << "x1 = 1 # comment\n"
       << "x2 = hallo\n"
       << "x3 = no\n"
-      << "array = \"1   2 3 4 5\t6 7 8\"\n"
+      << "array = 1   2 3 4 5\t6 7 8\n"
       << "\n"
       << "[Foo]\n"
       << "peng = ligapokal\n";
