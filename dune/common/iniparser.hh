8a47e99d07ed35e45cec148686715d9d11e1b308
Elias Pipping <elias.pipping@fu-berlin.de>
2017-05-18 22:11:23 +0200

    Cleanup
    - Use Dune::Exception
    - Use camelCase and be consistent in the usage of Start/Begin

--- dune/common/iniparser.hh
+++ dune/common/iniparser.hh
@@ -1,11 +1,12 @@
 #ifndef DUNE_COMMON_INIPARSER_HH
 #define DUNE_COMMON_INIPARSER_HH
 
-#include <exception>
 #include <fstream>
 #include <string>
 
-class ParsingException : public std::exception {
+#include <dune/common/exceptions.hh>
+
+class ParsingException : public Dune::Exception {
 public:
   ParsingException(std::string line, std::string reason = "") {
     message_ = "Unable to parse line: " + line;
@@ -13,7 +14,7 @@ public:
       message_ += " (" + reason + ")";
   }
 
-  const char *what() const noexcept { return message_.c_str(); }
+  const char *what() const noexcept override { return message_.c_str(); }
 
 private:
   std::string message_;
@@ -31,80 +32,80 @@ template <class Action> void parse(std::istream &instream, Action &&store) {
 
   std::string prefix;
   std::string line;
-  // _start is inclusive, _end is exclusive
+  // Start is inclusive, End is exclusive
   while (std::getline(instream, line)) {
-    auto content_start = line.find_first_not_of(ws);
-    if (content_start == std::string::npos)
+    auto contentStart = line.find_first_not_of(ws);
+    if (contentStart == std::string::npos)
       continue; // Skip lines with nothing but whitespace
 
-    switch (line[content_start]) {
+    switch (line[contentStart]) {
     case '#': // Handle comments
       continue;
     case '[': { // Handle prefixes
-      size_t prefix_start = line.find_first_not_of(ws, content_start + 1);
-      if (prefix_start == std::string::npos)
+      size_t prefixStart = line.find_first_not_of(ws, contentStart + 1);
+      if (prefixStart == std::string::npos)
         throw ParsingException(line);
 
-      size_t prefix_end;
-      size_t content_end;
-      if (line[prefix_start] == ']') {
-        prefix_end = prefix_start; // empty prefix
-        content_end = prefix_end + 1;
+      size_t prefixEnd;
+      size_t contentEnd;
+      if (line[prefixStart] == ']') {
+        prefixEnd = prefixStart; // empty prefix
+        contentEnd = prefixEnd + 1;
       } else {
-        prefix_end =
-            line.find_first_not_of(identifierCharacters, prefix_start + 1);
-        if (prefix_end == std::string::npos)
+        prefixEnd =
+            line.find_first_not_of(identifierCharacters, prefixStart + 1);
+        if (prefixEnd == std::string::npos)
           throw ParsingException(line);
 
-        content_end = line.find_first_not_of(ws, prefix_end);
-        if (content_end == std::string::npos || line[content_end] != ']')
+        contentEnd = line.find_first_not_of(ws, prefixEnd);
+        if (contentEnd == std::string::npos || line[contentEnd] != ']')
           throw ParsingException(line);
         else
-          content_end += 1; // make end exclusive
+          contentEnd += 1; // make end exclusive
       }
 
       // After the content, only comments are allowed
-      size_t trailing_start = line.find_first_not_of(ws, content_end);
-      if (trailing_start != std::string::npos && line[trailing_start] != '#')
+      size_t trailingStart = line.find_first_not_of(ws, contentEnd);
+      if (trailingStart != std::string::npos && line[trailingStart] != '#')
         throw ParsingException(line, "unexpected content after prefix");
 
       // Only set the prefix if the entire line could be parsed.
-      prefix = line.substr(prefix_start, prefix_end - prefix_start);
+      prefix = line.substr(prefixStart, prefixEnd - prefixStart);
       break;
     }
     default: { // Handle anything else (i.e., assignments)
       std::string key, value;
 
-      size_t key_end =
-          line.find_first_not_of(identifierCharacters, content_start + 1);
-      if (key_end == std::string::npos)
+      size_t keyEnd =
+          line.find_first_not_of(identifierCharacters, contentStart + 1);
+      if (keyEnd == std::string::npos)
         throw ParsingException(line);
-      key = line.substr(content_start, key_end - content_start);
+      key = line.substr(contentStart, keyEnd - contentStart);
 
-      size_t equal_pos = line.find_first_not_of(ws, key_end);
+      size_t equal_pos = line.find_first_not_of(ws, keyEnd);
       if (equal_pos == std::string::npos || line[equal_pos] != '=')
         throw ParsingException(line);
 
-      size_t value_start = line.find_first_not_of(ws, equal_pos + 1);
-      if (value_start == std::string::npos)
+      size_t valueStart = line.find_first_not_of(ws, equal_pos + 1);
+      if (valueStart == std::string::npos)
         throw ParsingException(line, "missing value in assignment");
 
-      size_t value_end;
-      switch (line[value_start]) {
+      size_t valueEnd;
+      switch (line[valueStart]) {
       case '#': // Allowing the empty string as a value here.
         value = "";
         // signal that we need no comment handling
-        value_end = std::string::npos;
+        valueEnd = std::string::npos;
         break;
       case '\"':
       case '\'': {
-        char const quote = line[value_start];
-        size_t chunk_begin = value_start;
-        size_t chunk_end;
+        char const quote = line[valueStart];
+        size_t chunkStart = valueStart;
+        size_t chunkEnd;
         while (true) {
-          chunk_end =
-              line.find_first_of(std::string("\\") + quote, chunk_begin + 1);
-          if (chunk_end == std::string::npos) {
+          chunkEnd =
+              line.find_first_of(std::string("\\") + quote, chunkStart + 1);
+          if (chunkEnd == std::string::npos) {
             std::string multiline_comment_line;
             if (!std::getline(instream, multiline_comment_line))
               throw ParsingException(
@@ -113,39 +114,39 @@ template <class Action> void parse(std::istream &instream, Action &&store) {
             line += multiline_comment_line;
             continue;
           }
-          value += line.substr(chunk_begin + 1, chunk_end - (chunk_begin + 1));
+          value += line.substr(chunkStart + 1, chunkEnd - (chunkStart + 1));
 
-          chunk_end += 1; // make end exclusive
-          if (line[chunk_end - 1] == quote)
+          chunkEnd += 1; // make end exclusive
+          if (line[chunkEnd - 1] == quote)
             break;
 
-          if (chunk_end >= line.size())
+          if (chunkEnd >= line.size())
             throw ParsingException(line, "line ended with a single backslash");
 
-          char const escaped_char = line[chunk_end];
+          char const escaped_char = line[chunkEnd];
           if (escaped_char == '\\' || escaped_char == quote)
             value += escaped_char;
           else if (escaped_char == 'n')
             value += '\n';
           else
             throw ParsingException(line, "unexpected escape in quoted string");
-          chunk_begin = chunk_end;
+          chunkStart = chunkEnd;
         }
-        value_end = chunk_end;
+        valueEnd = chunkEnd;
       } break;
       default: { // simple, unquoted string
-        if (simpleStringInvalidCharacter.find(line[value_start]) !=
+        if (simpleStringInvalidCharacter.find(line[valueStart]) !=
             std::string::npos)
           throw ParsingException(line, "invalid character in simple string");
-        value_end = line.find_first_of(ws + simpleStringInvalidCharacter,
-                                       value_start + 1);
-        value = line.substr(value_start, value_end - value_start);
+        valueEnd = line.find_first_of(ws + simpleStringInvalidCharacter,
+                                      valueStart + 1);
+        value = line.substr(valueStart, valueEnd - valueStart);
       }
       }
 
       // After the content, only comments are allowed
-      size_t trailing_start = line.find_first_not_of(ws, value_end);
-      if (trailing_start != std::string::npos && line[trailing_start] != '#')
+      size_t trailingStart = line.find_first_not_of(ws, valueEnd);
+      if (trailingStart != std::string::npos && line[trailingStart] != '#')
         throw ParsingException(line, "unexpected content after assignment");
 
       store(prefix, key, value);
