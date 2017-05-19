f8484a95b8ddc26efd3534b23c34cb823e42a5c5
Elias Pipping <elias.pipping@fu-berlin.de>
2017-05-19 13:57:24 +0200

    Make whitespace part of the identifierWhitelist

--- dune/common/iniparser.hh
+++ dune/common/iniparser.hh
@@ -46,7 +46,7 @@ template <class Action> void parse(std::istream &instream, Action &&store) {
   // Characters that could make up an identifier.
   std::string const identifierWhitelist = "abcdefghijklmnopqrstuvwxyz"
                                           "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                          "0123456789._+-";
+                                          "0123456789._+-" + ws;
   std::string const simpleStringBlacklist = "'\"\\#";
 
   std::string prefix;
@@ -67,7 +67,7 @@ template <class Action> void parse(std::istream &instream, Action &&store) {
         throw ParsingException(line,
                                "declaration of scope not terminated by ']'");
 
-      size_t prefixEnd = line.find_first_not_of(ws + identifierWhitelist, prefixStart);
+      size_t prefixEnd = line.find_first_not_of(identifierWhitelist, prefixStart);
       if (prefixEnd == std::string::npos)
         throw ParsingException(line,
                                "declaration of scope not terminated by ']'");
@@ -89,7 +89,7 @@ template <class Action> void parse(std::istream &instream, Action &&store) {
 
       size_t keyStart = contentStart;
       // intentionally re-read the first character to validate it
-      size_t keyEnd = line.find_first_not_of(identifierWhitelist + ws, keyStart);
+      size_t keyEnd = line.find_first_not_of(identifierWhitelist, keyStart);
       if (keyEnd == std::string::npos)
         throw ParsingException(line, "'=' missing from assignment");
       if (line[keyEnd] != '=')
