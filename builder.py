41b857c187bb1703ec6a5f6007d6e02a97455a09
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-06-16 13:52:32 +0200

    [builder] use CMake for dependency tracking
    Instead of generating one CMake target "generated_module", we now define a
    CMake target for every module create in dune-py. Before loading the module,
    we always call cmake to make sure the module is rebuilt if dependencies change.
    
    Note: Changes in the source code of the generated module remain untracked for
          now.

--- builder.py
+++ builder.py
@@ -36,41 +36,36 @@ class Builder:
         tagfile = os.path.join(self.dune_py_dir, ".noconfigure")
         if not os.path.isfile(tagfile):
             if comm.rank == 0:
-                logger.info('Started building dune-py module')
                 dune.common.module.make_dune_py_module(self.dune_py_dir)
                 output = dune.common.module.build_dune_py_module(self.dune_py_dir)
-                logger.info('Successfully built dune-py module')
             comm.barrier()
         else:
             logger.info('using pre configured dune-py module')
 
-
-    def compile(self, source, target="generated_module" ):
-        with open(os.path.join(self.generated_dir, "generated_module.hh"), 'w') as out:
-            out.write(source)
-
+    def compile(self, target='all'):
         cmake_args = ["cmake", "--build", self.dune_py_dir, "--target", target]
         if self.build_args is not None:
             cmake_args += ['--'] + self.build_args
         cmake = subprocess.Popen(cmake_args, cwd=self.generated_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = cmake.communicate()
         logger.debug(buffer_to_str(stdout))
-        return cmake.returncode, stdout, stderr
+        if cmake.returncode > 0:
+            logger.error(buffer_to_str(stderr))
+            raise self.CompileError(buffer_to_str(stderr))
 
     def load(self, moduleName, source, pythonName):
         if comm.rank == 0:
-            if not os.path.isfile(os.path.join(self.generated_dir, moduleName + ".so")) or self.force:
-                logger.info("Started compiling " + pythonName)
-                returncode, stdout, stderr = self.compile(source)
-                logger.debug(buffer_to_str(stdout))
-                if returncode > 0:
-                    logger.error(buffer_to_str(stderr))
-                    raise self.CompileError(buffer_to_str(stderr))
+            logger.info("Loading " + pythonName)
+            sourceFileName = os.path.join(self.generated_dir, moduleName + ".cc")
+            if not os.path.isfile(sourceFileName):
+                with open(os.path.join(sourceFileName), 'w') as out:
+                    out.write(str(source))
+                with open(os.path.join(self.generated_dir, "CMakeLists.txt"), 'a') as out:
+                    out.write("dune_add_pybind11_module(NAME " + moduleName + " EXCLUDE_FROM_ALL COMPILE_DEFINITIONS USING_COREPY)\n")
+                # update build system
+                self.compile()
 
-                os.rename(os.path.join(self.generated_dir, "generated_module.so"), os.path.join(self.generated_dir, moduleName + ".so"))
-                os.rename(os.path.join(self.generated_dir, "generated_module.hh"), os.path.join(self.generated_dir, moduleName + ".hh"))
-
-                logger.info('Successfully compiled ' + pythonName)
+            self.compile(moduleName)
 
         comm.barrier()
 
