f562b1b5d815215835967fdbbe1b37b68e6b20c6
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2017-07-21 12:19:09 +0200

    Add `makeVirtualFunction` helper
    The `makeVirtualFunction` helper allows to easily convert a lambda
    function (or any function object) to a `VirtualFunction` implementing
    the virtual `evaluate` method:
    
    ```c++
    void doSomething(const VirtualFunction<double, double>& f);
    
    auto f = makeVirtualFunction<double, double>(
        [](double x) { return x*x; });
    doSomething(f);
    ```

--- dune/common/function.hh
+++ dune/common/function.hh
@@ -3,6 +3,7 @@
 #ifndef DUNE_FUNCTION_HH
 #define DUNE_FUNCTION_HH
 
+#include <utility>
 
 #include "typetraits.hh"
 
@@ -58,6 +59,9 @@ namespace Dune {
   /**
    * \brief Virtual base class template for function classes.
    *
+   * \see makeVirtualFunction for a helper to convert lambda functions to
+   * `VirtualFunction` objects.
+   *
    * \tparam DomainType The type of the input variable is 'const DomainType &'
    *
    * \tparam RangeType The type of the output variable is 'RangeType &'
@@ -78,6 +82,58 @@ namespace Dune {
      */
     virtual void evaluate(const typename Traits::DomainType& x, typename Traits::RangeType& y) const = 0;
   }; // end of VirtualFunction class
+
+  namespace Impl {
+
+  template<typename Domain, typename Range, typename F>
+  class LambdaVirtualFunction final
+    : public VirtualFunction<Domain, Range>
+  {
+  public:
+    LambdaVirtualFunction(F&& f)
+      : f_(std::move(f))
+      {}
+
+    LambdaVirtualFunction(const F& f)
+      : f_(f)
+      {}
+
+    void evaluate(const Domain& x, Range& y) const override
+      {
+        y = f_(x);
+      }
+
+  private:
+    const F f_;
+  };
+
+  } /* namespace Impl */
+
+  /**
+   * \brief make `VirtualFunction` out of a function object
+   *
+   * This helper function wraps a function object into a class implementing
+   * the `VirtualFunction` interface.  It allows for easy use of lambda
+   * expressions in places that expect a `VirtualFunction`:
+     \code
+     void doSomething(const VirtualFunction<double, double>& f);
+
+     auto f = makeVirtualFunction<double, double>(
+         [](double x) { return x*x; });
+     doSomething(f);
+     \endcode
+   *
+   * \returns object of a class derived from `VirtualFunction<Domain, Range>`
+   *
+   * \tparam Domain domain of the function
+   * \tparam Range  range of the function
+   */
+  template<typename Domain, typename Range, typename F>
+  Impl::LambdaVirtualFunction< Domain, Range, std::decay_t<F> >
+  makeVirtualFunction(F&& f)
+  {
+    return {std::forward<F>(f)};
+  };
 
   /** @} end documentation */
 
