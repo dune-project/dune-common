6a4a1f776724497d64355dcfdb77b89b810a23b7
Steffen Müthing <muething@dune-project.org>
2017-08-22 17:01:46 +0200

    [backports] Add support for the detection idiom from library TS v2
    This patch adds the detection idiom as proposed in N4502 to
    `Dune::Std`. If there is support in stdlib, that gets pulled in,
    otherwise we provide the (rather straightforward) fallback
    implementation - most of the diff is actually just Doxygen
    documentation for the feature.

--- dune/common/std/type_traits.hh
+++ dune/common/std/type_traits.hh
@@ -7,6 +7,10 @@
 #include <dune/common/typetraits.hh>
 #include <dune/common/typeutilities.hh>
 
+#if __cpp_lib_experimental_detect >= 201505
+#include <experimental/type_traits>
+#endif
+
 namespace Dune
 {
 
@@ -189,11 +193,196 @@ namespace Std
   {};
 
 
+#if __cpp_lib_experimental_detect >= 201505
+
+  using std::experimental::nonesuch;
+  using std::experimental::detected_or;
+  using std::experimental::is_detected;
+  using std::experimental::detected_t;
+  using std::experimental::is_detected_v;
+  using std::experimental::detected_or_t;
+  using std::experimental::is_detected_exact;
+  using std::experimental::is_detected_exact_v;
+  using std::experimental::is_detected_convertible;
+  using std::experimental::is_detected_convertible_v;
+
+#else // __cpp_lib_experimental_detect >= 201505
+
+  // fallback version of std::experimental::is_detected et al., heavily scribbled
+  // from cppreference.com (but there is actually not much implementation to the thing)
+
+#ifndef DOXYGEN
+
+  namespace Impl {
+
+    // default version of detector, this gets matched on failure
+    template<typename Default, typename Void, template<typename...> class Op, typename... Args>
+    struct detector
+    {
+      using value_t = std::false_type;
+      using type = Default;
+    };
+
+    // specialization of detector that matches if Op<Args...> can be instantiated
+    template<typename Default, template<typename...> class Op, typename... Args>
+    struct detector<Default, void_t<Op<Args...>>, Op, Args...>
+    {
+      using value_t = std::true_type;
+      using type = Op<Args...>;
+    };
+
+  }
+
+#endif // DOXYGEN
+
+  //! Type representing a lookup failure by std::detected_or and friends.
+  /**
+   * This type cannot be constructed, destroyed or copied.
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  struct nonesuch
+  {
+    nonesuch() = delete;
+    ~nonesuch() = delete;
+    nonesuch(const nonesuch&) = delete;
+    void operator=(const nonesuch&) = delete;
+  };
+
+  //! Detects whether `Op<Args...>` is valid and makes the result available.
+  /**
+   * This alias template is an alias for an unspecified class type with two
+   * nested `typedefs` `value_t` and `type`. It can be used to detect whether
+   * the meta function call `Op<Args...>` is valid and access the result of
+   * the call by inspecting the returned type, which is defined as follows:
+   *
+   * * If `Op<Args...>` can be instantiated, `value_t` is an alias for `std::true_type`
+   *   and `type` is an alias for `Op<Args...>`.
+   * * If `Op<Args...>` is invalid, `value_t` is an alias for `std::false_type`
+   *   and `type` is an alias for `Default`.
+   *
+   * This can be used to safely extract a nested `typedef` from a type `T` that
+   * might not define the `typedef`:
+     \code
+       struct A { using size_type = int ; };
+       struct B;
+
+       template<typename T>
+       using SizeType = typename T::size_type;
+
+       // this extracts the nested typedef for int
+       using st_a = typename detected_or<std::size_t,SizeType,A>::type;
+       // as there is no nested typedef in B, this yields std::size_t
+       using st_b = typename detected_or<std::size_t,SizeType,B>::type;
+     \endcode
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Default, template<typename...> class Op, typename... Args>
+  using detected_or = Impl::detector<Default,void,Op,Args...>;
+
+  //! Detects whether `Op<Args...>` is valid.
+  /**
+   * This alias template checks whether `Op<Args...>` can be instantiated. It is
+   * equivalent to `typename detected_or<nonesuch,Op,Args...>::value_t`.
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<template<typename...> class Op, typename... Args>
+  using is_detected = typename detected_or<nonesuch,Op,Args...>::value_t;
+
+  //! Detects whether `Op<Args...>` is valid and makes the result available as a value.
+  /**
+   * This constexpr variable checks whether `Op<Args...>` can be instantiated. It is
+   * equivalent to `is_detected<Op,Args...>::value`.
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<template<typename...> class Op, typename... Args>
+  constexpr bool is_detected_v = is_detected<Op,Args...>::value;
+
+  //! Returns `Op<Args...>` if that is valid; otherwise returns nonesuch.
+  /**
+   * This alias template can be used to instantiate `Op<Args...>` in a context that is
+   * not SFINAE-safe by appropriately wrapping the instantiation. If instantiation fails,
+   * the marker type nonesuch is returned instead.
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<template<typename...> class Op, typename... Args>
+  using detected_t = typename detected_or<nonesuch,Op,Args...>::type;
 
 
+  //! Returns `Op<Args...>` if that is valid; otherwise returns the fallback type `Default`.
+  /**
+   * This alias template can be used to instantiate `Op<Args...>` in a context that is
+   * not SFINAE-safe by appropriately wrapping the instantiation and automatically falling back
+   * to `Default` if instantiation fails.
+   *
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Default, template<typename...> class Op, typename... Args>
+  using detected_or_t = typename detected_or<Default,Op,Args...>::type;
 
+  //! Checks whether `Op<Args...>` is `Expected` without causing an error if `Op<Args...>` is invalid.
+  /**
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Expected, template<typename...> class Op, typename... Args>
+  using is_detected_exact = std::is_same<Expected,detected_t<Op,Args...>>;
 
+  //! Convenient access to the result value of is_detected_exact.
+  /**
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Expected, template<typename...> class Op, typename... Args>
+  constexpr bool is_detected_exact_v = is_detected_exact<Expected,Op,Args...>::value;
 
+  //! Checks whether `Op<Args...>` is convertible to `Target` without causing an error if `Op<Args...>` is invalid.
+  /**
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Target, template<typename...> class Op, typename... Args>
+  using is_detected_convertible = std::is_convertible<Target,detected_t<Op,Args...>>;
+
+  //! Convenient access to the result value of is_detected_convertible.
+  /**
+   * \note This functionality is part of the C++ library fundamentals TS v2 and might
+   *       or might not became part of C++2a.
+   *
+   * \ingroup CxxUtilities
+   */
+  template<typename Target, template<typename...> class Op, typename... Args>
+  constexpr bool is_detected_convertible_v = is_detected_convertible<Target,Op,Args...>::value;
+
+#endif // __cpp_lib_experimental_detect >= 201505
 
 } // namespace Std
 
