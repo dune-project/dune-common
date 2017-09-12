8e927217253e1f5b8c855aa1f72599f6433636bb
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2017-09-12 12:58:31 +0200

    use `get(...)` instead of catching `KeyError` exception
    This avoids duplicating the 'false' case.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,8 @@ import os
 
 from .builder import Builder
 
-try:
-    env_force = os.environ['DUNE_FORCE_BUILD'].upper()
-    if env_force == "1" or env_force == "TRUE":
-        builder = Builder(True)
-    else:
-        builder = Builder(False)
-except KeyError:
+env_force = os.environ.get('DUNE_FORCE_BUILD', 'FALSE').upper()
+if env_force in ('1', 'TRUE'):
+    builder = Builder(True)
+else:
     builder = Builder(False)
