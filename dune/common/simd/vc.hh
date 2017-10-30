ef5854d27def6ef4b0a0274d063aecc466a12173
Jö Fahlke <jorrit@jorrit.de>
2017-10-30 23:48:33 +0100

    [SIMD][Vc] Add missing include.

--- dune/common/simd/vc.hh
+++ dune/common/simd/vc.hh
@@ -13,6 +13,7 @@
 #include <dune/common/indices.hh>
 #include <dune/common/simd/base.hh>
 #include <dune/common/simd/defaults.hh> // for anyFalse()
+#include <dune/common/typetraits.hh>
 #include <dune/common/vc.hh>
 
 /** @defgroup SIMDVc SIMD Abstraction Implementation for Vc
