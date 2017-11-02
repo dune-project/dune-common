1578a578ea835f41c1f61ee1e19d95b6c18d76e6
Jö Fahlke <jorrit@jorrit.de>
2017-11-02 11:31:49 +0100

    [array.hh] Deprecate.
    Deprecate the header itself and `fill_array()`.  Can't deprecate `array` and
    `make_array()` since they are using-declarations.

--- dune/common/array.hh
+++ dune/common/array.hh
@@ -4,6 +4,15 @@
 #ifndef DUNE_ARRAY_HH
 #define DUNE_ARRAY_HH
 
+#ifndef DUNE_COMMON_ARRAY_HH_DISABLE_DEPRECATION_WARNING
+
+#warning This header is deprecated.
+#warning Instead of Dune::array, use std::array from <array>.
+#warning Instead of Dune::make_array(), use Dune::Std::make_array() from <dune/common/std/make_array.hh>.
+#warning Instead of Dune::fill_array(), use Dune::filledArray() from <dune/common/filled_array.hh>.
+
+#endif // DUNE_DISABLE_ARRAY_HH_DEPRECATION_WARNING
+
 /** \file
     \brief Fallback implementation of the std::array class (a static array)
  */
@@ -21,16 +30,24 @@ namespace Dune
    */
 
   // pull in default implementation
+  // deprecation does not work:
+  // [[deprecated("Use std::array from <array>")]]
   using std::array;
 
+  // deprecation does not work:
+  // [[deprecated("Use Dune::Std::make_array from <dune/common/std/make_array.hh>")]]
   using Dune::Std::make_array;
 
   //! Create an array and fill it with copies of the provided value.
   /**
    * \note This method is Dune-specific and not part of any C++ standard.
+   *
+   * \deprecated Use Dune::filledArray() from <dune/common/filled_array.hh>.
+   *
    * \ingroup CxxUtilities
    */
   template<typename T, std::size_t n>
+  [[deprecated("Use Dune::filledArray() from <dune/common/filled_array.hh>")]]
   std::array<T,n> fill_array(const T& t)
   {
     std::array<T,n> r;
