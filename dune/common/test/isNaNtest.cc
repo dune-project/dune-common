8b5813ea965b330cb8e457720a633a4b2dded19c
Lukas Renelt <l_rene02@uni-muenster.de>
2017-12-01 13:39:44 +0100

    temp: some basic testing

---
+++ dune/common/test/isNaNtest.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <complex>
+
+#include <dune/common/math.hh>
+
+int main(){
+  int a = 42;
+  const int b = 42;
+  double c = sqrt(-1.0);
+  std::complex<double> d(10.,c);
+  std::cout << std::boolalpha
+            << "isNaN(int): " << Dune::isNaN(a) << "\n"
+            << "isNaN(const int): " << Dune::isNaN(b) << "\n"
+            << "isNaN(42): " << Dune::isNaN(42) << "\n"
+            << "isNaN(NaN): " << Dune::isNaN(c) << "\n"
+            << "isNaN(std::complex<double> with NaN): " << Dune::isNaN(d) << "\n"
+            << "isComplex<std::complex>: " << Dune::Impl::isComplexLike<std::complex<int>>::value  << "\n"
+            << "isComplex<int>: " << Dune::Impl::isComplexLike<int>::value  << "\n"
+            << std::endl;
+}
