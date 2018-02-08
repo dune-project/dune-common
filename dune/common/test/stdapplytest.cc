06f715df2886a7117b09e699ed555a2affc33af7
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-02-08 15:15:42 +0100

    [test.stdapplytest] args->test_args to silence shadowing warning

--- dune/common/test/stdapplytest.cc
+++ dune/common/test/stdapplytest.cc
@@ -21,7 +21,7 @@ void nop(std::initializer_list<T>&&)
 
 int main()
 {
-  auto args = std::make_tuple(true, 2, 3, "abc");
+  auto test_args = std::make_tuple(true, 2, 3, "abc");
 
   Dune::TestSuite test;
 
@@ -32,16 +32,16 @@ int main()
             return stream.str();
           };
 
-  test.check(Dune::Std::apply(concat, args) == "1,2,3,abc") << "Dune::Std::apply failed with concat lambda";
+  test.check(Dune::Std::apply(concat, test_args) == "1,2,3,abc") << "Dune::Std::apply failed with concat lambda";
 
   auto makeTuple = [](auto&&... args) {
             return std::make_tuple(args...);
           };
 
-  test.check(Dune::Std::apply(makeTuple, args) == args) << "Dune::Std::apply failed with makeTuple lambda";
+  test.check(Dune::Std::apply(makeTuple, test_args) == test_args) << "Dune::Std::apply failed with makeTuple lambda";
 
   auto intTuple = std::make_tuple(1,2,3);
-  auto&& intTuple0 = Dune::Std::apply([](auto&& arg0, auto&&... args) -> decltype(auto) { return arg0; }, intTuple);
+  auto&& intTuple0 = Dune::Std::apply([](auto&& arg0, auto&&... /*args*/) -> decltype(auto) { return arg0; }, intTuple);
   intTuple0 = 42;
 
   test.check(std::get<0>(intTuple) == intTuple0) << "Dune::Std::apply does not properly return references";
