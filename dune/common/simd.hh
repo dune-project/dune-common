e75a7c65f52ef05f2b362217b2942d98f90bf6b7
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-02-08 15:10:30 +0100

    [simd.hh] fix warning about ignored const on return parameter

--- dune/common/simd.hh
+++ dune/common/simd.hh
@@ -399,7 +399,7 @@ namespace Dune
   }
 
   template<class T, std::size_t n, class V>
-  const bool lane(std::size_t l, const Vc::SimdMaskArray<T, n, V> &v)
+  bool lane(std::size_t l, const Vc::SimdMaskArray<T, n, V> &v)
   {
     assert(l < n);
     return v[l];
