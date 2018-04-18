ae594072855851d67a121c431990ab1fe51265d8
Jö Fahlke <jorrit@jorrit.de>
2018-04-18 17:57:54 +0200

    [std/constexpr.hh] Remove, deprecated in 2.6.

--- dune/common/std/constexpr.hh
+++
@@ -1,8 +0,0 @@
-#ifndef DUNE_COMMON_STD_CONSTEXPR_HH
-#define DUNE_COMMON_STD_CONSTEXPR_HH
-
-#warning The header dune/common/std/constexpr.hh is deprecated. Remove the include and use "constexpr".
-
-#define DUNE_CONSTEXPR constexpr
-
-#endif // #ifndef DUNE_COMMON_STD_CONSTEXPR_HH
