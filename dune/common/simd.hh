02e11e75cea9af8c00dda15ccc10c27f38aae7d7
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-04-19 15:18:44 +0200

    [simd] also specialize IsNumber for Vc::Vector

--- dune/common/simd.hh
+++ dune/common/simd.hh
@@ -475,11 +475,16 @@ namespace Dune
 
 #if HAVE_VC
   /*
-   * Specialize IsNumber for Vc::SimdArray to be able to use it as a
-   * scalar in DenseMatrix etc.
+   * Specialize IsNumber for Vc::SimdArray and Vc::Vector to be able to use
+   * it as a scalar in DenseMatrix etc.
    */
   template <typename T, std::size_t N>
   struct IsNumber<Vc::SimdArray<T, N>>
+    : public std::integral_constant<bool, IsNumber<T>::value> {
+  };
+
+  template <typename T, typename Abi>
+  struct IsNumber<Vc::Vector<T, Abi>>
     : public std::integral_constant<bool, IsNumber<T>::value> {
   };
 #endif // HAVE_VC
