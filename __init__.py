aa00763057f6e82abe70bd3a313b29d51f5535cb
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2018-06-09 15:33:16 +0100

    bug fix in getting proc rank

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ logging.basicConfig(format=logformat, level=loglevel)
 
 try:
     from mpi4py import MPI
-    if MPI.rank == 0:
+    if MPI.COMM_WORLD.Get_rank() == 0:
         logger.info('MPI initialized successfully')
 except ImportError:
     logger.info('mpi4py not found, MPI not initialized')
