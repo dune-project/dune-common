7f1b510bc02ef01509e722f1268ae32e615b6e88
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2018-06-19 14:05:25 +0200

    [variant] Apply bugfixes from dune-subgrid

--- dune/common/std/variant.hh
+++ dune/common/std/variant.hh
@@ -25,6 +25,9 @@ namespace Dune {
 namespace Std {
 namespace Impl {
 
+  // indicator value if something's not yet (or not any longer) valid
+  constexpr const auto invalidIndex = std::numeric_limits<size_t>::max();
+
   /* helper constructs to find position of a type T in a pack Ts... */
   template <typename T, typename... Ts>
   struct index_in_pack;
@@ -39,7 +42,7 @@ namespace Impl {
 
   template<typename Tp>
   struct Buffer_ : std::aligned_storage<sizeof(Tp)> {
-    using Storage = typename std::aligned_storage_t<sizeof(Tp)>::type;
+    using Storage = std::aligned_storage_t<sizeof(Tp)>;
     Storage storage_;
 
     void* addr() {
@@ -77,6 +80,9 @@ namespace Impl {
     const auto& get() const {
       return tp_;
     }
+
+    void reset() {};
+
     private:
     Tp tp_;
   };
@@ -92,6 +98,8 @@ namespace Impl {
       ::new (&tp_) Tp(std::forward<Args>(args)...);
     }
 
+    TypeStorage_() = delete;
+
     auto& get() {
       return *(tp_.ptr());
     }
@@ -99,12 +107,23 @@ namespace Impl {
       return *(tp_.ptr());
     }
 
+    void reset() {
+      // Properly destruct the member:
+      tp_.ptr()->~Tp();
+      // (the memory itself doesn't need to be free'd. This is done when the Buffer_ member gets destructed)
+    }
+
     private:
     Buffer_<Tp> tp_;
   };
 
   template<typename... T>
-  union variant_union_ {};
+  union variant_union_ {
+    // dummy (this should never be called)
+    void resetByIndex(size_t) {
+      assert(false);
+    };
+  };
 
   template<typename Head_, typename... Tail_>
   union variant_union_<Head_, Tail_...> {
@@ -138,11 +157,22 @@ namespace Impl {
       return tail_.getByIndex(std::integral_constant<size_t, N-1>());
     }
 
+    void resetByIndex(size_t indexToReset) {
+      if (indexToReset == 0) {
+        head_.reset();
+        return;
+      }
+      else {
+        tail_.resetByIndex(indexToReset-1);
+      }
+    }
+
     template<typename Tp>
-    void set(Tp obj) {
-      Dune::Hybrid::ifElse(std::is_same<Tp, Head_>(),
-        [&](auto&& id)    { head_=std::move(id(obj)); },
-        [&](auto&& id) { return id(tail_).set(std::move(obj)); }
+    void set(Tp&& obj) {
+      using T = std::decay_t<Tp>;
+      Dune::Hybrid::ifElse(std::is_same<T, Head_>(),
+        [&](auto&& id)    { id(head_)=std::forward<Tp>(obj); },
+        [&](auto&& id) { return id(tail_).set(std::forward<Tp>(obj)); }
       );
     }
 
@@ -151,7 +181,7 @@ namespace Impl {
     }
 
     private:
-    TypeStorage_<Head_, std::is_literal_type<Head_>::value> head_;
+    TypeStorage_<Head_, std::is_trivial<Head_>::value> head_;
     variant_union_<Tail_...> tail_;
   };
 
@@ -159,7 +189,8 @@ namespace Impl {
   struct variant_{
 
     constexpr variant_() :
-      unions_() {}
+      unions_(),
+      index_(invalidIndex) {}
 
     template<typename Tp>
     constexpr variant_(Tp obj) :
@@ -168,6 +199,45 @@ namespace Impl {
       {
         unions_.set(std::move(obj));
       }
+
+    variant_(variant_&& other) {
+      unions_ = std::move(other.unions_);
+      index_ = other.index_;
+      other.index_ = invalidIndex;
+    }
+
+    variant_(const variant_& other) {
+      index_ = other.index_;
+
+      namespace H = Dune::Hybrid;
+      H::forEach(H::integralRange(std::integral_constant<size_t, size_>()), [&](auto i) {
+            if(i==index_)
+              unions_.set(other.template get<i>());
+          });
+    }
+
+    variant_& operator=(const variant_& other) {
+      if(index_ != invalidIndex)
+        unions_.resetByIndex(index_);
+
+      index_ = other.index_;
+
+      namespace H = Dune::Hybrid;
+      H::forEach(H::integralRange(std::integral_constant<size_t, size_>()), [&](auto i) {
+            if(i==index_)
+              unions_.set(other.template get<i>());
+          });
+      return *this;
+    }
+
+    variant_& operator=(variant_&& other) {
+      unions_ = std::move(other.unions_);
+      index_ = other.index_;
+      other.index_ = invalidIndex;
+
+      return *this;
+    }
+
 
     template<typename Tp>
     auto& get() {
@@ -223,21 +293,25 @@ namespace Impl {
 
     template<size_t N>
     auto& get() {
-      if (index_ != N)
+      if (index_ != N || index_ == invalidIndex)
         DUNE_THROW(Dune::Exception, "Bad variant access.");
       return unions_.template getByIndex(std::integral_constant<size_t, N>());
     }
     template<size_t N>
     const auto& get() const {
-      if (index_ != N)
+      if (index_ != N || index_ == invalidIndex)
         DUNE_THROW(Dune::Exception, "Bad variant access.");
       return unions_.template getByIndex(std::integral_constant<size_t, N>());
     }
 
     template<typename Tp>
     constexpr Tp& operator=(Tp obj) {
-      unions_.set(std::move(obj));
       constexpr auto index = index_in_pack<Tp, T...>::value;
+      // before setting a new object into the buffer, we have to destruct the old element
+      if(not (index_ == index or index_ == invalidIndex)) {
+        unions_.resetByIndex(index_);
+      }
+      unions_.set(std::move(obj));
       index_=index;
       return unions_.getByIndex(std::integral_constant<size_t,index>());
     }
@@ -248,6 +322,12 @@ namespace Impl {
 
     constexpr auto size() const {
       return sizeof...(T);
+    }
+
+    ~variant_() {
+      if (index_ != invalidIndex) {
+        unions_.resetByIndex(index_);
+      }
     }
 
     /* \brief Apply visitor to the active variant.
@@ -367,33 +447,23 @@ namespace Impl {
   }
 
   template<typename Tp, typename ...T>
-  const auto* get_if(const variant<T...>* var) {
-    if (var == nullptr)
-      return (const Tp*) nullptr;
-    return var->template get_if<Tp>();
+  const auto* get_if(const variant<T...>& var) {
+    return var.template get_if<Tp>();
   }
 
   template<typename Tp, typename ...T>
-  auto* get_if(variant<T...>* var) {
-    if (var == nullptr)
-      return (Tp*) nullptr;
-    return var->template get_if<Tp>();
+  auto* get_if(variant<T...>& var) {
+    return var.template get_if<Tp>();
   }
 
   template<size_t N, typename ...T>
-  const auto* get_if(const variant<T...>* var) {
-    using Tp = std::decay_t<decltype(var->template get<N>())>;
-    if (var == nullptr)
-      return (const Tp*) nullptr;
-    return var->template get_if<N>();
+  const auto* get_if(const variant<T...>& var) {
+    return var.template get_if<N>();
   }
 
   template<size_t N, typename ...T>
-  auto* get_if(variant<T...>* var) {
-    using Tp = std::decay_t<decltype(var->template get<N>())>;
-    if (var == nullptr)
-      return (Tp*) nullptr;
-    return var->template get_if<N>();
+  auto* get_if(variant<T...>& var) {
+    return var.template get_if<N>();
   }
 
   template<typename Tp, typename ...T>
@@ -401,18 +471,11 @@ namespace Impl {
     return var.template holds_alternative<Tp>();
   }
 
-  template <typename T>
-  struct variant_size {};
-
   template <typename... T>
-  struct variant_size<variant<T...>>
-  : std::integral_constant<std::size_t, sizeof...(T)> { };
-
-  // this cannot be inline (as it is in the STL) as this would need C++17
-  template <typename T>
-  constexpr std::size_t variant_size_v = variant_size<T>::value;
+  constexpr auto variant_size_v(const variant<T...>&) {
+    return std::integral_constant<std::size_t,sizeof...(T)>::value;
+  }
 
 } // end namespace Std
 } // end namespace Dune
-#endif
 #endif
