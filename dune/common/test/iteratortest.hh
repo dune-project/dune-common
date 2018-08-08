dd0451f075d1cb7ad8eff479b5ca47a948c7dc22
Oliver Sander <oliver.sander@tu-dresden.de>
2018-08-08 04:38:01 +0200

    Canonicalize single-inclusion macro

--- dune/common/test/iteratortest.hh
+++ dune/common/test/iteratortest.hh
@@ -1,8 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
-#ifndef DUNE_ITERATORTEST_HH
-#define DUNE_ITERATORTEST_HH
+#ifndef DUNE_COMMON_TEST_ITERATORTEST_HH
+#define DUNE_COMMON_TEST_ITERATORTEST_HH
 #include <iostream>
 #include <algorithm>
 #include <dune/common/classname.hh>
