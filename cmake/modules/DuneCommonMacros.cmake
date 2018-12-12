f620ff407cc7a6cc40bcce7e11f133e84781ef90
Jö Fahlke <jorrit@jorrit.de>
2018-12-12 19:04:58 +0100

    Don't use cmake VERSION_GREATER_EQUAL comparison
    This is to appease cmake 3.5.1 on Ubuntu 16.04

--- cmake/modules/DuneCommonMacros.cmake
+++ cmake/modules/DuneCommonMacros.cmake
@@ -38,7 +38,7 @@ find_package(TBB OPTIONAL_COMPONENTS cpf allocator)
 # try to find the Vc library
 set(MINIMUM_VC_VERSION)
 if((CMAKE_CXX_COMPILER_ID STREQUAL Clang) AND
-    (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 7))
+    (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 7))
   message("Raising minimum acceptable Vc version to 1.4.1 due to use of Clang 7 (or later), see https://gitlab.dune-project.org/core/dune-common/issues/132")
   set(MINIMUM_VC_VERSION 1.4.1)
 endif()
