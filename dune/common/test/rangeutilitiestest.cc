5995381295e748e143b8ab350fec3d561e4628c8
Carsten Gräser <graeser@dune-project.org>
2019-01-08 12:28:04 +0100

    [test] Check transformedRangeView()

--- dune/common/test/rangeutilitiestest.cc
+++ dune/common/test/rangeutilitiestest.cc
@@ -1,14 +1,146 @@
 #include "config.h"
 
 #include <array>
+#include <map>
 #include <vector>
+#include <numeric>
+#include <type_traits>
 
 #include <dune/common/hybridutilities.hh>
 #include <dune/common/iteratorrange.hh>
 #include <dune/common/rangeutilities.hh>
 #include <dune/common/typetraits.hh>
+#include <dune/common/std/type_traits.hh>
 #include <dune/common/test/testsuite.hh>
 #include <dune/common/test/iteratortest.hh>
+
+
+template<class R>
+auto checkRangeIterators(R&& r)
+{
+  auto it = r.begin();
+  auto end = r.end();
+  auto op = [](const auto& x){};
+  return (testConstIterator(it, end, op)==0);
+}
+
+template<class R, class V>
+auto checkRandomAccessNumberRangeSums(R&& r, V sum, V first, V last)
+{
+  bool passed = true;
+  passed = passed and (std::accumulate(r.begin(), r.end(), 0) == sum);
+  passed = passed and (std::accumulate(r.begin()+1, r.end(), 0) == (sum-first));
+  passed = passed and (std::accumulate(r.begin(), r.end()-1, 0) == (sum-last));
+  passed = passed and (std::accumulate(r.begin()+1, r.end()-1, 0) == (sum-first-last));
+  return passed;
+}
+
+template<class T>
+struct is_const_reference : public Dune::Std::conjunction<std::is_reference<T>, std::is_const<std::remove_reference_t<T>>>
+{};
+
+template<class T>
+struct is_mutable_reference : public Dune::Std::conjunction<std::is_reference<T>, Dune::Std::negation<is_const_reference<T>>>
+{};
+
+
+auto testTransformedRangeView()
+{
+  Dune::TestSuite suite("Check transformedRangeView()");
+
+  // Check transformedRangeView with container range
+  Dune::Hybrid::forEach(std::make_tuple(std::array<int,3>({1,2,3}), std::vector<int>({1,2,3})), [&](auto&& a)
+  {
+    auto a_backup = a;
+    // Pass original range by l-value, modify it, and then traverse.
+    // This should traverse the modified original range.
+    {
+      auto r = Dune::transformedRangeView(a, [](auto&& x) { return 2*x;});
+      a[0] = 2;
+      suite.check(checkRandomAccessNumberRangeSums(r, 14, 4, 6))
+        << "incorrect values in transformedRangeView of l-value";
+      suite.check(checkRangeIterators(r))
+        << "iterator test fails for transformedRangeView of l-value";
+      a = a_backup;
+    }
+    // Pass original range by const l-value, modify it, and then traverse.
+    // This should traverse the modified original range.
+    {
+      const auto& a_const = a;
+      auto r = Dune::transformedRangeView(a_const, [](auto&& x) { return 2*x;});
+      a[0] = 2;
+      suite.check(checkRandomAccessNumberRangeSums(r, 14, 4, 6))
+        << "incorrect values in transformedRangeView of const l-value";
+      suite.check(checkRangeIterators(r))
+        << "iterator test fails for transformedRangeView of const l-value";
+      a = a_backup;
+    }
+    // Modify original range, pass it by r-value, restore it, and then traverse.
+    // This should traverse a modified copy of the range and not the restored
+    // original one.
+    {
+      a[0] = 2;
+      auto r = Dune::transformedRangeView(std::move(a), [](auto&& x) { return 2*x;});
+      a = a_backup;
+      suite.check(checkRandomAccessNumberRangeSums(r, 14, 4, 6))
+        << "incorrect values in transformedRangeView of r-value";
+      suite.check(checkRangeIterators(r))
+        << "iterator test fails for transformedRangeView of r-value";
+    }
+    // Check if returning real references in the transformation works
+    {
+      auto r = Dune::transformedRangeView(a, [](auto&& x) -> decltype(auto) { return x;});
+      suite.check(is_mutable_reference<decltype(*r.begin())>::value)
+        << "iterator with mutable-reference returning transformation does not return mutable references";
+      suite.check(&(*(r.begin())) == &(a[0]))
+        << "reference points to wrong location";
+      (*r.begin()) = 0;
+      suite.check(a[0] == 0)
+        << "modifying range by reference returning transformation failed";
+      a = a_backup;
+    }
+    // Check if returning real references in the transformation works
+    {
+      const auto& a_const = a;
+      auto r = Dune::transformedRangeView(a_const, [](auto&& x) -> decltype(auto) { return x;});
+      suite.check(is_const_reference<decltype(*r.begin())>::value)
+        << "iterator with const-reference returning transformation does not return const references";
+      suite.check(&(*(r.begin())) == &(a[0]))
+        << "reference points to wrong location";
+    }
+  });
+  // Check transformedRangeView with on the fly range
+  {
+    auto r = Dune::transformedRangeView(Dune::range(10), [](auto&& x) { return 2*x;});
+    suite.check(checkRandomAccessNumberRangeSums(r, 90, 0, 18))
+      << "transformation of on-the-fly gives incorrect results";
+    suite.check(checkRangeIterators(r))
+      << "iterator test fails for transformedRangeView";
+  }
+  // Check if we can indirectly sort via,{0,4} reference returning transformations
+  {
+    auto a = std::vector<int>{4,3,2,1,0};
+    auto r = Dune::transformedRangeView(Dune::range(1,4), [&](auto&& i) -> decltype(auto){ return a[i];});
+    std::sort(r.begin(), r.end());
+    suite.check(a == std::vector<int>{4,1,2,3,0})
+      << "sorting reference returning transformedRangeView failed";
+
+    auto r2 = Dune::transformedRangeView(std::array<int,3>{0, 2, 4}, [&](auto&& i) -> decltype(auto){ return a[i];});
+    std::sort(r2.begin(), r2.end());
+    suite.check(a == std::vector<int>{0,1,2,3,4})
+      << "sorting reference returning transformedRangeView failed";
+
+    // Remap values of certain keys in a std::map such that
+    // they are sorted according to the keys.
+    auto m = std::map<int, int>{{-1,5},{0,4}, {1,3}, {2,2}};
+    auto r3 = Dune::transformedRangeView(std::array<int,3>{1, -1, 2}, [&](auto&& i) -> decltype(auto){ return m[i];});
+    std::sort(r3.begin(), r3.end());
+    suite.check(m == std::map<int, int>{{1,2},{-1,3}, {2,5},{0,4}})
+      << "sorting reference returning transformedRangeView failed";
+  }
+  return suite;
+}
+
 
 
 int main()
@@ -110,6 +242,8 @@ int main()
     suite.check(testConstIterator(it, end, op)==0)
       << "iterator test fails for range(-10,11)";
   }
+
+  suite.subTest(testTransformedRangeView());
 
   return suite.exit();
 
