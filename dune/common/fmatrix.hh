ffb94a1bffad8c1f40c6a6b19a8b994e0ba237f6
Felix Müller <felix.mueller2@mailbox.tu-dresden.de>
2019-02-27 16:59:34 +0100

    FieldMatrix ctor made constexpr; size methods made static

--- dune/common/fmatrix.hh
+++ dune/common/fmatrix.hh
@@ -87,7 +87,7 @@ namespace Dune
     //===== constructors
     /** \brief Default constructor
      */
-    FieldMatrix () {}
+    constexpr FieldMatrix () {}
 
     /** \brief Constructor initializing the matrix from a list of vector
      */
@@ -169,8 +169,8 @@ namespace Dune
     }
 
     // make this thing a matrix
-    constexpr size_type mat_rows() const { return ROWS; }
-    constexpr size_type mat_cols() const { return COLS; }
+    static constexpr size_type mat_rows() { return ROWS; }
+    static constexpr size_type mat_cols() { return COLS; }
 
     row_reference mat_access ( size_type i )
     {
@@ -226,7 +226,7 @@ namespace Dune
     //===== constructors
     /** \brief Default constructor
      */
-    FieldMatrix () {}
+    constexpr FieldMatrix () {}
 
     /** \brief Constructor initializing the matrix from a list of vector
      */
@@ -275,8 +275,8 @@ namespace Dune
     }
 
     // make this thing a matrix
-    constexpr size_type mat_rows() const { return 1; }
-    constexpr size_type mat_cols() const { return 1; }
+    static constexpr size_type mat_rows() { return 1; }
+    static constexpr size_type mat_cols() { return 1; }
 
     row_reference mat_access ( size_type i )
     {
