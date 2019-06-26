5b3a3bbc8cf76f4ca5b4341ff6fea9588255b326
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2019-06-26 14:53:40 +0200

    FieldVector: do not use defaulted copy constructor with GCC 5.4
    The defaulted copy constructor causes an internal compiler error with
    GCC 5.4 as shipped with Ubuntu 16.04.

--- dune/common/fvector.hh
+++ dune/common/fvector.hh
@@ -122,8 +122,14 @@ namespace Dune {
       std::fill(_data.begin(),_data.end(),t);
     }
 
+#if __GNUC__ == 5 && !defined(__clang__)
+    // `... = default;` causes an internal compiler error on GCC 5.4 (Ubuntu 16.04)
+    //! copy constructor
+    FieldVector(const FieldVector& x) : _data(x._data) {}
+#else
     //! Copy constructor
     FieldVector (const FieldVector&) = default;
+#endif
 
     /** \brief Construct from a std::initializer_list */
     FieldVector (std::initializer_list<K> const &l)
