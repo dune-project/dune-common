a9fa2ee886dd438c18991b86f540647f93050388
Nils-Arne Dreier <n.dreier@uni-muenster.de>
2019-09-11 14:58:43 +0200

    comment out deprecated attributes
    will be uncommented after the 2.7 release

--- dune/common/parallel/collectivecommunication.hh
+++ dune/common/parallel/collectivecommunication.hh
@@ -1,2 +1,3 @@
-#warning "Deprecated header, use #include <dune/common/parallel/communication.hh> instead!"
+// Will be deprecated after the 2.7 release
+//#warning "Deprecated header, use #include <dune/common/parallel/communication.hh> instead!"
 #include <dune/common/parallel/communication.hh>
