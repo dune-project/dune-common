c9423d73be5376d563a04346304c93f543ee2824
Hensto <henrik.sto.official@protonmail.com>
2019-10-01 12:02:54 +0200

    Fixed Lapackcall for eigenvectors

--- dune/common/dynmatrixev.hh
+++ dune/common/dynmatrixev.hh
@@ -3,6 +3,7 @@
 #ifndef DUNE_DYNMATRIXEIGENVALUES_HH
 #define DUNE_DYNMATRIXEIGENVALUES_HH
 
+#include <algorithm>
 #include <memory>
 
 #include "dynmatrix.hh"
@@ -26,17 +27,20 @@ namespace Dune {
         \param[in]  matrix matrix eigenvalues are calculated for
         \param[out] eigenValues FieldVector that contains eigenvalues in
                     ascending order
+        \param[out] eigenVectors (optional) list of right eigenvectors
 
         \note LAPACK::dgeev is used to calculate the eigen values
      */
     template <typename K, class C>
     static void eigenValuesNonSym(const DynamicMatrix<K>& matrix,
-                                  DynamicVector<C>& eigenValues)
+                                  DynamicVector<C>& eigenValues,
+                                  std::vector<DynamicVector<K>>* eigenVectors = nullptr
+      )
     {
       {
         const long int N = matrix.rows();
         const char jobvl = 'n';
-        const char jobvr = 'n';
+        const char jobvr = eigenVectors ? 'v' : 'n';
 
 
         // matrix to put into dgeev
@@ -55,15 +59,17 @@ namespace Dune {
         // working memory
         auto eigenR = std::make_unique<double[]>(N);
         auto eigenI = std::make_unique<double[]>(N);
-        auto work = std::make_unique<double[]>(3*N);
+
+        const long int lwork = eigenVectors ? 4*N : 3*N;
+        auto work = std::make_unique<double[]>(lwork);
+        auto vr = eigenVectors ? std::make_unique<double[]>(N*N) : std::unique_ptr<double[]>{};
 
         // return value information
         long int info = 0;
-        const long int lwork = 3*N;
 
         // call LAPACK routine (see fmatrixev_ext.cc)
         eigenValuesNonsymLapackCall(&jobvl, &jobvr, &N, matrixVector.get(), &N,
-                                    eigenR.get(), eigenI.get(), nullptr, &N, nullptr, &N, work.get(),
+                                    eigenR.get(), eigenI.get(), nullptr, &N, vr.get(), &N, work.get(),
                                     &lwork, &info);
 
         if( info != 0 )
@@ -75,6 +81,15 @@ namespace Dune {
         eigenValues.resize(N);
         for (int i=0; i<N; ++i)
           eigenValues[i] = std::complex<double>(eigenR[i], eigenI[i]);
+
+        if (eigenVectors) {
+          eigenVectors->resize(N);
+          for (int i = 0; i < N; ++i) {
+            auto& v = (*eigenVectors)[i];
+            v.resize(N);
+            std::copy(vr.get() + N*i, vr.get() + N*(i+1), &v[0]);
+          }
+        }
       }
     }
 
