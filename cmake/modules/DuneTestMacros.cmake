dune-project/dune-common

commit a225b2da42e1007fc25fcade5a97440d190ef2f7

[cmake][win32] make skipped tests work on windows
skipped test targets are manually overwritten. We have use the TARGET_FILE
and can not create the name manually, as this will fail when the target name
differs from the executable name.

cmake/modules/DuneTestMacros.cmake
@@ -376,6 +376,9 @@ function(dune_add_test)
       if(TARGET "${ADDTEST_COMMAND}")
         set(ACTUAL_TESTCOMMAND "$<TARGET_FILE:${ADDTEST_COMMAND}>")
       else()
+        # this should only happen if this is an EXPECT_COMPILE_FAIL test,
+        # as we set ADDTEST_COMMAND to cmake in this case.
+        # Or if we run the test via some script.
         set(ACTUAL_TESTCOMMAND "${ADDTEST_COMMAND}")
       endif()
 
@@ -387,7 +390,7 @@ function(dune_add_test)
 
       # if this is a skipped test because a guard was false, overwrite the command
       if(SHOULD_SKIP_TEST)
-        set(ACTUAL_TESTCOMMAND ${CMAKE_CURRENT_BINARY_DIR}/${ADDTEST_NAME})
+        set(ACTUAL_TESTCOMMAND "$<TARGET_FILE:${ADDTEST_COMMAND}>")
         set(ACTUAL_CMD_ARGS)
       endif()
 

commit dfc5a6a5dd0fef39297e5f8939a0ce61210d773a

[test][cmake] Fix test command for skipped test
This fixes the bug (#183) introduced with a225b2da.
The command can be a complex path while for skipped tests
we actually want the test command to just be equal to
the target name (executable). This name is
expanded to a full path using the TARGET_FILE generator
expression so it should work on all platforms that CMake
supports.

cmake/modules/DuneTestMacros.cmake
@@ -380,6 +380,7 @@ function(dune_add_test)
         set(ACTUAL_TESTCOMMAND "${ADDTEST_COMMAND}")
       endif()
 
+      # modify test name and command for parallel tests
       if(NOT ${procnum} STREQUAL "1")
         set(ACTUAL_NAME "${ACTUAL_NAME}-mpi-${procnum}")
         set(ACTUAL_CMD_ARGS ${MPIEXEC_PREFLAGS} ${MPIEXEC_NUMPROC_FLAG} ${procnum} "${ACTUAL_TESTCOMMAND}" ${MPIEXEC_POSTFLAGS} ${ACTUAL_CMD_ARGS})
@@ -388,7 +389,7 @@ function(dune_add_test)
 
       # if this is a skipped test because a guard was false, overwrite the command
       if(SHOULD_SKIP_TEST)
-        set(ACTUAL_TESTCOMMAND "$<TARGET_FILE:${ADDTEST_COMMAND}>")
+        set(ACTUAL_TESTCOMMAND "$<TARGET_FILE:${ADDTEST_TARGET}>")
         set(ACTUAL_CMD_ARGS)
       endif()
 