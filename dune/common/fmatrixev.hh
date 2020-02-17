97610016a54dbd4bbf4e17bc8878a0c55febce7d
Lukas Renelt <l_rene02@uni-muenster.de>
2020-02-17 16:59:21 +0100

    add 3d specialization

--- dune/common/fmatrixev.hh
+++ dune/common/fmatrixev.hh
@@ -120,6 +120,200 @@ namespace Dune {
       }
     }
 
+    namespace Impl {
+       //compute the cross-product of two vectors
+      template<typename K>
+      inline FieldVector<K,3> crossProduct(FieldVector<K,3>& vec0, FieldVector<K,3>& vec1) {
+        return {vec0[1]*vec1[2] - vec0[2]*vec1[1], vec0[2]*vec1[0] - vec0[0]*vec1[2], vec0[0]*vec1[1] - vec0[1]*vec1[0]};
+      }
+
+      //see http://en.wikipedia.org/wiki/Eigenvalue_algorithm
+      template <typename K>
+        static K eigenValues3dImpl(const FieldMatrix<K, 3, 3>& matrix,
+                                FieldVector<K, 3>& eigenvalues)
+      {
+        using std::sqrt;
+        using std::acos;
+        using real_type = typename FieldTraits<K>::real_type;
+        const K pi = MathematicalConstants<K>::pi();
+        K p1 = matrix[0][1]*matrix[0][1] + matrix[0][2]*matrix[0][2] + matrix[1][2]*matrix[1][2];
+
+        if (p1 <= std::numeric_limits<K>::epsilon()) {
+          // A is diagonal.
+          eigenvalues[0] = matrix[0][0];
+          eigenvalues[1] = matrix[1][1];
+          eigenvalues[2] = matrix[2][2];
+          std::sort(eigenvalues.begin(), eigenvalues.end());
+        }
+        else
+        {
+          // q = trace(A)/3
+          K q = 0;
+          for (int i=0; i<3; i++)
+            q += matrix[i][i] / 3.0;
+
+          K p2 = (matrix[0][0] - q)*(matrix[0][0] - q) + (matrix[1][1] - q)*(matrix[1][1] - q) + (matrix[2][2] - q)*(matrix[2][2] - q) + 2.0 * p1;
+          K p = sqrt(p2 / 6);
+          // B = (1 / p) * (A - q * I);       // I is the identity matrix
+          FieldMatrix<K,3,3> B;
+          for (int i=0; i<3; i++)
+            for (int j=0; j<3; j++)
+              B[i][j] = (real_type(1.0)/p) * (matrix[i][j] - q*(i==j));
+
+          K r = B.determinant() / 2.0;
+
+          /*In exact arithmetic for a symmetric matrix  -1 <= r <= 1
+          but computation error can leave it slightly outside this range.
+          acos(z) function requires |z| <= 1, but will fail silently
+          and return NaN if the input is larger than 1 in magnitude.
+          Thus r is clamped to [-1,1].*/
+          r = std::min(std::max(r, -1.0), 1.0);
+          K phi = acos(r) / 3.0;
+
+          // the eigenvalues satisfy eig[2] <= eig[1] <= eig[0]
+          eigenvalues[2] = q + 2 * p * cos(phi);
+          eigenvalues[0] = q + 2 * p * cos(phi + (2*pi/3));
+          eigenvalues[1] = 3 * q - eigenvalues[0] - eigenvalues[2];     // since trace(matrix) = eig1 + eig2 + eig3
+        }
+        return r;
+      }
+
+      //see https://www.geometrictools.com/Documentation/RobustEigenSymmetric3x3.pdf
+      //Robustly compute a right-handed orthonormal set {u, v, evec0}.
+      template<typename K>
+      void orthoComp(const FieldVector<K,3>& evec0, FieldVector<K,3>& u, FieldVector<K,3>& v) {
+        if(std::abs(evec0[0]) > std::abs(evec0[1])) {
+          //The component of maximum absolute value is either evec0[0] or evec0[2].
+          auto L = 1.0 / ({evec0[0], evec0[2]}).two_norm();
+          u = L * FieldVector<K,3>({-evec0[2], 0.0, evec0[0]});
+        }
+        else {
+          //The component of maximum absolute value is either evec0[1] or evec0[2].
+          auto L = 1.0 / ({evec0[1], evec0[2]}).two_norm();
+          u = L * FieldVector<K,3>({0.0, evec0[2], -evec0[1]});
+        }
+        v = crossProduct(evec0, u);
+      }
+
+      //see https://www.geometrictools.com/Documentation/RobustEigenSymmetric3x3.pdf
+      template<typename K>
+      void eig0(const FieldMatrix<K,3,3>& matrix, K eval0, FieldVector<K,3> evec0) {
+        /* Compute a unit-length eigenvector for eigenvalue[i0].  The
+        matrix is rank 2, so two of the rows are linearly independent.
+        For a robust computation of the eigenvector, select the two
+        rows whose cross product has largest length of all pairs of
+        rows. */
+        using Vector = FieldVector<K,3>;
+        Vector row0 = {matrix[0][0]-eval0, matrix[0][1], matrix[0][2]};
+        Vector row1 = {matrix[1][0], matrix[1][1]-eval0, matrix[1][2]};
+        Vector row2 = {matrix[2][0], matrix[2][1], matrix[2][2]-eval0};
+
+        Vector r0xr1 = crossProduct(row0, row1);
+        Vector r0xr2 = crossProduct(row0, row2);
+        Vector r1xr2 = crossProduct(row1, row2);
+        auto d0 = r0xr1.two_norm();
+        auto d1 = r0xr2.two_norm();
+        auto d2 = r1xr2.two_norm();
+
+        auto dmax = d0 ;
+        int imax = 0;
+        if(d1>dmax) {
+          dmax = d1;
+          imax = 1;
+        }
+        if(d2>dmax)
+          imax = 2;
+
+        if(imax == 0)
+          evec0 = r0xr1 / d0;
+        else if(imax == 1)
+          evec0 = r0xr2 / d1;
+        else
+          evec0 = r1xr2 / d2;
+      }
+
+      //see https://www.geometrictools.com/Documentation/RobustEigenSymmetric3x3.pdf
+      template<typename K>
+      void eig1(const FieldMatrix<K,3,3>& matrix, const FieldVector<K,3>& evec0, FieldVector<K,3> evec1, K eval1) {
+        //return array([0,0,1])
+        using Vector = FieldVector<K,3>;
+
+        //Robustly compute a right-handed orthonormal set {u, v, evec0}.
+        Vector u,v;
+        orthoComp(evec0, u, v);
+
+        /* Let e be eval1 and let E be a corresponding eigenvector which
+        is a solution to the linear system (A - e*I)*E = 0.  The matrix
+        (A - e*I) is 3x3, not invertible (so infinitely many
+        solutions), and has rank 2 when eval1 and eval are different.
+        It has rank 1 when eval1 and eval2 are equal.  Numerically, it
+        is difficult to compute robustly the rank of a matrix.  Instead,
+        the 3x3 linear system is reduced to a 2x2 system as follows.
+        Define the 3x2 matrix J = [u,v] whose columns are the u and v
+        computed previously.  Define the 2x1 vector X = J*E.  The 2x2
+        system is 0 = M * X = (J^T * (A - e*I) * J) * X where J^T is
+        the transpose of J and M = J^T * (A - e*I) * J is a 2x2 matrix.
+        The system may be written as
+            +-                        -++-  -+       +-  -+
+            | U^T*A*U - e  U^T*A*V     || x0 | = e * | x0 |
+            | V^T*A*U      V^T*A*V - e || x1 |       | x1 |
+            +-                        -++   -+       +-  -+
+        where X has row entries x0 and x1. */
+
+        Vector Au = matrix.mv(u);
+        Vector Av = matrix.mv(v);
+
+        auto m00 = u.dot(Au) - eval1;
+        auto m01 = u.dot(Av);
+        auto m11 = v-dot(Av) - eval1;
+
+        /* For robustness, choose the largest-length row of M to compute
+        the eigenvector.  The 2-tuple of coefficients of U and V in the
+        assignments to eigenvector[1] lies on a circle, and U and V are
+        unit length and perpendicular, so eigenvector[1] is unit length
+        (within numerical tolerance). */
+        auto absM00 = std::abs(m00);
+        auto absM01 = std::abs(m01);
+        auto absM11 = std::abs(m11);
+        if(absM00 >= absM11) {
+          auto maxAbsComp = std::max(absM00, absM01);
+          if(maxAbsComp > 0.0) {
+            if(absM00 >= absM01) {
+              m01 /= m00;
+              m00 = 1.0 / std::sqrt(1.0 + m01*m01);
+              m01 *= m00;
+            }
+            else {
+              m00 /= m01;
+              m01 = 1.0 / std::sqrt(1.0 + m00*m00);
+              m00 *= m01;
+              evec1 = m01*u - m00*v;
+            }
+          }
+          else
+            evec1 = u;
+        }
+        else {
+          auto maxAbsComp = std::max(absM11, absM01);
+          if(maxAbsComp > 0.0) {
+            if(absM11 >= absM01) {
+              m01 /= m11;
+              m11 = 1.0 / std::sqrt(1.0 + m01*m01);
+              m01 *= m11;
+            }
+            else {
+              m11 /= m01;
+              m01 = 1.0 / std::sqrt(1.0 + m11*m11);
+              m11 *= m01;
+              evec1 = m11*u - m01*v;
+            }
+          }
+          else
+            evec1 = u;
+        }
+      }
+    } //namespace Impl
+
     /** \brief Calculates the eigenvalues of a symmetric 3x3 field matrix
         \param[in]  matrix matrix eigenvalues are calculated for
         \param[out] eigenvalues Eigenvalues in ascending order
@@ -136,51 +330,66 @@ namespace Dune {
     static void eigenValues(const FieldMatrix<K, 3, 3>& matrix,
                             FieldVector<K, 3>& eigenvalues)
     {
-      using std::sqrt;
-      using std::acos;
-      using real_type = typename FieldTraits<K>::real_type;
-      const K pi = MathematicalConstants<K>::pi();
-      K p1 = matrix[0][1]*matrix[0][1] + matrix[0][2]*matrix[0][2] + matrix[1][2]*matrix[1][2];
+      K temp = Impl::eigenValues3dImpl(matrix, eigenvalues);
+      DUNE_UNUSED_PARAMETER(temp);
+    }
 
-      if (p1 <= 1e-8)
-      {
-        // A is diagonal.
-        eigenvalues[0] = matrix[0][0];
-        eigenvalues[1] = matrix[1][1];
-        eigenvalues[2] = matrix[2][2];
+    /** \brief calculates the eigenvectors of a symmetric field matrix
+        \param[in]  matrix matrix eigenvectors are calculated for
+        \param[out] eigenvectors FieldVector that contains eigenvectors with
+                    corresponding eigenvalues in ascending order
+        \param[out] eigenvalues FieldVector that contains eigenvalues
+                    in ascending order
+     */
+    template<typename K>
+    static void eigenVectorsAndValues(const FieldMatrix<K, 3, 3>& matrix,
+                                      FieldVector<FieldVector<K, 3>,3>& eigenvectors,
+                                      FieldVector<K,3>& eigenvalues)
+    {
+      using Vector = FieldVector<K,3>;
+      using Matrix = FieldMatrix<K,3,3>;
+
+      //compute eigenvalues
+      /* Precondition the matrix by factoring out the maximum absolute
+      value of the components.  This guards against floating-point
+      overflow when computing the eigenvalues.*/
+      K maxAbsElement = matrix.infinity_norm()
+      Matrix scaledMatrix = matrix / maxAbsElement;
+      K r = Impl::eigenValues3dImpl(scaledMatrix, eigenvalues);
+
+      K offDiagNorm = matrix[0][1]*matrix[0][1] + matrix[0][2]*matrix[0][2] + matrix[1][2]*matrix[1][2];
+      if(offDiagNorm <= std::numeric_limits<K>::epsilon())
+        eigenvectors = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}};
+      else {
+        /*Compute the eigenvectors so that the set
+        [evec[0], evec[1], evec[2]] is right handed and
+        orthonormal. */
+
+        FieldVector<Vector,3> evec(0.0);
+        if(r >= 0) {
+          Impl::eig0(scaledMatrix, eval[2], evec[2]);
+          Impl::eig1(scaledMatrix, evec[2], evec[1], eval[1]);
+          evec[0] = Impl::crossProduct(evec[1], evec[2]);
+        }
+        else {
+          Impl::eig0(scaledMatrix, eval[0], evec[0]);
+          Impl::eig1(scaledMatrix, evec[0], evec[1], eval[1]);
+          evec[2] = Impl::crossProduct(evec[0], evec[1]);
+        }
+        //sort eval/evec-pairs in ascending order
+        using EVPair = std::pair<K, FieldVector<K,3>>;
+        std::vector<EVPair> pairs;
+        for(std::size_t i=0; i<=2; ++i)
+          pairs[i] = EVPair(eval[i], evec[i]);
+        auto comp = [](EVPair x, EVPair y){ return x.first < y.first; };
+        std::sort(pairs, comp);
+        for(std::size_t i=0; i<=2; ++i){
+          eigenvalues[i] = pairs[i].first;
+          eigenvectors[i] = pairs[i].second;
+        }
       }
-      else
-      {
-        // q = trace(A)/3
-        K q = 0;
-        for (int i=0; i<3; i++)
-          q += matrix[i][i]/3.0;
-
-        K p2 = (matrix[0][0] - q)*(matrix[0][0] - q) + (matrix[1][1] - q)*(matrix[1][1] - q) + (matrix[2][2] - q)*(matrix[2][2] - q) + 2 * p1;
-        K p = sqrt(p2 / 6);
-        // B = (1 / p) * (A - q * I);       // I is the identity matrix
-        FieldMatrix<K,3,3> B;
-        for (int i=0; i<3; i++)
-          for (int j=0; j<3; j++)
-            B[i][j] = (real_type(1.0)/p) * (matrix[i][j] - q*(i==j));
-
-        K r = B.determinant() / 2.0;
-
-        // In exact arithmetic for a symmetric matrix  -1 <= r <= 1
-        // but computation error can leave it slightly outside this range.
-        K phi;
-        if (r <= -1)
-          phi = pi / 3.0;
-        else if (r >= 1)
-          phi = 0;
-        else
-          phi = acos(r) / 3;
-
-        // the eigenvalues satisfy eig[2] <= eig[1] <= eig[0]
-        eigenvalues[2] = q + 2 * p * cos(phi);
-        eigenvalues[0] = q + 2 * p * cos(phi + (2*pi/3));
-        eigenvalues[1] = 3 * q - eigenvalues[0] - eigenvalues[2];     // since trace(matrix) = eig1 + eig2 + eig3
-      }
+      //The preconditioning scaled the matrix, which scales the eigenvalues. Revert the scaling.
+      eigenvalues *= maxAbsElement;
     }
 
     /** \brief calculates the eigenvalues and/or eigenvectors of a symmetric field matrix
