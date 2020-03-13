9ce05a646b7ed708428a2fe5e5408febb480eaea
Christoph Grüninger <gruenich@dune-project.org>
2020-03-13 20:30:43 +0100

    Deprecate utility.hh

--- dune/common/std/utility.hh
+++ dune/common/std/utility.hh
@@ -1,12 +1,10 @@
 #ifndef DUNE_COMMON_STD_UTILITY_HH
 #define DUNE_COMMON_STD_UTILITY_HH
 
-#include <cstddef>
-
-#include <type_traits>
 #include <utility>
 
-#include <dune/common/typetraits.hh>
+#warning dune/common/std/utility.hh is deprecated and will be removed after Dune 2.8.\
+ Inclead <utility> instead
 
 namespace Dune
 {
@@ -14,30 +12,11 @@ namespace Dune
   namespace Std
   {
 
-
     using std::integer_sequence;
     using std::index_sequence;
     using std::make_integer_sequence;
     using std::make_index_sequence;
-
-    /**
-     * \brief Create index_sequence from 0 to sizeof...(T)-1
-     *
-     * This should do the same as std::index_sequence_for.
-     * But due to a bug in the sizeof... operator this
-     * may produce wrong results with clang<3.8.
-     *
-     * As a workaround we provide our own implementation
-     * that avoids this bug even if the std:: version
-     * exists.
-     *
-     * This implemenation can be dropped, once we require
-     * a minimum clang version that has this bug fixed (i.e. >=3.8).
-     */
-    template<typename... T>
-    using index_sequence_for = make_index_sequence<typename Dune::SizeOf<T...>{}>;
-
-
+    using std::index_sequence_for;
 
   } // namespace Std
 
