a81320374ff8b8cce31e16c9121de7a6b49dad8f
dedner <a.s.dedner@warwick.ac.uk>
2020-03-29 12:58:11 +0100

    imporve handling of includes in `importclass.py`

--- python/dune/generator/importclass.py
+++ python/dune/generator/importclass.py
@@ -11,27 +11,21 @@ def load(className, includeFiles, *args):
     source = '#include <config.h>\n\n'
     source += '#define USING_DUNE_PYTHON 1\n\n'
     includes = []
-    if isString(includeFiles):
-        if not os.path.dirname(includeFiles):
-            with open(includeFiles, "r") as include:
-                source += include.read()
-            source += "\n"
-        else:
-            source += "#include <"+includeFiles+">\n"
-            includes += [includeFiles]
-    elif hasattr(includeFiles,"readable"): # for IOString
-        with includeFiles as include:
-            source += include.read()
-        source += "\n"
-    elif isinstance(includeFiles, list):
-        for includefile in includeFiles:
+    if isString(includeFiles) or hasattr(includeFiles,"readable"):
+        includeFiles = [includeFiles]
+    for includefile in includeFiles:
+        if isString(includefile):
             if not os.path.dirname(includefile):
                 with open(includefile, "r") as include:
                     source += include.read()
                 source += "\n"
-        else:
-            source += "#include <"+includefile+">\n"
-            includes += [includefile]
+            else:
+                source += "#include <"+includefile+">\n"
+                includes += [includefile]
+        elif hasattr(includefile,"readable"): # for IOString
+            with includefile as include:
+                source += include.read()
+            source += "\n"
 
     argTypes = []
     for arg in args:
@@ -52,6 +46,7 @@ def load(className, includeFiles, *args):
     source += "PYBIND11_MODULE( " + moduleName + ", module )\n"
     source += "{\n"
 
+    includes += ["python/dune/generated/"+moduleName+".cc"]
     source += "  auto cls = Dune::Python::insertClass< "+className+\
                  " >( module, \"cls\","+\
                  "Dune::Python::GenerateTypeName(\""+className+"\"),"+\
@@ -62,5 +57,10 @@ def load(className, includeFiles, *args):
     source += "})"+" ".join([", pybind11::keep_alive< 1, {} >()".format(i+2) for i in range(len(argTypes))]) + "\n"
     source += ");\n"
     source += "}"
+
+    source = "#ifndef def_"+moduleName+\
+             "\n#define def_"+moduleName+"\n"+\
+             source+\
+             "\n#endif\n"
 
     return builder.load(moduleName, source, signature).cls(*args)
