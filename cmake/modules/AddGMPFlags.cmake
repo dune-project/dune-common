fb2b3dadbaf08ade1375c9961058c720e88e6416
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-05-23 20:33:16 +0200

    Update AddGMPFlags to link to imported target

--- cmake/modules/AddGMPFlags.cmake
+++ cmake/modules/AddGMPFlags.cmake
@@ -14,15 +14,7 @@
 function(add_dune_gmp_flags _targets)
   if(GMP_FOUND)
     foreach(_target ${_targets})
-      target_link_libraries(${_target} ${GMP_LIBRARIES})
-      set_property(TARGET ${_target}
-        APPEND_STRING
-        PROPERTY COMPILE_FLAGS "-DENABLE_GMP=1 ")
-      foreach(_path ${GMP_INCLUDE_DIRS})
-        set_property(TARGET ${_target}
-          APPEND_STRING
-          PROPERTY COMPILE_FLAGS "-I${_path}")
-      endforeach(_path ${GMP_INCLUDE_DIRS})
+      target_link_libraries(${_target} GMP::gmpxx)
     endforeach(_target ${_targets})
   endif(GMP_FOUND)
 endfunction(add_dune_gmp_flags)
