fe3a40a2084092ac2088d007db9c28d9365502f6
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-05-23 20:29:37 +0200

    Update AddPTScotchFlags to link to imported target

--- cmake/modules/AddPTScotchFlags.cmake
+++ cmake/modules/AddPTScotchFlags.cmake
@@ -14,16 +14,7 @@
 function(add_dune_ptscotch_flags _targets)
   if(PTSCOTCH_FOUND)
     foreach(_target ${_targets})
-      target_link_libraries(${_target} ${PTSCOTCH_LIBRARY} ${SCOTCH_LIBRARY}  ${PTSCOTCHERR_LIBRARY})
-      GET_TARGET_PROPERTY(_props ${_target} INCLUDE_DIRECTORIES)
-      string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
-      SET_TARGET_PROPERTIES(${_target} PROPERTIES INCLUDE_DIRECTORIES
-        "${_props};${PTSCOTCH_INCLUDE_DIRS}")
-      GET_TARGET_PROPERTY(_props ${_target} COMPILE_DEFINITIONS)
-      string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
-      SET_TARGET_PROPERTIES(${_target} PROPERTIES COMPILE_DEFINITIONS
-        "${_props};")
+      target_link_libraries(${_target} PTScotch::PTScotch)
     endforeach(_target ${_targets})
-    add_dune_mpi_flags(${_targets})
-  endif(PTSCOTCH_FOUND)
+  endif()
 endfunction(add_dune_ptscotch_flags)
