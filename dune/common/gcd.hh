67313ac9f97b201dc2f180bc757be893a2af9acf
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-05-27 13:30:24 +0000

    [cleanup] Deprecate gcd, advertise C++17 STL solution

--- dune/common/gcd.hh
+++ dune/common/gcd.hh
@@ -2,6 +2,7 @@
 // vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_GCD_HH
 #define DUNE_GCD_HH
+#warning "This header is deprecated and will be removed after Dune release 2.8. Use std::gcd instead"
 
 namespace Dune
 {
@@ -62,7 +63,7 @@ namespace Dune
    * @brief Calculator of the greatest common divisor.
    */
   template<long a, long b>
-  struct Gcd
+  struct [[deprecated("Will be removed after Dune 2.8. Use std::gcd from <numeric> instead!")]] Gcd
   {
     /**
      * @brief The greatest common divisior of a and b. */
