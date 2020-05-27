35cf2f071299963b36f9ce2d1e4c7bd73bc6b10e
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2020-05-27 13:30:24 +0000

    [cleanup] Deprecate gcd, advertise C++17 STL solution

--- dune/common/gcd.hh
+++ dune/common/gcd.hh
@@ -2,71 +2,19 @@
 // vi: set et ts=4 sw=2 sts=2:
 #ifndef DUNE_GCD_HH
 #define DUNE_GCD_HH
+#warning "This header is deprecated and will be removed after Dune release 2.8. Use std::gcd instead"
 
 namespace Dune
 {
   /**
-   * @addtogroup Common
-   * @{
-   */
-  /**
-   * @file
-   * \brief Statically compute the greatest common divisor of two integers
-   */
-
-#ifndef DOXYGEN
-  /**
-   * @brief Helper for calculating the gcd.
-   */
-  template<long a, long b, bool bo>
-  struct GcdHelper
-  {};
-
-
-  template<long a, long b>
-  struct GcdHelper<a,b,true>
-  {
-    /**
-     * @brief Check that a>b.
-     */
-    static void conceptCheck()
-    {
-      static_assert(b<a, "b<a must hold!");
-      static_assert(0<b, "b must be positive");
-    }
-
-
-    /**
-     * @brief The greatest common divisor of the numbers a and b.
-     */
-    const static long gcd = GcdHelper<b,a%b,true>::gcd;
-  };
-
-  template<long a, long b>
-  struct GcdHelper<a,b,false>
-  {
-    /**
-     * @brief The greatest common divisor of the numbers a and b.
-     */
-    const static long gcd = GcdHelper<b,a,true>::gcd;
-  };
-  template<long a>
-  struct GcdHelper<a,0,true>
-  {
-    const static long gcd=a;
-  };
-
-#endif
-
-  /**
    * @brief Calculator of the greatest common divisor.
    */
   template<long a, long b>
-  struct Gcd
+  struct [[deprecated("Will be removed after Dune 2.8. Use std::gcd from <numeric> instead!")]] Gcd
   {
     /**
      * @brief The greatest common divisior of a and b. */
-    const static long value = GcdHelper<a,b,(a>b)>::gcd;
+    constexpr static long value = std::gcd(a,b);
   };
 
   /**
