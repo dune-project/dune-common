f41d3fc9ead78c2398094a9a56db057b671ca675
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-05-29 15:41:18 +0200

    Move HAVE_METIS and dune_register_package_flags to AddMETISFlags

--- cmake/modules/AddMETISFlags.cmake
+++ cmake/modules/AddMETISFlags.cmake
@@ -10,7 +10,15 @@
 #       A list of targets to use METIS with.
 #
 
+# register HAVE_METIS for config.h
+set(HAVE_METIS ${METIS_FOUND})
 
+# register METIS library as dune package
+if(METIS_FOUND)
+  dune_register_package_flags(LIBRARIES METIS::METIS)
+endif()
+
+# Add function to link targets against METIS library
 function(add_dune_metis_flags _targets)
   if(METIS_FOUND)
     foreach(_target ${_targets})
