296c1647533d6586d908fa12f1d3ce9a3312c9f2
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-20 14:44:10 +0100

    Initial version.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import sys, os
+from setuptools import find_packages
+
+try:
+    from skbuild import setup
+except ImportError:
+    print('Please update pip, you need pip 10 or greater,\n'
+          ' or you need to install the PEP 518 requirements in pyproject.toml yourself', file=sys.stderr)
+    raise
+
+        # remove existing dune-py module
+        # os.system('rm -rf ' + os.path.join(get_install_prefix(), '.cache', 'dune-py'))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dune-common",
+    version="2.8.dev314",
+    author="The Dune Core developers",
+    author_email="dune@lists.dune-project.org",
+    description="""Basis infrastructure classes for all Dune modules""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.dune-project.org/core/dune-common",
+    packages=find_packages(where="python"),
+    package_dir={"": "python"},
+    install_requires=['numpy', 'mpi4py'],
+    # cmake_install_dir=sys.prefix,
+    cmake_args=['-DBUILD_SHARED_LIBS=TRUE',
+                '-DDUNE_ENABLE_PYTHONBINDINGS=TRUE',
+                '-DDUNE_GRID_GRIDTYPE_SELECTOR=ON',
+                '-DALLOW_CXXFLAGS_OVERWRITE=ON',
+                '-DUSE_PTHREADS=ON',
+                '-DCMAKE_BUILD_TYPE=Release',
+                '-DCMAKE_DISABLE_FIND_PACKAGE_LATEX=TRUE',
+                '-DCMAKE_DISABLE_DOCUMENTATION=TRUE']
+)
