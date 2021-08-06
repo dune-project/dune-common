76d9f915425ef553149742124be91b835668ea73
Santiago Ospina De Los Rios <sospinar@gmail.com>
2021-08-06 14:17:15 +0200

    Add deprecation message on the demangle CMake file

---
+++ cmake/modules/DuneCxaDemangle.cmake
@@ -0,0 +1,4 @@
+message(DEPRECATION
+  "The cmake file 'DuneCxaDemangle.cmake' is deprecated. "
+  "Include the 'dune/common/classname.hh' header if you want to ensure that "
+  "HAVE_CXA_DEMANGLE is defined")
