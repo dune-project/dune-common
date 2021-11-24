f5428858a47dd32212dbf925cc031b2aba06846f
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2021-11-24 13:53:35 +0100

    [python][cleanup] Fix some pep8 violations in packagemetadata

--- python/dune/packagemetadata.py
+++ python/dune/packagemetadata.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from setuptools import find_namespace_packages
-import sys, os, io, re, ast
+import sys
+import os
+import io
+import re
+import ast
 import json
 import shlex
-import pkgutil
-import importlib, subprocess
+import subprocess
 import email.utils
 import glob
 import logging
@@ -29,8 +32,8 @@ class Version:
             if not match:
                 raise ValueError('Invalid version: \'' + s + '\'.')
             self.major = int(match.group('major'))
-            self.minor = int(match.group('minor')) if match.group( 'minor' ) else 0
-            self.revision = int(match.group('revision')) if match.group( 'revision' ) else 0
+            self.minor = int(match.group('minor')) if match.group('minor') else 0
+            self.revision = int(match.group('revision')) if match.group('revision') else 0
 
     def __str__(self):
         return str(self.major) + '.' + str(self.minor) + '.' + str(self.revision)
@@ -60,7 +63,7 @@ class Version:
 class VersionRequirement:
     def __init__(self, s):
         if s:
-            match = re.match('(?P<operator>(>|>=|==|<=|<))\s*(?P<version>[0-9.]+)', s)
+            match = re.match(r'(?P<operator>(>|>=|==|<=|<))\s*(?P<version>[0-9.]+)', s)
             if not match:
                 raise ValueError('Invalid version qualifier: \'' + s + '\'.')
             self.version = Version(match.group('version'))
@@ -79,7 +82,7 @@ class VersionRequirement:
             else:
                 raise ValueError('Invalid comparison operator: \'' + operator + '\'.')
         else:
-            self.operator = lambda a, b : True
+            self.operator = lambda a, b: True
             self.version = None
 
     def __bool__(self):
@@ -107,28 +110,31 @@ class VersionRequirement:
         else:
             return ''
 
+
 class Description:
     def __init__(self, fileName=None, **kwargs):
         data = kwargs.copy()
 
-        valid_entries = ['Module','Maintainer','Version','Maintainer',
-                         'Depends','Suggests','Python-Requires',
-                         'Whitespace-Hook',
-                         'Author','Description','URL']
+        validEntries = [
+            'Module', 'Maintainer', 'Version', 'Maintainer',
+            'Depends', 'Suggests', 'Python-Requires',
+            'Whitespace-Hook',
+            'Author', 'Description', 'URL',
+        ]
 
         if fileName is not None:
             with io.open(fileName, 'r', encoding='utf-8') as file:
                 import re
                 for line in file:
                     line = line.strip()
-                    if not line or line[ 0 ] == '#':
+                    if not line or line[0] == '#':
                         continue
                     m = re.search(r'^([a-zA-Z0-9-_]+):(.*)', line)
                     if m:
                         key = m.group(1)
                         val = m.group(2)
-                        if not key in valid_entries:
-                            raise ValueError('Invalid dune.module entry %s (%s).' % (key,fileName))
+                        if key not in validEntries:
+                            raise ValueError('Invalid dune.module entry %s (%s).' % (key, fileName))
                         data[key.lower()] = val.strip()
         try:
             self.name = data['module']
@@ -167,7 +173,7 @@ class Description:
             elif wshook == 'no':
                 self.whitespace_hook = False
             else:
-              raise ValueError('Invalid value for whitespace-hook: ' + wshook + '.')
+                raise ValueError('Invalid value for whitespace-hook: ' + wshook + '.')
         except KeyError:
             self.whitespace_hook = None
 
@@ -181,7 +187,7 @@ class Description:
                         deps.append((m, VersionRequirement(None)))
             else:
                 while s:
-                    match = re.match('(?P<module>[a-zA-Z0-9_\-]+)(\s*\((?P<version>[^)]*)\))?((?P<pyversion>[^\s]*))?', s)
+                    match = re.match(r'(?P<module>[a-zA-Z0-9_\-]+)(\s*\((?P<version>[^)]*)\))?((?P<pyversion>[^\s]*))?', s)
                     if not match:
                         raise ValueError('Invalid dependency list.')
                     deps.append((match.group('module'), VersionRequirement(match.group('version') or match.group('pyversion'))))
@@ -243,7 +249,8 @@ class Data:
             self.python_requires = [((pr[0], '(<= '+self.version+')') if pr[0].startswith('dune-') else pr) for pr in self.python_requires]
 
     def asPythonRequirementString(self, requirements):
-        return [(r[0]+str(r[1])).replace("("," ").replace(")","").replace(" ","") for r in requirements]
+        return [(r[0]+str(r[1])).replace("(", " ").replace(")", "").replace(" ", "") for r in requirements]
+
 
 def cmakeArguments(cmakeArgs):
     if cmakeArgs is None:
@@ -257,20 +264,21 @@ def cmakeArguments(cmakeArgs):
     else:
         raise ValueError('definitions must be a list or a dictionary.')
 
+
 def defaultCmakeFlags():
     # defaults
     flags = dict([
-        ('CMAKE_BUILD_TYPE','Release'),
-        ('CMAKE_INSTALL_RPATH_USE_LINK_PATH','TRUE'),
-        ('DUNE_ENABLE_PYTHONBINDINGS','TRUE'),
-        ('ALLOW_CXXFLAGS_OVERWRITE','ON'),
-        ('CMAKE_DISABLE_FIND_PACKAGE_LATEX','TRUE'),
-        ('CMAKE_DISABLE_FIND_PACKAGE_Doxygen','TRUE'),
-        ('INKSCAPE','FALSE'),
+        ('CMAKE_BUILD_TYPE', 'Release'),
+        ('CMAKE_INSTALL_RPATH_USE_LINK_PATH', 'TRUE'),
+        ('DUNE_ENABLE_PYTHONBINDINGS', 'TRUE'),
+        ('ALLOW_CXXFLAGS_OVERWRITE', 'ON'),
+        ('CMAKE_DISABLE_FIND_PACKAGE_LATEX', 'TRUE'),
+        ('CMAKE_DISABLE_FIND_PACKAGE_Doxygen', 'TRUE'),
+        ('INKSCAPE', 'FALSE'),
     ])
     # if inVEnv():
     #     flags['DUNE_PYTHON_VIRTUALENV_PATH'] = sys.prefix
-    flags = cmakeArguments(flags) # make cmake command line out of dict
+    flags = cmakeArguments(flags)  # make cmake command line out of dict
     # test environment for additional flags
     cmakeFlags = os.environ.get('DUNE_CMAKE_FLAGS')
     # split cmakeFlags and add them to flags
@@ -280,6 +288,7 @@ def defaultCmakeFlags():
     if cmakeFlags is not None:
         flags += shlex.split(cmakeFlags)
     return flags
+
 
 def inVEnv():
     # check whether we are in a anaconda environment
@@ -298,10 +307,11 @@ def inVEnv():
     # If none of the above conditions triggered, this is probably no virtualenv interpreter
     return 0
 
+
 def get_dune_py_dir():
     try:
-        basedir = os.path.realpath( os.environ['DUNE_PY_DIR'] )
-        basedir = os.path.join(basedir,'dune-py')
+        basedir = os.path.realpath(os.environ['DUNE_PY_DIR'])
+        basedir = os.path.join(basedir, 'dune-py')
         return basedir
     except KeyError:
         pass
@@ -320,11 +330,13 @@ def get_dune_py_dir():
 
     raise RuntimeError('Unable to determine location for dune-py module. Please set the environment variable "DUNE_PY_DIR".')
 
+
 def forceConfigure():
     # force a reconfiguration of dune-py by deleting tagfile
     tagfile = os.path.join(get_dune_py_dir(), ".noconfigure")
     if os.path.exists(tagfile):
         os.remove(tagfile)
+
 
 def metaData(version=None, dependencyCheck=True):
     data = Data(version)
@@ -337,11 +349,14 @@ def metaData(version=None, dependencyCheck=True):
             with io.open('pyproject.toml', 'r', encoding='utf-8') as f:
                 for line in f:
                     if not line.startswith("#") and 'requires' in line:
-                        line = line.split('=',maxsplit=1)[1].strip()
+                        line = line.split('=', maxsplit=1)[1].strip()
                         modules = ast.literal_eval(line)
-                        modules = [x for x in modules
-                                      if x not in ["setuptools", "wheel", "scikit-build", "cmake", "ninja", "requests"]
-                                  ]
+                        modules = [
+                            x for x in modules
+                            if x not in [
+                                "setuptools", "wheel", "scikit-build", "cmake", "ninja", "requests"
+                            ]
+                        ]
                         for dep in data.depends:
                             if not any([mod.startswith(dep[0]) for mod in modules]):
                                 raise RuntimeError("""
@@ -365,30 +380,31 @@ def metaData(version=None, dependencyCheck=True):
             print("Warning: no README[.md] file found so providing a default 'long_description' for this package")
 
     setupParams = {
-        "name":data.name,
-        "version":data.version,
-        "author":data.author,
-        "author_email":data.author_email,
-        "description":data.description,
-        "long_description":long_description,
-        "long_description_content_type":"text/markdown",
-        "url":data.url if data.url is not None else '',
-        "classifiers":[
+        "name": data.name,
+        "version": data.version,
+        "author": data.author,
+        "author_email": data.author_email,
+        "description": data.description,
+        "long_description": long_description,
+        "long_description_content_type": "text/markdown",
+        "url": data.url if data.url is not None else '',
+        "classifiers": [
             "Programming Language :: C++",
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GNU General Public License (GPL)",
         ],
-        "cmake_args":cmake_flags
+        "cmake_args": cmake_flags,
       }
     if os.path.isdir('python'):
-      setupParams.update({
-            "packages":find_namespace_packages(where="python"),
-            "package_dir":{"": "python"},
-            "install_requires":install_requires,
-            "python_requires":'>=3.4',
-         })
+        setupParams.update({
+            "packages": find_namespace_packages(where="python"),
+            "package_dir": {"": "python"},
+            "install_requires": install_requires,
+            "python_requires": ">=3.4",
+        })
 
     from skbuild.command.build_py import build_py
+
     class DunepyConfigure(build_py):
         def run(self):
             build_py.run(self)
@@ -402,10 +418,12 @@ def metaData(version=None, dependencyCheck=True):
 
 
 class MetaDataDict(dict):
-    # Define some data processing patterns
-    def combine_across_modules(self,key):
+    """Dictionary with some data processing patterns"""
+
+    def combine_across_modules(self, key):
         return list(m[key] for m in self.values())
-    def zip_across_modules(self,key, value):
+
+    def zip_across_modules(self, key, value):
         result = {}
         for moddata in self.values():
             # todo: space is bad separator for list of paths - needs
@@ -413,13 +431,15 @@ class MetaDataDict(dict):
             for k, v in zip(moddata[key].split(" "), moddata[value].split(";")):
                 # we don't store paths for module that have not been found (suggested)
                 # and we also skip the path if it is empty (packaged module)
-                if v.endswith("NOTFOUND") or v == "": continue
+                if v.endswith("NOTFOUND") or v == "":
+                    continue
                 # make sure build directory (if found) is unique across modules
                 if k in result and not result[k] == v:
                     raise ValueError(f"build dir {v} for module {k} is expected to be unique across the given metadata - found {result[k]}")
                 result[k] = v
         return result
-    def unique_value_across_modules(self,key, default=""):
+
+    def unique_value_across_modules(self, key, default=""):
         values = set(m[key] for m in self.values() if not m[key] == "")
         if len(values) > 1:
             raise ValueError(f"Key {key} is expected to be unique across the given metadata. Got {values}")
@@ -427,6 +447,7 @@ class MetaDataDict(dict):
             return default
         value, = values
         return value
+
 
 def loadExternalModules():
     """Check which external modules are currently registered in dune-py"""
@@ -486,6 +507,7 @@ def extract_metadata():
 
     return result
 
+
 # the current meta data is internally cached
 _metaData = extract_metadata()
 
@@ -496,15 +518,17 @@ def currentMetaData():
 
 
 def extractCMakeFlags():
-    duneOptsFile=None
+    duneOptsFile = None
     cmakeFlags = {}
     for x in _metaData.combine_across_modules("CMAKE_FLAGS"):
         for y in x.split(";"):
             try:
-                k,v = y.split(":=",1)
-                if k=="DUNE_OPTS_FILE": duneOptsFile=v
-                else: cmakeFlags[k] = v.strip()
-            except ValueError: # no '=' in line
+                k, v = y.split(":=", 1)
+                if k == "DUNE_OPTS_FILE":
+                    duneOptsFile = v
+                else:
+                    cmakeFlags[k] = v.strip()
+            except ValueError:  # no '=' in line
                 pass
 
     # add flags from some opts file
@@ -514,33 +538,36 @@ def extractCMakeFlags():
         #       Should be possible by checking return code of the subprocess
         #       so that other bash errors are also caught and warned about
         command = ['bash', '-c', 'source ' + duneOptsFile + ' && echo "$CMAKE_FLAGS"']
-        proc = subprocess.Popen(command, stdout = subprocess.PIPE)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
         stdout, _ = proc.communicate()
-        cmake_args = shlex.split( stdout.decode('utf-8') )
+        cmake_args = shlex.split(stdout.decode('utf-8'))
     else:
         cmake_args = []
 
     # check environment variable
-    cmake_args += shlex.split( os.environ.get('CMAKE_FLAGS','') )
+    cmake_args += shlex.split(os.environ.get('CMAKE_FLAGS', ''))
 
     for y in cmake_args:
         try:
-            k,v = y.split("=",1)
-            if k.startswith('-D'): k = k[2:]
+            k, v = y.split("=", 1)
+            if k.startswith('-D'):
+                k = k[2:]
             cmakeFlags[k] = v.strip()
-        except ValueError: # no '=' in line
+        except ValueError:  # no '=' in line
             pass
 
     # try to unify 'ON' and 'OFF' values
-    for k,v in cmakeFlags.items():
-        if v.upper() in ['ON','TRUE','T','1','YES',"Y"]:
+    for k, v in cmakeFlags.items():
+        if v.upper() in ['ON', 'TRUE', 'T', '1', 'YES', 'Y']:
             cmakeFlags[k] = True
-        elif v.upper() in ['OFF','FALSE','F','0','NO','N']:
+        elif v.upper() in ['OFF', 'FALSE', 'F', '0', 'NO', 'N']:
             cmakeFlags[k] = False
 
     return cmakeFlags
 
+
 cmakeFlags = extractCMakeFlags()
+
 
 def registerExternalModule(moduleName, modulePath):
     """Register an external module into the dune-py machinery
@@ -559,7 +586,7 @@ def registerExternalModule(moduleName, modulePath):
     # check if this module is being registered for the first time or if the location of its metafile has changed
     if moduleName not in _externalPythonModules or modulePath != _externalPythonModules[moduleName]:
 
-        _externalPythonModules[moduleName]=modulePath
+        _externalPythonModules[moduleName] = modulePath
         logger.info("Registered external module {}".format(moduleName))
 
         # if dune-py has already been created
