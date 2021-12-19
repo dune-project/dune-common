bcf51d4359b2b835a1a8374cfdb4069b9911a145
Christoph Grüninger <gruenich@dune-project.org>
2021-12-19 14:47:28 +0100

    [cmake] Remove variables from config.h.cmake that were removed in 2.7
    They were for backwards compatibilty.

--- config.h.cmake
+++ config.h.cmake
@@ -100,20 +100,7 @@
 /* end private */
 
 
-/* old feature support macros which were tested until 2.7, kept around for one more release */
-/* As these are now always supported due to the new compiler requirements, they are directly */
-/* defined without an explicit test. */
-#define DUNE_HAVE_CXX_CLASS_TEMPLATE_ARGUMENT_DEDUCTION 1
-#define DUNE_HAVE_CXX_OPTIONAL 1
-#define DUNE_HAVE_CXX_VARIANT 1
-#define DUNE_SUPPORTS_CXX_THROW_IN_CONSTEXPR 1
-#define DUNE_HAVE_C_ALIGNED_ALLOC 1
-#define DUNE_HAVE_CXX_BOOL_CONSTANT 1
-#define DUNE_HAVE_CXX_EXPERIMENTAL_BOOL_CONSTANT 0
-#define DUNE_HAVE_HEADER_EXPERIMENTAL_TYPE_TRAITS 0
-#define DUNE_HAVE_CXX_APPLY 1
-#define DUNE_HAVE_CXX_EXPERIMENTAL_APPLY 0
-#define HAVE_IS_INDEXABLE_SUPPORT 1
+/* old feature support macros which were tested until 2.8, kept around for one more release */
 
 /* Define to ENABLE_UMFPACK if the UMFPack library is available */
 #cmakedefine HAVE_UMFPACK ENABLE_SUITESPARSE
