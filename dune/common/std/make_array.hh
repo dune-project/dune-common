1fae1c30f4026c5324db043d8e0ed91d8822ba36
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2022-02-25 18:46:17 +0000

    [array] Fix deprecation period

--- dune/common/std/make_array.hh
+++ dune/common/std/make_array.hh
@@ -8,7 +8,7 @@
 #include <experimental/array>
 #endif
 
-#warning make_array.hh is deprecated and will be removed after Dune 2.8. \
+#warning make_array.hh is deprecated and will be removed after Dune 2.9. \
   Use deduction guide of `std::array` or `std::to_array`.
 
 namespace Dune {
