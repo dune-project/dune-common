950abc4736bfbfdb64f87e5ea2c3f1ee8527d999
Carsten Gräser <graeser@dune-project.org>
2022-02-28 17:50:02 +0100

    Extend transpose(...) utility function
    This changes the internal wrapper `Impl::TransposedMatrixWrapper`
    and the `Dune::transpose(matrix)` interface function.
    
    Changes to `Impl::TransposedMatrixWrapper`:
    
    * The wrapper now always stores a value instead of a reference.
      By also adding support to pass and store a `std::reference_wrapper`
      this still allows to opt-in storing a reference to a matrix.
    * Add `mv` and `mtv` to the wrapper
    * Add conversion to dense matrix to the wrapper
    * The wrapper also works for dynamically sized matrices
    * Specialize `FieldTraits` for wrapper
    
    Changes to `Dune::transpose(matrix)`:
    
    * If the passed matrix supports `matrix.transposed()`,
      then `Dune::transpose(matrix)` will return the result
      of this method.
    * If the passed matrix does not support `matrix.transposed()`,
      then `Dune::transpose(matrix)` will return an `Impl::TransposedMatrixWrapper`
      storing a copy of the passed matrix.
    * If an `std::reference_wrapper` of a matrix is passed,
      then `Dune::transpose(matrix)` will return an `Impl::TransposedMatrixWrapper`
      storing this `std::reference_wrapper` and thus a reference to the
      matrix.
    
    Furthermore this introduces a header `matrixconcepts.hh` with some
    common internal matrix checks that will be used in other headers, too.
    Currently this is purely internal but one may want to publish polished
    matrix concepts later on.

---
+++ dune/common/matrixconcepts.hh
@@ -0,0 +1,87 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_COMMON_MATRIXCONCEPTS_HH
+#define DUNE_COMMON_MATRIXCONCEPTS_HH
+
+
+
+#include <utility>
+#include <type_traits>
+
+#include <dune/common/std/type_traits.hh>
+
+
+
+namespace Dune {
+
+  template<class, int, int>
+  class FieldMatrix;
+
+  template<class>
+  class DynamicMatrix;
+
+}
+
+namespace Dune::Impl {
+
+
+  // Some traits for checking matrix concepts. Currently these are
+  // all technical internal helpers that just serve different headers
+  // to do internal checks and are thus collected here.
+
+  template<class T>
+  using IsMatrixHelper = std::void_t<decltype(std::declval<T>().N(), std::declval<T>().M())>;
+
+  template<class T>
+  struct IsMatrix : public Dune::Std::is_detected<IsMatrixHelper, T> {};
+
+  // Check if T is a matrix by checking for member functions N() and M().
+  template<class T>
+  constexpr bool IsMatrix_v = Impl::IsMatrix<T>::value;
+
+
+
+  template<class T>
+  using IsStaticSizeMatrixHelper = std::void_t<decltype(T::rows, T::cols)>;
+
+  template<class T>
+  struct IsStaticSizeMatrix : public Dune::Std::is_detected<IsStaticSizeMatrixHelper, T> {};
+
+  // Check if T is a statically sized matrix by checking for static members rows and cols.
+  template<class T>
+  constexpr bool IsStaticSizeMatrix_v = Impl::IsStaticSizeMatrix<T>::value;
+
+
+
+  template<class T>
+  class IsFieldMatrix : public std::false_type {};
+
+  template< class K, int ROWS, int COLS>
+  class IsFieldMatrix<Dune::FieldMatrix<K, ROWS, COLS>> : public std::true_type {};
+
+  // Check if T is an instance of FieldMatrix
+  template<class T>
+  constexpr bool IsFieldMatrix_v = Impl::IsFieldMatrix<T>::value;
+
+
+
+  template<class T>
+  class IsDenseMatrix : public std::false_type {};
+
+  template<class K, int ROWS, int COLS>
+  class IsDenseMatrix<Dune::FieldMatrix<K, ROWS, COLS>> : public std::true_type {};
+
+  template<class K>
+  class IsDenseMatrix<Dune::DynamicMatrix<K>> : public std::true_type {};
+
+  // Check if T is a dense matrix. This is implemented by specialization.
+  template<class T>
+  constexpr bool IsDenseMatrix_v = Impl::IsDenseMatrix<T>::value;
+
+
+
+} // namespace Dune::Impl
+
+
+
+#endif // DUNE_COMMON_MATRIXCONCEPTS_HH
