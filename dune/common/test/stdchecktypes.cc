78a159d076c9fc0717773bf854945d764e366d75
Robert K <robertk@posteo.org>
2022-08-01 23:14:23 +0200

    [feature] Added check for detected_or_fallback_t.

---
+++ dune/common/test/stdchecktypes.cc
@@ -0,0 +1,52 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <tuple>
+#include <utility>
+#include <sstream>
+
+#include <dune/common/std/type_traits.hh>
+
+
+struct A
+{
+  typedef long type;
+};
+
+struct B
+{
+};
+
+
+template <class Traits>
+struct C
+{
+  template <class T>
+  using Fallback = int;
+
+  template <class T>
+  using Target = typename T::type;
+
+  using type = Dune::detected_or_fallback_t<Fallback, Target, Traits>;
+
+  void foo() { };
+};
+
+
+int main()
+{
+  C< A > ca;
+
+  ca.foo();
+
+
+  // this should produce a compiler warning
+  C< B > cb;
+
+  cb.foo();
+
+}
