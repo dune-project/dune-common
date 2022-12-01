c62baf84306be311dfa39240dabf08fb81f722d2
dedner <a.s.dedner@warwick.ac.uk>
2022-12-01 09:23:15 +0000

    [Python] add a time series pickler

--- python/dune/common/pickle.py
+++ python/dune/common/pickle.py
@@ -1,30 +1,34 @@
 # SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
 # SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 
+import json as _json
 import pickle as _pickle
 import pickletools
-import importlib, sys
-def dump(objects, f, protocol=None):
+import importlib, sys, os
+def dump(objects, f, protocol=None, includeJITSources=True):
     if not (isinstance(objects,list) or isinstance(objects,tuple)):
         raise TypeError("only pass in tuples/lists to pickle")
     obj = objects
-    objDump = _pickle.dumps(obj,protocol)
-    # add source code for all required jit modules - extracct all modules from the pickle stream and
-    # store the ones relating to jit modules in 'dune.generated'
-    mods = []
-    for opcode,arg,pos in pickletools.genops(objDump):
-        try:
-            if "dune.generated" in arg:
-                mods += [arg]
-        except:
-            pass
-    modsCode = []
-    for m in mods:
-        fname = importlib.import_module(m).__file__
-        fname = fname.replace(".so",".cc")
-        with open(fname,"r") as fmod:
-            modsCode += [[m, fmod.read()]]
-    _pickle.dump(modsCode,f,protocol)
+    if includeJITSources:
+        objDump = _pickle.dumps(obj,protocol)
+        # add source code for all required jit modules - extracct all modules from the pickle stream and
+        # store the ones relating to jit modules in 'dune.generated'
+        mods = []
+        for opcode,arg,pos in pickletools.genops(objDump):
+            try:
+                if "dune.generated" in arg:
+                    mods += [arg]
+            except:
+                pass
+        modsCode = []
+        for m in mods:
+            fname = importlib.import_module(m).__file__
+            fname = fname.replace(".so",".cc")
+            with open(fname,"r") as fmod:
+                modsCode += [[m, fmod.read()]]
+        _pickle.dump(modsCode,f,protocol)
+    else:
+        _pickle.dump([],f,protocol)
     _pickle.dump(obj,f,protocol)
 
 def load(f):
@@ -51,3 +55,41 @@ def load(f):
 
     obj = _pickle.load(f)
     return obj
+
+class SeriesPickler:
+    def __init__(self, fileBase, objects=None, *, append=False, protocol=None):
+        self.fileBase = fileBase
+        self.objects = objects
+        self.protocol = protocol
+        self.seriesFileName = fileBase+".series."+"dbf"
+        if append and os.path.exists(self.seriesFileName):
+            f = open(self.seriesFileName,"r+")
+            self.data = json.load(self.f)
+            self.count = max(self.data.keys())+1
+        else:
+            self.data = {}
+            self.count = 0
+
+    def _dumpFileName(self,count):
+        return self.fileBase+"."+str(count).zfill(6)+".dbf"
+
+    def dump(self,data):
+        assert self.objects is not None, "if used to dump data the 'objects' to dump need to be provided in the ctor"
+        dumpFileName = self._dumpFileName(self.count)
+        if not type(data) is dict:
+            raise TypeError("data passed to 'dump' should be a dictonary")
+        data["dumpFileName"] = dumpFileName
+        self.data[self.count] = data
+        with open(self.seriesFileName,"w") as f:
+            _json.dump(self.data,f,indent=2)
+        with open(dumpFileName,"wb") as f:
+            dump(self.objects,f,protocol=self.protocol,includeJITSources=(self.count==0))
+        self.count += 1
+    def load(self,count=None):
+        if count is None:
+            count = self.count
+            self.count += 1
+        data = self.data[count]
+        with open(data["dumpFileName"],"rb") as f:
+            obj = load(f,protocol=self.protocol)
+        return data,obj
