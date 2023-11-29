1d5dfe1214965a948a241c831a201c148589e492
Carsten Gräser <graeser@dune-project.org>
2023-11-29 09:25:58 +0100

    [bugfix] Include config.h in lrutest.cc
    This fixes a linker error for me. Without this patch I get:
    
    > rutest.cc:(.text._ZN3MPI9IntracommC2Ev[_ZN3MPI9IntracommC5Ev]+0x18): undefined reference to `MPI::Comm::Comm()'
    > /usr/bin/ld: CMakeFiles/lrutest.dir/lrutest.cc.o: in function `MPI::Intracomm::Intracomm(ompi_communicator_t*)':
    > lrutest.cc:(.text._ZN3MPI9IntracommC2EP19ompi_communicator_t[_ZN3MPI9IntracommC5EP19ompi_communicator_t]+0x2c): undefined reference to `MPI::Comm::Comm()'
    > /usr/bin/ld: CMakeFiles/lrutest.dir/lrutest.cc.o: in function `MPI::Op::Init(void (*)(void const*, void*, int, MPI::Datatype const&), bool)':
    > lrutest.cc:(.text._ZN3MPI2Op4InitEPFvPKvPviRKNS_8DatatypeEEb[_ZN3MPI2Op4InitEPFvPKvPviRKNS_8DatatypeEEb]+0x2a): undefined reference to `ompi_mpi_cxx_op_intercept'
    > /usr/bin/ld: CMakeFiles/lrutest.dir/lrutest.cc.o:(.data.rel.ro._ZTVN3MPI3WinE[_ZTVN3MPI3WinE]+0x48): undefined reference to `MPI::Win::Free()'
    > /usr/bin/ld: CMakeFiles/lrutest.dir/lrutest.cc.o:(.data.rel.ro._ZTVN3MPI8DatatypeE[_ZTVN3MPI8DatatypeE]+0x78): undefined reference to `MPI::Datatype::Free()'

--- dune/common/test/lrutest.cc
+++ dune/common/test/lrutest.cc
@@ -2,6 +2,9 @@
 // vi: set et ts=4 sw=2 sts=2:
 // SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
 // SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <assert.h>
 #include <iostream>
 #include <dune/common/lru.hh>
