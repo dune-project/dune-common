f83eea336f750986c6e35d214836c76191dbb09b
Robert K <robertk@posteo.org>
2024-05-08 11:43:33 +0200

    [cleanup][RemoteIndices] Remove warning due to missing cast.

--- dune/common/parallel/remoteindices.hh
+++ dune/common/parallel/remoteindices.hh
@@ -1287,7 +1287,7 @@ namespace Dune {
       // wait for completion of pending requests
       MPI_Status* statuses = new MPI_Status[neighbourIds.size()];
 
-      if(MPI_ERR_IN_STATUS==MPI_Waitall(neighbourIds.size(), requests, statuses)) {
+      if(int(MPI_ERR_IN_STATUS)==MPI_Waitall(neighbourIds.size(), requests, statuses)) {
         for(size_type i=0; i < neighbourIds.size(); ++i)
           if(statuses[i].MPI_ERROR!=MPI_SUCCESS) {
             std::cerr<<rank<<": MPI_Error occurred while receiving message."<<std::endl;
