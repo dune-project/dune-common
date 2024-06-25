45aa00d39f6f9d5666ab06decb284fcec00a3621
Santiago Ospina De Los Ríos <sospinar@gmail.com>
2024-06-25 14:56:09 +0000

    Skip target file if module is found within a cmake super-build

--- cmake/modules/DuneProject.cmake
+++ cmake/modules/DuneProject.cmake
@@ -317,8 +317,13 @@ set(HAVE_${_upcase_module} TRUE)
 # Lines that are set by the CMake build system via the variable DUNE_CUSTOM_PKG_CONFIG_SECTION
 ${DUNE_CUSTOM_PKG_CONFIG_SECTION}
 
+# If this file is found in a super build that includes ${ProjectName}, the 
+# `${ProjectName}-targets.cmake`-file has not yet been generated. This variable
+# determines whether the configuration of ${ProjectName} has been completed.
+get_property(${ProjectName}_IN_CONFIG_MODE GLOBAL PROPERTY ${ProjectName}_LIBRARIES DEFINED)
+
 #import the target
-if(${ProjectName}_LIBRARIES)
+if(${ProjectName}_LIBRARIES AND NOT ${ProjectName}_IN_CONFIG_MODE)
   get_filename_component(_dir \"\${CMAKE_CURRENT_LIST_FILE}\" PATH)
   include(\"\${_dir}/${ProjectName}-targets.cmake\")
 endif()
