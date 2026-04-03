cb3bf0f87677253fca82bde8e195cadb4a01702a
Simon Praetorius <simon.praetorius@tu-dresden.de>
2026-04-03 16:03:50 +0200

    cmake docs: migrate sphinx and mathjax find-module docs

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -1,30 +1,30 @@
 # SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
 # SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 
-# .. cmake_module::
-#
-#    Find Sphinx - the python documentation tool
-#
-#    You may set the following variables to modify the
-#    behaviour of this module:
-#
-#    :ref:`SPHINX_ROOT`
-#       the path to look for sphinx with the highest priority
-#
-#    The following variables are set by this module:
-#
-#    :code:`SPHINX_FOUND`
-#       whether Sphinx was found
-#
-#    :code:`SPHINX_EXECUTABLE`
-#       the path to the sphinx-build executable
-#
-# .. cmake_variable:: SPHINX_ROOT
-#
-#   You may set this variable to have :ref:`FindSphinx` look
-#   for the :code:`sphinx-build` executable in the given path
-#   before inspecting system paths.
-#
+#[=======================================================================[.rst:
+FindSphinx
+==========
+
+Find Sphinx, the Python documentation generator.
+
+This find-module searches for the ``sphinx-build`` executable and provides the
+usual ``find_package(Sphinx)`` result variables.
+
+.. cmake:variable:: SPHINX_ROOT
+
+  Preferred search prefix for the ``sphinx-build`` executable. If set, this
+  path is searched with higher priority before falling back to the system
+  search path.
+
+.. cmake:variable:: SPHINX_FOUND
+
+  Boolean result indicating whether Sphinx was found.
+
+.. cmake:variable:: SPHINX_EXECUTABLE
+
+  Full path to the discovered ``sphinx-build`` executable.
+
+#]=======================================================================]
 
 #TODO export version.
 
