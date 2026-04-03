edbec0177b005ac9f3103a1a989d85dbba5d2fe2
Simon Praetorius <simon.praetorius@tu-dresden.de>
2026-04-03 15:20:33 +0200

    cmake docs: migrate pybind11 and symlink helper docs

--- cmake/modules/DuneSymlinkOrCopy.cmake
+++ cmake/modules/DuneSymlinkOrCopy.cmake
@@ -1,119 +1,112 @@
 # SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
 # SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
 
-# This module provides convenience macros to provide files from the source tree in the build tree.
-#
-# It provides the following macros:
-#
-#   dune_add_copy_command(filename)
-#
-# This macro adds a file-copy command.
-# The file_name is the name of a file that exists
-# in the source tree. This file will be copied
-# to the build tree when executing this command.
-# Notice that this does not create a top-level
-# target. In order to do this you have to additionally
-# call add_custom_target(...) with dependency
-# on the file.
-#
-#   dune_add_copy_target(target_name file_name)
-#
-# This macro adds a file-copy target under given target_name.
-# The file_name is the name of a file that exists
-# in the source tree. This file will be copied
-# to the build tree.
-#
-#   dune_add_copy_dependency(target file_name)
-#
-# This macro adds a copy-dependency to a target
-# The file_name is the name of a file that exists
-# in the source tree. This file will be copied
-# to the build tree.
-#
-#
-# .. cmake_function:: dune_add_copy_command
-#
-#    .. cmake_param:: filename
-#       :positional:
-#       :single:
-#       :required:
-#
-#    TODO DOC ME!
-#
-# .. cmake_function:: dune_add_copy_target
-#
-#    .. cmake_param:: target_name
-#       :positional:
-#       :single:
-#       :required:
-#
-#    .. cmake_param:: filename
-#       :positional:
-#       :single:
-#       :required:
-#
-#    TODO DOC ME!
-#
-# .. cmake_function:: dune_add_copy_dependency
-#
-#    .. cmake_param:: target
-#       :positional:
-#       :single:
-#       :required:
-#
-#    .. cmake_param:: filename
-#       :positional:
-#       :single:
-#       :required:
-#
-#    TODO DOC ME!
-#
-# .. cmake_function:: dune_symlink_to_source_tree
-#
-#    .. cmake_param:: NAME
-#       :single:
-#
-#       The name of the symlink, defaults to :code:`src_dir`.
-#
-#    This function will place a symlink into every subdirectory
-#    of the build tree, that allows to jump to the corresponding
-#    source directory. Call this from your top-level :code:`CMakeLists.txt`
-#    to enable it for a given module. To enable it for all modules,
-#    set the variable :ref:`var_dune_symlink_to_source_tree` instead.
-#    If used on Windows systems, a warning is issued.
-#
-# .. cmake_variable:: DUNE_SYMLINK_TO_SOURCE_TREE
-#
-#    If this variable is set to TRUE, the functionality of
-#    :cmake:command:`dune_symlink_to_source_tree()` is enabled in all modules.
-#    This will place symlinks to the corresponding source directory
-#    in every subdirectory of the build directory.
-#
-# .. cmake_variable:: DUNE_SYMLINK_RELATIVE_LINKS
-#
-#    If this variable is set to TRUE, the buildsystem will create relative
-#    links instead of absolute ones.
-#
-# .. cmake_function:: dune_symlink_to_source_files
-#
-#    .. cmake_param:: FILES
-#       :multi:
-#       :required:
-#
-#       The list of files to symlink
-#
-#    .. cmake_param:: DESTINATION
-#       :multi:
-#       :required:
-#
-#       Relative path of the target directory
-#
-#    Create symlinks in the build tree that
-#    point to particular files in the source directory. This is usually
-#    used for grid and ini files and the like. On Windows systems,
-#    a warning is issued and copying is used as a fallback to
-#    symlinking.
-#
+#[=======================================================================[.rst:
+DuneSymlinkOrCopy
+=================
+
+Convenience commands to expose files from the source tree inside the build
+tree by copying or symlinking them.
+
+.. cmake:command:: dune_add_copy_command
+
+  Add a custom command that copies a source-tree file into the current build
+  directory.
+
+  .. code-block:: cmake
+
+    dune_add_copy_command(<filename>)
+
+  ``filename``
+    Path to a file relative to the current source directory.
+
+  This command only creates the custom command for the copied output file. It
+  does not add a top-level target by itself.
+
+.. cmake:command:: dune_add_copy_target
+
+  Add a custom target that copies a source-tree file into the current build
+  directory.
+
+  .. code-block:: cmake
+
+    dune_add_copy_target(<target-name> <filename>)
+
+  ``target-name``
+    Name of the custom target to create.
+
+  ``filename``
+    Path to a file relative to the current source directory.
+
+  The created target depends on the copied file output created by
+  :cmake:command:`dune_add_copy_command()`.
+
+.. cmake:command:: dune_add_copy_dependency
+
+  Add a file copy step as a dependency of an existing target.
+
+  .. code-block:: cmake
+
+    dune_add_copy_dependency(<target> <filename>)
+
+  ``target``
+    Existing target that should depend on the copied file.
+
+  ``filename``
+    Path to a file relative to the current source directory.
+
+  This command creates an auxiliary copy target and adds it as a dependency of
+  ``target``.
+
+.. cmake:command:: dune_symlink_to_source_tree
+
+  Create directory symlinks from the build tree back into the source tree.
+
+  .. code-block:: cmake
+
+    dune_symlink_to_source_tree([NAME <link-name>])
+
+  ``NAME``
+    Name of the symlink created in each build directory. The default is
+    ``src_dir``.
+
+  Call this from a top-level ``CMakeLists.txt`` to create a symlink in every
+  build-tree subdirectory that points to the corresponding source directory. On
+  Windows, a warning is issued instead.
+
+.. cmake:variable:: DUNE_SYMLINK_TO_SOURCE_TREE
+
+  If set to ``TRUE``, the functionality of
+  :cmake:command:`dune_symlink_to_source_tree()` is enabled globally for a
+  module.
+
+.. cmake:variable:: DUNE_SYMLINK_RELATIVE_LINKS
+
+  If set to ``TRUE``, relative symlinks are created instead of absolute ones.
+
+.. cmake:command:: dune_symlink_to_source_files
+
+  Create symlinks, or copies on Windows, for selected source files in the
+  current build directory.
+
+  .. code-block:: cmake
+
+    dune_symlink_to_source_files(
+      FILES <files>...
+      [DESTINATION <relative-path>]
+    )
+
+  ``FILES``
+    Files relative to the current source directory that should be mirrored into
+    the build directory.
+
+  ``DESTINATION``
+    Relative destination directory below the current build directory.
+
+  On Windows, copying is used as a fallback because symlink creation is not
+  generally available.
+
+#]=======================================================================]
 include_guard(GLOBAL)
 
 macro(dune_add_copy_command file_name)
